e6b53db43f843a5a6013fe563fcfe56fc965b017
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2016-01-13 17:13:29 +0100

    fix reload issue by enabling HashLocationStrategy (closes #2460, see #2473)

--- src/app/boot.ts
+++ src/app/boot.ts
@@ -1,5 +1,10 @@
 import {bootstrap}    from 'angular2/platform/browser'
 import {AppComponent} from './components/app.component'
 import {ROUTER_PROVIDERS} from 'angular2/router';
+import {provide} from 'angular2/core';
+import {LocationStrategy, HashLocationStrategy} from 'angular2/router';
 
-bootstrap(AppComponent, [ROUTER_PROVIDERS]);
+bootstrap(AppComponent, [
+	ROUTER_PROVIDERS, 
+	provide(LocationStrategy, { useClass: HashLocationStrategy })
+]);

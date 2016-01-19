fc6a6441ce4e77d42c1f1e562c8ce2af80e453da
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2016-01-19 12:51:59 +0100

    Implement detail view edit workflow.

--- src/app/components/overview.component.ts
+++ src/app/components/overview.component.ts
@@ -8,16 +8,43 @@ import {provide} from "angular2/core";
     templateUrl: 'templates/overview.html'
 })
 
+/**
+ * @author Sebastian Cuy
+ * @author Daniel M. de Oliveira
+ */
 export class OverviewComponent implements OnInit {
 
+    public selectedObject: IdaiFieldObject;
     public objects: IdaiFieldObject[];
 
     constructor(private datastore: Datastore) {
+    }
 
+    deepCopyObject(from: IdaiFieldObject,to: IdaiFieldObject) {
+        to._id = from._id;
+        to.title = from.title;
+    }
+
+    onSelect(object: IdaiFieldObject) {
+        this.selectedObject = { _id: "", title: ""};
+        this.deepCopyObject(object,this.selectedObject);
+    }
+
+    getObjectIndex( id: String ) {
+        for (var i in this.objects) {
+            if (this.objects[i]._id==id) return i;
+        }
+        return null;
+    }
+
+    save(object: IdaiFieldObject) {
+        this.deepCopyObject(
+            object,
+            this.objects[this.getObjectIndex(object._id)]
+        );
     }
 
     ngOnInit() {
-
         this.datastore.all({}).then(objects => {
             this.objects = objects;
         });

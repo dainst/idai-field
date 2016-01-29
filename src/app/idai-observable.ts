ab2068f1ff71f0dbd0f3cc96a73b5ee091278971
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2016-01-29 17:52:31 +0100

    Remove obsolete artifacts.

--- src/app/idai-observable.ts
+++
@@ -1,7 +0,0 @@
-import {IdaiObserver} from "./idai-observer";
-export abstract class IdaiObservable {
-
-    abstract subscribe(observer: IdaiObserver): any;
-
-    abstract notifyObservers(): any;
-}

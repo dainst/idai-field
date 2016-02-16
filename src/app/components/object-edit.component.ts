0cb9a84b8b3cfb5c697cb611f4d171f63d496ef2
Jan Wieners <jan@jan-wieners.de>
2016-02-16 16:16:02 +0100

    Moved save functionality from object edit component to object-list service.

--- src/app/components/object-edit.component.ts
+++ src/app/components/object-edit.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, SimpleChange, OnChanges} from 'angular2/core';
 import {IdaiFieldObject} from "../model/idai-field-object";
 import {Datastore} from "../services/datastore";
 import {Messages} from "../services/messages";
+import {ObjectList} from "../services/object-list";
 
 
 /**
@@ -16,78 +17,20 @@ import {Messages} from "../services/messages";
 
 export class ObjectEditComponent implements OnChanges {
 
-    @Input() selectedObject: IdaiFieldObject;
     private saveTimer: number;
-    private changed: boolean; // indicates that the currently edited object differs from the one
-                              // saved on the local datastore.
 
-    constructor(private datastore: Datastore,
-                private messages: Messages) { }
+    constructor(private objectList: ObjectList) { }
 
     /**
      * Saves the currently selected object to the local datastore.
      */
     private saveSelected() {
-        this.save(this.selectedObject);
-    }
-
-    /**
-     * Saves the object to the local datastore.
-     * @param object
-     */
-    private save(object: IdaiFieldObject) {
-
-        // Replace with proper validation
-        if (!object.identifier || object.identifier.length == 0)
-            return;
-
-        this.changed = false;
-        object.synced = 0;
-
-        if (object.id) {
-            this.update(object);
-        } else {
-            this.create(object);
-        }
-    }
-
-    /**
-     * Updates an existing object in the local datastore.
-     * @param object
-     */
-    private update(object: IdaiFieldObject) {
-
-        object.valid=true;
-
-        this.datastore.update(object).then(
-            () => this.messages.deleteMessages(),
-            err => {
-                this.messages.addMessage('danger', 'Object Identifier already exists.');
-                object.valid=false;
-            }
-        );
-    }
-
-    /**
-     * Saves the given object as a new object in the local datastore.
-     * @param object
-     */
-    private create(object: IdaiFieldObject) {
-
-        object.valid=true;
-
-        this.datastore.create(object).then(
-            () => this.messages.deleteMessages(),
-            err => {
-                this.messages.addMessage('danger', 'Object Identifier already exists.');
-                object.valid=false;
-            }
-        );
+        this.objectList.save(this.objectList.getSelectedObject());
     }
 
     onKey(event: any) {
 
-        this.changed = true;
+        this.objectList.setChanged();
 
         if (this.saveTimer)
             clearTimeout(this.saveTimer);
@@ -97,16 +40,11 @@ export class ObjectEditComponent implements OnChanges {
 
     ngOnChanges(changes: { [propName: string]: SimpleChange }) {
 
+        /*
         if (this.saveTimer) {
             clearTimeout(this.saveTimer);
             this.saveTimer = undefined;
         }
-
-        var previousObject: IdaiFieldObject = changes["selectedObject"].previousValue;
-        if (!previousObject || !previousObject.valid) return;
-
-        if (this.changed && previousObject) {
-            this.save(previousObject);
-        }
+        */
     }
 }
--- src/app/services/object-list.ts
+++ src/app/services/object-list.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "angular2/core";
-
 import {IdaiFieldObject} from "../model/idai-field-object";
+import {Datastore} from "./datastore";
+import {Messages} from "./messages";
 
 /**
  * @author Thomas Kleinke
@@ -11,15 +12,26 @@ import {IdaiFieldObject} from "../model/idai-field-object";
 
 export class ObjectList {
 
+    constructor(private datastore: Datastore,
+                private messages: Messages) {}
+
     /**
      * The Object currently selected in the list and shown in the edit component.
      */
     private selectedObject: IdaiFieldObject;
+
+    /**
+     * Indicates that the current instance of the selectedObject differs from the one
+     * saved in the local datastore.
+     */
+    private changed: boolean;
+
     /**
      * The object under creation which has not been yet put to the list permanently.
      * As soon as it is validated successfully newObject is set to "undefined" again.
      */
     private newObject: any;
+
     private objects: IdaiFieldObject[];
 
     public getObjects() {
@@ -46,5 +58,70 @@ export class ObjectList {
         this.selectedObject = object;
     }
 
+    public setChanged() {
+        this.changed = true;
+    }
 
+    /**
+     * Saves the object to the local datastore.
+     * @param object
+     */
+    public save(object: IdaiFieldObject) {
+
+        // Replace with proper validation
+        if (!object.identifier || object.identifier.length == 0)
+            return;
+
+        this.changed = false;
+        object.synced = 0;
+
+        if (object.id) {
+            this.update(object);
+        } else {
+            this.create(object);
+        }
+    }
+
+    /**
+     * Updates an existing object in the local datastore.
+     * @param object
+     */
+    private update(object: IdaiFieldObject) {
+
+        object.valid = true;
+
+        this.datastore.update(object).then(
+            () => this.messages.deleteMessages(),
+            err => {
+                this.messages.addMessage('danger', 'Object Identifier already exists.');
+                object.valid = false;
+            }
+        );
+    }
+
+    /**
+     * Saves the given object as a new object in the local datastore.
+     * @param object
+     */
+    private create(object: IdaiFieldObject) {
+
+        object.valid=true;
+
+        this.datastore.create(object).then(
+            () => this.messages.deleteMessages(),
+            err => {
+                this.messages.addMessage('danger', 'Object Identifier already exists.');
+                object.valid=false;
+            }
+        );
+    }
+
+    public validateAndSave(object: IdaiFieldObject) {
+
+        if (!object || !object.valid) return;
+
+        if (this.changed && object) {
+            this.save(object);
+        }
+    }
 }

a86298f07d43df57fd4973c1c0ec2a46382423a6
Daniel <danielmarreirosdeoliveira@googlemail.com>
2016-02-18 17:39:32 +0100

    Enable third object list test.

--- src/test/object-list.spec.ts
+++ src/test/object-list.spec.ts
@@ -1,4 +1,4 @@
-import {describe,expect,it,xit, inject, beforeEachProviders} from 'angular2/testing';
+import {describe,expect,fit,it,xit, inject, beforeEachProviders} from 'angular2/testing';
 import {provide} from "angular2/core";
 import {IdaiFieldObject} from "../app/model/idai-field-object";
 import {ObjectList} from "../app/services/object-list";
@@ -73,61 +73,45 @@ export function main() {
             })
         );
 
-        /**
         it('should not update an existing not valid object on changing object',
             inject([ Messages],
-                ( messages:Messages) => {
+            ( messages:Messages) => {
 
-                    var oldVersion : IdaiFieldObject =
-                    { "identifier": "ob4", "title": "Luke Skywalker", "synced": 0, "valid": true, "id" : id };
-                    var promise=new Promise((resolve, reject) => { resolve(oldVersion)});
+                var id="abc";
 
-                    mockDatastore.update.and.callFake(function() {
-                        return new Promise((resolve, reject) => { reject('failed')});
-                    });
-                    mockDatastore.refresh.and.callFake(function() {
-                        return promise;
-                    });
+                var oldVersion : IdaiFieldObject =
+                { "identifier": "ob4", "title": "Luke Skywalker", "synced": 0, "valid": true, "id" : id };
 
-                    var id="abc";
+                mockDatastore.update=function() {
+                    return {
+                        then: function(suc,err) {
+                            err("fail");
+                        }
+                    };
+                };
+                mockDatastore.refresh=function() {
+                    return {
+                        then: function(suc,err) {
+                            suc(oldVersion);
+                        }
+                    };
+                };
 
+                var selectFirst : IdaiFieldObject =
+                { "identifier": "ob4", "title": "Luke Skywalker 222", "synced": 0, "valid": true, "id" : id };
+                var selectThen : IdaiFieldObject =
+                { "identifier": "ob5", "title": "Boba Fett", "synced": 0, "valid": true };
 
-                    var selectFirst : IdaiFieldObject =
-                    { "identifier": "ob4", "title": "Luke Skywalker 222", "synced": 0, "valid": true, "id" : id };
-                    var selectThen : IdaiFieldObject =
-                    { "identifier": "ob5", "title": "Boba Fett", "synced": 0, "valid": true };
+                var objects : IdaiFieldObject[] = [selectFirst];
+                var objectList = new ObjectList(mockDatastore,messages);
+                objectList.setObjects(objects);
 
-                    var objects : IdaiFieldObject[] = [selectFirst];
-                    var objectList = new ObjectList(mockDatastore,messages);
-                    objectList.setObjects(objects);
+                objectList.setSelectedObject(selectFirst);
+                objectList.setChanged();
+                objectList.setSelectedObject(selectThen); // it will try to save selectFirst now.
 
-
-                    objectList.setSelectedObject(selectFirst);
-                    objectList.setChanged();
-                    objectList.setSelectedObject(selectThen); // it will try to save selectFirst now.
-
-
-                    promise.then(
-                        restoredObject=>
-                            {setTimeout(function(){
-                                expect((<Datastore> mockDatastore).update).toHaveBeenCalled();
-                                expect((<Datastore> mockDatastore).refresh).toHaveBeenCalled();
-                                expect(objectList.getObjects()[0]).toBe(oldVersion)}
-                                ,500);
-                            },
-                        err=>{console.log("err")}
-                    );
-                })
-        );*/
-
-        // TODO
-        //it('should update an existing object on changing object',
-
-        // TODO
-        //it('should restore a non existing object on changing object if not valid',
-
-        // TODO
-        //it('should mark an object invalid when storing not successful',
-
+                expect(objectList.getObjects()[0]).toBe(oldVersion);
+            }
+        ));
     });
 }

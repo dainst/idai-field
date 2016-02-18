6c25bdc0f3df2669968307a007d06041239fb2ff
Daniel <danielmarreirosdeoliveira@googlemail.com>
2016-02-18 16:30:08 +0100

    Add one test and prepare another.

--- src/test/object-list.spec.ts
+++ src/test/object-list.spec.ts
@@ -18,14 +18,25 @@ export function main() {
 
         ]);
 
+        var mockDatastore;
+
+        beforeEach(function(){
+            mockDatastore   = jasmine.createSpyObj('mockDatastore', [ 'create','update','refresh' ]);
+            mockDatastore.create.and.callFake(function() {
+                return new Promise((resolve, reject) => { resolve('ok')});
+            });
+            mockDatastore.update.and.callFake(function() {
+                return new Promise((resolve, reject) => { resolve('ok')});
+            });
+            mockDatastore.refresh.and.callFake(function() {
+                return new Promise((resolve, reject) => { resolve('ok')});
+            });
+        });
+
+
         it('should create a non existing object on changing object',
             inject([ Messages],
             ( messages:Messages) => {
-
-                var mockDatastore   = jasmine.createSpyObj('someObject', [ 'create' ]);
-                mockDatastore.create.and.callFake(function() {
-                    return new Promise((resolve, reject) => { resolve('ok')});
-                });
 
                 var objectList = new ObjectList(mockDatastore,messages);
 
@@ -41,6 +52,73 @@ export function main() {
                 expect((<Datastore> mockDatastore).create).toHaveBeenCalledWith(selectFirst);
             })
         );
+
+
+        it('should update an existing object on changing object',
+            inject([ Messages],
+            ( messages:Messages) => {
+
+                var objectList = new ObjectList(mockDatastore,messages);
+
+                var selectFirst : IdaiFieldObject =
+                { "identifier": "ob4", "title": "Luke Skywalker", "synced": 0, "valid": true, "id" : "abc" };
+                var selectThen : IdaiFieldObject =
+                { "identifier": "ob5", "title": "Boba Fett", "synced": 0, "valid": true };
+
+                objectList.setSelectedObject(selectFirst);
+                objectList.setChanged();
+                objectList.setSelectedObject(selectThen); // it will try to save selectFirst now.
+
+                expect((<Datastore> mockDatastore).update).toHaveBeenCalledWith(selectFirst);
+            })
+        );
+
+        /**
+        it('should not update an existing not valid object on changing object',
+            inject([ Messages],
+                ( messages:Messages) => {
+
+                    var oldVersion : IdaiFieldObject =
+                    { "identifier": "ob4", "title": "Luke Skywalker", "synced": 0, "valid": true, "id" : id };
+                    var promise=new Promise((resolve, reject) => { resolve(oldVersion)});
+
+                    mockDatastore.update.and.callFake(function() {
+                        return new Promise((resolve, reject) => { reject('failed')});
+                    });
+                    mockDatastore.refresh.and.callFake(function() {
+                        return promise;
+                    });
+
+                    var id="abc";
+
+
+                    var selectFirst : IdaiFieldObject =
+                    { "identifier": "ob4", "title": "Luke Skywalker 222", "synced": 0, "valid": true, "id" : id };
+                    var selectThen : IdaiFieldObject =
+                    { "identifier": "ob5", "title": "Boba Fett", "synced": 0, "valid": true };
+
+                    var objects : IdaiFieldObject[] = [selectFirst];
+                    var objectList = new ObjectList(mockDatastore,messages);
+                    objectList.setObjects(objects);
+
+
+                    objectList.setSelectedObject(selectFirst);
+                    objectList.setChanged();
+                    objectList.setSelectedObject(selectThen); // it will try to save selectFirst now.
+
+
+                    promise.then(
+                        restoredObject=>
+                            {setTimeout(function(){
+                                expect((<Datastore> mockDatastore).update).toHaveBeenCalled();
+                                expect((<Datastore> mockDatastore).refresh).toHaveBeenCalled();
+                                expect(objectList.getObjects()[0]).toBe(oldVersion)}
+                                ,500);
+                            },
+                        err=>{console.log("err")}
+                    );
+                })
+        );*/
 
         // TODO
         //it('should update an existing object on changing object',

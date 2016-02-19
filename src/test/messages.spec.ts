c084ea8736984e5cacf8286cc7a1a7ec621f35a1
jan@jan-wieners.de <jan@jan-wieners.de>
2016-02-19 13:31:58 +0100

    Created tests for messages service.

--- src/test/messages.spec.ts
+++ src/test/messages.spec.ts
@@ -8,6 +8,7 @@ import {Messages} from "../app/services/messages";
 
 /**
  * @author Daniel M. de Oliveira
+ * @author Jan G. Wieners
  */
 export function main() {
     describe('Messages ', () => {
@@ -17,10 +18,34 @@ export function main() {
 
                 var messages = new Messages();
                 messages.add("myId","hallo","warn");
-                expect(messages.getMessages()["myId"].content).toBe("hallo");
+                expect(messages.getMessages()[0].content).toBe("hallo");
                 messages.delete("myId");
-                expect(messages.getMessages()["myId"]).toBe(undefined);
+                expect(messages.getMessages()[0]).toBe(undefined);
             }
         );
+
+        it('add two messages with the same identifier',
+            function(){
+
+                var messages = new Messages();
+                messages.add("myId","hallo","warn");
+                messages.add("myId","hallo","warn");
+                expect(messages.getMessages()[0].content).toBe("hallo");
+                expect(messages.getMessages().length).toBe(1);
+            }
+        );
+
+        it('add two messages with different identifiers',
+            function(){
+
+                var messages = new Messages();
+                messages.add("myId","hallo","warn");
+                messages.add("myId2","hallo2","warn");
+                expect(messages.getMessages()[0].content).toBe("hallo");
+                expect(messages.getMessages()[1].content).toBe("hallo2");
+                expect(messages.getMessages().length).toBe(2);
+            }
+        );
+
     })
 }

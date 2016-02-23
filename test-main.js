6c40a5994e954496da8a42e279a204e8761d0d5b
Thomas Kleinke <tkleinke@smail.uni-koeln.de>
2016-02-23 15:15:32 +0100

    set jasmine default timeout interval to a higher value to prevent tests from failing occasionally

--- test-main.js
+++ test-main.js
@@ -3,7 +3,7 @@
 // Turn on full stack traces in errors to help debugging
 Error.stackTraceLimit=Infinity;
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
 // Cancel Karma's synchronous start,
 // we will call `__karma__.start()` later, once all the specs are loaded.

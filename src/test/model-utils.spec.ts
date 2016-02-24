72f4c0223b751e579194d44d02bbe2dc7a550a41
Jan Wieners <jan@jan-wieners.de>
2016-02-24 13:14:54 +0100

    Add tests for cloning objects and filtering unwanted object propierties.

--- src/test/model-utils.spec.ts
+++ src/test/model-utils.spec.ts
@@ -9,7 +9,7 @@ import {ModelUtils} from "../app/model/model-utils";
 export function main() {
     describe('ModelUtils', () => {
 
-        it('should clone an IdaiFieldObject with all of its properties',
+        it('should clone an IdaiFieldObject with all of its properties if no filter properties are given',
             function(){
 
                 var initialObject = {
@@ -19,10 +19,43 @@ export function main() {
                     "valid": true,
                     "type": "Object"
                 };
-                var clonedObject = ModelUtils.clone(initialObject);
-
-                //clonedObject.identifier = "obNew"; // make test fail
+                var clonedObject = ModelUtils.filterUnwantedProps(initialObject);
                 expect(clonedObject).toEqual(initialObject);
+            }
+        );
+
+        it('should create a full copy of an IdaiFieldObject, not just a reference to the object',
+            function(){
+
+                var initialObject = {
+                    "identifier": "ob1",
+                    "title": "Title",
+                    "synced": 0,
+                    "valid": true,
+                    "type": "Object"
+                };
+                var clonedObject = ModelUtils.filterUnwantedProps(initialObject);
+                expect(clonedObject).not.toBe(initialObject);
+            }
+        );
+
+        it('should create a clone of an IdaiFieldObject which lacks defined properties',
+            function(){
+
+                var initialObject = {
+                    "identifier": "ob1",
+                    "title": "Title",
+                    "synced": 0,
+                    "valid": true,
+                    "type": "Object"
+                };
+                var clonedObject = ModelUtils.filterUnwantedProps(initialObject, ['title', 'synced', 'type']);
+
+                var filteredObject = {
+                    "identifier": "ob1",
+                    "valid": true
+                };
+                expect(clonedObject).toEqual(filteredObject);
             }
         );
     })

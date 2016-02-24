f75c904924d185d820dd51d8bec822bdb8ea56f6
Jan Wieners <jan@jan-wieners.de>
2016-02-24 14:05:33 +0100

    Remove filter properties from filterUnwantedProps.

--- src/app/model/model-utils.ts
+++ src/app/model/model-utils.ts
@@ -9,19 +9,16 @@ export class ModelUtils {
     /**
      *
      * @param object
-     * @param properties
      * @returns new IdaiFieldObject without the properties which we don't want
      *   to be sent to the backend.
      */
-    public static filterUnwantedProps(object: IdaiFieldObject, properties?: string[]) : IdaiFieldObject {
+    public static filterUnwantedProps(object: IdaiFieldObject) : IdaiFieldObject {
 
         var o = JSON.parse(JSON.stringify(object));
 
-        if (properties) {
-            for (var i = properties.length; i--;) {
-                delete o[properties[i]];
-            }
-        }
+        delete o.synced;
+        delete o.valid;
+
         return o;
     }
 }

8c448ef14687ab3fca31c800502cba75ddfb1b2c
Thomas Kleinke <tkleinke@smail.uni-koeln.de>
2016-02-29 18:09:58 +0100

    added unit test for save method of IdaiFieldBackend service (closes #2644)

--- src/test/idai-field-backend.spec.ts
+++ src/test/idai-field-backend.spec.ts
@@ -1,24 +1,29 @@
-import {fdescribe,describe,expect,fit,it,xit, inject,beforeEach, beforeEachProviders} from 'angular2/testing';
+import {fdescribe, describe, expect, fit, it, xit, inject, beforeEach, beforeEachProviders} from 'angular2/testing';
+import {IdaiFieldBackend} from "../app/services/idai-field-backend";
+import {ModelUtils} from "../app/model/model-utils";
 import {provide} from "angular2/core";
 import {IdaiFieldObject} from "../app/model/idai-field-object";
-import {ObjectList} from "../app/services/object-list";
-import {Datastore} from "../app/services/datastore";
-import {Messages} from "../app/services/messages";
-import {IdaiFieldBackend} from "../app/services/idai-field-backend";
-import {Observable} from "../../node_modules/rxjs/Observable";
-
+import filter = webdriver.promise.filter;
+import {Headers} from "angular2/http";
 
 /**
  * @author Daniel M. de Oliveira
+ * @author Thomas Kleinke
  */
 export function main() {
     describe('IdaiFieldBackend', () => {
 
         var config = {
             backend : {
-                uri : "uri"
+                uri : "uri",
+                credentials: "user:password",
             }
         };
+
+        var object = { "id": "id1", "identifier": "ob1", "title": "Object 1", "synced": 0, "valid": true,
+            "type": "Object" };
+
+        var filteredObject = { "id": "id1", "identifier": "ob1", "title": "Object 1", "type": "Object" };
 
         var successFunction = function() {
             return {
@@ -36,16 +41,28 @@ export function main() {
             };
         };
 
+        var put = function(obj: IdaiFieldObject) {
+            return {
+                subscribe: function(suc, err) {
+                    suc(obj);
+                }
+            };
+        };
+
         var mockHttp;
         var idaiFieldBackend : IdaiFieldBackend;
         var j;
 
-        beforeEach(function(){
-            mockHttp   = jasmine.createSpyObj('mockHttp', [ 'get' ]);
-            mockHttp.get.and.callFake(successFunction);
+        beforeEachProviders(() => [
+            provide(ModelUtils, {useClass: ModelUtils})
+        ]);
 
-            idaiFieldBackend =
-                new IdaiFieldBackend(mockHttp,config);
+        beforeEach(function(){
+            mockHttp = jasmine.createSpyObj('mockHttp', [ 'get', 'put' ]);
+            mockHttp.get.and.callFake(successFunction);
+            mockHttp.put.and.callFake(put);
+
+            idaiFieldBackend = new IdaiFieldBackend(mockHttp, config);
             j=0;
         });
 
@@ -78,6 +95,23 @@ export function main() {
                 idaiFieldBackend.checkConnection();
 
                 expect(j).toBe(1);
+            }
+        );
+
+        it('should save an object to the backend',
+            function(done) {
+
+                var headers: Headers = new Headers();
+                headers.append('Authorization', 'Basic ' + btoa(config.backend.credentials));
+
+                idaiFieldBackend.save(object).then(obj => {
+                    expect(mockHttp.put).toHaveBeenCalledWith(config.backend.uri + 'objects/' + object.id,
+                        JSON.stringify(filteredObject), { headers: headers });
+                    done();
+                }).catch(err => {
+                    fail(err);
+                    done();
+                });
             }
         );
     })

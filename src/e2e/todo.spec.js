e770a5fedb9a87b0b835d9fa27afe734a6f8d078
Daniel <danielmarreirosdeoliveira@googlemail.com>
2016-03-21 15:04:39 +0100

    Trigger ci success.

--- src/e2e/todo.spec.js
+++ src/e2e/todo.spec.js
@@ -2,6 +2,6 @@ describe('idai field app', function() {
 
     it('should the correct title', function() {
         browser.get('/');
-        expect(browser.getTitle()).toEqual("iDAI.field 3")
+        expect(browser.getTitle()).toEqual("iDAI.field 2")
     });
 });

31af2de2c507de9d63555693c6192f68ab808df4
Daniel <danielmarreirosdeoliveira@googlemail.com>
2016-04-19 14:58:43 +0200

    Create e2e for one basic scenario.

---
+++ src/e2e/basic-scenarios.spec.js
@@ -0,0 +1,14 @@
+describe('idai field app', function() {
+
+    it('should create a new object of type object ', function() {
+        browser.get('/');
+
+        element(by.id('object-overview-button-create-object')).click().then(function(){
+            element(by.id('create-object-option-0')).click().then(function(){
+                expect(element(by.id('object-type-heading')).getText()).toEqual("Object");
+                element(by.id('object-edit-input-identifier')).sendKeys('12345');
+                expect(element(by.id('object-overview-identifier-0')).getText()).toEqual("12345");
+            });
+        });
+    });
+});
--- src/e2e/todo.spec.js
+++
@@ -1,7 +0,0 @@
-describe('idai field app', function() {
-
-    it('should the correct title', function() {
-        browser.get('/');
-        expect(browser.getTitle()).toEqual("iDAI.field 2")
-    });
-});

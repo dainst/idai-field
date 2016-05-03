3b982fb04f5d1be0de61f70905c9b54dbebcfdea
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2016-05-03 16:53:11 +0200

    Make build pass.

--- src/test/unit/idai-field-backend.spec.ts
+++ src/test/unit/idai-field-backend.spec.ts
@@ -10,7 +10,7 @@ import {Headers} from "angular2/http";
  * @author Thomas Kleinke
  */
 export function main() {
-    fdescribe('IdaiFieldBackend', () => {
+    describe('IdaiFieldBackend', () => {
 
         var config = {
             backend : {

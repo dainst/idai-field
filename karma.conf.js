666866fb3f2cc48cd39f0d0fd9aa8b0ae56e1473
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2016-05-13 12:29:19 +0100

    Rework component test to work with rc.1.

--- karma.conf.js
+++ karma.conf.js
@@ -8,10 +8,10 @@ module.exports = function(config) {
         frameworks: ['jasmine'],
 
         files: [
-            //'node_modules/traceur/bin/traceur-runtime.js',
             'node_modules/zone.js/dist/zone.js',
-            'node_modules/zone.js/dist/long-stack-trace-zone.js',
             'node_modules/zone.js/dist/jasmine-patch.js',
+            'node_modules/zone.js/dist/async-test.js',
+            'node_modules/zone.js/dist/fake-async-test.js',
             'node_modules/systemjs/dist/system.src.js',
             'node_modules/reflect-metadata/Reflect.js',
 
@@ -21,10 +21,15 @@ module.exports = function(config) {
             { pattern: 'node_modules/systemjs/dist/system-polyfills.js', included: false, watched: false },
 
             { pattern: 'app/**/*.js', included: false, watched: true },
+            { pattern: 'templates/*.html', included: false, watched: true },
             { pattern: 'test/**/*.spec.js', included: false, watched: true },
 
             'test-main.js'
         ],
+
+        proxies: {
+            '/templates/': '/base/templates/'
+        },
 
         exclude: [
             'node_modules/@angular/**/*_spec.js'
--- test/object-edit.component.spec.ts
+++ test/object-edit.component.spec.ts
@@ -1,35 +1,31 @@
-import {describe, expect, it, fit, xit, inject, injectAsync, beforeEachProviders,
+import {fdescribe, describe, xdescribe,expect, it, fit, xit,inject, async, beforeEachProviders,
         } from '@angular/core/testing';
-import {TestComponentBuilder, ComponentFixture} from '@angular/compiler/testing';
+import {TestComponentBuilder} from '@angular/compiler/testing';
 import {ObjectEditComponent} from '../app/components/object-edit.component'
-import {provide, Injectable} from "@angular/core";
-import {IdaiFieldObject} from "../app/model/idai-field-object";
+import {provide, Component} from "@angular/core";
 import {ObjectList} from "../app/services/object-list";
 import {Datastore} from "../app/datastore/datastore";
 import {Messages} from "../app/services/messages";
-import {ProjectConfiguration} from "../app/model/project-configuration";
-
-//import 'zone.js/dist/zone';
 
 
 /**
- * @author Daniel M. de Oliveira
+ * @author Daniel de Oliveira
  * @author Jan G. Wieners
  * @author Sebastian Cuy
  * @author Thomas Kleinke
  */
 export function main() {
-    // TODO exclude this as long as the template loading problem isn't solved within the new build process
-    xdescribe('ObjectEditComponent', () => {
+
+    describe('ObjectEditComponent', () => {
 
         class MockDatastore {}
         class MockMessages  {}
 
-        class MockProjectConfiguration {
-            public getTypes = function() {
+        var projectConfiguration = {
+            getTypes : function() {
                 return ["Section", "Feature", "Lot", "Context", "Object" ];
-            };
-            public getFields = function() {
+            },
+            getFields : function() {
                 return [{
                     "field": "Material",
                     "valuelist": [
@@ -39,7 +35,7 @@ export function main() {
                     ]
                 }];
             }
-        }
+        };
 
         var selectedObject = {
             "identifier": "ob1",
@@ -49,13 +45,9 @@ export function main() {
             "type": "Object"
         };
 
-        var getElementContent = function(componentFixture, selector: string): string[] {
+        var getElementContent = function(fixture, selector: string): string[] {
 
-            componentFixture.componentInstance.object = selectedObject;
-            componentFixture.componentInstance.projectConfiguration = new MockProjectConfiguration();
-            componentFixture.detectChanges();
-
-            var compiled = componentFixture.debugElement.nativeElement;
+            var compiled = fixture.debugElement.nativeElement;
             var labels = [];
             var nodeList = compiled.querySelectorAll(selector);
 
@@ -67,34 +59,39 @@ export function main() {
         };
 
 
-
         beforeEachProviders(() => [
             provide(Datastore, { useClass: MockDatastore }),
             provide(ObjectList, { useClass: ObjectList }),
             provide(Messages, { useClass: MockMessages }),
-            provide(ProjectConfiguration, { useClass: MockProjectConfiguration}),
             provide(ObjectEditComponent, {useClass: ObjectEditComponent}),
-            provide(TestComponentBuilder, {useClass: TestComponentBuilder}),
         ]);
 
-        it('should contain the specified elements',
-            injectAsync([TestComponentBuilder,ObjectList], (tcb: TestComponentBuilder) => {
-                return tcb.createAsync(ObjectEditComponent)
-                    .then((componentFixture: ComponentFixture) => {
 
-                        var labels;
-                        labels = getElementContent(componentFixture, 'label');
+        it('should build without a problem',
+            async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
+                tcb.createAsync(TestComponent)
+                    .then((fixture) => {
 
-                        expect(labels).toContain('Material');
+                        fixture.componentInstance.selectedObject = selectedObject;
+                        fixture.componentInstance.projectConfiguration = projectConfiguration;
+                        fixture.detectChanges();
 
-                        // labels = getElementContent(componentFixture, 'option');
-                        //
-                        // expect(labels).toContain('Alabaster');
-                        // expect(labels).toContain('Amber');
-                        // expect(labels).toContain('Antler');
+                        expect(getElementContent(fixture, 'label')).toContain('Material');
+
+                        var labels = getElementContent(fixture, 'option');
+
+                        expect(labels).toContain('Alabaster');
+                        expect(labels).toContain('Amber');
+                        expect(labels).toContain('Antler');
                     });
-                }
-            )
-        ,5000);
+            }))
+        );
     });
 }
+
+@Component({
+    selector: 'oec',
+    template: '<object-edit [(object)]="selectedObject" [(projectConfiguration)]="projectConfiguration"></object-edit>',
+    directives: [ObjectEditComponent]
+})
+class TestComponent {}

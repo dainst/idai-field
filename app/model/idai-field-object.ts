f72dc0f55c624bf58e0190d9499c67ec1569fb8c
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2016-06-07 18:31:48 +0200

    Move title field from Entity to IdaiFieldObject.

--- app/model/idai-field-object.ts
+++ app/model/idai-field-object.ts
@@ -3,5 +3,6 @@ import {Entity} from "idai-components-2/idai-components-2";
 export interface IdaiFieldObject extends Entity {
     synced: number;
     modified?: Date;
+    title: string;
     created?: Date;
 }

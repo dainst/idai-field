e83d2b20aea4ac21eea81ec21620272470a40c2d
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2016-06-24 19:29:11 +0200

    Factor out overview spec and add todo.

---
+++ e2e/overview.spec.js
@@ -0,0 +1,44 @@
+var common = require("./common.js");
+
+describe('overview component', function() {
+    
+    it ("should change the selection when saving via modal", function() {
+        common.createObject("1")
+            .then(common.selectObject(0))
+            .then(common.typeInIdentifier("2"))
+            .then(common.clickCreateObjectButton())
+            .then(common.scrollUp)
+            .then(clickSaveInModal)
+            .then(common.scrollUp)
+            .then(function(){
+                expect(element(by.id('object-overview-note-0')).getText()).toEqual("Neues Objekt");
+            })
+    });
+
+    it ("should change the selection when saving via modal", function() {
+        common.createObject("1")
+            .then(common.createObject("2"))
+            .then(common.selectObject(0))
+            .then(common.typeInIdentifier("2a"))
+            .then(common.selectObject(1))
+            .then(common.scrollUp)
+            .then(clickSaveInModal)
+            .then(common.scrollUp)
+            .then(function(){
+                expect(element.all(by.css('#objectList .list-group-item')).get(1)
+                    .getAttribute('class')).toContain('selected')
+            })
+    });
+
+    
+    
+    
+    // TODO write test that prevents change of selection if the save was confirmed
+    // via modal but there was an error. 
+    
+    
+    
+    function clickSaveInModal() {
+        return element(by.id('overview-save-confirmation-modal-save-button')).click();
+    }
+});

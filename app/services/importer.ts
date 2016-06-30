993e70c8491b152607fb6ce8c5a717898d36a640
Thomas Kleinke <tkleinke@smail.uni-koeln.de>
2016-06-30 14:59:36 +0200

    reset counter variables in importer after displaying a message

--- app/services/importer.ts
+++ app/services/importer.ts
@@ -8,17 +8,18 @@ import {M} from "../m";
 
 /**
  * @author Daniel de Oliveira
+ * @author Thomas Kleinke
  */
 @Injectable()
 export class Importer {
 
-    private userNotificationInPreparation=false;
-    private nrErrorsCurrentImport:number=0;
-    private nrSuccessesCurrentImport:number=0;
+    private userNotificationInPreparation = false;
+    private nrErrorsCurrentImport: number = 0;
+    private nrSuccessesCurrentImport: number = 0;
     
     constructor(
-        private objectReader:ObjectReader,
-        private messages:Messages,
+        private objectReader: ObjectReader,
+        private messages: Messages,
         private project: Project,
         private datastore: Datastore
     ) {}
@@ -34,16 +35,16 @@ export class Importer {
 
                 this.datastore.update(doc).then(
                     ()=>{
-                        this.nrSuccessesCurrentImport=this.nrSuccessesCurrentImport+1;
+                        this.nrSuccessesCurrentImport++;
                         this.prepareNotification();
                     },
                     ()=>{
-                        this.nrErrorsCurrentImport=this.nrErrorsCurrentImport+1;
+                        this.nrErrorsCurrentImport++;
                         this.prepareNotification();
                     }
                 );
             },()=>{
-                this.nrErrorsCurrentImport=this.nrErrorsCurrentImport+1;
+                this.nrErrorsCurrentImport++;
                 this.prepareNotification();
             });
         }.bind(this));
@@ -53,13 +54,13 @@ export class Importer {
         
         if (!this.userNotificationInPreparation) {
             setTimeout(this.notifyUser.bind(this), 800);
-            this.userNotificationInPreparation=true;
+            this.userNotificationInPreparation = true;
         }
     }
 
     private notifyUser() {
 
-        this.userNotificationInPreparation=false;
+        this.userNotificationInPreparation = false;
         
         if (this.nrSuccessesCurrentImport > 0) {
             this.project.fetchAllDocuments();
@@ -68,7 +69,9 @@ export class Importer {
             } else {
                 this.messages.add(M.IMPORTER_SUCCESS_MULTIPLE, [this.nrSuccessesCurrentImport.toString()]);
             }
+            this.nrSuccessesCurrentImport = 0;
         }
+
         if (this.nrErrorsCurrentImport > 0) {
             this.project.fetchAllDocuments();
             if (this.nrErrorsCurrentImport == 1) {
@@ -76,6 +79,7 @@ export class Importer {
             } else {
                 this.messages.add(M.IMPORTER_FAILURE_MULTIPLE, [this.nrErrorsCurrentImport.toString()]);   
             }
+            this.nrErrorsCurrentImport = 0;
         }
     }
 }

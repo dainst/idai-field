8f696734ee407ac375d377b8ee15c758c3079f7f
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2016-08-08 13:43:39 +0200

    Adjust sample objects. Add 'relations' level.

--- app/datastore/sample-objects.ts
+++ app/datastore/sample-objects.ts
@@ -5,7 +5,7 @@ export var DOCS: IdaiFieldDocument[] = [
         "id" : "o1", "resource" : { "id": "o1",
             "identifier": "ob1",
             "title": "Obi One Kenobi",
-            "cuts" : ["o2"],
+            "relations" : { "cuts" : ["o2"] },
             "type": "object"
         },
         "synced": 0
@@ -14,26 +14,29 @@ export var DOCS: IdaiFieldDocument[] = [
         "id" : "o2", "resource" : { "id" : "o2",
             "identifier": "ob2",
             "title": "Qui Gon Jinn",
-            "isCutBy" : ["o1"],
+            "relations" : { "isCutBy" : ["o1"] },
             "type": "object"
         },
         "synced": 0
     },
     {
         "id" : "o3", "resource" : { "id": "o3",
-            "identifier": "ob3", "title": "Luke Skywalker", "type": "object"
+            "identifier": "ob3", "title": "Luke Skywalker", "type": "object",
+            "relations" : {}
         },
         "synced": 0
     },
     {
         "id" : "o4", "resource" : { "id": "o4",
-            "identifier": "ob4", "title": "Han Solo", "type": "object"
+            "identifier": "ob4", "title": "Han Solo", "type": "object",
+            "relations" : {}
         },
         "synced": 0
     },
     {
         "id" : "o5", "resource" : { "id": "o5",
-            "identifier": "ob5", "title": "Boba Fett", "type": "object"
+            "identifier": "ob5", "title": "Boba Fett", "type": "object",
+            "relations" : {}
         },
         "synced": 0
     }

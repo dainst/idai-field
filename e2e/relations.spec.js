727150b7dafef082ba04fb38d761527e13f6320f
Daniel <danielmarreirosdeoliveira@googlemail.com>
2016-09-07 12:27:17 +0200

    Update relations.spec.js

--- e2e/relations.spec.js
+++ e2e/relations.spec.js
@@ -1,6 +1,6 @@
 var common = require("./common.js");
 
-describe('relations', function() {
+fdescribe('relations', function() {
 
     function addRelation() {
         return element.all(by.tagName('relation-picker-group')).first()
@@ -24,7 +24,7 @@ describe('relations', function() {
         browser.get('/');
     });
 
-    it('should create a new relation and the corresponding inverse relation', function() {
+    fit('should create a new relation and the corresponding inverse relation', function() {
         // expect(getFirstRelationOfGroup(0).isPresent()).toBe(false); known not to work on ci
 
         common.createObject("o1")

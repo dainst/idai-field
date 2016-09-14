b05628e7e09e7e78cf3558b28e18bb2d70d0419e
Thomas Kleinke <tkleinke@smail.uni-koeln.de>
2016-09-14 19:32:30 +0200

    exclude idai-components-2 e2e tests in karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,8 @@ module.exports = function(config) {
         exclude: [
             'node_modules/@angular/**/*_spec.js',
             'node_modules/idai-components-2/lib/test/**/*.js',
-            'node_modules/idai-components-2/test/**/*.js'
+            'node_modules/idai-components-2/test/**/*.js',
+            'node_modules/idai-components-2/e2e/**/*.js'
         ],
 
         reporters: ['dots'],

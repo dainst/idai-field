bf9ce5a438d1cf4bb9a54f067dc3e9900cf9a532
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2016-09-18 17:40:28 +0200

    Make overview home showing just blank space.

--- app/overview/overview-home.component.ts
+++ app/overview/overview-home.component.ts
@@ -1,19 +1,10 @@
-import {Component, OnInit, Inject, ViewChild, TemplateRef} from '@angular/core';
-import {Router} from '@angular/router';
-import {IdaiFieldDocument} from '../model/idai-field-document';
-import {ObjectList} from "./object-list";
-import {Messages} from "idai-components-2/idai-components-2";
-import {M} from "../m";
-import {DocumentEditChangeMonitor} from "idai-components-2/idai-components-2";
-import {Validator} from "../model/validator";
-import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
-import {PersistenceService} from "./persistence-service";
+import {Component} from "@angular/core";
 
 @Component({
     moduleId: module.id,
-    template: `<h1>Overview home</h1>`
+    template: ``
 })
 
 /**
  */
-export class OverviewHomeComponent implements OnInit {}
+export class OverviewHomeComponent {}

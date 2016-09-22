3e8cf586e091635b4a274dc14ff8572f248e00ed
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2016-09-22 20:34:47 +0200

    Remove OverviewHomeComponent.

--- app/overview/overview.module.ts
+++ app/overview/overview.module.ts
@@ -3,7 +3,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {overviewRouting} from './overview.routing';
 import {OverviewComponent} from './overview.component';
 import {DocumentViewComponent} from './document-view/document-view.component';
-import {OverviewHomeComponent} from './overview-home.component';
 import {DocumentEditWrapperComponent} from './document-edit-wrapper.component';
 import {IdaiComponents2Module} from 'idai-components-2/idai-components-2';
 import {MapWrapperComponent} from './map-wrapper.component';
@@ -20,7 +19,6 @@ import {MapComponent} from "./map/map.component";
     declarations: [
         OverviewComponent,
         DocumentViewComponent,
-        OverviewHomeComponent,
         DocumentEditWrapperComponent,
         MapWrapperComponent,
         MapComponent

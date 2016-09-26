d730be86849f59acc0253a67d8a25a12496a54b0
Thomas Kleinke <tkleinke@smail.uni-koeln.de>
2016-09-26 14:20:03 +0200

    use newest leaflet typings

--- app/overview/map/idai-field-polygon.ts
+++ app/overview/map/idai-field-polygon.ts
@@ -3,7 +3,4 @@ import {IdaiFieldDocument} from "../../model/idai-field-document";
 export interface IdaiFieldPolygon extends L.Polygon {
 
     document?: IdaiFieldDocument;
-
-    // TODO Remove this as soon as the typings file for Leaflet 1.0.0-rc.3 has been completed
-    on?(type: string, fn: any, context?: any): any;
 }

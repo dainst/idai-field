ef1e7499318d31c4f2c2d501913dee165a9fe3e9
fabianzwodrei <fabian@zwodrei.com>
2016-11-14 22:17:04 +0100

    configuration validator checks for mandatory fields and their position

--- app/configuration-validator.ts
+++ app/configuration-validator.ts
@@ -6,17 +6,54 @@ interface ConfigurationValidationResult {
 }
 
 export class ConfigurationValidator {
+    private errors : string[]
+    private projectConfig : ProjectConfiguration;
+
+    private mandatoryFields = [{
+        name : "identifier",
+        inputType : "text",
+        index: 0
+    },{
+        name : "shortDescription",
+        inputType : "text",
+        index: 1
+    }];
+
+    private validateMandatoryFields () {
+        var typesList = this.projectConfig.getTypesList();
+        
+        this.mandatoryFields.forEach(mandatoryField => {
+            typesList.forEach(type => {
+                var mandatoryFieldFoundAt = -1;
+                type.getFields().forEach(function (field, index) {
+                    if(field.name == mandatoryField.name) {
+                        // if necessary, move mandatory field to right index
+                        if ((mandatoryFieldFoundAt = index) != mandatoryField.index)
+                            type.fields.splice(mandatoryField.index, 0, type.fields.splice(mandatoryFieldFoundAt, 1)[0]);
+                        return;
+                    }
+                });
+                // if mandatory field was not found add it
+                if (mandatoryFieldFoundAt == -1) {
+                    type.fields.splice(mandatoryField.index, 0, mandatoryField);
+                    this.errors.push("Configuration: Type " + type.name + " has no " + mandatoryField.name + ". It was added.");
+                }
+            })
+        })
+    }
     
     public validate(projectConfiguration: ProjectConfiguration) : ConfigurationValidationResult {
 
-        var errors : string[] = [];
+        this.projectConfig = projectConfiguration
+        this.errors = [];
 
-        if(!projectConfiguration.getTypesMap()["image"]) {
-            errors.push(M.CONFIG_VALIDATION_IMAGE_MISSING);
-        }
+        if(!this.projectConfig.getTypesMap()["image"])
+            this.errors.push(M.CONFIG_VALIDATION_IMAGE_MISSING);
+
+        this.validateMandatoryFields();
         
         return {
-            errors:  errors
+            errors:  this.errors
         }
     }
 }

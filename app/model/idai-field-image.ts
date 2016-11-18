84e091d4d6c273683e761305d4f62d1dfd67cb74
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2016-11-18 14:34:57 +0100

    add model class for images

---
+++ app/model/idai-field-image.ts
@@ -0,0 +1,10 @@
+import {Resource} from "idai-components-2/core";
+import {IdaiFieldResource} from "./idai-field-resource";
+
+export interface IdaiFieldImage extends Resource {
+    identifier: string;
+    filename: string;
+    width: number;
+    height: number;
+    depicts?: IdaiFieldResource;
+}

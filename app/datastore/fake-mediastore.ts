496f74f1cec0a388af945fe435b0785a6dba48ad
Daniel <danielmarreirosdeoliveira@googlemail.com>
2016-11-23 13:49:24 +0100

    Return values as specified to prevent 'method of undefined' errors.

--- app/datastore/fake-mediastore.ts
+++ app/datastore/fake-mediastore.ts
@@ -1,63 +1,28 @@
 import {Observable} from "rxjs/Observable";
 import {Mediastore} from 'idai-components-2/datastore';
 
-
+/**
+ * @author Daniel de Oliveira
+ */
 export class FakeMediastore implements Mediastore {
 
-    private basePath: string = 'store/';
-    
-    constructor() {
-        console.log("fake")
-    }
-    
-    /**
-     * @param key the identifier for the data
-     * @param data the binary data to be stored
-     * @returns {Promise<any>} resolve -> (),
-     *   reject -> the error message
-     */
-    public create(key: string, data: any): Promise<any> {
-        return null;
+    public create(): Promise<any> {
+        return new Promise<any>((resolve)=>{resolve();});
     }
 
-    /**
-     * @param key the identifier for the data
-     * @returns {Promise<any>} resolve -> (data), the data read with the key,
-     *  reject -> the error message
-     */
-    public read(key: string): Promise<any> {
-
-        return null;
+    public read(): Promise<any> {
+        return new Promise<any>((resolve)=>{resolve();});
     }
 
-    /**
-     * @param key the identifier for the data
-     * @param data the binary data to be stored
-     * @returns {Promise<any>} resolve -> (),
-     *   reject -> the error message
-     */
-    public update(key: string, data: any): Promise<any> {
-
-        return null;
+    public update(): Promise<any> {
+        return new Promise<any>((resolve)=>{resolve();});
     }
 
-    /**
-     * @param key the identifier for the data to be removed
-     * @returns {Promise<any>} resolve -> (),
-     *   reject -> the error message
-     */
-    public remove(key: string): Promise<any> {
-
-        return null;
+    public remove(): Promise<any> {
+        return new Promise<any>((resolve)=>{resolve();});
     }
 
-    /**
-     * Subscription enables clients to get notified
-     * when files get modified via one of the accessor
-     * methods defined here.
-     */
     public objectChangesNotifications(): Observable<File> {
-        return null;
+        return Observable.create( () => {});
     }
-
 }

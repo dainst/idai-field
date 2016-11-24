5bb4774516e959b6113bc5a080c646edad7a4e0c
Simon Hohl <mail@dersmon.net>
2016-11-24 09:37:18 +0100

    Added image grid -> single view -> grid test.

--- test/e2e/images/images-grid.spec.js
+++ test/e2e/images/images-grid.spec.js
@@ -40,11 +40,33 @@ describe('image grid tests', function(){
     });
 
     it('deselecting all images by clicking appropriate button', function() {
-        var cell = element(by.css('.cell'));
+        var cell = element.all(by.css('.cell')).first();
         cell.click().then(function(){
             expect(cell.getAttribute('class')).toMatch('selected');
             element(by.id('deselect-images')).click().then(function(){
                 expect(cell.getAttribute('class')).not.toMatch('selected');
+            });
+        });
+    });
+
+    it('testing navigation grid -> single view -> grid', function() {
+        var xpath = '//div[@class="fieldname"][text()="filename"]/following-sibling::div[@class="fieldvalue"]';
+        var originalCell = element.all(by.css('.cell')).first();
+
+        originalCell.element(by.css('.tag.tag-default')).getText().then(function(initialId){
+            browser.actions().doubleClick(originalCell).perform();
+            browser.wait(EC.presenceOf(element(by.id('document-view'))));
+
+            expect(element(by.xpath(xpath)).getText())
+                .toEqual(initialId);
+
+            var backButton = element(by.id('document-view-button-back-to-map'));
+
+            backButton.click().then(function(){
+                browser.wait(EC.presenceOf(element.all(by.css('.cell')).first()));
+
+                expect(element.all(by.css('.cell')).first().element(by.css('.tag.tag-default')).getText())
+                    .toEqual(initialId)
             });
         });
     });

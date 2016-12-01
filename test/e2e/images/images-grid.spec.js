895d564550def920406034821d2dbe60ff3b51ba
Daniel <danielmarreirosdeoliveira@googlemail.com>
2016-12-01 19:47:17 +0100

    Remove f.

--- test/e2e/images/images-grid.spec.js
+++ test/e2e/images/images-grid.spec.js
@@ -94,7 +94,7 @@ describe('image grid tests -- ', function(){
             });
     });
 
-    fit('user should be able to navigate from grid to view, and back to grid.', function() {
+    it('user should be able to navigate from grid to view, and back to grid.', function() {
         var xpath = '//div[@class="fieldname"][text()="Dateiname"]/following-sibling::div[@class="fieldvalue"]';
 
         gridPage.getCellImageName(0).then(function(imageName){

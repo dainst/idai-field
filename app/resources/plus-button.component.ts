d8894ba8f038a7623d7c39ea093cef19982de14c
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-01-09 20:18:30 +0100

    removed image type & children from type list in plus button menu

--- app/resources/plus-button.component.ts
+++ app/resources/plus-button.component.ts
@@ -1,6 +1,6 @@
 import {Component} from "@angular/core";
 import {Router} from "@angular/router";
-import {ConfigLoader, WithConfiguration} from "idai-components-2/configuration";
+import {ConfigLoader, IdaiType, ProjectConfiguration} from "idai-components-2/configuration";
 
 @Component({
     selector: 'plus-button',
@@ -12,15 +12,18 @@ import {ConfigLoader, WithConfiguration} from "idai-components-2/configuration";
  * @author Thomas Kleinke
  * @author Daniel de Oliveira
  */
-export class PlusButtonComponent extends WithConfiguration {
+export class PlusButtonComponent {
 
+    private typesTreeList: IdaiType[];
     private type: string;
 
     constructor(
         private router: Router,
         configLoader: ConfigLoader)
     {
-        super(configLoader);
+        configLoader.configuration().subscribe(result => {
+            this.initializeTypesTreeList(result.projectConfiguration);
+        });
     }
 
     public startDocumentCreation(geometryType: string) {
@@ -31,9 +34,20 @@ export class PlusButtonComponent extends WithConfiguration {
             this.router.navigate(['resources/editGeometry', 'new:' + this.type, geometryType]);
         }
     }
-    
+
     public reset() {
 
         this.type = undefined;
+    }
+
+    private initializeTypesTreeList(projectConfiguration: ProjectConfiguration) {
+
+        this.typesTreeList = [];
+
+        for (var type of projectConfiguration.getTypesTreeList()) {
+            if (type.name != "image") {
+                this.typesTreeList.push(type);
+            }
+        }
     }
 }

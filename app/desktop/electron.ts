54d760ec4e6d0659d0f62340d6eb183c07b2a310
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2017-01-13 12:32:26 +0000

    Add electron exports.

---
+++ app/desktop/electron.ts
@@ -0,0 +1,7 @@
+const electron = (<any>window).require('electron');
+
+export const {BrowserWindowProxy} = electron;
+export const {desktopCapturer} = electron;
+export const {ipcRenderer} = electron;
+export const {remote} = electron;
+export const {webFrame} = electron;

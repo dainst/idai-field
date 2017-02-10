e7d8fdb4a6cfe28bbade919ec633f1dca4f23ed0
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2017-02-10 11:47:29 +0100

    fix drag & drop image upload (closes #4351)

--- app/images/drop-area.component.ts
+++ app/images/drop-area.component.ts
@@ -45,9 +45,8 @@ export class DropAreaComponent extends WithConfiguration {
     public onDrop(event) {
 
         event.preventDefault();
-        this.chooseType().then(
-            type => this.uploadFiles(event.dataTransfer.files, type)
-        );
+        let files = event.dataTransfer.files;
+        this.chooseType().then(type => this.uploadFiles(files, type));
     }
 
     public onSelectImages(event) {
@@ -74,6 +73,8 @@ export class DropAreaComponent extends WithConfiguration {
     }
 
     private uploadFiles(files: File[], type: IdaiType) {
+
+        console.log("uploadFiles", files);
 
         if (files && files.length > 0) {
             for (var i = 0; i < files.length; i++) this.uploadFile(files[i], type);

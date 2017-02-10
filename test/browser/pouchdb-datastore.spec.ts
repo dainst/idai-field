65310935672f14e7161d93179d788299ae26f6d1
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-02-10 22:54:25 +0000

    Provoke err.

--- test/browser/pouchdb-datastore.spec.ts
+++ test/browser/pouchdb-datastore.spec.ts
@@ -27,7 +27,7 @@ export function main() {
 
         beforeEach(
             function () {
-                datastore = new PouchdbDatastore();
+                datastore = new PouchdbDatastore("a","b");
             }
         );
 

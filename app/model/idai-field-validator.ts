1d28d1ea0df6df3e65da8bc429cdfa8f30f1a77c
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-02-17 09:56:46 +0000

    Extract method.

--- app/model/idai-field-validator.ts
+++ app/model/idai-field-validator.ts
@@ -2,7 +2,6 @@ import {ConfigLoader} from 'idai-components-2/configuration'
 import {ReadDatastore} from 'idai-components-2/datastore'
 import {Validator} from 'idai-components-2/persist';
 import {IdaiFieldDocument} from './idai-field-document';
-import {Datastore, Query, FilterSet, Filter} from "idai-components-2/datastore";
 import {M} from "../m";
 
 
@@ -24,20 +23,21 @@ export class IdaiFieldValidator extends Validator {
     protected validateCustom(doc:IdaiFieldDocument): Promise<any> {
         return new Promise<any>((resolve,reject) => {
 
-            let q : Query = {q:doc.resource['identifier'],filterSets:[]};
+            this.datastore.find({q:doc.resource['identifier'],filterSets:[]},'identifier').then(results => {
 
-            this.datastore.find(q,'identifier').then(results => {
-
-                let other = 0;
-                for (let result of results) {
-                  if (result.resource['identifier'] == doc.resource['identifier']) { // necessary because at the moment find 'o2' will also find 'o22'
-                      if (result.resource['id'] != doc.resource['id']) other++;
-                  }
-                }
-                if (other > 0) return reject([M.DATASTORE_IDEXISTS]);
-
+                if (IdaiFieldValidator.isDuplicate(results,doc)) return reject([M.DATASTORE_IDEXISTS]);
                 resolve();
             });
         });
+    }
+
+    private static isDuplicate(results,doc) {
+        let other = 0;
+        for (let result of results) {
+            if (result.resource['identifier'] == doc.resource['identifier']) { // necessary because at the moment find 'o2' will also find 'o22'
+                if (result.resource['id'] != doc.resource['id']) other++;
+            }
+        }
+        return (other > 0);
     }
 }

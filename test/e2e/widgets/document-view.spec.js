a55074dd31092e91d1fe335d6bf809476fd97e88
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-02-22 11:22:45 +0000

    Add failing test for problem with stale relations.

--- test/e2e/widgets/document-view.spec.js
+++ test/e2e/widgets/document-view.spec.js
@@ -7,15 +7,15 @@ var delays = require('../config/delays');
 /**
  * @author Daniel de Oliveira
  */
-describe('resources view tests --', function() {
+describe('document view tests --', function() {
 
     beforeEach(function () {
         resourcesPage.get();
     });
 
-    it('fields view should show the fields present in the object', function () {
+    it('document view should show the fields present in the object', function () {
         resourcesPage.createResource('1', 0);
-        resourcesPage.selectObjectByIndex(0);
+        resourcesPage.selectResourceByIdentifier("1");
         expect(documentViewPage.getFieldName(0)).toBe('Identifier'); // with the correct field label
         expect(documentViewPage.getFieldValue(0)).toBe('1');
     });
@@ -23,17 +23,17 @@ describe('resources view tests --', function() {
     /**
      * Addresses an issue where fields were shown double.
      */
-    it('fields view should show only the fields present in the object', function () {
+    it('document view should show only the fields present in the object', function () {
         resourcesPage.createResource('1', 0);
-        resourcesPage.selectObjectByIndex(0);
+        resourcesPage.selectResourceByIdentifier("1");
         documentViewPage.getFields().then(function(items) {
             expect(items.length).toBe(1);
         });
     });
 
-    it('fields view should show the relations present in the object', function () {
+    it('document view should show the relations present in the object', function () {
         resourcesPage.createLink();
-        resourcesPage.selectObjectByIndex(1);
+        resourcesPage.selectResourceByIdentifier("1");
         expect(documentViewPage.getRelationName(0)).toBe('Sohn von'); // with the correct relation label
         expect(documentViewPage.getRelationValue(0)).toBe('2');
     });
@@ -41,11 +41,32 @@ describe('resources view tests --', function() {
     /**
      * Addresses an issue where relations were shown double.
      */
-    it('fields view should show only relations present in the object', function () {
+    it('document view should show only relations present in the object', function () {
         resourcesPage.createLink();
-        resourcesPage.selectObjectByIndex(1);
+        resourcesPage.selectResourceByIdentifier("1");
         documentViewPage.getRelations().then(function(relations) {
             expect(relations.length).toBe(1);
+        });
+    });
+
+    /**
+     * Addresses an issue where relations were still shown after cancelling edit and discarding changes
+     * (they were not saved though).
+     */
+    xit('fields view should show only relations present in the object', function () {
+        resourcesPage.createResource('1');
+        resourcesPage.createResource('2');
+        resourcesPage.selectResourceByIdentifier("1");
+        resourcesPage.clickEditDocument();
+        resourcesPage.clickAddRelationForGroupWithIndex(0);
+        resourcesPage.typeInRelationByIndices(0, 0, '2');
+        resourcesPage.clickRelationSuggestionByIndices(0, 0, 0);
+        resourcesPage.selectResourceByIdentifier("1");
+        resourcesPage.clickDiscardInModal();
+
+        browser.wait(EC.visibilityOf(element(by.tagName('document-view'))), delays.ECWaitTime); // to prove document view is visible
+        documentViewPage.getRelations().then(function(relations) {
+            expect(relations.length).toBe(0);
         });
     });
 });

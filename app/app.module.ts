bba300f7485c8c2d01059d26b7c0bb415ef37244
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-02-22 21:27:47 +0000

    Fix mediastorepath for httpmediastore.

--- app/app.module.ts
+++ app/app.module.ts
@@ -67,7 +67,7 @@ import {CachedDatastore} from "./datastore/cached-datastore";
                     return new FileSystemMediastore(path, CONFIG['environment'] == 'test');
                 // running in browser
                 } else {
-                    let path = CONFIG['mediastorepath'] ? CONFIG['mediastorepath'] : 'mediastore';
+                    let path = CONFIG['mediastorepath'] ? CONFIG['mediastorepath'] : 'sample-images';
                     return new HttpMediastore(http, path);
                 }
             },

18641e2f75e905784fe34b9385665ee6edcd630d
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-02-24 10:52:48 +0000

    Convert 2 more files.

--- test/e2e/navbar.page.js
+++ test/e2e/navbar.page.js
@@ -1,16 +1,14 @@
+"use strict";
+var Protractor_1 = require("Protractor");
 'use strict';
-var EC = protractor.ExpectedConditions;
+var EC = Protractor_1.protractor.ExpectedConditions;
 var delays = require('./config/delays');
-
-
-var NavbarPage = function() {
-
+var NavbarPage = function () {
     // click
-
     this.clickNavigateToResources = function () {
-        browser.wait(EC.visibilityOf(element(by.css('navbar ul li:nth-child(1)'))), delays.ECWaitTime);
-        element(by.css('navbar ul li:nth-child(1)')).click();
+        Protractor_1.browser.wait(EC.visibilityOf(Protractor_1.element(Protractor_1.by.css('navbar ul li:nth-child(1)'))), delays.ECWaitTime);
+        Protractor_1.element(Protractor_1.by.css('navbar ul li:nth-child(1)')).click();
     };
 };
-
 module.exports = new NavbarPage();
+//# sourceMappingURL=navbar.page.js.map
---
+++ test/e2e/navbar.page.ts
@@ -0,0 +1,19 @@
+import {browser,protractor,element,by} from 'Protractor';
+
+
+'use strict';
+var EC = protractor.ExpectedConditions;
+var delays = require('./config/delays');
+
+
+var NavbarPage = function() {
+
+    // click
+
+    this.clickNavigateToResources = function () {
+        browser.wait(EC.visibilityOf(element(by.css('navbar ul li:nth-child(1)'))), delays.ECWaitTime);
+        element(by.css('navbar ul li:nth-child(1)')).click();
+    };
+};
+
+module.exports = new NavbarPage();

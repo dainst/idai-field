7239a84b87ea9eefd1c20d47cc84109f815e0ed9
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-03-07 14:18:40 +0100

    use IdaiFieldDatastore in ImportComponent

--- app/import/merge-geometries-import-strategy.ts
+++ app/import/merge-geometries-import-strategy.ts
@@ -10,9 +10,7 @@ import {IdaiFieldDocument} from "../model/idai-field-document";
  */
 export class MergeGeometriesImportStrategy implements ImportStrategy {
 
-    constructor(
-                private datastore: IdaiFieldDatastore)  // TODO check this. this is strange, i can define the type here, but on the caller i just pass a regular datastore, so this a call to findByIdentifier would fail
-    { }
+    constructor(private datastore: IdaiFieldDatastore) { }
 
     importDoc(doc: Document): Promise<any> {
         let idaiFieldDoc = doc as IdaiFieldDocument;

7cc17fa7c5162b7777a92d938891ca4c387797fe
Daniel <danielmarreirosdeoliveira@googlemail.com>
2017-03-29 19:50:22 +0100

    Suppress compile error.

--- app/imagestore/converter.ts
+++ app/imagestore/converter.ts
@@ -1,9 +1,16 @@
 import {nativeImage} from 'electron';
+// suppress compile errors TODO remove when typings are there
+interface NI { createFromBuffer(key:any):any; }
 
+/**
+ * @author F.Z.
+ * @author Daniel de Oliveira
+ */
 export class Converter {
 
     public convert(data) {
-        let img = nativeImage.createFromBuffer(Buffer.from(data));
+        let img = (nativeImage as NI) // TODO see TODO at top of the page
+            .createFromBuffer(Buffer.from(data));
         img = img.resize({height: 320});
         return img.toJPEG(60);
     }

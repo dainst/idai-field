5e6872ad962930b793132626b10a28102d3d860c
Sebastian Cuy <sebastian.cuy@dainst.de>
2017-04-11 14:52:31 +0200

    remove fdescribe

--- test/e2e/resources/resources-syncing.spec.ts
+++ test/e2e/resources/resources-syncing.spec.ts
@@ -13,7 +13,7 @@ const documentViewPage = require('../widgets/document-view.page');
 /**
  * @author Sebastian Cuy
  */
-fdescribe('resources/syncing', function() {
+describe('resources/syncing', function() {
 
     const defaultConf = path.resolve(__dirname, '../../../config/config.json');
     const tempConf = path.resolve(__dirname, './config.json.tmp');

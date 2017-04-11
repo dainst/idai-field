a347a16b67c9b66d4c19c7d8f211e53ec04ee64d
Sebastian Cuy <sebastian.cuy@dainst.de>
2017-04-11 12:07:05 +0200

    make conflict save test work without affecting other tests (closes #4742)

--- test/e2e/resources/resources-syncing.spec.ts
+++ test/e2e/resources/resources-syncing.spec.ts
@@ -3,15 +3,21 @@ import {browser} from 'protractor';
 import * as PouchDB from 'pouchdb';
 import * as cors from 'pouchdb-server/lib/cors'
 import * as express from 'express';
-let expressPouchDB = require('express-pouchdb');
+const expressPouchDB = require('express-pouchdb');
+const fs = require('fs-extra');
+const path = require('path');
 
-let resourcesPage = require('./resources.page');
-let documentViewPage = require('../widgets/document-view.page');
+const resourcesPage = require('./resources.page');
+const documentViewPage = require('../widgets/document-view.page');
 
 /**
  * @author Sebastian Cuy
  */
-xdescribe('resources/syncing', function() {
+describe('resources/syncing', function() {
+
+    const defaultConf = path.resolve(__dirname, '../../../config/config.json');
+    const tempConf = path.resolve(__dirname, './config.json.tmp');
+    const syncConf = path.resolve(__dirname, './resources-syncing.config.json');
 
     let db;
     let testResource =  {
@@ -31,7 +37,6 @@ xdescribe('resources/syncing', function() {
             app.use('/', pouchDbApp);
             app.listen(3000, function () {
                 new PouchDB('idai-field-documents').destroy().then(() => {
-                    console.log("PouchDB Server listening on port 3000");
                     resolve(new PouchDB('idai-field-documents'));
                 });
             });
@@ -39,7 +44,6 @@ xdescribe('resources/syncing', function() {
             .then(newDb => db = newDb)
             .then(() => db.post(testDocument))
             .then(result => {
-                console.log("saved test doc", result);
                 testDocument._id = result.id;
                 testDocument._rev = result.rev;
             });
@@ -49,16 +53,23 @@ xdescribe('resources/syncing', function() {
         testDocument.resource.identifier = 'test2';
         return db.put(testDocument)
             .then(result => {
-                console.log("updated test doc", result);
                 testDocument._id = result.id;
                 testDocument._rev = result.rev;
             })
             .catch(err => console.log("failure while updating test doc", err));
     }
 
+    beforeAll(() => {
+        fs.copySync(defaultConf, tempConf);
+        fs.copySync(syncConf, defaultConf);
+    });
+
+    afterAll(() => {
+        fs.moveSync(tempConf, defaultConf, { overwrite: true });
+    });
+
     beforeEach(done => {
-        resourcesPage.get()
-            .then(setupTestDB)
+        setupTestDB()
             .then(done)
             .catch(err => { console.error(err) });
     });
@@ -68,15 +79,14 @@ xdescribe('resources/syncing', function() {
            .catch(err => { console.error("Error in afterEach", err) });
     });
 
-    xit('should detect conflict on save', function(done) {
+    it('should detect conflict on save', function(done) {
 
-        browser.waitForAngular()
+        resourcesPage.get()
             .then(() => resourcesPage.typeInIdentifierInSearchField('test1'))
             .then(() => resourcesPage.clickSelectResource('test1'))
             .then(() => documentViewPage.clickEditDocument())
             .then(updateTestDoc)
             .then(() => DocumentEditWrapperPage.clickSaveDocument())
-            .then(() => console.log("done"))
             .then(done)
             .catch(err => fail(err));
     });

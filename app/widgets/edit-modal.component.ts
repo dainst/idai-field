bb929485e2a9f0125d8ac2ba11a28197cf313c23
fabianzwodrei <fabian@zwodrei.com>
2017-06-01 18:01:04 +0200

    fixing tests and adding messages to modal

--- app/widgets/edit-modal.component.ts
+++ app/widgets/edit-modal.component.ts
@@ -2,6 +2,8 @@ import {Component,ViewChild,TemplateRef} from '@angular/core';
 import {NgbActiveModal, NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 import {IdaiFieldDocument} from 'idai-components-2/idai-field-model';
 import {DocumentEditChangeMonitor} from "idai-components-2/documents";
+import {Messages} from 'idai-components-2/messages';
+
 
 
 @Component({
@@ -24,7 +26,8 @@ export class EditModalComponent {
 
     constructor(public activeModal: NgbActiveModal,
                 private modalService: NgbModal,
-                private documentEditChangeMonitor: DocumentEditChangeMonitor
+                private documentEditChangeMonitor: DocumentEditChangeMonitor,
+                private messages: Messages
     ) {
     }
 

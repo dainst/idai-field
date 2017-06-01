97bc1c93e885903fe35911382420168d2d8feb0b
fabianzwodrei <fabian@zwodrei.com>
2017-06-01 17:18:53 +0200

    fixes selection of docs from relations in map, revisions

--- app/resources/map-wrapper.component.ts
+++ app/resources/map-wrapper.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit, OnDestroy, Input} from '@angular/core';
 import {ResourcesComponent} from './resources.component';
-import {ReadDatastore} from 'idai-components-2/datastore';
 import {PersistenceManager} from 'idai-components-2/persist';
-import {ConfigLoader} from 'idai-components-2/configuration';
 import {IdaiFieldDocument, IdaiFieldGeometry} from 'idai-components-2/idai-field-model';
 import {SettingsService} from '../settings/settings-service';
 
@@ -20,63 +18,20 @@ import {SettingsService} from '../settings/settings-service';
 export class MapWrapperComponent implements OnInit, OnDestroy {
 
     @Input() activeDoc: IdaiFieldDocument;
-    private activeType: string;
-    private activeTypeLabel: string;
+
+
     private docs: IdaiFieldDocument[];
 
     @Input() editMode: boolean = false;
 
     constructor(
-        private datastore: ReadDatastore,
         private resourcesComponent: ResourcesComponent,
-        private configLoader: ConfigLoader,
         private persistenceManager: PersistenceManager,
         private settingsService: SettingsService
     ) {
     }
 
-    public selectDocument(documentToJumpTo: IdaiFieldDocument) {
-        if (documentToJumpTo) {
-            if (this.docs.indexOf(documentToJumpTo) == -1) {
-                this.resourcesComponent.queryChanged({q: '', type: 'resource', prefix: true}).then(
-                    () => {
-                        this.scrollToDocument(documentToJumpTo);
-                        //this.router.navigate(['resources', { id: documentToJumpTo.resource.id }]);
-                    }
-                )
-            } else {
-                this.scrollToDocument(documentToJumpTo);
-                //this.router.navigate(['resources', {id: documentToJumpTo.resource.id}]);
-            }
-        } else {
-            //this.router.navigate(['resources']);
-        }
-    }
-
-    private scrollToDocument(doc) {
-
-        let element = document.getElementById('resource-' + doc.resource.identifier);
-        if (element) element.scrollIntoView({ behavior: 'smooth' });
-    }
-
-    private setActiveDoc(id) {
-        if (id) {
-            this.datastore.get(id).then(document=> {
-                this.activeDoc = document as IdaiFieldDocument;
-                this.activeType = this.activeDoc.resource.type;
-                this.configLoader.getProjectConfiguration().then(projectConfiguration => {
-                    this.activeTypeLabel = projectConfiguration.getLabelForType(this.activeType); 
-                });
-                this.resourcesComponent.setSelected(<IdaiFieldDocument>document);
-            });
-        } else {
-            this.activeDoc = null;
-            this.resourcesComponent.setSelected(null);
-        }   
-    }
-
     ngOnInit(): void {
-
         this.resourcesComponent.getDocuments().subscribe(result => {
            this.docs = result as IdaiFieldDocument[];
         });
@@ -113,7 +68,6 @@ export class MapWrapperComponent implements OnInit, OnDestroy {
     }
 
     private removeEmptyDocument() {
-        
         var selectedDocument = this.resourcesComponent.getSelected();
         if (selectedDocument && !selectedDocument.resource.id && !selectedDocument.resource.geometry) {
             this.resourcesComponent.remove(selectedDocument);
@@ -121,9 +75,7 @@ export class MapWrapperComponent implements OnInit, OnDestroy {
     }
 
     private save() {
-
         this.persistenceManager.setOldVersions([this.resourcesComponent.getSelected()]);
-
         this.persistenceManager.persist(this.resourcesComponent.getSelected(), this.settingsService.getUsername()).then(
             () => {
                 this.resourcesComponent.getSelected()['synced'] = 0;

8a50f98667fbf9f0180e74e3fc6c886422e2148e
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-06-14 19:24:58 +0200

    remove unused ListWrapperComponent

--- app/resources/resources.module.ts
+++ app/resources/resources.module.ts
@@ -6,7 +6,6 @@ import {GeometryViewComponent} from './geometry-view.component';
 import {EditableMapComponent} from './map/editable-map.component';
 import {MapWrapperComponent} from './map-wrapper.component';
 import {ListComponent} from './list/list.component';
-import {ListWrapperComponent} from './list-wrapper.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {PlusButtonComponent} from './plus-button.component';
 import {LayerMapState} from './map/layer-map-state';
@@ -29,7 +28,6 @@ import {IdaiWidgetsModule} from 'idai-components-2/widgets'
         EditableMapComponent,
         MapWrapperComponent,
         ListComponent,
-        ListWrapperComponent,
         PlusButtonComponent
     ],
     providers: [

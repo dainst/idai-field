ed558f7219889a13f711fa3e9bb3fa7a8c4295f5
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-06-14 18:12:49 +0200

    only save resources in list component if identifier/short description were altered

--- app/resources/list/list.component.ts
+++ app/resources/list/list.component.ts
@@ -3,6 +3,7 @@ import {IdaiFieldDocument} from 'idai-components-2/idai-field-model';
 import {ConfigLoader, IdaiType, ProjectConfiguration} from 'idai-components-2/configuration';
 import {PersistenceManager} from 'idai-components-2/persist';
 import {Messages} from 'idai-components-2/messages';
+import {DocumentEditChangeMonitor} from 'idai-components-2/documents';
 import {M} from '../../m';
 import {SettingsService} from '../../settings/settings-service';
 import {EditModalComponent} from '../../widgets/edit-modal.component';
@@ -14,6 +15,10 @@ import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
     templateUrl: './list.html'
 })
 
+/**
+ * @author Fabian Z.
+ * @author Thomas Kleinke
+ */
 export class ListComponent {
 
     @Input() documents: IdaiFieldDocument[];
@@ -30,6 +35,7 @@ export class ListComponent {
         private persistenceManager: PersistenceManager,
         private settingsService: SettingsService,
         private modalService: NgbModal,
+        private documentEditChangeMonitor: DocumentEditChangeMonitor,
         configLoader: ConfigLoader
     ) {
 
@@ -67,13 +73,17 @@ export class ListComponent {
         }
     }
 
-    public save(document: IdaiFieldDocument){
+    public save(document: IdaiFieldDocument) {
+
+        if (!this.documentEditChangeMonitor.isChanged()) return;
 
         const oldVersion = JSON.parse(JSON.stringify(document));
 
         this.persistenceManager.persist(document, this.settingsService.getUsername(), [oldVersion]).then(
-            doc => this.messages.add([M.WIDGETS_SAVE_SUCCESS]),
-            msgWithParams => this.messages.add(msgWithParams)
+            () => {
+                this.documentEditChangeMonitor.reset();
+                this.messages.add([M.WIDGETS_SAVE_SUCCESS]);
+            }, msgWithParams => this.messages.add(msgWithParams)
         );
     }
 
@@ -105,5 +115,10 @@ export class ListComponent {
                 this.messages.add([M.WIDGETS_SAVE_SUCCESS]);
             }, msgWithParams => this.messages.add(msgWithParams)
         );
+    }
+
+    public markAsChanged() {
+
+        this.documentEditChangeMonitor.setChanged();
     }
 }

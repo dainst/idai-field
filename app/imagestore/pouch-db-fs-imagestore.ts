93ea9988e2fce6a288516ba01a3294c1e34d9c6a
Sebastian Cuy <sebastian.cuy@dainst.de>
2017-06-21 11:38:05 +0200

    add imagestore that stores thumbs in pouchdb

---
+++ app/imagestore/pouch-db-fs-imagestore.ts
@@ -0,0 +1,162 @@
+import * as fs from 'fs';
+import {BlobMaker} from "./blob-maker";
+import {Converter} from "./converter";
+import {M} from "../m";
+import {Imagestore} from "./imagestore";
+import * as PouchDB from "pouchdb";
+
+/**
+ * A hybrid image store that uses the file system to store the original images
+ * but keeps thumbnails as PouchDB attachments in order to be able to sync them.
+ */
+export class PouchDbFsImagestore implements Imagestore {
+
+    private projectName = 'test';
+    private projectPath = undefined;
+    private db = undefined;
+
+    constructor(
+        private converter: Converter,
+        private blobMaker: BlobMaker,
+        private basePath: string) {
+
+        if (this.basePath.substr(-1) != '/') this.basePath += '/';
+        if (!fs.existsSync(this.basePath)) fs.mkdirSync(this.basePath);
+        console.log("constructor");
+    }
+
+    public select(projectName: string): void {
+
+        this.projectName = projectName;
+        this.projectPath = this.basePath + projectName + '/';
+
+        if (!fs.existsSync(this.projectPath)) fs.mkdirSync(this.projectPath);
+
+        if (projectName == 'test') this.loadSampleData();
+
+        this.db = new PouchDB(projectName);
+    }
+
+    /**
+     * @param key the identifier for the data
+     * @param data the binary data to be stored
+     * @returns {Promise<any>} resolve -> (),
+     *   reject -> the error message
+     */
+    public create(key: string, data: ArrayBuffer): Promise<any> {
+        return this.write(key, data);
+    }
+
+    private write(key, data, update = false): Promise<any> {
+        let flag = update ? 'w' : 'wx';
+        return new Promise((resolve, reject) => {
+            fs.writeFile(this.projectPath + key, Buffer.from(data), {flag: flag}, (err) => {
+                if (err) reject([M.IMAGES_ERROR_MEDIASTORE_WRITE, key]);
+                else {
+                    let blob = this.converter.convert(data);
+                    let getRev = Promise.resolve(null);
+                    if (update) getRev = this.db.get(key).then(result => result._rev);
+                    getRev.then(rev => this.db.putAttachment(key, "thumb", rev, Buffer.from(blob), "image/jpeg")
+                        .then(() => resolve())
+                        .catch(err => {
+                            console.error(err);
+                            reject([M.IMAGES_ERROR_MEDIASTORE_WRITE, key])
+                        });
+                }
+            });
+        });
+    }
+
+    /**
+     * Loads an image from the mediastore and generates a blob. Returns an url through which it is accessible.
+     * @param key must be an identifier of an existing file in the mediastore.
+     * @param sanitizeAfter
+     * @param boolean image will be loaded as thumb, default: true
+     * @return {Promise<string>} Promise that returns the blob url.
+     *  In case of error the promise gets rejected with msgWithParams.
+     */
+    public read(key:string, sanitizeAfter:boolean = false, thumb:boolean = true): Promise<string> {
+
+        let readFun = this.readOriginal.bind(this);
+        if (thumb) readFun = this.readThumb.bind(this);
+        return readFun(key).then(data => {
+            if (data == undefined) {
+                console.error("data read was undefined for", key, "thumbnails was", thumb);
+                return Promise.reject([M.IMAGESTORE_ERROR_MEDIASTORE_READ, key]);
+            }
+            return this.blobMaker.makeBlob(data,sanitizeAfter);
+        }).catch(err => {
+            console.error(err);
+            return Promise.reject([M.IMAGESTORE_ERROR_MEDIASTORE_READ, key]);
+        });
+    }
+
+    private readOriginal(key: string): Promise<ArrayBuffer> {
+        let path = this.projectPath + key;
+        return new Promise((resolve, reject) => {
+            fs.readFile(path, (err, data) => {
+                if (err) reject(err);
+                else resolve(data);
+            });
+        });
+    }
+
+    private readThumb(key: string): Promise<ArrayBuffer> {
+        return this.db.getAttachment(key, "thumb");
+    }
+
+    /**
+     * @param key the identifier for the data
+     * @param data the binary data to be stored
+     * @returns {Promise<any>} resolve -> (),
+     *   reject -> the error message
+     */
+    public update(key: string, data: ArrayBuffer): Promise<any> {
+        return this.write(key, data, true);
+    }
+
+    /**
+     * @param key the identifier for the data to be removed
+     * @returns {Promise<any>} resolve -> (),
+     *   reject -> the error message
+     */
+    public remove(key: string): Promise<any> {
+
+        return new Promise((resolve, reject) => {
+            fs.unlink(this.projectPath + key, (err) => {
+                if (err) reject(err);
+                else {
+                    this.db.get(key)
+                        .then(result => result._rev)
+                        .then(rev => this.db.removeAttachment(key, "thumb", rev))
+                        .then(() => resolve())
+                        .catch(err => {
+                            console.error(err);
+                            return reject([M.IMAGESTORE_ERROR_DELETE, key])
+                        });
+                }
+            })
+        });
+    }
+
+    private loadSampleData(): void {
+
+        const base = "/test/test-data/imagestore-samples/";
+
+        let path = process.cwd() + base;
+        if (!fs.existsSync(path)) path = process.resourcesPath + base;
+        this.copyFilesOfDir(path, this.projectPath);
+
+    }
+
+    private copyFilesOfDir (path, dest):void {
+        fs.readdir(path, (err, files) => {
+            files.forEach(file => {
+                if(!fs.statSync(path + file).isDirectory()) {
+                    fs.createReadStream(path + file).pipe(fs.createWriteStream(dest + file));
+                }
+            });
+            console.debug("Successfully put samples from " + path + " to " + dest );
+        });
+    }
+}
---
+++ test/node/pouch-db-fs-imagestore.spec.ts
@@ -0,0 +1,137 @@
+import {M} from "../../app/m";
+import * as PouchDB from "pouchdb";
+/**
+ * @author Sebastian Cuy
+ */
+
+// override nodes require function in order to make special
+// systemjs requires starting with '@node' work
+const Module = require('module');
+const originalRequire = Module.prototype.require;
+Module.prototype.require = function() {
+    if (arguments[0].startsWith('@node')) arguments[0] = arguments[0].substring(6);
+    return originalRequire.apply(this, arguments);
+};
+
+import {PouchDbFsImagestore} from "../../app/imagestore/pouch-db-fs-imagestore";
+
+import fs = require('fs');
+import rimraf = require('rimraf');
+
+// helper functions for converting strings to ArrayBuffers and vice versa
+function str2ab(str: string): ArrayBuffer {
+    const buf = new ArrayBuffer(str.length); // 2 bytes for each char
+    const bufView = new Uint8Array(buf);
+    for (let i=0, strLen=str.length; i<strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
+
+function ab2str(buf: ArrayBuffer): string {
+    return String.fromCharCode.apply(null, new Uint8Array(buf));
+}
+
+describe('FileSystemImagestore', () => {
+
+
+    let store: PouchDbFsImagestore;
+    const storeProjectPath = 'store/unittest/';
+
+    beforeEach(() => {
+        const mockBlobMaker = jasmine.createSpyObj('blobProxy',['makeBlob']);
+        mockBlobMaker.makeBlob.and.callFake((data)=>{return data});
+        const mockConverter = jasmine.createSpyObj('converter',['convert']);
+        mockConverter.convert.and.callFake((data)=>{return data});
+
+        store = new PouchDbFsImagestore(mockConverter,mockBlobMaker,'store/');
+        store.select('unittest');
+    });
+
+    afterEach(done => {
+        rimraf(storeProjectPath, () => {
+            new PouchDB('unittest').destroy().then(() => done());
+        });
+
+    });
+
+    it('should create a file', (done) => {
+
+        console.log("should create a file");
+
+        store.create('test_create', str2ab('asdf')).then(() => {
+            fs.readFile(storeProjectPath + 'test_create', (err, data) => {
+                if (err) fail(err);
+                expect(data.toString()).toEqual('asdf');
+                done();
+            });
+        }).catch(err => {
+            fail(err);
+            done();
+        });
+    });
+
+    it('should read a file', (done) => {
+
+        store.create('test_read', str2ab('qwer'))
+            .then(() => { return store.read('test_read',false,false); })
+            .then((data) => {
+                expect(data.toString()).toEqual('qwer');
+                done();
+            })
+            .catch(err => {
+                fail(err);
+                done();
+            });
+    });
+
+    it('should update a file', (done) => {
+
+        store.create('test_update', str2ab('yxcv'))
+            .then(() => { return store.update('test_update', str2ab('yxcvb')); })
+            .then(() => {
+                fs.readFile(storeProjectPath + 'test_update', (err, data) => {
+                    expect(err).toBe(null);
+                    expect(data.toString()).toEqual('yxcvb');
+                    done();
+                });
+            })
+            .catch(err => {
+                fail(err);
+                done();
+            });
+    });
+
+    it('should remove a file', (done) => {
+
+        store.create('test_remove', str2ab('sdfg'))
+            .then(() => {
+                return store.remove('test_remove')
+                    .then(() => {
+                        store.read('test_remove',false,false)
+                            .then(() => {
+                                fail('reading removed file worked unexpectedly');
+                                done();
+                            })
+                            .catch(err => {
+                                expect(err[0]).toEqual(M.IMAGESTORE_ERROR_MEDIASTORE_READ);
+
+                                fs.readFile(storeProjectPath + 'test_remove', (err) => {
+                                    expect(err).toBeTruthy();
+                                    expect(err.code).toEqual('ENOENT');
+                                    done();
+                                });
+                            });
+                    })
+                    .catch(err => {
+                        fail(err);
+                        done();
+                    })
+            })
+            .catch(err => {
+                fail(err);
+                done();
+            });
+    });
+
+});

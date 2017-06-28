ba29429560a450f6d60eee80dc10ed7db30668b4
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-06-28 17:32:33 +0200

    allow different views for resources component; set main type for resources component from view

--- app/navbar.component.ts
+++ app/navbar.component.ts
@@ -1,5 +1,6 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from '@angular/core';
 import {Messages} from 'idai-components-2/messages';
+import {ConfigLoader, ViewDefinition} from 'idai-components-2/configuration';
 
 @Component({
     moduleId: module.id,
@@ -11,8 +12,21 @@ import {Messages} from 'idai-components-2/messages';
  * @author Thomas Kleinke
  * @author Daniel de Oliveira
  */
-export class NavbarComponent {
-    constructor(private messages: Messages) {}
+export class NavbarComponent implements OnInit {
+
+    public views: Array<ViewDefinition> = [];
+
+    constructor(private messages: Messages,
+                private configLoader: ConfigLoader) {}
+
+    public ngOnInit() {
+
+        this.configLoader.getProjectConfiguration().then(
+            projectConfiguration => this.views = projectConfiguration.getViewsList()
+        ).catch(msgWithParams => {
+            this.messages.add(msgWithParams);
+        });
+    }
 
     public setMessagesHidden(shown) {
         this.messages.setHidden(!shown);

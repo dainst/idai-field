f874129098253b72bbc73055b1b2c052cf824a48
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-07-02 15:42:07 +0200

    start on project page

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -5,9 +5,7 @@ import {ExportComponent} from'./export/export.component';
 import {SettingsComponent} from './settings/settings.component';
 
 const routes: Routes = [
-    // TODO This should not be hardcoded
-    { path: '', redirectTo: 'resources/excavation', pathMatch: 'full' },
-
+    { path: '', redirectTo: 'resources/project', pathMatch: 'full' },
     { path: 'resources/:view', component: ResourcesComponent },
     { path: 'resources/:view/edit/:tab/:id', component: ResourcesComponent },
     { path: 'import', component: ImportComponent },

030f9cb491d11762bf984be90e3b01229036ac20
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-07-04 10:51:05 +0200

    work with mockdb instead of pouchdb

--- test/browser/datastore/cached-datastore.spec.ts
+++ test/browser/datastore/cached-datastore.spec.ts
@@ -1,36 +1,14 @@
-import {PouchdbDatastore} from "../../../app/datastore/pouchdb-datastore";
 import {Document} from "idai-components-2/core";
 import {CachedDatastore} from "../../../app/datastore/cached-datastore";
-import {PouchdbManager} from "../../../app/datastore/pouchdb-manager";
 
 /**
  * @author Daniel de Oliveira
  */
 export function main() {
 
-    describe('CachedDatastore', () => {
+    fdescribe('CachedDatastore', () => {
 
         let datastore: CachedDatastore;
-        let pouchdb: PouchdbDatastore;
-        let pouchdbManager: PouchdbManager;
-
-        let mockProjectConfiguration = jasmine.createSpyObj(
-            'mockProjectConfiguration',
-            ['getParentTypes']
-        );
-        mockProjectConfiguration.getParentTypes.and.callFake(type => {
-            if (type == 'root') return [];
-            if (type == 'type1') return ['root'];
-            if (type == 'type1.1') return ['type1','root'];
-            if (type == 'type2') return ['root'];
-        });
-
-        let mockConfigLoader = jasmine.createSpyObj(
-            'mockConfigLoader',
-            [ 'getProjectConfiguration' ]
-        );
-        mockConfigLoader.getProjectConfiguration
-            .and.callFake(() => Promise.resolve(mockProjectConfiguration));
 
         function doc(sd, identifier?): Document {
             return {
@@ -46,26 +24,30 @@ export function main() {
 
         beforeEach(
             function () {
-                spyOn(console, 'debug'); // to suppress console.debug output
-                spyOn(console, 'error'); // to suppress console.error output
-                pouchdbManager = new PouchdbManager(mockConfigLoader);
-                pouchdb = new PouchdbDatastore(mockConfigLoader, pouchdbManager);
-                pouchdbManager.select('testdb');
-                datastore = new CachedDatastore(pouchdb);
+                const mockdb = jasmine.createSpyObj('mockdb', ['find','documentChangesNotifications','create']);
+                mockdb.find.and.callFake(function() {
+                    const d = doc('sd1');
+                    d.resource.id = '1';
+                    return Promise.resolve([d]);
+                });
+                mockdb.create.and.callFake(function(dd) {
+                    dd.resource.id = '1';
+                    return Promise.resolve(dd); // TODO this actually isn't as it should be. The cached instance should be the one given as a param to CachedDatastore.create, not the one returned by the inner datastore.
+                });
+                mockdb.documentChangesNotifications.and.callFake(function() {return {subscribe: function(){}}});
+
+                datastore = new CachedDatastore(mockdb);
             }
         );
 
-       afterEach(function(done) {
-           pouchdbManager.destroy().then(() => done());
-        });
-
        it('should return the cached instance on calling find', function(done) {
 
-            let doc1 = doc('sd1');
+            let doc1 = doc('sd1','identifier1');
 
             datastore.create(doc1)
-                .then(() => datastore.find({q: 'sd1'}))
+                .then(() => datastore.find({q: 'sd1'})) // mockdb returns other instance
                 .then(result => {
+                    expect((result[0] as Document).resource['identifier']).toBe('identifier1');
                     doc1.resource['shortDescription'] = 's4';
                     expect((result[0] as Document).resource['shortDescription']).toBe('s4');
                     done();

3ed8da8477c78d1aa8a5e23b729b4fc72d2dd0f7
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-07-04 17:59:35 +0200

    make images grid page typescript

--- test/e2e/images/images-grid.page.ts
+++ test/e2e/images/images-grid.page.ts
@@ -1,75 +1,74 @@
 import {browser,protractor,element,by} from 'protractor';
 
 'use strict';
-var EC = protractor.ExpectedConditions;
-var delays = require('../config/delays');
+let EC = protractor.ExpectedConditions;
+let delays = require('../config/delays');
 
-var common = require("../common.js");
+let common = require("../common.js");
 
-var ImagesGridPage = function() {
+export class ImagesGridPage {
 
-    this.selectedClass = 'selected';
-    this.getAllCells = function() {
+    public static selectedClass = 'selected';
+
+    public static getAllCells = function() {
         return element.all(by.css('.cell'));
     };
-    this.getCell = function (index) {
-        return this.getAllCells().get(index);
+    public static getCell = function (index) {
+        return ImagesGridPage.getAllCells().get(index);
     };
-    this.clickCell = function (index) {
-        return this.getCell(index).click();
+    public static clickCell = function (index) {
+        return ImagesGridPage.getCell(index).click();
     };
-    this.doubleClickCell = function (index) {
-        return browser.actions().doubleClick(this.getCell(index)).perform();
+    public static doubleClickCell = function (index) {
+        return browser.actions().doubleClick(ImagesGridPage.getCell(index)).perform();
     };
-    this.getCellImageName = function(index) {
-        return this.getCell(index).element(by.css('.tag.tag-default')).getText();
+    public static getCellImageName = function(index) {
+        return ImagesGridPage.getCell(index).element(by.css('.tag.tag-default')).getText();
     };
-    this.getCellFilenameElement = function(filename) {
+    public static getCellFilenameElement = function(filename) {
         return element(by.xpath('//span[@class="tag tag-default"][text()="' + filename + '"]'));
     };
-    this.clickDeselectButton = function () {
+    public static clickDeselectButton = function () {
         return element(by.id('deselect-images')).click();
     };
-    this.clickDeleteButton = function () {
+    public static clickDeleteButton = function () {
         return element(by.id('delete-images')).click();
     };
-    this.clickCreateRelationsButton = function () {
+    public static clickCreateRelationsButton = function () {
         return element(by.id('create-relations-btn')).click();
     };
-    this.clickConfirmDeleteButton = function () {
+    public static clickConfirmDeleteButton = function () {
         return element(by.id('delete-images-confirm')).click();
     };
-    this.clickCancelDeleteButton = function () {
+    public static clickCancelDeleteButton = function () {
         return element(by.id('delete-images-cancel')).click();
     };
-    this.getDeleteConfirmationModal = function() {
+    public static getDeleteConfirmationModal = function() {
         return element(by.css('.modal-dialog'));
     };
-    this.getLinkModal = function() {
+    public static getLinkModal = function() {
         return element(by.id('link-modal'));
     };
-    this.typeInIdentifierInLinkModal = function(identifier) {
-        return common.typeIn(this.getLinkModal().element(by.id('object-search')), identifier);
+    public static typeInIdentifierInLinkModal = function(identifier) {
+        return common.typeIn(ImagesGridPage.getLinkModal().element(by.id('object-search')), identifier);
     };
-    this.getSuggestedResourcesInLinkModalByIdentifier = function (identifier) {
-        return this.getLinkModal().element(by.id('resource-'+identifier))
+    public static getSuggestedResourcesInLinkModalByIdentifier = function (identifier) {
+        return ImagesGridPage.getLinkModal().element(by.id('resource-'+identifier))
     };
-    this.uploadImage = function (filePath) {
+    public static uploadImage = function (filePath) {
         return element(by.id('file')).sendKeys(filePath);
     };
-    this.clickUploadArea = function() {
+    public static clickUploadArea = function() {
         return browser.actions()
             .mouseMove(element(by.css(".droparea")), {x: 10, y: 10})
             .click()
             .perform();
     };
-    this.get = function() {
+    public static get = function() {
         browser.get('#/images');
         browser.wait(EC.presenceOf(element(by.css('.cell'))), delays.ECWaitTime, 'Waiting for image cells.')
     };
-    this.chooseImageSubtype = function (index) {
+    public static chooseImageSubtype = function (index) {
         return element(by.id('choose-image-subtype-option-' + index)).click();
     };
-};
-
-module.exports = new ImagesGridPage();
+}
--- test/e2e/images/images-grid.spec.ts
+++ test/e2e/images/images-grid.spec.ts
@@ -1,106 +1,105 @@
 import {browser, protractor, element, by} from 'protractor';
 
-var path = require('path');
+let path = require('path');
 
-var EC = protractor.ExpectedConditions;
-var delays = require('../config/delays');
+let EC = protractor.ExpectedConditions;
+let delays = require('../config/delays');
 
-var gridPage = require('./images-grid.page');
-var viewPage = require('./images-view.page');
-
+let viewPage = require('./images-view.page');
+import {ImagesGridPage} from './images-grid.page';
 
 describe('image grid --', function() {
 
     beforeEach(function () {
-        gridPage.get();
+        ImagesGridPage.get();
         browser.wait(EC.visibilityOf(element(by.id('idai-field-brand'))), delays.ECWaitTime);
     });
 
     it('deselect cells', function() {
-        gridPage.getAllCells().then(function(cells) {
-            var first = 0;
-            var last = cells.length - 1;
+        ImagesGridPage.getAllCells().then(function(cells) {
+            let first = 0;
+            let last = cells.length - 1;
 
             cells[first].click();
-            expect(cells[first].getAttribute('class')).toMatch(gridPage.selectedClass);
+            expect(cells[first].getAttribute('class')).toMatch(ImagesGridPage.selectedClass);
             cells[first].click();
-            expect(cells[first].getAttribute('class')).not.toMatch(gridPage.selectedClass);
+            expect(cells[first].getAttribute('class')).not.toMatch(ImagesGridPage.selectedClass);
             if (last != first) {
                 cells[last].click();
-                expect(cells[last].getAttribute('class')).toMatch(gridPage.selectedClass);
+                expect(cells[last].getAttribute('class')).toMatch(ImagesGridPage.selectedClass);
                 cells[last].click();
-                expect(cells[last].getAttribute('class')).not.toMatch(gridPage.selectedClass);
+                expect(cells[last].getAttribute('class')).not.toMatch(ImagesGridPage.selectedClass);
 
                 if (last > 1) {
                     var middle = Math.floor(0.5 * (cells.length));
                     cells[middle].click();
-                    expect(cells[middle].getAttribute('class')).toMatch(gridPage.selectedClass);
+                    expect(cells[middle].getAttribute('class')).toMatch(ImagesGridPage.selectedClass);
                     cells[middle].click();
-                    expect(cells[middle].getAttribute('class')).not.toMatch(gridPage.selectedClass);
+                    expect(cells[middle].getAttribute('class')).not.toMatch(ImagesGridPage.selectedClass);
                 }
             }
         });
     });
 
     it('deselect images by clicking the corresponding button', function() {
-        gridPage.clickCell(0);
-        expect(gridPage.getCell(0).getAttribute('class')).toMatch(gridPage.selectedClass);
-        gridPage.clickDeselectButton();
-        expect(gridPage.getCell(0).getAttribute('class')).not.toMatch(gridPage.selectedClass);
+        ImagesGridPage.clickCell(0);
+        expect(ImagesGridPage.getCell(0).getAttribute('class')).toMatch(ImagesGridPage.selectedClass);
+        ImagesGridPage.clickDeselectButton();
+        expect(ImagesGridPage.getCell(0).getAttribute('class')).not.toMatch(ImagesGridPage.selectedClass);
     });
 
     it('delete an image in the grid view', function() {
-        gridPage.getCellImageName(0)
+        ImagesGridPage.getCellImageName(0)
             .then(function (filename) {
-                gridPage.getCell(0).click();
-                gridPage.clickDeleteButton();
-                gridPage.clickConfirmDeleteButton();
-                browser.wait(EC.stalenessOf(gridPage.getDeleteConfirmationModal()), delays.ECWaitTime);
-                browser.wait(EC.stalenessOf(gridPage.getCellFilenameElement(filename)), delays.ECWaitTime);
+                ImagesGridPage.getCell(0).click();
+                ImagesGridPage.clickDeleteButton();
+                ImagesGridPage.clickConfirmDeleteButton();
+                browser.wait(EC.stalenessOf(ImagesGridPage.getDeleteConfirmationModal()), delays.ECWaitTime);
+                browser.wait(EC.stalenessOf(ImagesGridPage.getCellFilenameElement(filename)), delays.ECWaitTime);
             });
     });
 
     it('delete two images in the grid view', function() {
-        gridPage.getCellImageName(0)
+        ImagesGridPage.getCellImageName(0)
             .then(function (image1filename) {
-                gridPage.getCellImageName(1)
+                ImagesGridPage.getCellImageName(1)
                     .then(function (image2filename) {
-                        gridPage.getCell(0).click();
-                        gridPage.getCell(1).click();
-                        gridPage.clickDeleteButton();
-                        gridPage.clickConfirmDeleteButton();
-                        browser.wait(EC.stalenessOf(gridPage.getDeleteConfirmationModal()), delays.ECWaitTime);
-                        browser.wait(EC.stalenessOf(gridPage.getCellFilenameElement(image1filename)),
+                        ImagesGridPage.getCell(0).click();
+                        ImagesGridPage.getCell(1).click();
+                        ImagesGridPage.clickDeleteButton();
+                        ImagesGridPage.clickConfirmDeleteButton();
+                        browser.wait(EC.stalenessOf(ImagesGridPage.getDeleteConfirmationModal()), delays.ECWaitTime);
+                        browser.wait(EC.stalenessOf(ImagesGridPage.getCellFilenameElement(image1filename)),
                             delays.ECWaitTime);
-                        browser.wait(EC.stalenessOf(gridPage.getCellFilenameElement(image2filename)),
+                        browser.wait(EC.stalenessOf(ImagesGridPage.getCellFilenameElement(image2filename)),
                             delays.ECWaitTime);
                     })
             });
     });
 
     it('connect a resource with an image in the grid view', function() {
-        var imageToConnect = gridPage.getCell(0);
+        var imageToConnect = ImagesGridPage.getCell(0);
         var resourceId = 'tf1';
         var resourceIdentifier = 'testf1';
 
         imageToConnect.click();
-        expect(imageToConnect.getAttribute('class')).toMatch(gridPage.selectedClass);
-        gridPage.clickCreateRelationsButton();
-        gridPage.typeInIdentifierInLinkModal(resourceIdentifier);
-        gridPage.getSuggestedResourcesInLinkModalByIdentifier(resourceIdentifier).click();
+        expect(imageToConnect.getAttribute('class')).toMatch(ImagesGridPage.selectedClass);
+        ImagesGridPage.clickCreateRelationsButton();
+        ImagesGridPage.typeInIdentifierInLinkModal(resourceIdentifier);
+        ImagesGridPage.getSuggestedResourcesInLinkModalByIdentifier(resourceIdentifier).click();
         expect(imageToConnect.all(by.id('related-resource-'+resourceId)).first().isPresent()).toBeTruthy();
     });
 
     it('cancel an image delete in the modal.', function() {
-        var elementToDelete = gridPage.getCell(0);
+        var elementToDelete = ImagesGridPage.getCell(0);
 
-        gridPage.getCellImageName(0)
+        ImagesGridPage.getCellImageName(0)
             .then(function (imageName) {
                 var xpath = '//span[@class="tag tag-default"][text()="'+ imageName + '"]';
                 elementToDelete.click();
-                gridPage.clickDeleteButton();
-                gridPage.clickCancelDeleteButton();
-                browser.wait(EC.stalenessOf(gridPage.getDeleteConfirmationModal()), delays.ECWaitTime);
+                ImagesGridPage.clickDeleteButton();
+                ImagesGridPage.clickCancelDeleteButton();
+                browser.wait(EC.stalenessOf(ImagesGridPage.getDeleteConfirmationModal()), delays.ECWaitTime);
                 browser.wait(EC.presenceOf(element(by.xpath(xpath))), delays.ECWaitTime)
             });
     });
@@ -108,14 +107,14 @@ describe('image grid --', function() {
     it('navigate from grid to view, and back to grid', function() {
         var xpath = '//h3[@class="fieldname"][text()="Dateiname"]/following-sibling::div[@class="fieldvalue"]';
 
-        gridPage.getCellImageName(0).then(function(imageName){
-            gridPage.doubleClickCell(0);
+        ImagesGridPage.getCellImageName(0).then(function(imageName){
+            ImagesGridPage.doubleClickCell(0);
             browser.wait(EC.presenceOf(viewPage.getDocumentCard()), delays.ECWaitTime);
-            expect(element(by.xpath(xpath)).getText()).toEqual(imageName);
+            element(by.xpath(xpath)).getText().then(imageName=>{expect(imageName).toEqual(imageName)});
 
             viewPage.clickBackToGridButton();
-            browser.wait(EC.presenceOf(gridPage.getCell(0)), delays.ECWaitTime);
-            expect(gridPage.getCellImageName(0)).toEqual(imageName);
+            browser.wait(EC.presenceOf(ImagesGridPage.getCell(0)), delays.ECWaitTime);
+            ImagesGridPage.getCellImageName(0).then(imageName=>{expect(imageName).toEqual(imageName)});
         });
     });
 
@@ -124,9 +123,9 @@ describe('image grid --', function() {
         var fileName = 'Aldrin_Apollo_11.jpg';
         var xpath = '//span[@class="tag tag-default"][text()="' + fileName + '"]';
 
-        gridPage.clickUploadArea();
-        gridPage.uploadImage(path.resolve(__dirname, '../../test-data/' + fileName));
-        gridPage.chooseImageSubtype(0);
+        ImagesGridPage.clickUploadArea();
+        ImagesGridPage.uploadImage(path.resolve(__dirname, '../../test-data/' + fileName));
+        ImagesGridPage.chooseImageSubtype(0);
         browser.wait(EC.presenceOf(element(by.xpath(xpath))), delays.ECWaitTime);
     });
 });

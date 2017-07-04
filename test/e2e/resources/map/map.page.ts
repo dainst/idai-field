47a421366950c0950fd9035dcabedd56880b3127
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-07-04 14:26:54 +0200

    make map page a ts class

--- test/e2e/resources/map/map.page.ts
+++ test/e2e/resources/map/map.page.ts
@@ -1,20 +1,18 @@
 import {browser,protractor,element,by} from 'protractor';
 
-var delays = require('../../config/delays');
-var EC = protractor.ExpectedConditions;
+let delays = require('../../config/delays');
+let EC = protractor.ExpectedConditions;
 
 
 
-var MapPage = function () {
+export class MapPage {
 
-    var mapElement = element(by.id("map-container"));
-
-    this.clickMap = function(x, y) {
+    public static clickMap = function(x, y) {
         return new Promise(function(resolve){
             return browser.wait(EC.visibilityOf(element(by.id('map-container'))), delays.ECWaitTime)
                 .then(function() {
                     browser.actions()
-                        .mouseMove(mapElement, {x: x, y: y})
+                        .mouseMove(element(by.id("map-container")), {x: x, y: y})
                         .click()
                         .perform()
                         .then(function () {
@@ -26,23 +24,21 @@ var MapPage = function () {
         });
     };
 
-    this.setMarker = function (x, y) {
+    public static setMarker = function (x, y) {
         return this.clickMap(x, y);
     };
 
-    this.setPolygon = function (pointArray) {
-        var _this = this;
+    public static setPolygon = function (pointArray) {
+        const _this = this;
         return function () {
-            for(var i = 0; i < pointArray.length; ++i){
+            for(let i = 0; i < pointArray.length; ++i){
                 _this.clickMap(pointArray[i][0], pointArray[i][1]);
             }
         }
     };
 
-    this.clickMapOption = function(optionName) {
+    public static clickMapOption = function(optionName) {
         browser.wait(EC.presenceOf(element(by.id('map-editor-button-'+optionName))), delays.ECWaitTime);
         return element(by.id('map-editor-button-'+optionName)).click();
     };
-};
-
-module.exports = new MapPage();
+}

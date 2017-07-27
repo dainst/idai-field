c45c602e9cae985fae3c62f3021e1d74bbe77ffb
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-07-27 16:15:16 +0100

    Revert "remove obsolete browser electron distinction"
    This reverts commit 1709ee9ea51c1838cac5b13c803c186ff0f26c4e.

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,4 +1,20 @@
 (function(global) {
-    config.map['papaparse'] = '@node/papaparse';
+
+    if(typeof process != 'object') {
+        console.log('Running in browser, disabling NodeJS functionality.');
+        config.map['fs'] = '@empty';
+        config.map['electron'] = '@empty';
+        config.map['express'] = '@empty';
+        config.map['express-pouchdb'] = '@empty';
+    } else {
+        console.log('Running as electron app, enabling NodeJS functionality.');
+        // ensure that pouchdb is loaded using node's require
+        // in order to trigger use of leveldb backend
+        //config.map['pouchdb'] = '@node/pouchdb';
+        // make sure papaparse is loaded using node's require
+        // in order to avoid errors
+        config.map['papaparse'] = '@node/papaparse';
+    }
     System.config(config);
+
 })(this);

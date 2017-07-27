ee4495005ca80fb60741543c2d95a7bb6968cfbc
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-07-27 15:24:51 +0100

    disable one and disable other test

--- test/e2e/resources/resources-project.spec.ts
+++ test/e2e/resources/resources-project.spec.ts
@@ -92,7 +92,7 @@ describe('resources/project --', function() {
         ResourcesPage.getSelectedMainTypeDocumentOption().then(value => expect(value[0]).toContain('building2'));
     });
 
-    it ('create, switch project', () => {
+    xit ('create, switch project', () => {
 
         ProjectPage.clickCreateProject();
         ProjectPage.typeInProjectName('abc');
@@ -107,14 +107,14 @@ describe('resources/project --', function() {
         NavbarPage.clickNavigateToExcavation();
         ResourcesPage.getListItemIdentifierText(0).then(text => expect(text).toEqual('context1'));
 
-        // NavbarPage.clickNavigateToProject();
-        // NavbarPage.clickSelectProject(1);
-        // NavbarPage.clickNavigateToSettings();
-        // NavbarPage.clickNavigateToProject();
-        // ResourcesPage.getListItemIdentifierText(0).then(text => expect(text).toEqual('abc_t1'));
+        NavbarPage.clickNavigateToProject();
+        NavbarPage.clickSelectProject(1);
+        NavbarPage.clickNavigateToSettings();
+        NavbarPage.clickNavigateToProject();
+        ResourcesPage.getListItemIdentifierText(0).then(text => expect(text).toEqual('abc_t1'));
     });
 
-    xit ('delete project', () => {
+    it ('delete project', () => {
 
         NavbarPage.clickNavigateToExcavation();
         ResourcesPage.getListItemIdentifierText(0).then(text => expect(text).toEqual('context1'));

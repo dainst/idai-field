1461975173839e7dea58cde85ec05ff7dd78faad
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-07-31 12:43:07 +0100

    move test

---
+++ test/e2e/flaky/resources.spec.ts
@@ -0,0 +1,34 @@
+import {browser, by, element, protractor} from "protractor";
+import {ResourcesPage} from "../resources/resources.page";
+
+let EC = protractor.ExpectedConditions;
+let delays = require('../config/delays');
+
+
+/**
+ * @author Daniel de Oliveira
+ * @author Thomas Kleinke
+ */
+describe('resources --', () => {
+
+
+    beforeEach(() => {
+        ResourcesPage.get();
+        browser.wait(EC.visibilityOf(element(by.id('create-main-type-document-button'))), delays.ECWaitTime);
+    });
+
+    it('should delete a main type resource', () => {
+        ResourcesPage.performCreateMainTypeResource('newTrench');
+        ResourcesPage.getListItemEls().then(elements => expect(elements.length).toBe(0));
+        ResourcesPage.clickEditMainTypeResource();
+        ResourcesPage.clickDeleteDocument();
+        ResourcesPage.clickDeleteInModal();
+        ResourcesPage.getSelectedMainTypeDocumentOption().then(value => expect(value[0]).toContain('trench1'));
+        ResourcesPage.getListItemEls().then(elements => expect(elements.length).toBeGreaterThan(0));
+        ResourcesPage.clickEditMainTypeResource();
+        ResourcesPage.clickDeleteDocument();
+        ResourcesPage.clickDeleteInModal();
+        ResourcesPage.getSelectedMainTypeDocumentOption().then(value => expect(value.length).toBe(0));
+        ResourcesPage.getListItemEls().then(elements => expect(elements.length).toBe(0));
+    });
+});

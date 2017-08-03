5c2152541de9698f384ff128bfae9d25e7ec3b76
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-08-03 18:44:12 +0100

    adjust apidoc

--- app/datastore/idai-field-datastore.ts
+++ app/datastore/idai-field-datastore.ts
@@ -6,20 +6,26 @@ import {SyncState} from "./sync-state";
  * The interface for datastores supporting
  * the idai-field application.
  *
- * The find method accepts the following constraints:
- *   resource.relations.isRecordedIn
- *   resource.relations.liesWithin
- *   resource.identifier
- *
- * Also, the find returns the documents in order.
- * It sorts the objects by lastModified (as per the modified array) descending.
- * If two documents have the exact same lastModified, there is no second sort criterium
- * so the order between them is unspecified.
- *
  * @author Sebastian Cuy
  * @author Daniel de Oliveira
  */
 export abstract class IdaiFieldDatastore extends Datastore {
+
+    /* find
+     *
+     * In addition to {@link Datastore#find}, {@link IdaiFieldDatastore#find}
+     * has some extra specifications:
+     *
+     * The find method accepts the following constraints:
+     *   resource.relations.isRecordedIn
+     *   resource.relations.liesWithin
+     *   resource.identifier
+     *
+     * Also, find returns the documents in order.
+     * It sorts the objects by lastModified (as per the modified array) descending.
+     * If two documents have the exact same lastModified, there is no second sort criterium
+     * so the order between them is unspecified.
+     */
 
     abstract findConflicted(): Promise<IdaiFieldDocument[]>;
 

3e54cd9c4650978d807380cee8c838e70fba424d
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-08-10 13:43:37 +0200

    replace object with type

--- test/browser/datastore/fulltext-indexer.spec.ts
+++ test/browser/datastore/fulltext-indexer.spec.ts
@@ -1,4 +1,5 @@
 import {FulltextIndexer} from "../../../app/datastore/fulltext-indexer";
+import {browser} from "protractor";
 
 /**
  * @author Daniel de Oliveira
@@ -34,70 +35,69 @@ export function main() {
         });
 
         it('match one with with different search terms', () => {
-            fi.add(doc('1', 'identifier1', 'object'));
-            expect(fi.get('identifier1', ['object']))
+            fi.add(doc('1', 'identifier1', 'type'));
+            expect(fi.get('identifier1', ['type']))
                 .toEqual([{id: '1', date: '2018-01-01'}]);
-            expect(fi.get('ide', ['object']))
+            expect(fi.get('ide', ['type']))
                 .toEqual([{id: '1', date: '2018-01-01'}]);
         });
 
         it('match two with the same search term', () => {
-            fi.add(doc('1', 'identifier1', 'object'));
-            fi.add(doc('2', 'identifier2', 'object'));
-            expect(fi.get('identifier', ['object']))
+            fi.add(doc('1', 'identifier1', 'type'));
+            fi.add(doc('2', 'identifier2', 'type'));
+            expect(fi.get('identifier', ['type']))
                 .toEqual([{id: '1', date: '2018-01-01'}, {id: '2', date: '2018-01-01'}]);
         });
 
         it('match in all types', () => {
-            fi.add(doc('1', 'identifier1', 'object'));
+            fi.add(doc('1', 'identifier1', 'type'));
             expect(fi.get('identifier', undefined))
                 .toEqual([{id: '1', date: '2018-01-01'}]);
         });
 
         it('match in multiple selected types', () => {
-            fi.add(doc('1', 'identifier1', 'object1'));
-            fi.add(doc('2', 'identifier2', 'object2'));
-            fi.add(doc('3', 'identifier3', 'object3'));
-            expect(fi.get('identifier', ['object1', 'object2']))
+            fi.add(doc('1', 'identifier1', 'type1'));
+            fi.add(doc('2', 'identifier2', 'type2'));
+            fi.add(doc('3', 'identifier3', 'type3'));
+            expect(fi.get('identifier', ['type1', 'type2']))
                 .toEqual([{id: '1', date: '2018-01-01'}, {id: '2', date: '2018-01-01'}]);
         });
 
         it('do not match search term', () => {
-            fi.add(doc('1', 'iden', 'object'));
-            expect(fi.get('identifier', ['object']))
+            fi.add(doc('1', 'iden', 'type'));
+            expect(fi.get('identifier', ['type']))
                 .toEqual([]);
         });
 
         it('do not match search in type', () => {
-            fi.add(doc('1', 'iden', 'object1'));
-            expect(fi.get('identifier', ['object2']))
+            fi.add(doc('1', 'iden', 'type1'));
+            expect(fi.get('identifier', ['type2']))
                 .toEqual([]);
         });
 
         it('no types present', () => {
-            expect(fi.get('identifier', ['object']))
+            expect(fi.get('identifier', ['type']))
                 .toEqual([]);
         });
 
         it('clear', () => {
-            fi.add(doc('1', 'identifier1', 'object'));
+            fi.add(doc('1', 'identifier1', 'type'));
             fi.clear();
-            expect(fi.get('identifier', ['object']))
+            expect(fi.get('identifier', ['type']))
                 .toEqual([]);
         });
 
         xit('rough size estimate', () => {
             console.log("start")
-
             let str;
             for (let i=0; i < 100000;i++) {
                 str = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 20);
                 // console.log("string",str)
-                fi.add(doc('1', str, 'object'));
+                fi.add(doc('1', str, 'type'));
             }
 
             fi.print();
-            console.log(fi.get(str, ['object']))
+            console.log(fi.get(str, ['type']))
         }, 20000);
 
         // TODO index more fields

fe0216a7fd2870563a357d06853604db5fa1af10
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-08-13 18:34:39 +0200

    remove unused

--- app/model/model-util.ts
+++ app/model/model-util.ts
@@ -4,9 +4,9 @@
 export class ModelUtil {
 
     public static getLastModified(object): string {
+        
         if (object.modified && object.modified.length > 0) {
             return object.modified[object.modified.length - 1].date;
         } else return object.created.date;
     }
-
 }

2c923c1d9f80c0f0ff87ee54ff12da4166a2d58a
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-08-15 22:56:26 +0200

    write complete unit test for extension

--- app/datastore/conflict-resolving-extension.ts
+++ app/datastore/conflict-resolving-extension.ts
@@ -32,8 +32,6 @@ export class ConflictResolvingExtension {
         this.promise = this.promise.then(() => {
             if (this.hasUnhandledConflicts(document)) {
                 return this.handleConflicts(document, userName);
-            } else {
-                return Promise.resolve();
             }
         });
 
@@ -46,14 +44,17 @@ export class ConflictResolvingExtension {
             let promise: Promise<any> = Promise.resolve();
 
             for (let conflictedRevision of conflictedRevisions) {
+
                 promise = promise.then(() => {
                     this.inspectedRevisionsIds.push(conflictedRevision['_rev']);
 
                     this.getPreviousRevision(conflictedRevision).then(previousRevision => {
+
                         const result = this.conflictResolver.tryToSolveConflict(
                             <any> document, <any> conflictedRevision, <any> previousRevision);
 
                         if (result['resolvedConflicts'] > 0 || result['unresolvedConflicts'] == 0) {
+
                             return this.datastore.update(document).then(() => {
                                 if (!result['unresolvedConflicts']) {
                                     return this.datastore.removeRevision(document.resource.id, conflictedRevision['_rev']);
@@ -82,22 +83,23 @@ export class ConflictResolvingExtension {
         return this.datastore.fetch(revision.resource.id, { revs_info: true })
             .then(doc => doc['_revs_info']).then(history => {
 
-            const previousRevisionNumber: number = this.getRevisionNumber(revision) - 1;
+                const previousRevisionNumber: number = this.getRevisionNumber(revision) - 1;
 
-            if (previousRevisionNumber < 1) return Promise.resolve(undefined);
+                if (previousRevisionNumber < 1) return Promise.resolve(undefined);
 
-            const prefix = previousRevisionNumber.toString() + '-';
-            let previousRevisionId: string;
+                const prefix = previousRevisionNumber.toString() + '-';
+                let previousRevisionId: string;
 
-            for (let historyElement of history) {
-                if (historyElement.rev.startsWith(prefix) && historyElement.status == 'available') {
-                    previousRevisionId = historyElement.rev;
-                    break;
+                for (let historyElement of history) {
+                    if (historyElement.rev.startsWith(prefix) && historyElement.status == 'available') {
+                        previousRevisionId = historyElement.rev;
+                        break;
+                    }
                 }
-            }
 
-            return this.datastore.fetch(revision.resource.id, { rev: previousRevisionId });
-        });
+                return this.datastore.fetch(revision.resource.id, { rev: previousRevisionId });
+            }
+        );
     }
 
     private getConflictedRevisions(document: Document, userName: string): Promise<Array<Document>> {
@@ -114,16 +116,16 @@ export class ConflictResolvingExtension {
         return Promise.all(promises)
             .catch(() => Promise.reject([M.DATASTORE_NOT_FOUND]))
             .then(revisions => {
-            let result: Array<Document> = [];
+                const result: Array<Document> = [];
 
-            for (let revision of revisions) {
-                const lastAction: Action = revision.modified && revision.modified.length > 0 ?
-                    revision.modified[revision.modified.length - 1] : revision.created;
-                if (lastAction.user == userName) result.push(revision);
-            }
+                for (let revision of revisions) {
+                    const lastAction: Action = revision.modified && revision.modified.length > 0 ?
+                        revision.modified[revision.modified.length - 1] : revision.created;
+                    if (lastAction.user == userName) result.push(revision); // TODO unit test that
+                }
 
-            return Promise.resolve(result);
-        });
+                return Promise.resolve(result);
+            });
     }
 
     private hasUnhandledConflicts(document: Document): boolean {
--- test/browser/datastore/conflict-resolving-extension.spec.ts
+++ test/browser/datastore/conflict-resolving-extension.spec.ts
@@ -1,4 +1,6 @@
+import {Document} from 'idai-components-2/core';
 import {ConflictResolvingExtension} from '../../../app/datastore/conflict-resolving-extension';
+
 /**
  * @author Daniel de Oliveira
  */
@@ -8,23 +10,70 @@ export function main() {
 
         it('match one with with different search terms', done => {
 
-            const datastore = jasmine.createSpyObj('datastore', ['fetch']);
+            const originalRevision: Document = {
+                resource: {
+                    type: 'object',
+                    id: '1',
+                    relations: {}
+                },
+                created: {user: 'testuser1', date: new Date()},
+                modified: [{user: 'testuser1', date: new Date()}]
+            };
+            originalRevision['_rev'] = '1-hij';
+
+            const conflictedRevision: Document = {
+                resource: {
+                    type: 'object',
+                    id: '1',
+                    relations: {}
+                },
+                created: {user: 'testuser1', date: new Date()},
+                modified: [{user: 'testuser1', date: new Date()}]
+            };
+            conflictedRevision['_rev'] = '2-xyz';
+
+            const latestRevision: Document = {
+                resource: {
+                    type: 'object',
+                    id: '1',
+                    relations: {}
+                },
+                created: {user: 'testuser1', date: new Date()},
+                modified: [{user: 'testuser1', date: new Date()}]
+            };
+            latestRevision['_rev'] = '2-abc';
+            latestRevision['_conflicts'] = ['2-xyz'];
+
+            const datastore = jasmine.createSpyObj('datastore', ['fetch', 'update', 'removeRevision']);
             const conflictResolver = jasmine.createSpyObj('conflictResolver', ['tryToSolveConflict']);
             const extension = new ConflictResolvingExtension();
+
+            conflictResolver.tryToSolveConflict.and.returnValue({
+                resolvedConflicts: 1,
+                unresolvedConflicts: 0
+            });
+
+            datastore.update.and.callFake(() => Promise.resolve(undefined));
+            datastore.removeRevision.and.callFake(() => Promise.resolve(undefined));
+            datastore.fetch.and.callFake((resourceId, options) => {
+                if (resourceId != '1') return Promise.reject(undefined);
+
+                if (options['revs_info']) return Promise.resolve({_revs_info:[{rev: '1-hij', status: 'available'}]});
+                if (options['rev'] == '2-xyz') return Promise.resolve(conflictedRevision);
+                if (options['rev'] == '1-hij') return Promise.resolve(originalRevision);
+                return Promise.reject(undefined);
+            });
 
             extension.setDatastore(datastore);
             extension.setConflictResolver(conflictResolver);
 
-            extension.autoResolve({
-               resource: {
-                   type: 'object',
-                   id: '1',
-                   relations: {}
-               }
-            }, 'testuser').then(() => {
+            extension.autoResolve(latestRevision, 'testuser1').then(() => {
+                expect(conflictResolver.tryToSolveConflict)
+                    .toHaveBeenCalledWith(
+                        latestRevision, conflictedRevision, originalRevision);
+                expect(datastore.update).toHaveBeenCalledWith(latestRevision);
                 done();
             })
         });
-
     });
 }

fb64bf6e345223c90d32296827a35d0a45ac0454
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-08-23 19:44:48 +0200

    move project e2e tests from flaky suite back to resources project tests

--- test/e2e/resources/resources-project.spec.ts
+++ test/e2e/resources/resources-project.spec.ts
@@ -60,6 +60,97 @@ describe('resources/project --', function() {
         if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
     }
 
+    it('create & switch project', () => {
+
+        performCreateProject();
+
+        ResourcesPage.performCreateResource('abc_t1', 0);
+        NavbarPage.clickNavigateToBuilding();
+        NavbarPage.clickNavigateToProject();
+        browser.sleep(delays.shortRest);
+
+        ResourcesPage.getListItemIdentifierText(0).then(text => expect(text).toEqual('abc_t1'));
+
+        ProjectPage.clickProjectsBadge();
+
+        ProjectPage.getProjectNameOptionText(1).then(t=>{
+            expect(t).toContain('test')
+        });
+        NavbarPage.clickSelectProject(1);
+
+        browser.sleep(delays.shortRest * 20);
+
+        NavbarPage.clickNavigateToSettings();
+        NavbarPage.clickNavigateToExcavation();
+
+        browser.sleep(delays.shortRest * 5);
+        ResourcesPage.typeInIdentifierInSearchField('con');
+        browser.sleep(delays.shortRest * 5);
+
+        ResourcesPage.getListItemIdentifierText(0).then(text => expect(text).toEqual('context1'));
+
+        ProjectPage.clickProjectsBadge();
+
+        ProjectPage.getProjectNameOptionText(1).then(t=>{
+            expect(t).toContain('abc')
+        });
+        NavbarPage.clickSelectProject(1);
+        browser.sleep(delays.shortRest * 10);
+
+        NavbarPage.clickNavigateToProject();
+        ResourcesPage.getListItemIdentifierText(0).then(text => expect(text).toEqual('abc_t1'));
+    });
+
+    it('do not create a project of an already existing name', () => {
+
+        ProjectPage.clickProjectsBadge();
+        ProjectPage.clickCreateProject();
+        ProjectPage.typeInProjectName('test');
+        ProjectPage.clickConfirmProjectOperation();
+
+        expect(NavbarPage.getMessageText()).toContain('existiert bereits');
+    });
+
+    it('delete project', () => {
+
+        performCreateProject();
+
+        ProjectPage.clickProjectsBadge();
+        ProjectPage.getProjectNameOptionText(0).then(t => { expect(t).toContain('abc') });
+        ProjectPage.getProjectNameOptionText(1).then(t => { expect(t).toContain('test') });
+
+        ProjectPage.clickDeleteProject();
+        browser.sleep(delays.shortRest);
+
+        ProjectPage.typeInProjectName('abc');
+        ProjectPage.clickConfirmProjectOperation();
+
+        browser.sleep(delays.shortRest * 10);
+
+        NavbarPage.clickNavigateToBuilding();
+        browser.sleep(delays.shortRest * 15);
+        NavbarPage.clickNavigateToExcavation();
+        browser.sleep(delays.shortRest * 5);
+        ResourcesPage.typeInIdentifierInSearchField('con');
+        browser.sleep(delays.shortRest * 5);
+
+        ResourcesPage.getListItemIdentifierText(0).then(text => expect(text).toEqual('context1'));
+
+        ProjectPage.clickProjectsBadge();
+        ProjectPage.getProjectNameOptionText(0).then(t => { expect(t).toContain('test') });
+    });
+
+    it ('do not delete last project', () => {
+
+        ProjectPage.clickProjectsBadge();
+
+        ProjectPage.clickDeleteProject();
+        ProjectPage.typeInProjectName('test');
+        ProjectPage.clickConfirmProjectOperation();
+
+        expect(NavbarPage.getMessageText()).toContain('vorhanden sein');
+    });
+
     it('basic stuff', () => {
 
         ResourcesPage.performCreateResource('trench2', 0);
@@ -78,27 +169,6 @@ describe('resources/project --', function() {
         NavbarPage.clickNavigateToProject();
         ResourcesPage.getListItemIdentifierText(0).then(text => expect(text).toEqual('trench2'));
         ResourcesPage.getListItemIdentifierText(1).then(text => expect(text).toEqual('trench1'));
-    });
-
-    it('do not create a project of an already existing name', () => {
-
-        ProjectPage.clickProjectsBadge();
-        ProjectPage.clickCreateProject();
-        ProjectPage.typeInProjectName('test');
-        ProjectPage.clickConfirmProjectOperation();
-
-        expect(NavbarPage.getMessageText()).toContain('existiert bereits');
-    });
-
-    it ('do not delete last project', () => {
-
-        ProjectPage.clickProjectsBadge();
-
-        ProjectPage.clickDeleteProject();
-        ProjectPage.typeInProjectName('test');
-        ProjectPage.clickConfirmProjectOperation();
-
-        expect(NavbarPage.getMessageText()).toContain('vorhanden sein');
     });
 
     it('switch views after click on relation link', () => {
@@ -265,5 +335,26 @@ describe('resources/project --', function() {
         ResourcesPage.clickListModeButton();
         browser.wait(EC.stalenessOf(ResourcesPage.getSelectedTypeFilterButton()), delays.ECWaitTime);
         browser.wait(EC.invisibilityOf(ResourcesPage.getListItemEl('testf1')), delays.ECWaitTime);
+    });
+
+    it('restore resources state after restarting client', () => {
+
+        performCreateProject();
+
+        ResourcesPage.performCreateResource('excavation1', 0);
+        ResourcesPage.performCreateResource('excavation2', 0);
+        ResourcesPage.clickChooseTypeFilter(1);
+
+        NavbarPage.clickNavigateToExcavation();
+        ResourcesPage.clickSelectMainTypeDocument(1);
+        ResourcesPage.clickListModeButton();
+
+        ProjectPage.get();
+        browser.wait(EC.presenceOf(MapPage.getMapContainer()), delays.ECWaitTime);
+        browser.wait(EC.presenceOf(ResourcesPage.getSelectedTypeFilterButton()), delays.ECWaitTime);
+
+        NavbarPage.clickNavigateToExcavation();
+        browser.wait(EC.stalenessOf(MapPage.getMapContainer()), delays.ECWaitTime);
+        ResourcesPage.getSelectedMainTypeDocumentOption().then(value => expect(value[0]).toContain('excavation1'));
     });
 });

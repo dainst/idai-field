ef35ea8e0729eae8ed59c3c1d77ec9184ad78f62
Sebastian Cuy <sebastian.cuy@dainst.de>
2017-08-29 17:13:31 +0200

    change user input label and add tooltip (closes #6134)

--- app/settings/settings.module.ts
+++ app/settings/settings.module.ts
@@ -4,11 +4,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {IdaiDocumentsModule} from 'idai-components-2/documents';
 import {FormsModule} from '@angular/forms';
 import {SettingsComponent} from "./settings.component";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
     imports: [
         WidgetsModule,
         BrowserModule,
+        NgbModule,
         IdaiDocumentsModule,
         FormsModule
     ],

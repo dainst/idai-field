5fcc13fbe669f070844af16a0bf31135e1520c9f
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-08-30 12:03:47 +0100

    restructure in preparation of further work

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
 'use strict';
 
 const electron = require('electron');
-const menuTemplate = require('./menu.js');
-const Menu = electron.Menu;
 const fs = require('fs');
+const menuTemplate = require('./menu.js');
 
-// Module to control application life.
-const app = electron.app;
-
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 var mainWindow;
 
-// Load configuration
+
+// Load configuration -
+
 if (process.argv.length > 2) {
-  global.configurationPath = 'config/Configuration.json';
-  global.configPath = process.argv[2];
+    global.configurationPath = 'config/Configuration.json';
+    global.configPath = process.argv[2];
 } else {
-  global.configurationPath = '../config/Configuration.json';
-  global.configPath = process.resourcesPath + '/config/config.json';
+    global.configurationPath = '../config/Configuration.json';
+    global.configPath = process.resourcesPath + '/config/config.json';
+}
+
+// Copy config file to appData if no config file exists in appData
+function copyConfigFile(srcPath, destPath) {
+
+    if (!fs.existsSync(destPath)) {
+        var config = fs.readFileSync(srcPath, 'utf-8');
+        fs.writeFileSync(destPath, config);
+    }
 }
 
 if (global.configPath.indexOf('config.test.json') == -1) {
@@ -36,19 +41,14 @@ if (global.configPath.indexOf('config.test.json') == -1) {
 global.config = JSON.parse(fs.readFileSync(global.configPath, 'utf-8'));
 console.log('Using config file: ' + global.configPath);
 
-// Copy config file to appData if no config file exists in appData
-function copyConfigFile(srcPath, destPath) {
+// -- Configuration
 
-  if (!fs.existsSync(destPath)) {
-    var config = fs.readFileSync(srcPath, 'utf-8');
-    fs.writeFileSync(destPath, config);
-  }
-}
+
+
 
 function createWindow() {
 
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
+  mainWindow = new electron.BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
@@ -59,8 +59,8 @@ function createWindow() {
 
   // mainWindow.webContents
 
-  const menu = Menu.buildFromTemplate(menuTemplate);
-  Menu.setApplicationMenu(menu);
+  const menu = electron.Menu.buildFromTemplate(menuTemplate);
+    electron.Menu.setApplicationMenu(menu);
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
@@ -79,18 +79,18 @@ function createWindow() {
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', createWindow);
+electron.app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+electron.app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit();
+      electron.app.quit();
   }
 });
 
-app.on('activate', function () {
+electron.app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {

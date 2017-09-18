40190a9f7eeca4d99f4e5e002bb030731d97803c
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-09-18 22:24:33 +0100

    extract method

--- app/resources/resources.component.ts
+++ app/resources/resources.component.ts
@@ -491,11 +491,7 @@ export class ResourcesComponent implements AfterViewChecked {
             this.populateMainTypeDocuments()
                 .then(() => {
                     this.invalidateTypeFiltersIfNecessary();
-                    if (result.document.resource.type == this.view.mainType) { // TODO extract method for whole if else
-                        this.selectMainTypeDocument(result.document);
-                    } else {
-                        this.selectDocument(result.document);
-                    }
+                    this.handleDocumentSelectionOnSaved(result.document);
                 })
             , closeReason => {
 
@@ -504,21 +500,13 @@ export class ResourcesComponent implements AfterViewChecked {
 
                 if (closeReason == 'deleted') {
                     this.selectedDocument = undefined;
-                    if (document == this.selectedMainTypeDocument) return this.handleMainTypeDocumentOnDeleted(document);
+                    if (document == this.selectedMainTypeDocument) return this.handleMainTypeDocumentOnDeleted();
                 }
             }
         ).then(() => this.populateDocumentList()); // do this in every case, since this is also the trigger for the map to get repainted with updated documents
 
         doceditRef.componentInstance.setDocument(document);
         if (activeTabName) doceditRef.componentInstance.setActiveTab(activeTabName);
-    }
-
-    private handleMainTypeDocumentOnDeleted(document: Document) {
-
-        this.resourcesState.removeActiveLayersIds(this.view.name,
-            this.selectedMainTypeDocument.resource.id);
-        this.resourcesState.setLastSelectedMainTypeDocumentId(this.view.name, undefined);
-        return this.populateMainTypeDocuments();
     }
 
     public startEditGeometry() {
@@ -572,6 +560,23 @@ export class ResourcesComponent implements AfterViewChecked {
     public setScrollTarget(doc: IdaiFieldDocument) {
 
         this.scrollTarget = doc;
+    }
+
+    private handleDocumentSelectionOnSaved(document: IdaiFieldDocument) {
+
+        if (document.resource.type == this.view.mainType) {
+            this.selectMainTypeDocument(document);
+        } else {
+            this.selectDocument(document);
+        }
+    }
+
+    private handleMainTypeDocumentOnDeleted() {
+
+        this.resourcesState.removeActiveLayersIds(this.view.name,
+            this.selectedMainTypeDocument.resource.id);
+        this.resourcesState.setLastSelectedMainTypeDocumentId(this.view.name, undefined);
+        return this.populateMainTypeDocuments();
     }
 
     private scrollToDocument(doc: IdaiFieldDocument) : boolean {

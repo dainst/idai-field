43f6eda46d93a5424118ce9b2993c831b10f9fcc
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-09-21 13:17:14 +0100

    refactor for style; add author

--- app/datastore/pouchdb-proxy.ts
+++ app/datastore/pouchdb-proxy.ts
@@ -1,48 +1,64 @@
+/**
+ * @author Sebastian Cuy
+ */
 export class PouchdbProxy {
 
-    constructor(private rdy: Promise<any>) { }
+    constructor(private rdy: Promise<any>) {
 
-    public switchDb(rdy: Promise<any>) {
+    }
+
+    public switchDb(rdy: Promise<any>) { // TODO remove unused
+
         this.rdy = rdy;
     }
 
     public ready() {
+
         return this.rdy;
     }
 
     public put(document, options?): Promise<any> {
+
         return this.rdy.then(db => db.put(document, options));
     }
 
     public remove(document, revisionId?): Promise<any> {
+
         return this.rdy.then(db => db.remove(document, revisionId));
     }
 
     public query(index, opts): Promise<any> {
+
         return this.rdy.then(db => db.query(index, opts));
     }
 
     public sync(url, opts): Promise<any> {
+
         return this.rdy.then(db => db.sync(url, opts));
     }
 
     public get(id, opts={}): Promise<any> {
+
         return this.rdy.then(db => db.get(id, opts));
     }
 
     public changes(opts): Promise<any> {
+
         return this.rdy.then(db => db.changes(opts));
     }
 
     public putAttachment(docId, attachmentId, rev, attachment, type): Promise<any> {
+
         return this.rdy.then(db => db.putAttachment(docId, attachmentId, rev, attachment, type));
     }
 
     public getAttachment(docId, attachmentId): Promise<any> {
+
         return this.rdy.then(db => db.getAttachment(docId, attachmentId));
     }
 
     public removeAttachment(docId, attachmentId, rev): Promise<any> {
+
         return this.rdy.then(db => db.removeAttachment(docId, attachmentId, rev));
     }
 

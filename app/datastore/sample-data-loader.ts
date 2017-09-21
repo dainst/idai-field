e74050e689957b57b9c64d73baf4a1f15d01efcd
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-09-21 14:44:47 +0100

    add author tags

--- app/datastore/sample-data-loader.ts
+++ app/datastore/sample-data-loader.ts
@@ -8,6 +8,11 @@ import {AppState} from '../app-state';
 
 
 @Injectable()
+/**
+ * @author Sebastian Cuy
+ * @author Thomas Kleinke
+ * @author Daniel de Oliveira
+ */
 export class SampleDataLoader implements AbstractSampleDataLoader {
 
     constructor(private converter: Converter,

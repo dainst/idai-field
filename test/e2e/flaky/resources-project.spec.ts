bdebf46f889db1b6440658d3052ab71b41dcd8b8
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-09-22 13:23:55 +0100

    uncomment more code

--- test/e2e/flaky/resources-project.spec.ts
+++ test/e2e/flaky/resources-project.spec.ts
@@ -89,25 +89,25 @@ describe('resources/project --', function() {
         NavbarPage.clickSelectProject(1);
 
         browser.sleep(delays.shortRest * 20);
-        //
-        // NavbarPage.clickNavigateToSettings();
-        // NavbarPage.clickNavigateToExcavation();
-        //
-        // browser.sleep(delays.shortRest * 5);
-        // ResourcesPage.typeInIdentifierInSearchField('con');
-        // browser.sleep(delays.shortRest * 5);
-        //
-        // ResourcesPage.getListItemIdentifierText(0).then(text => expect(text).toEqual('context1'));
-        //
-        // ProjectPage.clickProjectsBadge();
-        //
-        // ProjectPage.getProjectNameOptionText(1).then(t=>{
-        //     expect(t).toContain('abc')
-        // });
-        // NavbarPage.clickSelectProject(1);
-        // browser.sleep(delays.shortRest * 10);
-        //
-        // NavbarPage.clickNavigateToProject();
-        // ResourcesPage.getListItemIdentifierText(0).then(text => expect(text).toEqual('abc_t1'));
+
+        NavbarPage.clickNavigateToSettings();
+        NavbarPage.clickNavigateToExcavation();
+
+        browser.sleep(delays.shortRest * 5);
+        ResourcesPage.typeInIdentifierInSearchField('con');
+        browser.sleep(delays.shortRest * 5);
+
+        ResourcesPage.getListItemIdentifierText(0).then(text => expect(text).toEqual('context1'));
+
+        ProjectPage.clickProjectsBadge();
+
+        ProjectPage.getProjectNameOptionText(1).then(t=>{
+            expect(t).toContain('abc')
+        });
+        NavbarPage.clickSelectProject(1);
+        browser.sleep(delays.shortRest * 10);
+
+        NavbarPage.clickNavigateToProject();
+        ResourcesPage.getListItemIdentifierText(0).then(text => expect(text).toEqual('abc_t1'));
     });
 });

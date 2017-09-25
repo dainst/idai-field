20c83f11f3157231c28c0db780db70f7aa1639b2
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-09-25 17:03:44 +0200

    clean up native jsonl parser

--- app/import/native-jsonl-parser.ts
+++ app/import/native-jsonl-parser.ts
@@ -1,7 +1,7 @@
-import {Observable} from "rxjs/Observable";
-import {Document} from "idai-components-2/core";
-import {M} from "../m";
-import {AbstractParser} from "./abstract-parser";
+import {Observable} from 'rxjs/Observable';
+import {Document} from 'idai-components-2/core';
+import {M} from '../m';
+import {AbstractParser} from './abstract-parser';
 
 /**
  * @author Sebastian Cuy
@@ -10,22 +10,23 @@ import {AbstractParser} from "./abstract-parser";
 export class NativeJsonlParser extends AbstractParser {
 
     public parse(content: string): Observable<Document> {
+        
         this.warnings = [];
         return Observable.create(observer => {
-            NativeJsonlParser.parseContent(content,observer,NativeJsonlParser.makeDoc);
+            NativeJsonlParser.parseContent(content, observer, NativeJsonlParser.makeDoc);
             observer.complete();
         });
     }
 
-    private static makeDoc(line) {
+    private static makeDoc(line: string) {
+        
         let resource = JSON.parse(line);
         if (!resource.relations) resource.relations = {};
-        return {
-            resource: resource
-        };
+
+        return { resource: resource };
     }
 
-    private static parseContent(content,observer,makeDocFun) {
+    private static parseContent(content: string, observer: any, makeDocFun: Function) {
 
         let lines = content.split('\n');
         let len = lines.length;
@@ -33,12 +34,10 @@ export class NativeJsonlParser extends AbstractParser {
         for (let i = 0; i < len; i++) {
 
             try {
-
-                if (lines[i].length > 0) observer.next(makeDocFun(lines[i]))
-
+                if (lines[i].length > 0) observer.next(makeDocFun(lines[i]));
             } catch (e) {
-                console.error('parse content error. reason: ',e);
-                observer.error([M.IMPORT_FAILURE_INVALIDJSONL,i+1]);
+                console.error('parse content error. reason: ', e);
+                observer.error([M.IMPORT_FAILURE_INVALIDJSONL, i + 1]);
                 break;
             }
         }

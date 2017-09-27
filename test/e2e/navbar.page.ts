7bcd7e32f5a4aef9861172a2ebe9d76f5c9ebc5d
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-09-27 22:56:43 +0200

    display settings as icon

--- test/e2e/navbar.page.ts
+++ test/e2e/navbar.page.ts
@@ -8,45 +8,55 @@ export class NavbarPage {
     // click
 
     public static clickNavigateToProject() {
+
         return common.click(element.all(by.css('.nav-link')).get(0));
     }
 
     public static clickNavigateToExcavation() {
+
         return common.click(element.all(by.css('.nav-link')).get(1));
     };
 
     public static clickNavigateToBuilding() {
+
         return common.click(element.all(by.css('.nav-link')).get(2));
     };
 
     public static clickNavigateToImages() {
+
         return common.click(element.all(by.css('.nav-link')).get(4));
     };
 
     public static clickNavigateToSettings() {
-        return common.click(element.all(by.css('.nav-link')).get(7));
+
+        return common.click(element(by.id('settings-button')));
     };
 
     public static clickConflictsButton() {
+
         return common.click(element(by.id('taskbar-conflicts-button')));
     };
 
     public static clickConflictResolverLink(identifier) {
+
         return common.click(element(by.id('taskbar-conflict-' + identifier)));
     };
 
     public static clickSelectProject = function(option) {
+
         browser.wait(EC.presenceOf(element(by.id('projectSelectBox'))), delays.ECWaitTime);
         element.all(by.css('#projectSelectBox option')).get(option).click();
     };
 
     public static clickCloseMessage(index = 0) {
+
         common.click(element(by.css('#message-' + index + ' button')));
     };
 
     // await
 
     public static awaitAlert(text, matchExactly = true) {
+
         if (matchExactly) {
             browser.wait(EC.presenceOf(element(by.xpath("//span[@class='message-content' and normalize-space(text())='"+text+"']"))), delays.ECWaitTime);
         }
@@ -58,6 +68,7 @@ export class NavbarPage {
     // get text
 
     public static getMessageText() {
+
         browser.sleep(200);
         browser.ignoreSynchronization = true;
         let text =  element(by.id('message-0')).getText();
@@ -66,6 +77,7 @@ export class NavbarPage {
     };
 
     public static getActiveNavLinkLabel() {
+
         browser.wait(EC.visibilityOf(element(by.css('#navbarSupportedContent .nav-link.active'))), delays.ECWaitTime);
         return element(by.css('#navbarSupportedContent .nav-link.active')).getText();
     }

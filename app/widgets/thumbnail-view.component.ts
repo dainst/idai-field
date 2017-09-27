e3817ecdb9e596cbf7174899779a7b93febac54d
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-09-27 16:36:17 +0200

    remove unused dependency

--- app/widgets/thumbnail-view.component.ts
+++ app/widgets/thumbnail-view.component.ts
@@ -2,7 +2,6 @@ import {Component, OnChanges, Input} from '@angular/core';
 import {Router} from '@angular/router';
 import {Document} from 'idai-components-2/core';
 import {Datastore} from 'idai-components-2/datastore';
-import {Messages} from 'idai-components-2/messages';
 import {Imagestore} from '../imagestore/imagestore';
 import {BlobMaker} from '../imagestore/blob-maker';
 import {ImageContainer} from '../imagestore/image-container';
@@ -27,8 +26,7 @@ export class ThumbnailViewComponent implements OnChanges {
     constructor(
         private imagestore: Imagestore,
         private datastore: Datastore,
-        private router: Router,
-        private messages: Messages
+        private router: Router
     ) {}
 
     public selectImage(documentToJumpTo: Document) {

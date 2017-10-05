28f43d36146505df326193553f37011424ebabd9
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-10-05 13:01:41 +0200

    use btn-primary for ok buttons, btn-danger for delete & btn-secondary for abort, close etc.

--- app/imageoverview/remove-link-modal.component.ts
+++ app/imageoverview/remove-link-modal.component.ts
@@ -5,11 +5,11 @@ import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
     selector: 'remove-link-modal',
     template: `
         <div class="modal-body">
-            Bei den ausgewählten Bildern werden sämtliche Verknüpfungen zu anderen Ressourcen entfernt. Ok?
+            Bei den ausgewählten Bildern werden sämtliche Verknüpfungen zu anderen Ressourcen entfernt.
         </div>
         <div class="modal-footer">
-            <button type="button" id="remove-link-confirm" class="btn btn-secondary" (click)="activeModal.close('close')">
-                Ok
+            <button type="button" id="remove-link-confirm" class="btn btn-danger" (click)="activeModal.close('close')">
+                Verknüpfungen löschen
             </button>
             <button type="button" id="remove-link-cancel" class="btn btn-secondary" (click)="activeModal.dismiss('cancel')">
                 Abbrechen

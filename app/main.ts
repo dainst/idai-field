5cfde55c870d63b56b8cc01a7cb968df921e816a
daniel <danielmarreirosdeoliveira@googlemail.com>
2017-10-06 18:13:07 +0200

    remove unused config.d.ts

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,3 @@
-/// <reference path="../config/config.d.ts" />
-
 import {enableProdMode} from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
--- config/config.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'config/config.json!json'{
-    var json:any;
-    export = json;
-}

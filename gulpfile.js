de345af1f1a6d23f72fbdd844eda0b0db7a59142
daniel <danielmarreirosdeoliveira@googlemail.com>
2017-10-06 17:19:29 +0200

    remove unused gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
 var typescript = require('gulp-typescript');
 var fs = require('fs');
-var path = require('path');
-var pkg = require('./package.json');
-var webserver = require('gulp-webserver');
-var replace = require('gulp-replace');
 
 // compile sass and concatenate to single css file in build dir
 gulp.task('convert-sass', function () {
@@ -28,21 +23,6 @@ gulp.task('convert-sass', function () {
         .pipe(concat('app.css'))
         .pipe(gulp.dest('app'));
 });
-
-function watch() {
-    gulp.watch('app/**/*.scss', ['convert-sass']);
-}
-
-gulp.task('webserver-watch', function () {
-    gulp.src('./') // Yes, ./ is right. While developing, for convenience reasons
-    // e2e tests should run against the base dir,
-    // instead the dist dir. Only in ci the dist has to be tested.
-        .pipe(webserver({
-            port: 8081
-        }));
-    watch();
-});
-
 
 const tscConfig = require('./tsconfig.json');
 gulp.task('compile', ['convert-sass'], function () {
@@ -66,41 +46,6 @@ gulp.task('compile', ['convert-sass'], function () {
         .src('test/**/*.ts')
         .pipe(typescript(tscConfig.compilerOptions))
         .pipe(gulp.dest('test/'));
-});
-
-// builds an electron app package for different platforms
-gulp.task('package', [], function () {
-
-    packager({
-        dir: 'dist/',
-        name: pkg.name,
-        platform: ['win32'],
-        arch: 'all',
-        version: '1.6.1',
-        appBundleId: pkg.name,
-        appVersion: pkg.version,
-        'download.cache': 'cache/',
-        helperBundleId: pkg.name,
-        icon: 'dist/img/logo',
-        out: 'release/'
-    }, function (err, appPath) {
-        if (err)
-            throw err;
-
-        var folderPaths = appPath.toString().split(',');
-        for (var i in folderPaths) {
-            var fileName = folderPaths[i].substring(folderPaths[i].lastIndexOf(path.sep) + 1);
-            var output = fs.createWriteStream('release/' + fileName + '.zip');
-
-            var archive = archiver('zip');
-            archive.on('error', function (err) {
-                throw err;
-            });
-            archive.pipe(output);
-            archive.directory(folderPaths[i], fileName, {'name': fileName});
-            archive.finalize();
-        }
-    });
 });
 
 function createConfig(path) {

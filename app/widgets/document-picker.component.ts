9cef7f9ace08595c894e60564f4704ee6fa956bf
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-10-11 07:52:31 +0200

    refactor for style and simplicity

--- app/widgets/document-picker.component.ts
+++ app/widgets/document-picker.component.ts
@@ -1,14 +1,13 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Datastore, Query} from 'idai-components-2/datastore';
 import {IdaiFieldDocument} from 'idai-components-2/idai-field-model';
-import {ConfigLoader} from 'idai-components-2/configuration';
+import {ProjectConfiguration} from 'idai-components-2/configuration';
 
 @Component({
     selector: 'document-picker',
     moduleId: module.id,
     templateUrl: './document-picker.html'
 })
-
 /**
  * @author Sebastian Cuy
  * @author Thomas Kleinke
@@ -18,13 +17,14 @@ export class DocumentPickerComponent {
     @Input() relationName: string;
     @Input() relationRangeType: string;
 
-    @Output() documentSelected: EventEmitter<IdaiFieldDocument> = new EventEmitter<IdaiFieldDocument>();
+    @Output() documentSelected: EventEmitter<IdaiFieldDocument> =
+        new EventEmitter<IdaiFieldDocument>();
 
     public documents: Array<IdaiFieldDocument>;
     protected query: Query = { q: '' };
 
     constructor(private datastore: Datastore,
-                private configLoader: ConfigLoader) {
+                private projectConfiguration: ProjectConfiguration) {
 
         this.query = {};
         this.fetchDocuments();
@@ -43,43 +43,48 @@ export class DocumentPickerComponent {
         } else {
             delete this.query.types;
         }
-
         this.fetchDocuments();
     }
 
     /**
-     * Populates the document list with all documents from the datastore which match the current query.
+     * Populates the document list with all documents
+     * from the datastore which match the current query.
      */
     public fetchDocuments() {
 
-        if ((!this.query.q || this.query.q == '') && (!this.query.types || this.query.types.length == 0)) {
+        if ((!this.query.q || this.query.q == '') &&
+                (!this.query.types || this.query.types.length == 0)) {
+
             this.documents = [];
             return;
         }
 
         this.datastore.find(this.query)
-            .then(documents => this.filterDocuments(documents as Array<IdaiFieldDocument>))
-            .then(filteredDocuments => this.documents = filteredDocuments)
-            .catch(err => console.error(err));
+            .then(
+                documents => {
+                    this.documents =
+                        this.filterNotAllowedRelationDomainTypes(
+                            documents as Array<IdaiFieldDocument>)
+                },
+                err => console.error(err));
     }
 
-    private filterDocuments(documents: Array<IdaiFieldDocument>): Promise<Array<IdaiFieldDocument>> {
+    private filterNotAllowedRelationDomainTypes(
+            documents: Array<IdaiFieldDocument>): Array<IdaiFieldDocument> {
 
-        return this.configLoader.getProjectConfiguration()
-            .then(projectConfiguration => {
+        const result: Array<IdaiFieldDocument> = [];
 
-                let result: Array<IdaiFieldDocument> = [];
+        for (let document of documents) {
 
-                for (let document of documents) {
+            if (this.projectConfiguration.isAllowedRelationDomainType(
+                    document.resource.type,
+                    this.relationRangeType,
+                    this.relationName)) {
 
-                    if (projectConfiguration.isAllowedRelationDomainType(document.resource.type,
-                            this.relationRangeType, this.relationName)) {
-                        result.push(document);
-                    }
-                }
+                result.push(document);
+            }
+        }
 
-                return Promise.resolve(result);
-            }).catch(msgWithParams => Promise.reject(msgWithParams));
+        return result;
     }
-    
 }

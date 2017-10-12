1a6f3ba29b99e0ade51b0ad650afd4d9f10af716
daniel <danielmarreirosdeoliveira@googlemail.com>
2017-10-12 13:05:46 +0200

    fix another problem with packaged app startup

--- main.js
+++ main.js
@@ -22,14 +22,23 @@ var configSourcePath = undefined;
 if (process.argv && process.argv.length > 2) {
     configSourcePath = process.argv[2];
 }
+if (configSourcePath) { // is environment 'development' (npm start) or 'test' (npm run e2e)
+    global.configurationPath = 'config/Configuration.json';
+}
 
-if (!configSourcePath) { // is environment 'production' or 'development'
+
+if (!configSourcePath || // is environment 'production' (packaged app)
+    configSourcePath.indexOf('dev') !== -1) { // is environment 'development' (npm start)
 
     global.appDataPath = electron.app.getPath('appData') + '/' + electron.app.getName();
     copyConfigFile(global.appDataPath + '/config.json', global.appDataPath);
     global.configPath = global.appDataPath + '/config.json';
 
-} else { // is environment 'test'
+    if (!configSourcePath) { // is environment 'production' (packaged app)
+        global.configurationPath = '../config/Configuration.json'
+    }
+
+} else { // is environment 'test' (npm run e2e)
 
     global.configPath = configSourcePath;
     global.appDataPath = 'test/test-temp';
@@ -37,7 +46,7 @@ if (!configSourcePath) { // is environment 'production' or 'development'
 
 console.log('Using config file: ' + global.configPath);
 global.config = JSON.parse(fs.readFileSync(global.configPath, 'utf-8'));
-global.configurationPath = 'config/Configuration.json';
+
 
 // -- CONFIGURATION
 
@@ -101,17 +110,17 @@ electron.app.on('ready', function createWindow() {
 
 // Quit when all windows are closed.
 electron.app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-      electron.app.quit();
-  }
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        electron.app.quit();
+    }
 });
 
 electron.app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
 });

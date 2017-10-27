5605572f169e5d00cdbe01809afd58e0f6655273
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-10-27 08:37:00 +0200

    add delay

--- test/e2e/settings/settings.spec.ts
+++ test/e2e/settings/settings.spec.ts
@@ -58,6 +58,7 @@ describe('settings --', function() {
         NavbarPage.clickCloseMessage(1);
 
         NavbarPage.clickNavigateToImages();
+        browser.sleep(delays.shortRest * 50);
         ImageOverviewPage.clickUploadArea();
         ImageOverviewPage.uploadImage(path.resolve(__dirname, '../../test-data/Aldrin_Apollo_11.jpg'));
         NavbarPage.awaitAlert('Es können keine Dateien im Bilderverzeichnis gespeichert werden', false);

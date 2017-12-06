4f1b76c97a67e43c09f998ed3c8f19c564a0ec10
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-12-06 16:28:27 +0100

    add unit tests for subtract functionality of ResultSets

--- test/browser/util/result-sets.spec.ts
+++ test/browser/util/result-sets.spec.ts
@@ -2,6 +2,7 @@ import {ResultSets} from '../../../app/util/result-sets';
 
 /**
  * @author Daniel de Oliveira
+ * @author Thomas Kleinke
  */
 export function main() {
 
@@ -18,6 +19,7 @@ export function main() {
             expect(r.intersect(e => e.id)).toEqual([{id:'2'}]);
         });
 
+
         it('no intersection', () => {
 
             let r = new ResultSets();
@@ -28,6 +30,7 @@ export function main() {
             expect(r.intersect(e => e.id)).toEqual([]);
         });
 
+
         it('no intersection with more results', () => {
 
             let r = new ResultSets();
@@ -37,6 +40,29 @@ export function main() {
             r.add([{id:'4'},{id:'5'}]);
 
             expect(r.intersect(e => e.id)).toEqual([]);
+        });
+
+
+        it('subtract', () => {
+
+            let r = new ResultSets();
+
+            r.add([{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }]);
+            r.subtract([{ id: '3' }, { id: '4' }]);
+
+            expect(r.intersect(e => e.id)).toEqual([{ id: '1' }, { id: '2' }]);
+        });
+
+        it('subtract and add multiple', () => {
+
+            let r = new ResultSets();
+
+            r.add([{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }]);
+            r.add([{ id: '2' }, { id: '3' }, { id: '4' }, { id: '5' }]);
+            r.subtract([{ id: '3' }]);
+            r.subtract([{ id: '4' }]);
+
+            expect(r.intersect(e => e.id)).toEqual([{ id: '2' }]);
         });
     });
 }

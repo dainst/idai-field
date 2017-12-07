55934addd873e900ae608f41ff8ba079bfd23060
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-12-07 09:48:57 +0100

    Use takeOrMake

--- app/core/datastore/idai-field-type-converter.ts
+++ app/core/datastore/idai-field-type-converter.ts
@@ -4,6 +4,7 @@ import {IdaiFieldDocument} from 'idai-components-2/idai-field-model';
 import {ImageTypeUtility} from '../../common/image-type-utility';
 import {IdaiFieldImageDocument} from '../model/idai-field-image-document';
 import {Injectable} from "@angular/core";
+import {ObjectUtil} from '../../util/object-util';
 
 @Injectable()
 /**
@@ -35,13 +36,11 @@ export class IdaiFieldTypeConverter extends TypeConverter {
     public convert<T extends Document>(doc: Document): T {
 
         if (this.imageTypeUtility.isImageType(doc.resource.type)) {
-            const d = doc as IdaiFieldImageDocument;
-            if (!d.resource.identifier) d.resource.identifier = '';
-            if (!d.resource.relations.depicts) d.resource.relations.depicts = [];
+            ObjectUtil.takeOrMake(doc,'resource.identifier','');
+            ObjectUtil.takeOrMake(doc,'resource.relations.depicts', []);
         } else {
-            const d = doc as IdaiFieldDocument;
-            if (!d.resource.identifier) d.resource.identifier = '';
-            if (!d.resource.relations.isRecordedIn) d.resource.relations.isRecordedIn = [];
+            ObjectUtil.takeOrMake(doc,'resource.identifier','');
+            ObjectUtil.takeOrMake(doc,'resource.relations.isRecordedIn', []);
         }
 
         return doc as T;

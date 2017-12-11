0a47b2253f376aa1ae65a99d65beafc86be7847b
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-12-11 13:11:44 +0100

    return IdaiFieldDocumentFindResult/IdaiFieldImageDocumentFindResult in respective read datastores

--- app/core/datastore/idai-field-document-read-datastore.ts
+++ app/core/datastore/idai-field-document-read-datastore.ts
@@ -1,8 +1,19 @@
+import {Query} from 'idai-components-2/datastore';
 import {IdaiFieldDocument} from 'idai-components-2/idai-field-model';
-import {CachedReadDatastore} from './core/cached-read-datastore';
+import {CachedReadDatastore, IdaiFieldFindResult} from './core/cached-read-datastore';
+
+
+export interface IdaiFieldDocumentFindResult extends IdaiFieldFindResult<IdaiFieldDocument> {}
+
 
 /**
  * @author Daniel de Oliveira
+ * @author Thomas Kleinke
  */
-export abstract class IdaiFieldDocumentReadDatastore
-    extends CachedReadDatastore<IdaiFieldDocument> {}
+export abstract class IdaiFieldDocumentReadDatastore extends CachedReadDatastore<IdaiFieldDocument> {
+
+    public async find(query: Query): Promise<IdaiFieldDocumentFindResult> {
+
+        return super.find(query);
+    }
+}
--- app/core/datastore/idai-field-image-document-read-datastore.ts
+++ app/core/datastore/idai-field-image-document-read-datastore.ts
@@ -1,8 +1,19 @@
+import {Query} from 'idai-components-2/datastore';
 import {IdaiFieldImageDocument} from '../model/idai-field-image-document';
-import {CachedReadDatastore} from './core/cached-read-datastore';
+import {CachedReadDatastore, IdaiFieldFindResult} from './core/cached-read-datastore';
+
+
+export interface IdaiFieldImageDocumentFindResult extends IdaiFieldFindResult<IdaiFieldImageDocument> {}
+
 
 /**
  * @author Daniel de Oliveira
+ * @author Thomas Kleinke
  */
-export abstract class IdaiFieldImageDocumentReadDatastore
-    extends CachedReadDatastore<IdaiFieldImageDocument> {}
+export abstract class IdaiFieldImageDocumentReadDatastore extends CachedReadDatastore<IdaiFieldImageDocument> {
+
+    public async find(query: Query): Promise<IdaiFieldImageDocumentFindResult> {
+
+        return super.find(query);
+    }
+}

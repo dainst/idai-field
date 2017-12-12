3f85c0bd254686f98a318f6397fdf3d46f18c716
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-12-12 15:25:06 +0100

    add unit tests for method isRemoteChange of ChangeHistoryUtil

--- test/browser/core/model/change-history-util.spec.ts
+++ test/browser/core/model/change-history-util.spec.ts
@@ -7,72 +7,130 @@ export function main() {
 
     describe('ChangeHistoryUtil', () => {
 
-        const document1: any = {
-            resource: {
-                id: 'id1',
-                identifier: 'identifier1'
-            },
-            created: {
-                user: 'user1',
-                date: '2018-01-01T01:00:00.00Z'
-            },
-            modified: [
-                {
-                    user: 'user1',
-                    date: '2018-01-02T07:00:00.00Z'
-                }, {
-                    user: 'user1',
-                    date: '2018-01-02T14:00:00.00Z'
-                }
-            ]
-        };
+        let document1: any;
+        let document1ConflictedRevision1: any;
+        let document1ConflictedRevision2: any;
+        let document2: any;
+        let document3: any;
+        let document4: any;
 
-        const document2: any = {
-            resource: {
-                id: 'id2',
-                identifier: 'identifier2'
-            },
-            created: {
-                user: 'user1',
-                date: '2018-01-01T01:00:00.00Z'
-            },
-            modified: [
-                {
+
+        beforeEach(() => {
+
+            document1 = {
+                resource: {
+                    id: 'id1',
+                    identifier: 'identifier1'
+                },
+                created: {
                     user: 'user1',
-                    date: '2018-01-02T07:00:00.00Z'
-                }, {
+                    date: '2018-01-01T01:00:00.00Z'
+                },
+                modified: [
+                    {
+                        user: 'user1',
+                        date: '2018-01-02T07:00:00.00Z'
+                    }, {
+                        user: 'user1',
+                        date: '2018-01-02T14:00:00.00Z'
+                    }
+                ]
+            };
+
+            document1ConflictedRevision1 = {
+                resource: {
+                    id: 'id1',
+                    identifier: 'identifier1'
+                },
+                created: {
+                    user: 'user1',
+                    date: '2018-01-01T01:00:00.00Z'
+                },
+                modified: [
+                    {
+                        user: 'user1',
+                        date: '2018-01-02T07:00:00.00Z'
+                    }, {
+                        user: 'user1',
+                        date: '2018-01-02T14:00:00.00Z'
+                    }, {
+                        user: 'user2',
+                        date: '2018-01-03T21:00:00.00Z'
+                    }
+                ]
+            };
+
+            document1ConflictedRevision2 = {
+                resource: {
+                    id: 'id1',
+                    identifier: 'identifier1'
+                },
+                created: {
+                    user: 'user1',
+                    date: '2018-01-01T01:00:00.00Z'
+                },
+                modified: [
+                    {
+                        user: 'user1',
+                        date: '2018-01-02T07:00:00.00Z'
+                    }, {
+                        user: 'user1',
+                        date: '2018-01-02T14:00:00.00Z'
+                    }, {
+                        user: 'user1',
+                        date: '2018-01-03T14:00:00.00Z'
+                    }
+                ]
+            };
+
+            document2 = {
+                resource: {
+                    id: 'id2',
+                    identifier: 'identifier2'
+                },
+                created: {
+                    user: 'user1',
+                    date: '2018-01-01T01:00:00.00Z'
+                },
+                modified: [
+                    {
+                        user: 'user1',
+                        date: '2018-01-02T07:00:00.00Z'
+                    }, {
+                        user: 'user2',
+                        date: '2018-01-02T12:00:00.00Z'
+                    }, {
+                        user: 'user2',
+                        date: '2018-01-02T15:00:00.00Z'
+                    }
+                ]
+            };
+
+            document3 = {
+                resource: {
+                    id: 'id3',
+                    identifier: 'identifier3'
+                },
+                created: {
+                    user: 'user1',
+                    date: '2018-01-01T02:00:00.00Z'
+                },
+                modified: []
+            };
+
+            document4 = {
+                resource: {
+                    id: 'id4',
+                    identifier: 'identifier4'
+                },
+                created: {
                     user: 'user2',
-                    date: '2018-01-02T12:00:00.00Z'
-                }, {
-                    user: 'user2',
-                    date: '2018-01-02T15:00:00.00Z'
-                }
-            ]
-        };
+                    date: '2018-01-01T01:00:00.00Z'
+                },
+                modified: []
+            };
+        });
 
-        const document3: any = {
-            resource: {
-                id: 'id3',
-                identifier: 'identifier3'
-            },
-            created: {
-                user: 'user1',
-                date: '2018-01-01T02:00:00.00Z'
-            },
-            modified: []
-        };
-
-        const document4: any = {
-            resource: {
-                id: 'id4',
-                identifier: 'identifier4'
-            },
-            created: {
-                user: 'user2',
-                date: '2018-01-01T01:00:00.00Z'
-            },
-            modified: []
-        };
 
         it('merges two change histories', () => {
 
@@ -92,6 +150,7 @@ export function main() {
             expect(document1.modified[3].date).toEqual('2018-01-02T15:00:00.00Z');
         });
 
+
         it('merges two change histories of separately created documents', () => {
 
             ChangeHistoryUtil.mergeChangeHistories(document3, document4);
@@ -102,6 +161,26 @@ export function main() {
             expect(document3.modified.length).toBe(1);
             expect(document3.modified[0].user).toEqual('user1');
             expect(document3.modified[0].date).toEqual('2018-01-01T02:00:00.00Z');
+        });
+
+
+        it('detect remote change', () => {
+
+            expect(ChangeHistoryUtil.isRemoteChange(document1, [], 'user1'))
+                .toBe(false);
+            expect(ChangeHistoryUtil.isRemoteChange(document1, [], 'user2'))
+                .toBe(true);
+        });
+
+
+        it('detect remote change for conflicted document', () => {
+
+            expect(ChangeHistoryUtil.isRemoteChange(document1,
+                [document1ConflictedRevision1, document1ConflictedRevision2],
+                'user1')).toBe(true);
+            expect(ChangeHistoryUtil.isRemoteChange(document1,
+                [document1ConflictedRevision1, document1ConflictedRevision2],
+                'user2')).toBe(false);
         });
     });
 }

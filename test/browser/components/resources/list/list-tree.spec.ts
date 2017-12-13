66c6ff0652f5c8001969e2bd5f92ca5ac9478079
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-12-13 22:45:28 +0000

    Add some tests

--- test/browser/components/resources/list/list-tree.spec.ts
+++ test/browser/components/resources/list/list-tree.spec.ts
@@ -10,18 +10,58 @@ export function main() {
 
     describe('ListTree', () => {
 
+        let mockDatastore;
+
+        beforeEach(() => {
+            mockDatastore = jasmine.createSpyObj('datastore', ['get']);
+        });
+
+
         it('return two top level items', async done => {
 
             const doc1 = Static.doc('sd1', 'ident1', 'Find', 'id1');
             const doc2 = Static.doc('sd2', 'ident2', 'Find', 'id2');
             const documents = [doc1, doc2] as Array<IdaiFieldDocument>;
 
-            const mockDatastore = jasmine.createSpyObj('datastore', ['find']);
-
             const listTree = new ListTree(mockDatastore);
             const docRefTree = await listTree.buildTreeFrom(documents);
 
             expect(docRefTree.length).toBe(2);
+            done();
+        });
+
+
+        it('nest two items', async done => {
+
+            const doc1 = Static.doc('sd1', 'ident1', 'Find', 'id1');
+            const doc2 = Static.doc('sd2', 'ident2', 'Find', 'id2');
+            doc2.resource.relations['liesWithin'] = ['id1'];
+            const documents = [doc1, doc2] as Array<IdaiFieldDocument>;
+
+            const listTree = new ListTree(mockDatastore);
+            const docRefTree = await listTree.buildTreeFrom(documents);
+
+            expect(docRefTree.length).toBe(1);
+            expect(docRefTree[0].children.length).toBe(1);
+            expect(docRefTree[0].children[0].doc.resource.id).toEqual('id2');
+            done();
+        });
+
+
+        it('fetch missing parent of one', async done => {
+
+            mockDatastore.get.and.returnValue(Promise.resolve(Static.doc('sd1', 'ident1', 'Find', 'id1')));
+
+            const doc2 = Static.doc('sd2', 'ident2', 'Find', 'id2');
+            doc2.resource.relations['liesWithin'] = ['id1'];
+            const documents = [doc2] as Array<IdaiFieldDocument>;
+
+            const listTree = new ListTree(mockDatastore);
+            const docRefTree = await listTree.buildTreeFrom(documents);
+
+            expect(docRefTree.length).toBe(1);
+            expect(docRefTree[0].children.length).toBe(1);
+            expect(docRefTree[0].children[0].doc.resource.id).toEqual('id2');
             done();
         });
     })

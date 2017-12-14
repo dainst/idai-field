f4c4b2035e833a5ab468989bb7140180d1002da3
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-12-14 10:02:33 +0000

    Reenable one test

--- test/e2e/syncing/resources-syncing.spec.ts
+++ test/e2e/syncing/resources-syncing.spec.ts
@@ -308,18 +308,14 @@ describe('resources/syncing --', function() {
 
     // FLAKY
 
-    xit('detect an eventual conflict and mark the corresponding resource list item', done => {
+    it('detect an eventual conflict and mark the corresponding resource list item', async done => {
 
         const nr = '7';
 
-        return createOneDocument(nr)
-            .then(() => {
-                expect(ResourcesPage.getListItemEl('testf' + nr).getAttribute('class')).not.toContain('conflicted');
-                return createAlternateDocument(nr);
-            })
-            .then(() => {
-                expect(ResourcesPage.getListItemEl('testf' + nr).getAttribute('class')).toContain('conflicted');
-                done();
-            });
+        await createOneDocument(nr);
+        expect(ResourcesPage.getListItemEl('testf' + nr).getAttribute('class')).not.toContain('conflicted');
+        await createAlternateDocument(nr);
+        expect(ResourcesPage.getListItemEl('testf' + nr).getAttribute('class')).toContain('conflicted');
+        done();
     });
 });

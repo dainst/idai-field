b9f4ae435cdacd83d7af7e21256a0f0c55ba015f
phg <philipp.gerth@gmx.net>
2018-01-17 10:15:54 +0100

    Create new abstract class for lists

---
+++ app/components/resources/base-list.ts
@@ -0,0 +1,34 @@
+import {ResourcesComponent} from './resources.component';
+import {ViewFacade} from './view/view-facade';
+import {Loading} from '../../widgets/loading';
+import {NavigationPath} from './navigation-path';
+
+/**
+ * A base class for all lists, e.g. sidebarList and List components
+ *
+ * @author Philipp Gerth
+ */
+
+export class BaseList {
+
+    public navigationPath: NavigationPath = { elements: [] };
+
+    constructor(
+        public resourcesComponent: ResourcesComponent,
+        public viewFacade: ViewFacade,
+        public loading: Loading
+    ) {
+        this.viewFacade.pathToRootDocumentNotifications().subscribe(path => {
+            this.navigationPath = path;
+        });
+    }
+
+
+    public showPlusButton() {
+
+        return (!this.resourcesComponent.isEditingGeometry && this.resourcesComponent.ready
+            && !this.loading.showIcons && this.viewFacade.getQuery().q == ''
+            && (this.viewFacade.isInOverview() || this.viewFacade.getSelectedMainTypeDocument()));
+    }
+
+}

6f211eef7187e9b45853859f3f77f1e7c5c55021
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-02-19 16:51:50 +0000

    Remove unused function

--- app/util/list-util.ts
+++ app/util/list-util.ts
@@ -8,6 +8,3 @@ export const getAtIndex = <A>(as: Array<A>, i: number): A|undefined => getAtInde
 
 export const getAtIndexOr = <A>(as: Array<A>, i: number, defaultValue: A|undefined = undefined): A|undefined =>
     as.length < i ? defaultValue : as[i];
-
-
-export const removeAtIndex = <A>(as: Array<A>) => (i: number) => as.splice(i, 1);

d1bb2d2274fa96d1f8fb163c73b53c17692f3e17
Thomas Kleinke <thomas.kleinke@dainst.de>
2018-02-26 17:48:17 +0100

    simplify test & add checks

--- test/browser/components/resources/matrix/matrix-builder.spec.ts
+++ test/browser/components/resources/matrix/matrix-builder.spec.ts
@@ -19,22 +19,17 @@ export function main() {
 
             const feature1: IdaiFieldDocument = Static.idfDoc('Feature 1', 'feature1', 'Feature', 'f1');
             const feature2: IdaiFieldDocument = Static.idfDoc('Feature 2', 'feature2', 'Feature', 'f2');
-            const feature3: IdaiFieldDocument = Static.idfDoc('Feature 3', 'feature3', 'Feature', 'f3');
 
             feature1.resource.relations['isAfter'] = ['f2'];
-            feature2.resource.relations['isAfter'] = ['f3'];
-
             feature2.resource.relations['isBefore'] = ['f1'];
-            feature3.resource.relations['isBefore'] = ['f2'];
 
-            const matrix: Matrix = matrixBuilder.build([feature1, feature2, feature3]);
+            const matrix: Matrix = matrixBuilder.build([feature1, feature2]);
 
-            console.log(JSON.stringify(matrix));
-
-            expect(matrix.rows.length).toBe(3);
+            expect(matrix.rows.length).toBe(2);
             expect(matrix.rows[0].length).toBe(1);
             expect(matrix.rows[1].length).toBe(1);
-            expect(matrix.rows[2].length).toBe(1);
+            expect(matrix.rows[0][0]).toBe(feature1);
+            expect(matrix.rows[1][0]).toBe(feature2);
         });
 
     });

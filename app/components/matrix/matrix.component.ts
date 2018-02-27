72e473a7ff703ced78701e80447d5e2a567213cb
Thomas Kleinke <thomas.kleinke@dainst.de>
2018-02-27 20:59:16 +0100

    extract methods

--- app/components/matrix/matrix.component.ts
+++ app/components/matrix/matrix.component.ts
@@ -1,8 +1,13 @@
 import * as d3 from 'd3';
 import {Component, Input, OnChanges} from '@angular/core';
 import {IdaiFieldDocument} from 'idai-components-2/idai-field-model';
-import {MatrixBuilder} from './matrix-builder';
+import {MatrixBuilder, TreeNode} from './matrix-builder';
 import {Matrix} from './matrix';
+
+
+const COLUMN_WIDTH: number = 70;
+const ROW_HEIGHT: number = 70;
+const MARGIN: number = 14;
 
 
 @Component({
@@ -21,16 +26,11 @@ export class MatrixComponent implements OnChanges {
     ngOnChanges() {
 
         const matrix: Matrix = new MatrixBuilder().build(this.documents);
-
-        this.update(matrix);
+        MatrixComponent.update(matrix);
     }
 
 
-    private update(matrix: Matrix) {
-
-        const COLUMN_WIDTH: number = 70;
-        const ROW_HEIGHT: number = 70;
-        const MARGIN: number = 14;
+    private static update(matrix: Matrix) {
 
         const canvas = d3.select('#matrix')
             .attr('width', matrix.columnCount * (COLUMN_WIDTH + MARGIN) + MARGIN * 2)
@@ -38,7 +38,13 @@ export class MatrixComponent implements OnChanges {
 
         canvas.selectAll('g').remove();
 
-        const nodeElements = canvas.selectAll('g')
+        this.createNodeElements(matrix);
+    }
+
+
+    private static createNodeElements(matrix: Matrix) {
+
+        const nodeElements = d3.select('#matrix').selectAll('g')
             .data(matrix.nodes)
             .enter()
             .append('g')
@@ -47,6 +53,13 @@ export class MatrixComponent implements OnChanges {
                     + ',' + (node.row as number * (ROW_HEIGHT + MARGIN) + MARGIN) + ')';
             });
 
+        this.createCircles(nodeElements);
+        this.createTextLabels(nodeElements);
+    }
+
+
+    private static createCircles(nodeElements: d3.Selection<d3.BaseType, TreeNode, d3.BaseType, {}>) {
+
         nodeElements.append('circle')
             .attr('cx', COLUMN_WIDTH / 2)
             .attr('cy', ROW_HEIGHT / 2)
@@ -54,6 +67,10 @@ export class MatrixComponent implements OnChanges {
             .attr('stroke', 'black')
             .attr('stroke-width', 2)
             .attr('fill', '#748db5');
+    }
+
+
+    private static createTextLabels(nodeElements: d3.Selection<d3.BaseType, TreeNode, d3.BaseType, {}>) {
 
         nodeElements.append('text')
             .attr('x', COLUMN_WIDTH / 2)

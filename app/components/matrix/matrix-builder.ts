a48e3a88823fb6dd86dc9c2530581fb5f46bd3a0
Thomas Kleinke <thomas.kleinke@dainst.de>
2018-02-27 13:29:09 +0100

    build tree first when creating matrix in order to allow symmetric child positioning

--- app/components/matrix/matrix-builder.ts
+++ app/components/matrix/matrix-builder.ts
@@ -2,6 +2,17 @@ import {IdaiFieldDocument} from 'idai-components-2/idai-field-model';
 import {Matrix} from './matrix';
 
 
+interface TreeNode {
+
+    document: IdaiFieldDocument;
+    leftBranch: Array<TreeNode>;
+    belowChild?: TreeNode;
+    rightBranch: Array<TreeNode>;
+    row?: number;
+    column?: number;
+}
+
+
 /**
  * @author Thomas Kleinke
  * @author Daniel de Oliveira
@@ -9,19 +20,23 @@ import {Matrix} from './matrix';
 export class MatrixBuilder {
 
     private documents: Array<IdaiFieldDocument>;
-    private rows: Array<Array<IdaiFieldDocument|undefined>>;
+    private treeNodes: { [resourceId: string]: TreeNode };
+    private rows: Array<Array<IdaiFieldDocument>>;
 
 
     public build(documents: Array<IdaiFieldDocument>): Matrix {
 
         this.documents = documents;
+        this.treeNodes = {};
         this.rows = [];
 
         const rootDocument: IdaiFieldDocument|undefined = MatrixBuilder.findRootDocument(documents);
 
         if (!rootDocument) return MatrixBuilder.createEmptyMatrix();
 
-        this.addToRows(rootDocument);
+        const rootNode: TreeNode = this.buildTreeNode(rootDocument);
+
+        this.addToRows(rootNode);
 
         return {
             rows: this.rows
@@ -29,22 +44,82 @@ export class MatrixBuilder {
     }
 
 
-    private addToRows(document: IdaiFieldDocument, row: number = 0, column: number = 0) {
+    private buildTreeNode(document: IdaiFieldDocument): TreeNode {
 
-        if (!this.rows[row]) this.rows[row] = [];
-
-        this.rows[row][column] = document;
+        const treeNode: TreeNode = { document: document, leftBranch: [], rightBranch: [] };
 
         const relations: string[] = document.resource.relations['isAfter'];
 
-        if (!relations) return;
+        if (!relations) return treeNode;
+
+        const hasBelowChild: boolean = relations.length % 2 == 1;
+        const sideChildrenCount: number = Math.floor(relations.length / 2);
 
         for (let i = 0; i < relations.length; i++) {
             const isAfterDocument: IdaiFieldDocument|undefined = this.getDocument(relations[i]);
             if (!isAfterDocument) throw 'Document not found: ' + relations[i];
 
-            this.addToRows(isAfterDocument, row + 1, column + i);
+            const childNode: TreeNode = this.getTreeNode(isAfterDocument);
+
+            if (i < sideChildrenCount) {
+                treeNode.leftBranch.push(childNode);
+            } else if (i == sideChildrenCount && hasBelowChild) {
+                treeNode.belowChild = childNode;
+            } else {
+                treeNode.rightBranch.push(childNode);
+            }
         }
+
+        return treeNode;
+    }
+
+
+    private getTreeNode(document: IdaiFieldDocument): TreeNode {
+
+        const resourceId = document.resource.id as string;
+
+        if (!this.treeNodes[resourceId]) {
+            this.treeNodes[resourceId] = this.buildTreeNode(document);
+        }
+
+        return this.treeNodes[resourceId];
+    }
+
+
+    private addToRows(treeNode: TreeNode, row: number = 0, columnOffset: number = 0) {
+
+        if (!this.rows[row]) this.rows[row] = [];
+
+        const centralColumn: number = MatrixBuilder.getLeftSizeNodeWidth(treeNode) + columnOffset;
+
+        if (treeNode.row !== undefined && treeNode.column !== undefined) {
+            if (treeNode.row != row) treeNode.row = Math.max(treeNode.row, row);
+            treeNode.column = Math.floor((treeNode.column + centralColumn) / 2);
+        } else {
+            treeNode.row = row;
+            treeNode.column = centralColumn;
+        }
+
+        this.rows[treeNode.row][treeNode.column] = treeNode.document;
+
+        let currentColumn: number = columnOffset;
+
+        treeNode.leftBranch.forEach(childNode => {
+            this.addToRows(childNode, row + 1, currentColumn);
+            currentColumn += MatrixBuilder.getNodeColumnWidth(childNode);
+        });
+
+        if (treeNode.belowChild) {
+            this.addToRows(treeNode.belowChild, row + 1, centralColumn);
+            currentColumn += MatrixBuilder.getNodeColumnWidth(treeNode.belowChild);
+        } else {
+            currentColumn++;
+        }
+
+        treeNode.rightBranch.forEach(childNode => {
+            this.addToRows(childNode, row + 1, currentColumn);
+            currentColumn += MatrixBuilder.getNodeColumnWidth(childNode);
+        });
     }
 
 
@@ -59,6 +134,31 @@ export class MatrixBuilder {
         return documents.find(document => {
             return document.resource.relations['isAfter'] && !document.resource.relations['isBefore'];
         });
+    }
+
+
+    private static getLeftSizeNodeWidth(treeNode: TreeNode): number {
+
+        let leftBranchChildren: number = 0;
+
+        treeNode.leftBranch.forEach(childNode => {
+            leftBranchChildren += this.getNodeColumnWidth(childNode)
+        });
+
+        if (treeNode.belowChild) leftBranchChildren += this.getLeftSizeNodeWidth(treeNode.belowChild);
+
+        return leftBranchChildren;
+    }
+
+
+    private static getNodeColumnWidth(treeNode: TreeNode): number {
+
+        let columns: number = 1;
+
+        treeNode.leftBranch.forEach(childNode => columns += this.getNodeColumnWidth(childNode));
+        treeNode.rightBranch.forEach(childNode => columns += this.getNodeColumnWidth(childNode));
+
+        return columns;
     }
 
 

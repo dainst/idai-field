56ca6325975e1c6d3933d9ae1b7be7d3a50d3e40
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2018-03-06 16:41:09 +0000

    Pull a batch of tests into the jasmine suite

---
+++ test/node/core/importer/merge-geometries-import-strategy.spec.ts
@@ -0,0 +1,69 @@
+import {MergeGeometriesImportStrategy} from '../../../../app/core/importer/merge-geometries-import-strategy';
+import {ImportStrategy} from '../../../../app/core/importer/import-strategy';
+
+
+/**
+ * @author Daniel de Oliveira
+ * @author Thomas Kleinke
+ */
+describe('MergeGeometriesImportStrategy Tests ---', () => {
+
+    let strategy: ImportStrategy;
+    let mockValidator;
+    let mockDatastore;
+    let mockSettingsService;
+    let originalDoc;
+    let docToMerge;
+
+    beforeEach(() => {
+        originalDoc = {
+            resource: {
+                id: '1',
+                identifier: 'i1',
+                shortDescription: 'sd1'
+            }
+        };
+
+        docToMerge = {
+            resource: {
+                geometry: { a: 'b' }
+            }
+        };
+
+        mockValidator = jasmine.createSpyObj('validator', ['validate']);
+        mockValidator.validate.and.callFake(function() { return Promise.resolve(); });
+
+        mockDatastore = jasmine.createSpyObj('datastore', ['find','update']);
+        mockDatastore.find.and.callFake(
+            () => Promise.resolve({ documents: [originalDoc], totalCount: 1 }));
+        mockDatastore.update.and.callFake(() => Promise.resolve(undefined));
+
+        mockSettingsService = jasmine.createSpyObj('settingsService', ['getUsername']);
+        mockSettingsService.getUsername.and.callFake(function() { return 'testuser'; });
+
+        strategy = new MergeGeometriesImportStrategy(mockValidator, mockDatastore, mockSettingsService);
+    });
+
+    it('should merge geometry',
+        function(done) {
+            strategy.importDoc(docToMerge)
+                .then(() => {
+                    let importedDoc = mockDatastore.update.calls.mostRecent().args[0];
+                    expect(importedDoc.resource).toEqual({
+                        id: '1',
+                        identifier: 'i1',
+                        shortDescription: 'sd1',
+                        geometry: { a: 'b' }
+                    });
+                    expect(importedDoc.modified).toBeDefined();
+                    expect(importedDoc.modified.length).toBe(1);
+                    expect(importedDoc.modified[0].user).toEqual('testuser');
+                    expect(importedDoc.modified[0].date).toBeDefined();
+                    done();
+                }, err => {
+                    fail('should not fail ' + err);
+                    done();
+                })
+        }
+    )
+});
---
+++ test/node/model/validator.spec.ts
@@ -0,0 +1,275 @@
+import {ProjectConfiguration, ConfigLoader} from 'idai-components-2/configuration';
+import {Validator} from '../../../app/core/model/validator';
+import {M} from '../../../app/m';
+
+/**
+ * @author Daniel de Oliveira
+ * @author Thomas Kleinke
+ */
+describe('Validator', () => {
+
+    const projectConfiguration = new ProjectConfiguration(
+        {
+            types: [
+                {
+                    type: 'T',
+                    fields: [
+                        {
+                            name: 'id',
+                        },
+                        {
+                            name: 'type',
+                        },
+                        {
+                            name: 'optional',
+                        },
+                        {
+                            name: 'mandatory',
+                            mandatory: true
+                        },
+                        {
+                            name: 'number1',
+                            label: 'number1',
+                            inputType: 'float'
+                        },
+                        {
+                            name: 'number2',
+                            label: 'number2',
+                            inputType: 'float'
+                        }
+                    ]
+                },
+                {
+                    type: 'T2',
+                    fields: [
+                        {
+                            name: 'id',
+                        },
+                        {
+                            name: 'type',
+                        }
+                    ]
+                },
+            ],
+            relations: [
+                {
+                    name: 'isRelatedTo',
+                    domain: ['T'],
+                    range: ['T'],
+                    inverse: 'NO-INVERSE'
+                },
+                {
+                    name: 'isDepictedIn',
+                    domain: ['T'],
+                    range: ['T2'],
+                    inverse: 'NO-INVERSE'
+                }
+            ]
+        }
+    );
+
+
+    const configLoader = {
+        getProjectConfiguration: () => Promise.resolve(projectConfiguration)
+    };
+
+
+    it('should report nothing', done => {
+
+        const doc = {
+            resource: {
+                id: '1',
+                type: 'T',
+                mandatory: 'm',
+                relations: {
+                    isRelatedTo: ['2']
+                },
+            }
+        };
+        new Validator(<ConfigLoader> configLoader)
+            .validate(doc).then(() => done(), msgWithParams => fail(msgWithParams));
+    });
+
+
+    it('should report nothing when omitting optional property', done => {
+
+        const doc = {
+            resource: {
+                id: '1',
+                type: 'T',
+                mandatory: 'm',
+                relations: {},
+            }
+        };
+
+        new Validator(<ConfigLoader> configLoader)
+            .validate(doc).then(() => done(), msgWithParams => fail(msgWithParams));
+    });
+
+
+    it('should report error when omitting mandatory property', done => {
+
+        const doc = {
+            resource: {
+                id: '1',
+                type: 'T',
+                relations: {},
+            }
+        };
+
+        new Validator(<ConfigLoader> configLoader)
+            .validate(doc).then(() => fail(), msgWithParams => {
+            expect(msgWithParams).toEqual([M.VALIDATION_ERROR_MISSINGPROPERTY, 'T', 'mandatory']);
+            done();
+        });
+    });
+
+
+    it('should report error when leaving mandatory property empty', done => {
+
+        const doc = {
+            resource: {
+                id: '1',
+                type: 'T',
+                mandatory: '',
+                relations: {},
+            }
+        };
+
+        new Validator(<ConfigLoader> configLoader)
+            .validate(doc).then(() => fail(), msgWithParams => {
+                expect(msgWithParams).toEqual([M.VALIDATION_ERROR_MISSINGPROPERTY, 'T', 'mandatory']);
+                done();
+            });
+    });
+
+
+    it('should report a missing field definition', done => {
+
+        const doc = {
+            resource: {
+                id: '1',
+                type: 'T',
+                a: 'b',
+                mandatory: 'm',
+                relations: {},
+            }
+        };
+
+        new Validator(<ConfigLoader> configLoader)
+            .validate(doc).then(() => fail(), msgWithParams => {
+            expect(msgWithParams).toEqual([M.VALIDATION_ERROR_INVALIDFIELD, 'T', 'a']);
+            done();
+        });
+    });
+
+
+    it('should report missing field definitions', done => {
+
+        const doc = {
+            resource: {
+                id: '1',
+                type: 'T',
+                a: 'b',
+                b: 'a',
+                mandatory: 'm',
+                relations: {},
+            }
+        };
+
+        new Validator(<ConfigLoader> configLoader)
+            .validate(doc).then(() => fail(), msgWithParams => {
+            expect(msgWithParams).toEqual([M.VALIDATION_ERROR_INVALIDFIELDS, 'T', 'a, b']);
+            done();
+        });
+    });
+
+
+    it('should report a missing relation field definition', done => {
+
+        const doc = {
+            resource: {
+                id: '1',
+                type: 'T2',
+                relations: {
+                    isRelatedTo: ['2']
+                }
+            }
+        };
+
+        new Validator(<ConfigLoader> configLoader).validate(doc).then(
+            () => fail(),
+            msgWithParams => {
+                expect(msgWithParams).toEqual([M.VALIDATION_ERROR_INVALIDRELATIONFIELD, 'T2',
+                    'isRelatedTo']);
+                done();
+            });
+    });
+
+
+    it('should report missing relation field definitions', done => {
+
+        const doc = {
+            resource: {
+                id: '1',
+                type: 'T2',
+                relations: {
+                    isRelatedTo: ['2'],
+                    isDepictedIn: ['3']
+                }
+            }
+        };
+
+        new Validator(<ConfigLoader> configLoader).validate(doc).then(
+            () => fail(),
+            msgWithParams => {
+                expect(msgWithParams).toEqual([M.VALIDATION_ERROR_INVALIDRELATIONFIELDS, 'T2',
+                    'isRelatedTo, isDepictedIn']);
+                done();
+            });
+    });
+
+
+    it('should report invalid numeric field', done => {
+
+        const doc = {
+            resource: {
+                id: '1',
+                type: 'T',
+                mandatory: 'm',
+                number1: 'ABC',
+                relations: {}
+            }
+        };
+
+        new Validator(<ConfigLoader> configLoader).validate(doc).then(
+            () => fail(),
+            msgWithParams => {
+                expect(msgWithParams).toEqual([M.VALIDATION_ERROR_INVALID_NUMERIC_VALUE, 'T', 'number1']);
+                done();
+            });
+    });
+
+
+    it('should report invalid numeric fields', done => {
+
+        const doc = {
+            resource: {
+                id: '1',
+                type: 'T',
+                mandatory: 'm',
+                number1: 'ABC',
+                number2: 'DEF',
+                relations: {}
+            }
+        };
+
+        new Validator(<ConfigLoader> configLoader).validate(doc).then(
+            () => fail(),
+            msgWithParams => {
+                expect(msgWithParams).toEqual([M.VALIDATION_ERROR_INVALID_NUMERIC_VALUES, 'T',
+                    'number1, number2']);
+                done();
+            });
+    });
+});

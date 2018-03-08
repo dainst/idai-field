9ff7949856b97072f4e87d6c96c6b25b38366a05
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-03-08 13:55:04 +0000

    Organize imports

--- app/core/datastore/core/conflict-resolving-extension.ts
+++ app/core/datastore/core/conflict-resolving-extension.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Action, Document} from 'idai-components-2/core';
+import {Document} from 'idai-components-2/core';
 import {PouchdbDatastore} from './pouchdb-datastore';
 import {ConflictResolver} from './conflict-resolver';
 import {RevisionHelper} from './revision-helper';

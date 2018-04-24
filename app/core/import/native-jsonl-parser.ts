4300d73c415e4346a63a079efd430edad2ffe9b8
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-04-24 20:42:29 +0200

    Improve details

--- app/core/import/native-jsonl-parser.ts
+++ app/core/import/native-jsonl-parser.ts
@@ -14,23 +14,14 @@ export class NativeJsonlParser extends AbstractParser {
     public parse(content: string): Observable<Document> {
         
         this.warnings = [];
-        return Observable.create((observer: Observer<any>) => {
-            NativeJsonlParser.parseContent(content, observer, NativeJsonlParser.makeDoc);
+        return Observable.create((observer: Observer<Document>) => {
+            NativeJsonlParser.parseContent(content, observer);
             observer.complete();
         });
     }
 
 
-    private static makeDoc(line: string) {
-        
-        const resource = JSON.parse(line);
-        if (!resource.relations) resource.relations = {};
-
-        return { resource: resource };
-    }
-
-
-    private static parseContent(content: string, observer: any, makeDocFun: Function) {
+    private static parseContent(content: string, observer: Observer<Document>) {
 
         const lines = content.split('\n');
         const len = lines.length;
@@ -38,12 +29,21 @@ export class NativeJsonlParser extends AbstractParser {
         for (let i = 0; i < len; i++) {
 
             try {
-                if (lines[i].length > 0) observer.next(makeDocFun(lines[i]));
+                if (lines[i].length > 0) observer.next(NativeJsonlParser.makeDoc(lines[i]));
             } catch (e) {
                 console.error('parse content error. reason: ', e);
                 observer.error([M.IMPORT_FAILURE_INVALIDJSONL, i + 1]);
                 break;
             }
         }
+    }
+
+
+    private static makeDoc(line: string) {
+
+        const resource = JSON.parse(line);
+        if (!resource.relations) resource.relations = {};
+
+        return { resource: resource };
     }
 }

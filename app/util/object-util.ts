0c09b33f9bdedeffc4e0caeba77858399c08b859
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-04-27 20:23:25 +0100

    Fix minor issues

--- app/util/object-util.ts
+++ app/util/object-util.ts
@@ -41,14 +41,16 @@ export module ObjectUtil {
 
         return (function convertDates<O>(original: any, plain: any) {
 
-            for (let key of Object.keys(original)) {
+            if (original) {
+                for (let key of Object.keys(original)) {
 
-                if (original[key] instanceof Date) {
-                    plain[key] = new Date(original[key]);
-                } else if (typeof original[key] === 'object') {
-                    convertDates(original[key], plain[key])
+                    if (original[key] instanceof Date) {
+                        plain[key] = new Date(original[key]);
+                    } else if (typeof original[key] === 'object') {
+                        convertDates(original[key], plain[key])
+                    }
+
                 }
-
             }
             return plain;
 

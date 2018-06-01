177f393077c443d20ff758c097f69a142583bef5
tkleinke <thomas.kleinke@dainst.de>
2018-06-01 13:22:34 +0200

    un-xit syncing tests

--- test/e2e/syncing/resources-syncing.spec.ts
+++ test/e2e/syncing/resources-syncing.spec.ts
@@ -23,7 +23,7 @@ const common = require('../common');
  * @author Thomas Kleinke
  * @author Daniel de Oliveira
  */
-xdescribe('resources/syncing --', function() {
+describe('resources/syncing --', function() {
 
     const remoteSiteAddress = 'http://localhost:3001';
     const configPath = browser.params.configPath;

07fbf1481a2226ac11620f19e0554145e1671585
Thomas Kleinke <thomas.kleinke@dainst.de>
2018-06-28 20:42:06 +0200

    adjust to code style rules

--- app/core/datastore/field/idai-field-sample-objects.ts
+++ app/core/datastore/field/idai-field-sample-objects.ts
@@ -2,121 +2,121 @@ import {NewDocument} from 'idai-components-2/core';
 
 export const DOCS: NewDocument[] = [
     {
-        "resource": {
-            "id": "project",
-            "identifier": "test",
-            "shortDescription": "Testprojekt",
-            "relations": {},
-            "type": "Project"
+        'resource': {
+            'id': 'project',
+            'identifier': 'test',
+            'shortDescription': 'Testprojekt',
+            'relations': {},
+            'type': 'Project'
         }
     },
     {
-        "resource": {
-            "id": "t1",
-            "identifier": "trench1",
-            "shortDescription": "Goldener Schnitt",
-            "relations": {
-                "isRecordedIn": []
+        'resource': {
+            'id': 't1',
+            'identifier': 'trench1',
+            'shortDescription': 'Goldener Schnitt',
+            'relations': {
+                'isRecordedIn': []
             },
-            "geometry": {
-                "type": "Polygon",
-                "coordinates": [[
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [[
                     [27.18926513195038, 39.14123618602753],
                     [27.18935239315033, 39.141262888908386],
                     [27.18937313556671, 39.14121866226196],
                     [27.18929159641266, 39.14118945598602],
                     [27.18926513195038, 39.14123618602753]]]
             },
-            "type": "Trench"
+            'type': 'Trench'
         }
     },
     {
-        "resource": {
-            "id": "t2",
-            "identifier": "trench2",
-            "shortDescription": "Matrix-Schnitt",
-            "relations": {
-                "isRecordedIn": []
+        'resource': {
+            'id': 't2',
+            'identifier': 'trench2',
+            'shortDescription': 'Matrix-Schnitt',
+            'relations': {
+                'isRecordedIn': []
             },
-            "type": "Trench"
+            'type': 'Trench'
         }
     },
     {
-        "resource": {
-            "id": "si1",
-            "identifier": "SI1",
-            "shortDescription": "Ein Befund",
-            "relations": {
-                "isRecordedIn": ["t2"],
-                "isAfter": ["si2", "si5"],
+        'resource': {
+            'id': 'si1',
+            'identifier': 'SI1',
+            'shortDescription': 'Ein Befund',
+            'relations': {
+                'isRecordedIn': ['t2'],
+                'isAfter': ['si2', 'si5'],
             },
-            "type": "Feature"
+            'type': 'Feature'
         }
     },
     {
-        "resource": {
-            "id": "si2",
-            "identifier": "SI2",
-            "shortDescription": "Ein Erdbefund",
-            "relations": {
-                "isRecordedIn": ["t2"],
-                "isBefore": ["si1"],
-                "isAfter": ["si3"]
+        'resource': {
+            'id': 'si2',
+            'identifier': 'SI2',
+            'shortDescription': 'Ein Erdbefund',
+            'relations': {
+                'isRecordedIn': ['t2'],
+                'isBefore': ['si1'],
+                'isAfter': ['si3']
             },
-            "type": "Layer"
+            'type': 'Layer'
         }
     },
     {
-        "resource": {
-            "id": "si3",
-            "identifier": "SI3",
-            "shortDescription": "Architektur",
-            "relations": {
-                "isRecordedIn": ["t2"],
-                "isBefore": ["si2"],
-                "isAfter": ["si4"],
-                "isContemporaryWith": ["si5"]
+        'resource': {
+            'id': 'si3',
+            'identifier': 'SI3',
+            'shortDescription': 'Architektur',
+            'relations': {
+                'isRecordedIn': ['t2'],
+                'isBefore': ['si2'],
+                'isAfter': ['si4'],
+                'isContemporaryWith': ['si5']
             },
-            "type": "Architecture"
+            'type': 'Architecture'
         }
     },
     {
-        "resource": {
-            "id": "si4",
-            "identifier": "SI4",
-            "shortDescription": "Ein Befund",
-            "relations": {
-                "isRecordedIn": ["t2"],
-                "isBefore": ["si3"],
+        'resource': {
+            'id': 'si4',
+            'identifier': 'SI4',
+            'shortDescription': 'Ein Befund',
+            'relations': {
+                'isRecordedIn': ['t2'],
+                'isBefore': ['si3'],
             },
-            "type": "Feature"
+            'type': 'Feature'
         }
     },
     {
-        "resource": {
-            "id": "si5",
-            "identifier": "SI5",
-            "shortDescription": "Ein Befund",
-            "relations": {
-                "isRecordedIn": ["t2"],
-                "isBefore": ["si1"],
-                "isContemporaryWith": ["si3"]
+        'resource': {
+            'id': 'si5',
+            'identifier': 'SI5',
+            'shortDescription': 'Ein Befund',
+            'relations': {
+                'isRecordedIn': ['t2'],
+                'isBefore': ['si1'],
+                'isContemporaryWith': ['si3']
             },
-            "type": "Feature"
+            'type': 'Feature'
         }
     },
     {
-        "resource": {
-            "id": "si0",
-            "identifier": "SI0",
-            "shortDescription": "Ein Befund",
-            "relations": {
-                "isRecordedIn": ["t1"],
-                "includes": ["tf1"]
+        'resource': {
+            'id': 'si0',
+            'identifier': 'SI0',
+            'shortDescription': 'Ein Befund',
+            'relations': {
+                'isRecordedIn': ['t1'],
+                'includes': ['tf1']
             },
-            "geometry": {
-                "type": "Polygon",
-                "coordinates": [[
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [[
                     [27.189332902431488, 39.1412228345871],
                     [27.18933594226837, 39.14123010635376],
                     [27.18933880329132, 39.141228795051575],
@@ -124,56 +124,56 @@ export const DOCS: NewDocument[] = [
                     [27.189332902431488, 39.1412228345871]
                 ]]
             },
-            "type": "Feature"
+            'type': 'Feature'
         }
     },
     {
-        "resource": {
-            "id": "tf1",
-            "identifier": "testf1",
-            "shortDescription": "Testfund",
-            "relations": {
-                "isRecordedIn": ["t1"],
-                "liesWithin": ["si0"]
+        'resource': {
+            'id': 'tf1',
+            'identifier': 'testf1',
+            'shortDescription': 'Testfund',
+            'relations': {
+                'isRecordedIn': ['t1'],
+                'liesWithin': ['si0']
             },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [ 27.189335972070694, 39.14122423529625]
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [ 27.189335972070694, 39.14122423529625]
             },
-            "type": "Find"
+            'type': 'Find'
         }
     },
     {
-        "resource": {
-            "id": "o25",
-            "identifier": "PE07-So-07_Z001.jpg",
-            "shortDescription": "Test Layer 1",
-            "type": "Drawing",
-            "originalFilename" : "PE07-So-07_Z001.jpg",
-            "height" : 2423,
-            "width" : 3513,
-            "relations": {},
-            "georeference": {
-                "bottomLeftCoordinates": [39.1411810096, 27.1892609283],
-                "topLeftCoordinates": [39.1412672328, 27.1892609283],
-                "topRightCoordinates": [39.1412672328, 27.1893859555]
+        'resource': {
+            'id': 'o25',
+            'identifier': 'PE07-So-07_Z001.jpg',
+            'shortDescription': 'Test Layer 1',
+            'type': 'Drawing',
+            'originalFilename' : 'PE07-So-07_Z001.jpg',
+            'height' : 2423,
+            'width' : 3513,
+            'relations': {},
+            'georeference': {
+                'bottomLeftCoordinates': [39.1411810096, 27.1892609283],
+                'topLeftCoordinates': [39.1412672328, 27.1892609283],
+                'topRightCoordinates': [39.1412672328, 27.1893859555]
             }
         }
     },
     {
-        "resource": {
-            "id": "o26",
-            "identifier": "mapLayerTest2.png",
-            "shortDescription": "Test Layer 2",
-            "type": "Image",
-            "relations": {},
-            "originalFilename" : "mapLayerTest2.png",
-            "height" : 782,
-            "width" : 748,
-            "georeference": {
-                "bottomLeftCoordinates": [39.1412810096, 27.1893609283],
-                "topLeftCoordinates": [39.1413672328, 27.1893609283],
-                "topRightCoordinates": [39.1413672328, 27.1894859555]
+        'resource': {
+            'id': 'o26',
+            'identifier': 'mapLayerTest2.png',
+            'shortDescription': 'Test Layer 2',
+            'type': 'Image',
+            'relations': {},
+            'originalFilename' : 'mapLayerTest2.png',
+            'height' : 782,
+            'width' : 748,
+            'georeference': {
+                'bottomLeftCoordinates': [39.1412810096, 27.1893609283],
+                'topLeftCoordinates': [39.1413672328, 27.1893609283],
+                'topRightCoordinates': [39.1413672328, 27.1894859555]
             }
         }
     }

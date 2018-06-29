a85c2f3a8fb9cb5232df91381732650efba7c824
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-06-29 23:09:34 +0100

    Remove unused import

--- app/components/resources/view/documents-manager.ts
+++ app/components/resources/view/documents-manager.ts
@@ -12,7 +12,6 @@ import {subtract, unique} from 'tsfun';
 import {ResourcesStateManager} from './resources-state-manager';
 import {IdaiFieldFindResult} from '../../../core/datastore/core/cached-read-datastore';
 import {ResourcesState} from './state/resources-state';
-import {NavigationPath} from './state/navigation-path';
 
 
 /**

e4322dc53eda944592a2421666e8dc66b6dcfac6
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-07-02 12:10:53 +0100

    Adjust for tooltip in e2e

--- test/e2e/resources/resources-state.spec.ts
+++ test/e2e/resources/resources-state.spec.ts
@@ -394,6 +394,7 @@ describe('resources/state --', function() {
 
         NavbarPage.clickNavigateToExcavation();
         OperationBarPage.clickSwitchHierarchyMode();
+        browser.actions().mouseUp().mouseMove({x: 200, y: 200}).perform(); // avoid tooltip
         OperationBarPage.performSelectOperation(0);
         ResourcesPage.getListItemIdentifierText(0).then(text => expect(text).toEqual('SI0'));
         ResourcesPage.getListItemIdentifierText(1).then(text => expect(text).toEqual('SI1'));

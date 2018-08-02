0bf5c97b29662ae58a6a8bd4c52d6671dde4a08a
Thomas Kleinke <thomas.kleinke@dainst.de>
2018-08-02 16:14:29 +0200

    adjust to code style rules

--- app/common/state-serializer.ts
+++ app/common/state-serializer.ts
@@ -1,8 +1,13 @@
 export class StateSerializer {
+
     public async load(): Promise<any> {
+
         return null;
     };
+
+
     public async store(stateObject: any): Promise<any> {
+
         return null;
     }
 }

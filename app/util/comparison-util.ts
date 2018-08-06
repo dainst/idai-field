fe98456cea1f0a0c6187fe5052ab87a718c47d4c
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-08-06 22:18:46 +0200

    Remove seemingly superfluous function
    Seems fine, unit tests also pass

--- app/util/comparison-util.ts
+++ app/util/comparison-util.ts
@@ -50,27 +50,25 @@ export module ComparisonUtil {
         const type1: string = ComparisonUtil.getType(value1);
         const type2: string = ComparisonUtil.getType(value2);
 
-        if (type1 !== type2) {
-            return false;
-        } else if (type1 === 'object') {
-            return jsonEqual(value1)(value2);
-        } else if (type1 === 'array') {
+        if (type1 !== type2) return false;
+
+        if (type1 === 'array' && type2 === 'array') {
             return arrayEquivalentBy(jsonEqual)(value1)(value2);
-        } else {
-            return compareFields(value1, value2);
         }
+
+        return jsonEqual(value1)(value2);
     }
 
 
-    export function compareFields(field1: any, field2: any): boolean {
-
-        if (field1 instanceof Array && !(field2 instanceof Array)) return false;
-        if (!(field1 instanceof Array) && field2 instanceof Array) return false;
-
-        if (field1 instanceof Array) return arrayEquivalentBy(jsonEqual)(field1)(field2);
-
-        return field1 === field2;
-    }
+    // export function compareFields(field1: any, field2: any): boolean {
+    //
+    //     if (field1 instanceof Array && !(field2 instanceof Array)) return false;
+    //     if (!(field1 instanceof Array) && field2 instanceof Array) return false;
+    //
+    //     if (field1 instanceof Array) return arrayEquivalentBy(jsonEqual)(field1)(field2);
+    //
+    //     return field1 === field2;
+    // }
 
 
     export function getType(value: any): string {

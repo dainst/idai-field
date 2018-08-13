1263da2f95756e6a76789b6ef16d29496ed0d2c5
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-08-13 13:32:01 +0200

    Do todo

--- app/core/model/model-util.ts
+++ app/core/model/model-util.ts
@@ -1,5 +1,5 @@
 import {Document} from 'idai-components-2/core';
-import {getAtIndex} from '../../util/list-util';
+import {getAtIndex} from 'tsfun';
 
 /**
  * @author: Thomas Kleinke
@@ -19,13 +19,13 @@ export class ModelUtil {
         const targetIds: string[]|undefined = document.resource.relations[relationName];
         if (!targetIds) return undefined;
 
-        return getAtIndex(targetIds, index);
+        return getAtIndex(targetIds)(index);
     }
 }
 
 
-// TODO make use of on, rename to onId,
-export const hasEqualId = (l: Document|undefined) => (r: Document): boolean => (l != undefined && l.resource.id == r.resource.id);
+export const hasEqualId = (l: Document|undefined) =>
+    (r: Document): boolean => (l != undefined && l.resource.id === r.resource.id);
 
 export const hasId = (doc: Document) => doc.resource.id != undefined;
 

c979ac4ed6a46250dd7f11b1cd4f1b81a03f1842
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-08-13 13:26:24 +0200

    Organize imports

--- app/components/docedit/core/forms/dating.component.ts
+++ app/components/docedit/core/forms/dating.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Resource} from 'idai-components-2/core';
 import {DocumentEditChangeMonitor} from '../document-edit-change-monitor';
 

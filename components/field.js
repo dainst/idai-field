703becd83706a6ac109010540bbf2946bed18691
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-08-21 10:46:42 +0100

    Revert "Add components properly (git-wise)"
    This reverts commit 7475eca017267c9760c0b1ecda92585cbcc33dde.

--- components/field.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var idai_field_app_configurator_1 = require("./src/field/model/idai-field-app-configurator");
-exports.IdaiFieldAppConfigurator = idai_field_app_configurator_1.IdaiFieldAppConfigurator;
-var idai_field_map_module_1 = require("./src/field/map/idai-field-map.module");
-exports.IdaiFieldMapModule = idai_field_map_module_1.IdaiFieldMapModule;
-var map_component_1 = require("./src/field/map/map.component");
-exports.MapComponent = map_component_1.MapComponent;
-//# sourceMappingURL=field.js.map
--- components/widgets.ts
+++
@@ -1,2 +0,0 @@
-export {IdaiWidgetsModule} from './src/core/widgets/idai-widgets.module';
-export {TypeIconComponent} from './src/core/widgets/type-icon';

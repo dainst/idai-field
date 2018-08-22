41b913d74d0262882a61c52469a6eb71c96d5e99
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-08-22 15:55:42 +0100

    Revert "Decrease minWidth"
    This reverts commit 05fcae8f22af0219a1f4892095ba920d5e8a60bb.

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ function createWindow() {
     mainWindow = new electron.BrowserWindow({
         width: screenWidth >= 1680 ? 1680 : 1280,
         height: screenHeight >= 1050 ? 1050 : 800,
-        minWidth: 1000,
+        minWidth: 1220, // to allow for displaying project names like 'mmmmmmmmmmmmmmmmmm'
         minHeight: 600,
         webPreferences: {
             nodeIntegration: true,

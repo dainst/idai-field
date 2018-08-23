61df740d3c9c7621b79f8d033691c3f2e2860423
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-08-23 20:19:24 +0100

    Work through fixing karma tests

---
+++ test/subsystem/t.spec.ts
@@ -0,0 +1,22 @@
+// import {DAOsSpecHelper} from './daos-spec-helper';
+// import {Static} from '../unit/static';
+
+
+// export function main() {
+
+
+    describe('t', () => {
+
+
+        // create
+
+        it('t', async done => {
+
+           console.log("hi")
+            done();
+        });
+
+
+
+    });
+// }

93a2d00a87b0459065282d6c1e0a905e16ef2209
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-08-31 13:33:23 +0100

    Fix e2e relation index

--- test/e2e/resources/resources.page.ts
+++ test/e2e/resources/resources.page.ts
@@ -299,7 +299,7 @@ export class ResourcesPage {
 
         ResourcesPage.performCreateResource('1', "feature-architecture");
         ResourcesPage.performCreateResource('2', "feature-architecture");
-        ResourcesPage.performCreateRelation('2', '1', 1);
+        ResourcesPage.performCreateRelation('2', '1', 2);
     }
 
 

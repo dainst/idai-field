109a2ca41b91238a25d68ec5873b7e8b909505c0
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-09-10 22:45:52 +0100

    Refactor a bit

--- app/components/docedit/messages-conversion.ts
+++ app/components/docedit/messages-conversion.ts
@@ -3,6 +3,8 @@ import {M} from '../m';
 
 
 /**
+ * * Converts messages of Validator to messages of M for DoceditComponent.
+ *
  * @author Daniel de Oliveira
  */
 export module MessagesConversion {
@@ -11,15 +13,16 @@ export module MessagesConversion {
 
         if (msgWithParams.length === 0) return [];
         let replacement = undefined;
+        const msg = msgWithParams[0];
 
 
-        if (msgWithParams[0] === ValidationErrors.IDENTIFIER_EXISTS) replacement = M.MODEL_VALIDATION_ERROR_IDEXISTS;
+        if (msg === ValidationErrors.IDENTIFIER_EXISTS) replacement = M.MODEL_VALIDATION_ERROR_IDEXISTS;
         // TODO this gets thrown if missing identifier, probably because this is detected first
-        if (msgWithParams[0] === ValidationErrors.MISSING_PROPERTY) replacement = M.IMPORT_VALIDATION_ERROR_MISSINGPROPERTY;
+        if (msg === ValidationErrors.MISSING_PROPERTY) replacement = M.IMPORT_VALIDATION_ERROR_MISSINGPROPERTY;
         // if (errorWithParams[0] === ImportErrors.MISSING_IDENTIFIER) replacement = M.IMPORT_FAILURE_MISSING_IDENTIFIER;
 
         // TODO replace M msg by a msg specific to docedit component
-        if (msgWithParams[0] === ValidationErrors.NO_ISRECORDEDIN) replacement = M.IMPORT_VALIDATION_ERROR_NORECORDEDIN;
+        if (msg === ValidationErrors.NO_ISRECORDEDIN) replacement = M.IMPORT_VALIDATION_ERROR_NORECORDEDIN;
 
 
         if (replacement) (msgWithParams as any)[0] = replacement;
--- app/components/import/messages-conversion.ts
+++ app/components/import/messages-conversion.ts
@@ -3,7 +3,7 @@ import {M} from '../m';
 import {ImportErrors} from '../../core/import/import-errors';
 
 /**
- * Converts messages of Validator / Importer to messages of M for Import component.
+ * Converts messages of Validator / Importer to messages of M for ImportComponent.
  *
  * @author Daniel de Oliveira
  */
@@ -13,52 +13,52 @@ export module MessagesConversion {
 
         if (msgWithParams.length === 0) return [];
         let replacement = undefined;
+        const msg = msgWithParams[0];
 
 
-        if (msgWithParams[0] === ValidationErrors.INVALID_TYPE) replacement = M.IMPORT_VALIDATION_ERROR_INVALIDTYPE;
-        if (msgWithParams[0] === ValidationErrors.NO_ISRECORDEDIN) replacement = M.IMPORT_VALIDATION_ERROR_NORECORDEDIN;
-        if (msgWithParams[0] === ValidationErrors.NO_ISRECORDEDIN_TARGET) replacement = M.IMPORT_VALIDATION_ERROR_NORECORDEDINTARGET;
-        if (msgWithParams[0] === ValidationErrors.IDENTIFIER_EXISTS) replacement = M.MODEL_VALIDATION_ERROR_IDEXISTS;
-        if (msgWithParams[0] === ValidationErrors.MISSING_PROPERTY) replacement = M.IMPORT_VALIDATION_ERROR_MISSINGPROPERTY;
-        if (msgWithParams[0] === ValidationErrors.MISSING_GEOMETRY_TYPE) replacement = M.MODEL_VALIDATION_ERROR_MISSING_GEOMETRYTYPE;
-        if (msgWithParams[0] === ValidationErrors.MISSING_COORDINATES) replacement = M.MODEL_VALIDATION_ERROR_MISSING_COORDINATES;
-        if (msgWithParams[0] === ValidationErrors.INVALID_COORDINATES) replacement = M.MODEL_VALIDATION_ERROR_INVALID_COORDINATES;
-        if (msgWithParams[0] === ValidationErrors.UNSUPPORTED_GEOMETRY_TYPE) replacement = M.MODEL_VALIDATION_ERROR_UNSUPPORTED_GEOMETRYTYPE;
-        if (msgWithParams[0] === ValidationErrors.UNSUPPORTED_GEOMETRY_TYPE) replacement = M.MODEL_VALIDATION_ERROR_UNSUPPORTED_GEOMETRYTYPE;
+        if (msg === ValidationErrors.INVALID_TYPE) replacement = M.IMPORT_VALIDATION_ERROR_INVALIDTYPE;
+        if (msg === ValidationErrors.NO_ISRECORDEDIN) replacement = M.IMPORT_VALIDATION_ERROR_NORECORDEDIN;
+        if (msg === ValidationErrors.NO_ISRECORDEDIN_TARGET) replacement = M.IMPORT_VALIDATION_ERROR_NORECORDEDINTARGET;
+        if (msg === ValidationErrors.IDENTIFIER_EXISTS) replacement = M.MODEL_VALIDATION_ERROR_IDEXISTS;
+        if (msg === ValidationErrors.MISSING_PROPERTY) replacement = M.IMPORT_VALIDATION_ERROR_MISSINGPROPERTY;
+        if (msg === ValidationErrors.MISSING_GEOMETRY_TYPE) replacement = M.MODEL_VALIDATION_ERROR_MISSING_GEOMETRYTYPE;
+        if (msg === ValidationErrors.MISSING_COORDINATES) replacement = M.MODEL_VALIDATION_ERROR_MISSING_COORDINATES;
+        if (msg === ValidationErrors.INVALID_COORDINATES) replacement = M.MODEL_VALIDATION_ERROR_INVALID_COORDINATES;
+        if (msg === ValidationErrors.UNSUPPORTED_GEOMETRY_TYPE) replacement = M.MODEL_VALIDATION_ERROR_UNSUPPORTED_GEOMETRYTYPE;
+        if (msg === ValidationErrors.UNSUPPORTED_GEOMETRY_TYPE) replacement = M.MODEL_VALIDATION_ERROR_UNSUPPORTED_GEOMETRYTYPE;
 
-        if (msgWithParams[0] === ImportErrors.FILE_UNREADABLE) replacement = M.IMPORT_FAILURE_FILEUNREADABLE;
-        if (msgWithParams[0] === ImportErrors.FILE_INVALID_JSON) replacement = M.IMPORT_FAILURE_INVALIDJSON;
-        if (msgWithParams[0] === ImportErrors.FILE_INVALID_JSONL) replacement = M.IMPORT_FAILURE_INVALIDJSONL;
-        if (msgWithParams[0] === ImportErrors.INVALID_GEOJSON_IMPORT_STRUCT) replacement = M.IMPORT_FAILURE_INVALID_GEOJSON_IMPORT_STRUCT;
-        if (msgWithParams[0] === ImportErrors.MISSING_IDENTIFIER) replacement = M.IMPORT_FAILURE_MISSING_IDENTIFIER;
-        if (msgWithParams[0] === ImportErrors.WRONG_IDENTIFIER_FORMAT) replacement = M.IMPORT_FAILURE_IDENTIFIER_FORMAT;
-        if (msgWithParams[0] === ImportErrors.INVALID_CSV) replacement = M.IMPORT_FAILURE_INVALIDCSV;
-        if (msgWithParams[0] === ImportErrors.GENERIC_CSV_ERROR) replacement = M.IMPORT_FAILURE_GENERICCSVERROR;
-        if (msgWithParams[0] === ImportErrors.MANDATORY_CSV_FIELD_MISSING) replacement = M.IMPORT_FAILURE_MANDATORYCSVFIELDMISSING;
+        if (msg === ImportErrors.FILE_UNREADABLE) replacement = M.IMPORT_FAILURE_FILEUNREADABLE;
+        if (msg === ImportErrors.FILE_INVALID_JSON) replacement = M.IMPORT_FAILURE_INVALIDJSON;
+        if (msg === ImportErrors.FILE_INVALID_JSONL) replacement = M.IMPORT_FAILURE_INVALIDJSONL;
+        if (msg === ImportErrors.INVALID_GEOJSON_IMPORT_STRUCT) replacement = M.IMPORT_FAILURE_INVALID_GEOJSON_IMPORT_STRUCT;
+        if (msg === ImportErrors.MISSING_IDENTIFIER) replacement = M.IMPORT_FAILURE_MISSING_IDENTIFIER;
+        if (msg === ImportErrors.WRONG_IDENTIFIER_FORMAT) replacement = M.IMPORT_FAILURE_IDENTIFIER_FORMAT;
+        if (msg === ImportErrors.INVALID_CSV) replacement = M.IMPORT_FAILURE_INVALIDCSV;
+        if (msg === ImportErrors.GENERIC_CSV_ERROR) replacement = M.IMPORT_FAILURE_GENERICCSVERROR;
+        if (msg === ImportErrors.MANDATORY_CSV_FIELD_MISSING) replacement = M.IMPORT_FAILURE_MANDATORYCSVFIELDMISSING;
 
-        if (msgWithParams[0] === ImportErrors.GENERIC_DATASTORE_ERROR) replacement = M.IMPORT_FAILURE_GENERICDATASTOREERROR;
-        if (msgWithParams[0] === ImportErrors.INVALID_GEOMETRY) replacement = M.IMPORT_FAILURE_INVALIDGEOMETRY;
-        if (msgWithParams[0] === ImportErrors.ROLLBACK_ERROR) replacement = M.IMPORT_FAILURE_ROLLBACKERROR;
-        if (msgWithParams[0] === ImportErrors.MISSING_RESOURCE) replacement = M.IMPORT_FAILURE_MISSING_RESOURCE;
-        if (msgWithParams[0] === ImportErrors.MISSING_RELATION_TARGET) replacement = M.IMPORT_FAILURE_MISSING_RELATION_TARGET;
-        if (msgWithParams[0] === ImportErrors.INVALID_MAIN_TYPE_DOCUMENT) replacement = M.IMPORT_FAILURE_INVALID_MAIN_TYPE_DOCUMENT;
-        if (msgWithParams[0] === ImportErrors.OPERATIONS_NOT_ALLOWED_ON_IMPORT_TO_OPERATION) replacement = M.IMPORT_FAILURE_OPERATIONS_NOT_ALLOWED_ON_IMPORT_TO_OPERATION;
-        if (msgWithParams[0] === ImportErrors.NO_OPERATION_ASSIGNABLE) replacement = M.IMPORT_FAILURE_NO_OPERATION_ASSIGNABLE;
-        if (msgWithParams[0] === ImportErrors.NO_FEATURE_ASSIGNABLE) replacement = M.IMPORT_FAILURE_NO_FEATURE_ASSIGNABLE;
+        if (msg === ImportErrors.GENERIC_DATASTORE_ERROR) replacement = M.IMPORT_FAILURE_GENERICDATASTOREERROR;
+        if (msg === ImportErrors.INVALID_GEOMETRY) replacement = M.IMPORT_FAILURE_INVALIDGEOMETRY;
+        if (msg === ImportErrors.ROLLBACK_ERROR) replacement = M.IMPORT_FAILURE_ROLLBACKERROR;
+        if (msg === ImportErrors.MISSING_RESOURCE) replacement = M.IMPORT_FAILURE_MISSING_RESOURCE;
+        if (msg === ImportErrors.MISSING_RELATION_TARGET) replacement = M.IMPORT_FAILURE_MISSING_RELATION_TARGET;
+        if (msg === ImportErrors.INVALID_MAIN_TYPE_DOCUMENT) replacement = M.IMPORT_FAILURE_INVALID_MAIN_TYPE_DOCUMENT;
+        if (msg === ImportErrors.OPERATIONS_NOT_ALLOWED_ON_IMPORT_TO_OPERATION) replacement = M.IMPORT_FAILURE_OPERATIONS_NOT_ALLOWED_ON_IMPORT_TO_OPERATION;
+        if (msg === ImportErrors.NO_OPERATION_ASSIGNABLE) replacement = M.IMPORT_FAILURE_NO_OPERATION_ASSIGNABLE;
+        if (msg === ImportErrors.NO_FEATURE_ASSIGNABLE) replacement = M.IMPORT_FAILURE_NO_FEATURE_ASSIGNABLE;
 
 
-
-        if (msgWithParams[0] === ValidationErrors.INVALID_FIELDS) {
+        if (msg === ValidationErrors.INVALID_FIELDS) {
             replacement = msgWithParams.length > 2 && msgWithParams[2].indexOf(',') !== -1
                 ? M.IMPORT_VALIDATION_ERROR_INVALIDFIELDS
                 : M.IMPORT_VALIDATION_ERROR_INVALIDFIELD
         }
-        if (msgWithParams[0] === ValidationErrors.INVALID_RELATIONS) {
+        if (msg === ValidationErrors.INVALID_RELATIONS) {
             replacement = msgWithParams.length > 2 && msgWithParams[2].indexOf(',') !== -1
                 ? M.IMPORT_VALIDATION_ERROR_INVALIDRELATIONFIELDS
                 : M.IMPORT_VALIDATION_ERROR_INVALIDRELATIONFIELD
         }
-        if (msgWithParams[0] === ValidationErrors.INVALID_NUMERICAL_VALUES) {
+        if (msg === ValidationErrors.INVALID_NUMERICAL_VALUES) {
             replacement = msgWithParams.length > 2 && msgWithParams[2].indexOf(',') !== -1
                 ? M.IMPORT_VALIDATION_ERROR_INVALID_NUMERIC_VALUES
                 : M.IMPORT_VALIDATION_ERROR_INVALID_NUMERIC_VALUE

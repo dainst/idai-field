2f4b7729737505a8e3221a4c48f2e96caf626aad
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-09-10 22:18:37 +0100

    Fix 1 e2e by converting msg

--- app/components/docedit/docedit.component.ts
+++ app/components/docedit/docedit.component.ts
@@ -13,6 +13,7 @@ import {DocumentHolder} from './document-holder';
 import {TypeUtility} from '../../core/model/type-utility';
 import {ValidationErrors} from '../../core/model/validation-errors';
 import {M} from '../m';
+import {ImportErrors} from '../../core/import/import-errors';
 
 
 @Component({
@@ -153,6 +154,9 @@ export class DoceditComponent {
 
             let replacement = undefined;
             if (errorWithParams[0] === ValidationErrors.IDENTIFIER_EXISTS) replacement = M.MODEL_VALIDATION_ERROR_IDEXISTS;
+            // TODO this gets thrown if missing identifier, probably because this is detected first
+            if (errorWithParams[0] === ValidationErrors.MISSING_PROPERTY) replacement = M.IMPORT_VALIDATION_ERROR_MISSINGPROPERTY;
+            // if (errorWithParams[0] === ImportErrors.MISSING_IDENTIFIER) replacement = M.IMPORT_FAILURE_MISSING_IDENTIFIER;
 
             if (replacement) {
                 errorWithParams[0] = replacement;

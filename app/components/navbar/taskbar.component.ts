65a18b1d004675ef0894c112ed096a8edc284229
Thomas Kleinke <thomas.kleinke@dainst.de>
2018-09-26 19:05:50 +0200

    change sync icon when remote changes are received

--- app/components/navbar/taskbar.component.ts
+++ app/components/navbar/taskbar.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
 import {SettingsService} from '../../core/settings/settings-service';
+import {RemoteChangesStream} from '../../core/datastore/core/remote-changes-stream';
 
 
 @Component({
@@ -15,16 +16,45 @@ import {SettingsService} from '../../core/settings/settings-service';
 export class TaskbarComponent {
 
     public connected: boolean = false;
+    public receivingRemoteChanges: boolean = false;
+
+    private remoteChangesTimeout: any = undefined;
 
 
-    constructor(private settings: SettingsService) {
+    constructor(private changeDetectorRef: ChangeDetectorRef,
+                settingsService: SettingsService,
+                remoteChangesStream: RemoteChangesStream) {
 
-        settings.syncStatusChanges().subscribe(c => {
-            if (c === 'disconnected') {
-                this.connected = false;
-            } else if (c === 'connected') {
+        this.listenToSyncStatusChanges(settingsService);
+        this.listenToRemoteChanges(remoteChangesStream);
+    }
+
+
+    private listenToSyncStatusChanges(settingsService: SettingsService) {
+
+        settingsService.syncStatusChanges().subscribe(status => {
+            if (status === 'connected') {
                 this.connected = true;
+                this.changeDetectorRef.detectChanges();
+            } else if (status === 'disconnected') {
+                this.connected = false;
+                this.changeDetectorRef.detectChanges();
             }
+        });
+    }
+
+
+    private listenToRemoteChanges(remoteChangesStream: RemoteChangesStream) {
+
+        remoteChangesStream.notifications().subscribe(() => {
+            this.receivingRemoteChanges = true;
+            this.changeDetectorRef.detectChanges();
+
+            if (this.remoteChangesTimeout) clearTimeout(this.remoteChangesTimeout);
+            this.remoteChangesTimeout = setTimeout(() => {
+                this.receivingRemoteChanges = false;
+                this.changeDetectorRef.detectChanges();
+            }, 2000);
         });
     }
 }

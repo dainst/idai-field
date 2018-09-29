593a49bbafa9ddf590dc7b8e0f506ffc86227188
Thomas Kleinke <thomas.kleinke@dainst.de>
2018-09-30 00:42:30 +0200

    adjust to code style rules

--- test/unit/components/docedit/core/relation-picker-group.component.spec.ts
+++ test/unit/components/docedit/core/relation-picker-group.component.spec.ts
@@ -10,24 +10,22 @@ describe('RelationPickerGroupComponent', () => {
 
 
     beforeEach(() => {
-        document = { "resource" :
-                { "id": "id1", "identifier": "ob1", "type": "object", "relations" : {} }
-        };
+
+        document = { resource : { id: 'id1', identifier: 'ob1', type: 'object', relations : {} } };
 
         relationPickerGroupComponent = new RelationPickerGroupComponent();
         relationPickerGroupComponent.document = document;
-        relationPickerGroupComponent.relationDefinition = { "name": "Above", "inverse": "Below" };
+        relationPickerGroupComponent.relationDefinition = { name: 'Above', inverse: 'Below' };
     });
 
 
-    it('should create an empty relation array if no relation array exists and a new relation is created',
-        function() {
-            relationPickerGroupComponent.ngOnChanges();
-            relationPickerGroupComponent.createRelation();
+    it('should create an empty relation array if no relation array exists and a new relation is created', () => {
 
-            expect(document['resource']['relations']['Above'].length).toBe(1);
-            expect(document['resource']['relations']['Above'][0]).toBe("");
-        }
-    );
+        relationPickerGroupComponent.ngOnChanges();
+        relationPickerGroupComponent.createRelation();
+
+        expect(document.resource.relations['Above'].length).toBe(1);
+        expect(document.resource.relations['Above'][0]).toBe('');
+    });
 });
 

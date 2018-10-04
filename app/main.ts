1674a4b6f2783969811deb86d37b355bf59e1cb5
Thomas Kleinke <thomas.kleinke@dainst.de>
2018-10-04 14:58:24 +0200

    disable angular prod mode

--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,7 @@ import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app.module';
 
-if (require('electron').remote.getGlobal('mode') === 'production') enableProdMode();
+// TODO Activate prod mode
+//if (require('electron').remote.getGlobal('mode') === 'production') enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule);

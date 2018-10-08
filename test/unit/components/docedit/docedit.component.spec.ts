78975e94ddc5630bab2478f422a064a691a78134
Thomas Kleinke <thomas.kleinke@dainst.de>
2018-10-08 23:04:51 +0200

    fix DoceditComponent unit tests

--- test/unit/components/docedit/docedit.component.spec.ts
+++ test/unit/components/docedit/docedit.component.spec.ts
@@ -1,9 +1,9 @@
-/**
- * @author Daniel de Oliveira
- */
 import {DoceditComponent} from '../../../../app/components/docedit/docedit.component';
 import {DoceditActiveTabService} from '../../../../app/components/docedit/docedit-active-tab-service';
 
+/**
+ * @author Daniel de Oliveira
+ */
 describe('DoceditComponent', () => {
 
     let activeTabService: any;
@@ -14,14 +14,14 @@ describe('DoceditComponent', () => {
 
         const typeUtility = jasmine.createSpyObj('typeUtility', ['getSubtypes']);
         typeUtility.getSubtypes.and.returnValue({'Object':[]});
-        const documentHolder = jasmine.createSpyObj('documentHolder', ['setClonedDocument', 'getClonedDocument']);
+        const documentHolder = jasmine.createSpyObj('documentHolder', ['setClonedDocument']);
         const projectConfiguration = jasmine.createSpyObj('projectConfiguration', ['getFieldDefinitionLabel']);
         activeTabService = new DoceditActiveTabService();
 
         docedit = new DoceditComponent(
             undefined,
-            undefined,
             documentHolder,
+            undefined,
             undefined,
             undefined,
             typeUtility,

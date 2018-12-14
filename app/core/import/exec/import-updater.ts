32ca0d3b424f3bc56c40f958615121fcad548263
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-12-14 19:02:28 +0000

    Add todo

--- app/core/import/exec/import-updater.ts
+++ app/core/import/exec/import-updater.ts
@@ -3,16 +3,25 @@ import {Document} from 'idai-components-2/src/model/core/document';
 
 
 /**
+ * TODO change to batch updates and rename to BatchUpdater
+ * for a first version implement it like this:
+ *   check if the project contains conflicts. if yes. display a msg to the user, that import will run slow if conflicts not solved first.
+ *   then:
+ *     if contains conflicts: update or create everything like before. iterate over all docs separately
+ *     if not contains conflicts: do batch create or update for documents, batch update for relation documents.
+ *
  * @author Daniel de Oliveira
  * @author Thomas Kleinke
  */
-export module ImportUpdater { // TODO change to batch updates and rename to BatchUpdater
+export module ImportUpdater {
 
 
     export async function go(documents: Array<Document>,
                              targetDocuments: Array<Document>|undefined,
                              update: (document: Document, username: string) => Promise<Document>,
                              create: (document: Document, username: string) => Promise<Document>,
+                             /* bulkUpdate: (documents: Array<Document>, username: string) =>  */
+                             /* bulkCreate: (documents: Array<Document>, username: string) =>  */
                              username: string,
                              useUpdateMethod: boolean /* else new doc, then use create */) {
 

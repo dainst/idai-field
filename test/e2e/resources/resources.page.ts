ef536720ac89cb0154acc8837d3f88a3597996f0
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2019-01-23 16:17:56 +0100

    Fix tests, update electron, spectron

--- test/e2e/resources/resources.page.ts
+++ test/e2e/resources/resources.page.ts
@@ -301,7 +301,7 @@ export class ResourcesPage {
 
         ResourcesPage.performCreateResource('1', "feature-architecture");
         ResourcesPage.performCreateResource('2', "feature-architecture");
-        ResourcesPage.performCreateRelation('2', '1', 9);
+        ResourcesPage.performCreateRelation('2', '1', 8);
     }
 
 

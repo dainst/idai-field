62de430309522f57fe1e213ea0201f2f9f48108b
daniel <danielmarreirosdeoliveira@googlemail.com>
2019-02-14 10:11:39 +0000

    Make use of tsfun's clone

--- app/core/util/object-util.ts
+++ app/core/util/object-util.ts
@@ -1,28 +1,14 @@
-import {jsonClone} from 'tsfun';
+import {clone as tsfunClone, jsonClone} from 'tsfun';
 
-// @author Thomas Kleinke
-// @author Daniel de Oliveira
+/**
+ * @author Thomas Kleinke
+ * @author Daniel de Oliveira
+ */
+export function clone<T>(struct: T): T {
 
-
-export function clone<O>(object: O): O {
-
-    const clone = jsonClone(object);
-    return convertDates(object, clone);
-}
-
-
-function convertDates<O>(original: any, plain: any) {
-
-    if (original) {
-        for (let key of Object.keys(original)) {
-
-            if (original[key] instanceof Date) {
-                plain[key] = new Date(original[key]);
-            } else if (typeof original[key] === 'object') {
-                convertDates(original[key], plain[key])
-            }
-
-        }
-    }
-    return plain;
+    return tsfunClone(struct, ((item: any) => {
+        return item instanceof Date
+            ? new Date(item)
+            : jsonClone(item);
+    }));
 }

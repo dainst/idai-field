46e8015927d6d56259c09a8def85fa79eac79604
Thomas Kleinke <thomas.kleinke@dainst.de>
2019-02-26 15:36:40 +0100

    update tab label when operation identifier has been edited

--- app/components/tab-manager.ts
+++ app/components/tab-manager.ts
@@ -1,6 +1,9 @@
+import {Injectable} from '@angular/core';
 import {Observable, Observer} from 'rxjs';
+import {Document} from 'idai-components-2';
 import {NavigationPath} from './resources/view/state/navigation-path';
 import {ObserverUtil} from '../core/util/observer-util';
+import {PouchdbDatastore} from '../core/datastore/core/pouchdb-datastore';
 
 
 export type Tab = {
@@ -9,6 +12,7 @@ export type Tab = {
 }
 
 
+@Injectable()
 /**
  * @author Thomas Kleinke
  */
@@ -16,6 +20,12 @@ export class TabManager {
 
     private tabs: Array<Tab> = [];
     private observers: Array<Observer<Array<Tab>>> = [];
+
+
+    constructor(datastore: PouchdbDatastore) {
+
+        datastore.changesNotifications().subscribe(document => this.updateTabLabel(document));
+    }
 
 
     public notifications = (): Observable<NavigationPath> => ObserverUtil.register(this.observers);
@@ -38,5 +48,12 @@ export class TabManager {
     public closeTab(name: string) {
 
         this.tabs = this.tabs.filter(tab => tab.name !== name);
+    }
+
+
+    public updateTabLabel(document: Document) {
+
+        const tab: Tab|undefined = this.tabs.find(tab => tab.name === document.resource.id);
+        if (tab) tab.label = document.resource.identifier;
     }
 }

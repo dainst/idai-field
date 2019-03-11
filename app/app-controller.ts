dc14eb2aa864e76fdca38a427650fbf4d552b82b
Thomas Kleinke <thomas.kleinke@dainst.de>
2019-03-11 21:38:27 +0100

    extract method

--- app/app-controller.ts
+++ app/app-controller.ts
@@ -36,22 +36,29 @@ export class AppController {
             if (!remote.getGlobal('switches').provide_reset) return resolve();
 
             const control = express();
-            control.post('/reset', async (req: any, res: any) => {
+            control.use(express.json());
 
-                this.resourcesState.resetForE2E();
-                this.imagesState.resetForE2E();
-
-                this.tabManager.resetForE2E();
-                this.documentCache.resetForE2E();
-                await this.pouchdbManager.resetForE2E();
-                await this.pouchdbManager.reindex(this.indexFacade);
-
-                res.send('done');
+            control.post('/reset', async (request: any, result: any) => {
+                await this.reset();
+                result.send('done');
             });
+
             control.listen(3003, function() {
                 console.log('App Control listening on port 3003');
                 resolve();
             });
         });
+    }
+
+
+    private async reset() {
+
+        this.resourcesState.resetForE2E();
+        this.imagesState.resetForE2E();
+
+        this.tabManager.resetForE2E();
+        this.documentCache.resetForE2E();
+        await this.pouchdbManager.resetForE2E();
+        await this.pouchdbManager.reindex(this.indexFacade);
     }
 }

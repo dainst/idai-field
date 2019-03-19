945bd62f1517705b8ab37a6976e31c38d635ac87
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2019-03-19 11:52:40 +0100

    Reenable image overview e2es

--- test/e2e/images/image-overview.spec.ts
+++ test/e2e/images/image-overview.spec.ts
@@ -12,7 +12,7 @@ const delays = require('../config/delays');
 const common = require('../common');
 
 
-xdescribe('images --', function() {
+describe('images --', function() {
 
     const resourceId1 = 'tf1';
     const resourceId2 = 'si0';

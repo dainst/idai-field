89cca71c3ffcdeeb31fec35d3ef48a3cd7dd2e73
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2019-04-04 15:33:51 +0200

    Make position context menu

--- app/components/resources/map/list/sidebar-list.component.ts
+++ app/components/resources/map/list/sidebar-list.component.ts
@@ -1,11 +1,13 @@
 import {Component, Input} from '@angular/core';
-import {FieldDocument} from 'idai-components-2';
 import {ResourcesComponent} from '../../resources.component';
+import {ProjectConfiguration, FieldDocument} from 'idai-components-2';
 import {Loading} from '../../../../widgets/loading';
 import {ViewFacade} from '../../view/view-facade';
 import {NavigationService} from '../../navigation/navigation-service';
 import {BaseList} from '../../base-list';
 import {ResourcesMapComponent} from '../resources-map.component';
+import {isEmpty} from 'tsfun';
+import {RoutingService} from '../../../routing-service';
 
 
 @Component({
@@ -23,20 +25,24 @@ export class SidebarListComponent extends BaseList {
 
     @Input() activeTab: string;
 
+    public relationsToHide: string[] = ['isRecordedIn', 'liesWithin'];
+
+    public popoverOpened = false;
+    public highlightedDocument: FieldDocument|undefined = undefined;
+    public timeoutRunning = false;
+
 
     constructor(resourcesComponent: ResourcesComponent,
                 viewFacade: ViewFacade,
                 loading: Loading,
                 private navigationService: NavigationService,
-                private resourcesMapComponent: ResourcesMapComponent) {
+                private resourcesMapComponent: ResourcesMapComponent,
+                private projectConfiguration: ProjectConfiguration,
+                private routingService: RoutingService,) {
 
         super(resourcesComponent, viewFacade, loading)
     }
 
-
-    public moveInto = (document: FieldDocument) => this.navigationService.moveInto(document);
-
-    public jumpToView = (document: FieldDocument) => this.navigationService.jumpToView(document);
 
     public jumpToMatrix = (document: FieldDocument) => this.navigationService.jumpToMatrix(document);
 
@@ -51,6 +57,65 @@ export class SidebarListComponent extends BaseList {
 
     public closeContextMenu = () => this.resourcesMapComponent.closeContextMenu();
 
+
+    public moveInto(document: FieldDocument) {
+
+        this.closePopover();
+        this.navigationService.moveInto(document);
+    }
+
+
+    public jumpToView(document: FieldDocument) {
+
+        this.closePopover();
+        this.navigationService.jumpToView(document);
+    }
+
+    public hasVisibleRelations(document: FieldDocument|undefined) {
+
+        const selectedDoc = document;
+        if (!selectedDoc) return false;
+
+        const relations: any = selectedDoc.resource.relations;
+        if (isEmpty(relations)) return false;
+
+        return Object.keys(relations)
+            .filter(name => {
+                return this.projectConfiguration.isVisibleRelation(name, selectedDoc.resource.type)
+                    && !this.relationsToHide.includes(name)
+                    && relations[name].length > 0;
+            })
+            .length > 0;
+    }
+
+
+    public async jumpToResource(documentToSelect: FieldDocument) {
+
+        this.closePopover();
+        await this.routingService.jumpToResource(documentToSelect, 'relations');
+        this.resourcesComponent.setScrollTarget(documentToSelect);
+    }
+
+
+    public openPopover(document: FieldDocument) {
+
+        if (document) this.highlightedDocument = document;
+        this.popoverOpened = true;
+        this.timeoutRunning = false;
+    }
+
+    public closePopover() {
+
+        this.timeoutRunning = true;
+        setTimeout(async () => {
+
+            if (this.timeoutRunning) {
+                this.popoverOpened = false;
+                this.highlightedDocument = undefined;
+                this.timeoutRunning = false;
+            }
+        }, 50);
+    };
 
     public async select(document: FieldDocument, autoScroll: boolean = false) {
 

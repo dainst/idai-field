2e90dfe33cb91d8dc0a66a902d84cc6a830e5c8d
Thomas Kleinke <thomas.kleinke@dainst.de>
2019-04-30 18:06:35 +0200

    switch context menu orientation based on mouse click position to make sure the whole context menu is always visible

--- app/components/resources/map/context-menu.component.ts
+++ app/components/resources/map/context-menu.component.ts
@@ -1,10 +1,12 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, Output} from '@angular/core';
 import {ViewFacade} from '../view/view-facade';
 import {ResourcesComponent} from '../resources.component';
 import {ResourcesMapComponent} from './resources-map.component';
 
 
 export type ContextMenuAction = 'edit'|'move'|'delete'|'create-polygon'|'create-line-string'|'create-point'|'edit-geometry';
+
+type ContextMenuOrientation = 'top'|'bottom';
 
 
 @Component({
@@ -15,11 +17,13 @@ export type ContextMenuAction = 'edit'|'move'|'delete'|'create-polygon'|'create-
 /**
  * @author Thomas Kleinke
  */
-export class ContextMenuComponent {
+export class ContextMenuComponent implements OnChanges {
 
     @Input() position: { x: number, y: number };
 
     @Output() onSelectAction: EventEmitter<ContextMenuAction> = new EventEmitter<ContextMenuAction>();
+
+    public orientation: ContextMenuOrientation = 'top';
 
 
     constructor(
@@ -28,8 +32,28 @@ export class ContextMenuComponent {
         public viewFacade: ViewFacade) {}
 
 
+    ngOnChanges() {
+
+        this.orientation = this.computeOrientation(this.position.y);
+    }
+
+
     public selectAction(action: ContextMenuAction) {
 
         this.onSelectAction.emit(action);
+    }
+
+
+    public getBottomPosition(yPosition: number): number {
+
+        return window.innerHeight - yPosition;
+    }
+
+
+    private computeOrientation(yPosition: number): ContextMenuOrientation {
+
+        return yPosition <= window.innerHeight * 0.6
+            ? 'top'
+            : 'bottom';
     }
 }

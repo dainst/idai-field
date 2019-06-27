f61215c85c17abfcabd0e3624db9219caccd5365
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2019-06-27 14:03:35 +0200

    Allow to read out datings from multiple columns

--- app/core/import/parser/csv-parsing.ts
+++ app/core/import/parser/csv-parsing.ts
@@ -25,13 +25,40 @@ export module CsvParsing {
     }
 
 
-    function makeDocument(rowContents: string[], fields: string[]) {
+    function implodePaths(currentSegmentObject: any, pathSegments: any[], val: any) {
 
-        return rowContents.reduce((document, rowField, i) => {
+        let index = parseInt(pathSegments[0]);
+        if (isNaN(index)) index = pathSegments[0];
 
-            if (rowField) {
-                (document.resource as any)[fields[i]] = rowField;
-            }
+        if (pathSegments.length >= 2) {
+
+            let nextindex = parseInt(pathSegments[1]);
+            const newItem = isNaN(nextindex) ? {} : Array(nextindex + 1);
+
+            if (!currentSegmentObject[index]) currentSegmentObject[index] = newItem;
+
+            pathSegments.shift();
+            implodePaths(currentSegmentObject[index], pathSegments, val);
+
+        } else {
+
+            currentSegmentObject[index] = val;
+        }
+    }
+
+
+    function insertFieldIntoDocument(resource: any, field: any, fieldOfRow: any) {
+
+        if (field.includes('.')) implodePaths(resource, field.split('.'), fieldOfRow);
+        else (resource as any)[field] = fieldOfRow;
+    }
+
+
+    function makeDocument(fieldsOfRow: string[], fields: string[]) {
+
+        return fieldsOfRow.reduce((document, fieldOfRow, i) => {
+
+            if (fieldOfRow) insertFieldIntoDocument(document.resource, fields[i], fieldOfRow);
             return document;
 
         }, {resource: {}});
--- test/unit/core/import/parser/csv-parsing.spec.ts
+++ test/unit/core/import/parser/csv-parsing.spec.ts
@@ -7,9 +7,26 @@ describe('CsvParsing', () => {
 
         const documents = CsvParsing.parse('identifier,shortDescription,custom\n10,zehn,bla', 'type1');
         expect(documents.length).toBe(1);
-        expect(documents[0].resource.identifier).toBe('10');
+        expect(documents[0].resource.identifier).toBe('10'); // TODO should be number
         expect(documents[0].resource.shortDescription).toBe('zehn');
         expect(documents[0].resource.custom).toBe('bla');
         expect(documents[0].resource.type).toBe('type1');
+    });
+
+
+    it('implode datings', () => {
+
+        const lines = 'identifier,dating.0.begin.year,dating.0.end.year,dating.0.source,dating.0.label\n'
+            + 'identifier1,100,200,S,L';
+
+        const documents = CsvParsing.parse(lines, 'type1');
+        expect(documents.length).toBe(1);
+
+        const resource = documents[0].resource;
+        expect(resource.identifier).toBe('identifier1');
+        expect(resource.dating[0].begin.year).toBe('100');
+        expect(resource.dating[0].end.year).toBe('200');
+        expect(resource.dating[0].source).toBe('S');
+        expect(resource.dating[0].label).toBe('L');
     });
 });

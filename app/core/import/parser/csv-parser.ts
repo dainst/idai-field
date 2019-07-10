2707fbc4e409e343063f6c5dbcac7c8e0f9c9196
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2019-07-10 17:34:26 +0200

    Extract conversion functions

--- app/core/import/parser/csv-parser.ts
+++ app/core/import/parser/csv-parser.ts
@@ -1,7 +1,7 @@
 import {IdaiType, Document, Resource, Dating, Dimension} from 'idai-components-2';
 import {CsvRowsConversion} from './csv-rows-conversion';
 import {makeLines, Parser} from './parser';
-import {flow, map, on, is, isNot, includedIn} from 'tsfun';
+import {flow, map, on, is, isNot, includedIn, setOn, getOnOr} from 'tsfun';
 
 
 /**
@@ -31,20 +31,31 @@ export module CsvParser {
 
         return (content: string) => {
 
-            const documents = flow<any>(content,
-                makeLines,
-                CsvRowsConversion.parse(SEP),
-                map(insertTypeName(type)), // TODO make assoc function
-                map(insertIsChildOf(operationId)),
-                map(convertFieldType(type)),
-                map(toResource));
+            try {
 
-            return Promise.resolve(documents);
+                return Promise.resolve(doParse(type, operationId, content));
+
+            } catch (msgWithParams) {
+
+                return Promise.reject(msgWithParams);
+            }
         };
     };
 
 
-    function convertFieldType(type: IdaiType) { return (resource: Resource) => { // TODO handle errors
+    function doParse(type: IdaiType, operationId: string, content: string) {
+
+        return flow<any>(content,
+            makeLines,
+            CsvRowsConversion.parse(SEP),
+            map(insertTypeName(type)), // TODO make assoc function
+            map(insertIsChildOf(operationId)),
+            map(convertFieldTypes(type)),
+            map(toResource));
+    }
+
+
+    function convertFieldTypes(type: IdaiType) { return (resource: Resource) => { // TODO handle errors
 
         for (let fieldName of
             Object.keys(resource)
@@ -54,22 +65,17 @@ export module CsvParser {
             if (!fieldDefinition) continue; // TODO review
             // throw "CSV Parser - missing field definition " + fieldName;
 
-            if (fieldDefinition.inputType === 'boolean') {
-                if (resource[fieldName] === 'true') resource[fieldName] = true;
-                else if (resource[fieldName] === 'false') resource[fieldName] = false;
-                else throw "CSV Parser - boolean not parsable";
-            }
+            if (fieldDefinition.inputType === 'boolean') convertBoolean(resource, fieldName);
 
             if (fieldDefinition.inputType === 'dating') {
                 for (let dating of resource[fieldName] as Array<Dating>) {
                     // const dating = resource['dating']; // TODO should exist
 
-                    if (dating.begin && dating.begin.year) dating.begin.year = parseInt(dating.begin.year as any); // TODO handle errors
-                    if (dating.end && dating.end.year) dating.end.year = parseInt(dating.end.year as any);
-                    if (dating.margin) dating.margin = parseInt(dating.margin as any);
-
-                    if (dating.isImprecise && (dating.isImprecise as any) === 'true') dating.isImprecise = true; else dating.isImprecise = false;
-                    if (dating.isUncertain && (dating.isUncertain as any) === 'true') dating.isUncertain = true; else dating.isUncertain = false;
+                    convertNumber(dating, 'begin.year');
+                    convertNumber(dating, 'end.year');
+                    convertNumber(dating, 'margin');
+                    convertBoolean(dating, 'isImprecise');
+                    convertBoolean(dating, 'isUncertain');
                 }
             }
 
@@ -77,13 +83,13 @@ export module CsvParser {
                 for (let dimension of resource[fieldName] as Array<Dimension>) {
                     // const dimension // TODO should exist
 
-                    if (dimension.value) dimension.value = parseInt(dimension.value as any);
-                    if (dimension.rangeMin) dimension.rangeMin = parseInt(dimension.rangeMin as any);
-                    if (dimension.rangeMax) dimension.rangeMax = parseInt(dimension.rangeMax as any);
-                    if (dimension.inputValue) dimension.inputValue = parseInt(dimension.inputValue as any);
-                    if (dimension.inputRangeEndValue) dimension.inputRangeEndValue = parseInt(dimension.inputRangeEndValue as any);
-                    if (dimension.isImprecise && (dimension.isImprecise as any) === 'true') dimension.isImprecise = true; else dimension.isImprecise = false;
-                    if (dimension.isRange && (dimension.isRange as any) === 'true') dimension.isRange = true; else dimension.isRange = false;
+                    convertNumber(dimension, 'value');
+                    convertNumber(dimension, 'rangeMin');
+                    convertNumber(dimension, 'rangeMax');
+                    convertNumber(dimension, 'inputValue');
+                    convertNumber(dimension, 'inputRangeEndValue');
+                    convertBoolean(dimension, 'isImprecise');
+                    convertBoolean(dimension, 'isRange');
                 }
             }
 
@@ -99,5 +105,21 @@ export module CsvParser {
     }}
 
 
+    function convertNumber(container: any, path: string) {
 
+        const val = getOnOr(path, undefined)(container);
+        if (!val) return; // TODO review
+        const converted = parseInt(val);
+        if (isNaN(converted)) throw 'is NaN';
+        setOn(container, path)(converted);
+    }
+
+
+    function convertBoolean(container: any, path: string) {
+
+        const val = getOnOr(path, undefined)(container);
+        if (!val) throw "not existing";
+        if (isNot(includedIn(['true', 'false']))(val)) throw "cannot parse boolean";
+        setOn(container, path)(val === 'true');
+    }
 }

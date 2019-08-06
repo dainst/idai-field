3bd3a3f2f7dea2072b4dad70c03b18877914d7bc
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2019-08-06 13:31:21 +0200

    Remove todo

--- app/core/import/exec/relations-completer.ts
+++ app/core/import/exec/relations-completer.ts
@@ -107,7 +107,7 @@ export module RelationsCompleter {
 
         let totalDocsToUpdate: Array<Document> = [];
         for (let document of documents) {
-            totalDocsToUpdate = addOrOverwrite(totalDocsToUpdate, await getDocumentTargetDocsToUpdate(document)); // TODO do this with reduce
+            totalDocsToUpdate = addOrOverwrite(totalDocsToUpdate, await getDocumentTargetDocsToUpdate(document));
         }
         return totalDocsToUpdate;
     }

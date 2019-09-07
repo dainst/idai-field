f44858c58c11088f38527106b5289fe99973e9af
Thomas Kleinke <thomas.kleinke@dainst.de>
2019-09-07 18:08:12 +0200

    Change type picker layout

--- app/widgets/type-picker.component.ts
+++ app/widgets/type-picker.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {Component, Input, Output, EventEmitter, OnChanges} from '@angular/core';
 import {IdaiType} from 'idai-components-2';
 
 @Component({
@@ -6,18 +6,31 @@ import {IdaiType} from 'idai-components-2';
     moduleId: module.id,
     templateUrl: './type-picker.html'
 })
-
 /**
  * @author Thomas Kleinke
  */
-export class TypePickerComponent {
+export class TypePickerComponent implements OnChanges {
 
     @Input() typesTreeList: Array<IdaiType>;
-    @Input() selectedTypes: Array<IdaiType>;
+    @Input() selectedTypes: string[];
     @Input() allTypesOptionVisible: boolean = false;
     @Input() allowPickingAbstractTypes: boolean = false;
 
     @Output() onTypePicked: EventEmitter<IdaiType> = new EventEmitter<IdaiType>();
+
+
+    public types: Array<IdaiType> = [];
+
+
+    ngOnChanges() {
+
+        this.types = [];
+
+        this.typesTreeList.forEach(type => {
+            this.types.push(type);
+            if (type.children) this.types = this.types.concat(type.children);
+        });
+    }
 
 
     public pickType(type: IdaiType) {
@@ -25,5 +38,27 @@ export class TypePickerComponent {
         if (type && type.isAbstract && !this.allowPickingAbstractTypes) return;
 
         this.onTypePicked.emit(type);
+    }
+
+
+    public getTypeId(type: IdaiType): string {
+
+        return type.name.toLowerCase() + (type.parentType ? '-' + type.parentType.name.toLowerCase() : '');
+    }
+
+
+    public isChildType(type: IdaiType): boolean {
+
+        return type.parentType !== undefined
+            && this.types.find(typeToCheck => typeToCheck === type.parentType) !== undefined;
+    }
+
+
+    public isParentSelected(type: IdaiType): boolean {
+
+        if (!type.parentType || !this.selectedTypes) return false;
+
+        const parentName: string = type.parentType.name;
+        return this.selectedTypes.find(typeName => typeName === parentName) !== undefined;
     }
 }

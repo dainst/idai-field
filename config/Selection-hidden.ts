79c97c3bd4beb5a341b2bed2123287ea94accc57
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2019-09-16 19:31:12 +0100

    Make selection hidden script

---
+++ config/Selection-hidden.ts
@@ -0,0 +1,37 @@
+'use strict';
+
+import {arrayEquivalent, keysAndValues, isNot, undefinedOrEmpty} from 'tsfun';
+import {pureName} from 'idai-components-2';
+
+
+const fs = require('fs');
+
+
+const projectName: string = '';
+
+const selection = JSON.parse(fs.readFileSync(projectName === '' ? 'Selection-Default.json' : 'Selection-' + projectName + '.json'));
+const hidden = JSON.parse(fs.readFileSync(projectName === '' ? 'Hidden.json' : 'Hidden-' + projectName + '.json'));
+
+
+
+keysAndValues(hidden).forEach(([hiddenTypeName, hiddenValues]) => {
+
+    // find in selection
+    keysAndValues(selection).forEach(([selectionTypeName, selectionType]) => {
+
+        const selectionTypePureName = pureName(selectionTypeName);
+        if (selectionTypePureName === hiddenTypeName) {
+
+            if (isNot(undefinedOrEmpty)(selectionType['hidden'])) {
+                console.error('hidden already exists, ', hiddenTypeName)
+            } else {
+                console.log("selectiont", selectionTypeName)
+                console.log("hidden", hiddenValues)
+
+                selectionType['hidden'] = hiddenValues;
+            }
+        }
+    });
+});
+
+fs.writeFileSync(projectName === '' ? 'Selection-Default.json' : 'Selection-' + projectName + '.json', JSON.stringify(selection, null, 2));

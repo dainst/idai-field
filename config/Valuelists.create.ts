939600dc20a1efe68123f998a53bdf30ca8d3680
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2019-09-16 11:10:44 +0100

    Add script for extracting valuelists

---
+++ config/Valuelists.create.ts
@@ -0,0 +1,80 @@
+'use strict';
+
+import {zip, ObjectCollection, arrayEquivalent} from 'tsfun';
+
+const fs = require('fs');
+
+
+const projectName: string = '';
+
+
+
+const valuelists = JSON.parse(fs.readFileSync('Valuelists.json'));
+const fields = JSON.parse(fs.readFileSync(projectName === '' ? 'Fields.json' : 'Fields-' + projectName + '.json'));
+
+
+// TODO search for intellij abbrev for console.log
+
+function pureName(s: string) { // TODO remove duplicate
+
+    return  s.includes(':') ? s.substr(0, s.indexOf(':')) : s;
+}
+
+
+function keysAndValues<T>(o: ObjectCollection<T>) { // TODO put to utils
+
+    return zip(Object.keys(o))(Object.values(o));
+}
+
+
+/**
+ * @param valuelists modified in place
+ * @param field modified in place
+ * @param newValuelistName
+ * @param newValuelistValues
+ */
+function insert(valuelists, field, newValuelistName, newValuelistValues) {
+
+    const conflictedLists = keysAndValues(valuelists).reduce((conflictedLists: any, [k, v]) => {
+        const conflicted = arrayEquivalent(v['values'])(newValuelistValues);
+        return conflicted ? conflictedLists.concat([k]) : conflictedLists
+    }, []);
+
+
+    if (conflictedLists.length === 0) {
+        valuelists[newValuelistName] = {
+            createdBy: "",
+            description: { de: "", en: "" },
+            values: newValuelistValues
+        };
+    }
+
+    delete field['valuelist'];
+    field['valuelistId'] = newValuelistName;
+}
+
+
+function generateName(typeName: string, fieldName: string, projectName: string) {
+
+    const pureTypeName = pureName(typeName);
+    return pureTypeName + '-' + fieldName + '-' + (projectName === '' ? 'default' : projectName);
+}
+
+
+keysAndValues(fields).forEach(([typeName, type]) => {
+
+    keysAndValues(type['fields']).forEach(([fieldName, field]) => {
+
+        if (field['valuelist'] && !field['valuelistId']) {
+
+            const newValuelistName = generateName(typeName, fieldName, projectName);
+
+            if (valuelists[newValuelistName]) console.error("name already exists", newValuelistName);
+            else insert(valuelists, field, newValuelistName, field['valuelist']);
+        }
+    })
+});
+
+
+fs.writeFileSync('Valuelists.out.json', JSON.stringify(valuelists, null, 2));
+fs.writeFileSync(projectName === '' ? 'Fields.out.json' : 'Fields-' + projectName + '.out.json', JSON.stringify(fields, null, 2));

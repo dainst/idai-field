15519ac5104e1330cde9ae10a0e3d2f85f6ceaf4
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2019-09-21 11:47:01 +0100

    Adjust script

--- config/Scripts/Selection-To-Configuration.ts
+++ config/Scripts/Selection-To-Configuration.ts
@@ -17,10 +17,16 @@ const selection = JSON.parse(fs.readFileSync('Selection-' + projectName + '.json
 keysAndValues(selection).forEach(([selTypeName, selType]: any) => {
     keysAndValues(custom).forEach(([cusTypeName, cusType]: any) => {
 
-        if (selTypeName === cusTypeName) selType['fields'] = cusType['fields'];
+        if (selTypeName === cusTypeName) {
+            selType['fields'] = cusType['fields'];
+            selType['commons'] = cusType['commons'];
+            selType['parent'] = cusType['parent'];
+            selType['color'] = cusType['color'];
+        }
     })
 });
 
 
 
 fs.writeFileSync('Config-' + projectName + '.json', JSON.stringify(selection, null, 2));
+fs.writeFileSync('Fields-' + projectName + '.compare.json', JSON.stringify(custom, null, 2));

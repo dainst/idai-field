f6e581cdda5d7994a6301b081de8cc35e38fdd91
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2019-09-21 11:38:20 +0100

    Convert a bunch of configs

--- config/Scripts/Selection-To-Configuration.ts
+++ config/Scripts/Selection-To-Configuration.ts
@@ -6,7 +6,7 @@ import {arrayEquivalent, keysAndValues, isNot, undefinedOrEmpty} from 'tsfun';
 const fs = require('fs');
 
 
-const projectName: string = 'Boha';
+const projectName: string = 'Kephissostal';
 
 const custom = JSON.parse(fs.readFileSync('Fields-' + projectName + '.json'));
 const selection = JSON.parse(fs.readFileSync('Selection-' + projectName + '.json'));
@@ -15,12 +15,21 @@ const selection = JSON.parse(fs.readFileSync('Selection-' + projectName + '.json
 
 
 keysAndValues(selection).forEach(([selTypeName, selType]: any) => {
-    keysAndValues(custom).forEach(([cusTypeName, cusType]: any) => {
 
-        if (selTypeName === cusTypeName) selType['fields'] = cusType['fields'];
-    })
+
+    keysAndValues(custom).forEach(([cusTypeName, cusType]: any) => {
+        if (selTypeName === cusTypeName) {
+            selType['fields'] = cusType['fields'];
+            selType['parent'] = cusType['parent'];
+            selType['color'] = cusType['color'];
+
+            delete custom[cusTypeName];
+        }
+    });
+
 });
 
 
 
 fs.writeFileSync('Config-' + projectName + '.json', JSON.stringify(selection, null, 2));
+fs.writeFileSync('Fields-' + projectName + '.compare.json', JSON.stringify(custom, null, 2));

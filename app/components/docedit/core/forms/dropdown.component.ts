b5d2d8320869d2be44548f6d70452a7981f75fa1
Thomas Kleinke <thomas.kleinke@dainst.de>
2019-09-30 12:25:03 +0200

    Use valuelist from project field for input type dropdown

--- app/components/docedit/core/forms/dropdown.component.ts
+++ app/components/docedit/core/forms/dropdown.component.ts
@@ -1,6 +1,8 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
 import {Resource} from 'idai-components-2';
 import {Helper} from './helper';
+import {FieldDefinition} from '../../../../core/configuration/model/field-definition';
+import {SettingsService} from '../../../../core/settings/settings-service';
 
 
 @Component({
@@ -11,16 +13,28 @@ import {Helper} from './helper';
 /**
  * @author Fabian Z.
  * @author Daniel de Oliveira
+ * @author Thomas Kleinke
  */
-export class DropdownComponent {
+export class DropdownComponent implements OnChanges {
 
     @Input() resource: Resource;
-    @Input() field: any;
+    @Input() field: FieldDefinition;
+
+    public valuelist: string[];
+
+
+    constructor(private settingsService: SettingsService) {}
+
+
+    ngOnChanges() {
+
+        this.valuelist = this.getValuelist(this.field);
+    }
 
 
     public notIncludedInValueList() {
 
-        return Helper.notIncludedInValueList(this.resource, this.field.name, this.field.valuelist);
+        return Helper.notIncludedInValueList(this.resource, this.field.name, this.valuelist);
     }
 
 
@@ -33,5 +47,20 @@ export class DropdownComponent {
     public deleteItem() {
 
         delete this.resource[this.field.name];
+    }
+
+
+    private getValuelist(field: FieldDefinition): string[] {
+
+        return field.valuelist
+            ? field.valuelist
+            : this.getValuelistFromProjectField(field.valuelistFromProjectField as string);
+    }
+
+
+    private getValuelistFromProjectField(fieldName: string): string[] {
+
+        const field: string[]|undefined = this.settingsService.getProjectDocument().resource[fieldName];
+        return field && Array.isArray(field) ? field : [];
     }
 }

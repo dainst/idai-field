8233cf056816f42e39802e6789a0d470e176b875
Thomas Kleinke <thomas.kleinke@dainst.de>
2019-10-07 15:51:59 +0200

    Rebuild navigation path in constructor of ResourcesComponent

--- app/components/resources/resources.component.ts
+++ app/components/resources/resources.component.ts
@@ -66,6 +66,8 @@ export class ResourcesComponent implements AfterViewChecked, OnDestroy {
         this.viewFacade.navigationPathNotifications().subscribe((_: any) => {
             this.isEditingGeometry = false;
         });
+
+        this.viewFacade.rebuildNavigationPath();
     }
 
 

9efa97844033700badaba4df77fd693424c0f94b
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2019-10-29 10:33:58 +0000

    Extract function

--- app/core/import/exec/process/merge-document.ts
+++ app/core/import/exec/process/merge-document.ts
@@ -15,29 +15,32 @@ export function mergeDocument(into: Document, additional: NewDocument): Document
     const target = clone(into);
 
     target.resource =
-        Object.keys(additional.resource)
-            .filter(isNot(includedIn(Resource.CONSTANT_FIELDS)))
-            .reduce((target: Resource, fieldName: string) => {
-
-                if (additional.resource[fieldName] === null) delete target[fieldName];
-                else target[fieldName] = clone(additional.resource[fieldName]);
-
-                return target;
-            }, target.resource);
+        overwriteOrDeleteProperties(
+            target.resource,
+            additional.resource,
+            Resource.CONSTANT_FIELDS);
 
     if (!additional.resource.relations) return target;
 
     target.resource.relations =
-        Object.keys(additional.resource.relations) // TODO factor out more generic function
-            .filter(isnt(HIERARCHICAL_RELATIONS.RECORDED_IN))
-            .reduce((target: {[relationName: string]: string[]}, relationName: string) => {
-
-                if (additional.resource.relations[relationName] === null) delete target[relationName];
-                else target[relationName] = additional.resource.relations[relationName];
-
-                return target;
-            }, target.resource.relations ? target.resource.relations : {});
-
+        overwriteOrDeleteProperties(
+            target.resource.relations ? target.resource.relations : {},
+            additional.resource.relations,
+            [HIERARCHICAL_RELATIONS.RECORDED_IN]);
 
     return target;
+}
+
+
+function overwriteOrDeleteProperties(target: any|undefined, source: any, exclusions: string[]) {
+
+    return Object.keys(source)
+        .filter(isNot(includedIn(exclusions)))
+        .reduce((target: {[propertyName: string]: string[]}, propertyName: string) => {
+
+            if (source[propertyName] === null) delete target[propertyName];
+            else target[propertyName] = source[propertyName];
+
+            return target;
+        }, target ? target : {});
 }

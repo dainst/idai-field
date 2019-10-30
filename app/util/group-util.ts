583465a83a6ffe6478e52862b9b73a411c99702d
maxhaibt <55451612+maxhaibt@users.noreply.github.com>
2019-10-30 11:20:01 +0100

    Config-al-ula with content of WES created

---
+++ app/util/group-util.ts
@@ -0,0 +1,35 @@
+/**
+ * @author Daniel de Oliveira
+ * @author Thomas Kleinke
+ */
+import {FieldDefinition} from '../configuration/model/field-definition';
+
+
+export module GroupUtil {
+
+    export function sortGroups(fields: Array<FieldDefinition>, groupName: string) {
+
+        switch(groupName) {
+            case 'stem':
+                sortGroup(fields, ['identifier', 'shortDescription',
+                    'processor', 'campaign', 'description', 'diary', 'date', 'beginningDate', 'endDate']);
+                break;
+            case 'dimension':
+                sortGroup(fields, ['dimensionHeight',
+                    'dimensionLength', 'dimensionWidth', 'dimensionPerimeter',
+                    'dimensionDiameter', 'dimensionThickness', 'dimensionVerticalExtent', 'dimensionOther']);
+                break;
+        }
+    }
+
+
+    /**
+     * Fields not defined via 'order' are not considered
+     */
+    function sortGroup(fields: Array<FieldDefinition>, order: string[]) {
+
+        fields.sort((field1: FieldDefinition, field2: FieldDefinition) => {
+           return order.indexOf(field1.name) > order.indexOf(field2.name) ? 1 : -1;
+        });
+    }
+}

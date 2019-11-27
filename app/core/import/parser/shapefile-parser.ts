5ad66617089164cc647acddd3126c60d35d66f58
Thomas Kleinke <thomas.kleinke@dainst.de>
2019-11-27 10:37:04 +0100

    Clean up ShapefileParser code

--- app/core/import/parser/shapefile-parser.ts
+++ app/core/import/parser/shapefile-parser.ts
@@ -1,8 +1,7 @@
-import {Observable, Observer} from 'rxjs';
-import {NewDocument, Document} from 'idai-components-2';
 import {NativeJsonlParser} from './native-jsonl-parser';
 import {ParserErrors} from './parser-errors';
 import {Parser} from './parser';
+
 
 /**
  * @author Thomas Kleinke
@@ -11,13 +10,9 @@ export module ShapefileParser {
 
     export const parse: Parser = (content: string) => {
 
-        return new Promise((resolve: Function, reject: Function)=> {
+        return new Promise((resolve: Function, reject: Function) => {
             try {
-
-                resolve(NativeJsonlParser.parse(content))
-
-                    // .forEach((document: Document) => observer.next(document));
-
+                resolve(NativeJsonlParser.parse(content));
             } catch (err) {
                 reject([ParserErrors.SHAPEFILE_GENERIC]);
             }

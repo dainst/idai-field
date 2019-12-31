d79ce2dc1a6c618384c619d859e04e3634d516cf
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2019-12-31 21:49:55 +0000

    Add assertions to mergeResource

---
+++ app/core/import/exec/process/has-empty-associatives.ts
@@ -0,0 +1,15 @@
+import {values, isEmpty, is, isArray, isObject} from 'tsfun';
+
+
+/**
+ * @param struct
+ * @author Daniel de Oliveira
+ */
+export function hasEmptyAssociatives(struct: any): boolean {
+
+    if (!isArray(struct) && !isObject(struct)) return false;
+    if (isEmpty(struct)) return true;
+    return values(struct)
+        .map(hasEmptyAssociatives)
+        .some(is(true));
+}
---
+++ test/unit/core/import/exec/process/has-empty-associatives.spec.ts
@@ -0,0 +1,34 @@
+import {hasEmptyAssociatives} from '../../../../../../app/core/import/exec/process/has-empty-associatives';
+
+
+describe('hasEmptyAssociatives', () => {
+
+    it('empty array', () => {
+
+        expect(hasEmptyAssociatives([])).toBe(true);
+    });
+
+
+    it('empty object', () => {
+
+        expect(hasEmptyAssociatives({})).toBe(true);
+    });
+
+
+    it('filled array', () => {
+
+        expect(hasEmptyAssociatives([1])).toBe(false);
+    });
+
+
+    it('filled object', () => {
+
+        expect(hasEmptyAssociatives({ a: 1 })).toBe(false);
+    });
+
+
+    it('array - object - empty array', () => {
+
+        expect(hasEmptyAssociatives([{a: []}])).toBe(true);
+    });
+});

c353891ab415e8e2d0e2fd045de904d4f0406c7d
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-01-14 11:38:34 +0100

    Show isLike relation in document view

--- app/widgets/fields-view.component.ts
+++ app/widgets/fields-view.component.ts
@@ -8,7 +8,7 @@ import {GroupUtil} from '../core/util/group-util';
 import {GROUP_NAME, Name, ResourceId} from '../c';
 import {isBoolean} from '../utils';
 import {UtilTranslations} from '../core/util/util-translations';
-import {HIERARCHICAL_RELATIONS, POSITION_RELATIONS, TIME_RELATIONS} from '../core/model/relation-constants';
+import {HIERARCHICAL_RELATIONS, POSITION_RELATIONS, TIME_RELATIONS, IS_LIKE} from '../core/model/relation-constants';
 import {ProjectConfiguration} from '../core/configuration/project-configuration';
 import {IdaiType} from '../core/configuration/model/idai-type';
 import {RelationDefinition} from '../core/configuration/model/relation-definition';
@@ -251,6 +251,7 @@ export class FieldsViewComponent implements OnChanges {
             let group: string|undefined = undefined;
             if (TIME_RELATIONS.ALL.includes(relation.name)) group = 'time';
             if (POSITION_RELATIONS.ALL.includes(relation.name)) group = 'position';
+            if (relation.name === IS_LIKE) group = 'properties';
             if (!group) continue;
 
             this.relations[group].push({

38b3a8d4ab972a8a018848a076c908afd0992b65
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-01-21 11:36:46 +0000

    Update typescript

---
+++ test/e2e/config/protractor.d.ts
@@ -0,0 +1,10 @@
+// Note: This stub exists to override Protractor types which are incompatible with TS 3.7 as of 5.4.2 and 6.0.0
+declare module 'protractor' {
+    let browser: any;
+    let element: any;
+    let by: any;
+    let ExpectedConditions: any;
+    let until: any;
+    let Key: any;
+    let protractor: any;
+}

9e511d4bfc517cce19d48cc31fc0efd54ddf1a44
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-01-28 17:22:08 +0000

    Add test case

--- test/unit/core/images/row/image-row.spec.ts
+++ test/unit/core/images/row/image-row.spec.ts
@@ -22,4 +22,33 @@ describe('ImageRow', () => {
         expect(nextPageResult.newImageIds).toEqual(['i1']);
         expect(nextPageResult.scrollWidth).toBe(0);
     });
+
+
+    it('second page', () => {
+
+        const imageDocuments = [
+            {
+                resource: {
+                    type: 'Drawing', id: 'i1', identifier: 'I1', width: 200,
+                    height: 100, relations: { depicts: []},
+                    shortDescription: 'S1', originalFilename: 'blub1'
+                }
+            },
+            {
+                resource: {
+                    type: 'Drawing', id: 'i2', identifier: 'I2', width: 200,
+                    height: 100, relations: { depicts: []},
+                    shortDescription: 'S2', originalFilename: 'blub2'
+                }
+            }
+        ] as unknown as Array<ImageDocument>;
+
+        const imageRow = new ImageRow(300, 100, 200, imageDocuments);
+
+        const firstPageResult = imageRow.nextPage();
+        expect(firstPageResult.newImageIds).toEqual(['i1', 'i2']);
+        const secondPageResult = imageRow.nextPage();
+        expect(secondPageResult.newImageIds).toEqual([]);
+        expect(secondPageResult.scrollWidth).toBe(200);
+    });
 });

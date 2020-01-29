97b6f65532125aba03dd814a8664e84b15e10552
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-01-29 10:50:55 +0100

    Adjust second image row test & remove first one

--- test/unit/core/images/row/image-row.spec.ts
+++ test/unit/core/images/row/image-row.spec.ts
@@ -4,25 +4,6 @@ import {ImageRow} from '../../../../../app/core/images/row/image-row';
 
 describe('ImageRow', () => {
 
-    it('show a page', () => {
-
-        const imageDocuments = [
-            {
-                resource: {
-                    type: 'Drawing', id: 'i1', identifier: 'I1', width: 100,
-                    height: 100, relations: { depicts: [] }
-                }
-            }
-        ] as unknown as Array<ImageDocument>;
-
-        const imageRow = new ImageRow(1000, 100, 100, imageDocuments);
-
-        const nextPageResult = imageRow.nextPage();
-        expect(nextPageResult.newImageIds).toEqual(['i1']);
-        expect(nextPageResult.scrollWidth).toBe(0);
-    });
-
-
     it('switch pages', () => {
 
         const imageDocuments = [
@@ -50,17 +31,17 @@ describe('ImageRow', () => {
 
         const firstPageResult = imageRow.nextPage();
         expect(firstPageResult.newImageIds).toEqual(['i1', 'i2']);
-        expect(firstPageResult.scrollWidth).toBe(0);
+        expect(firstPageResult.positionLeft).toBe(0);
         // Show i1 and i2; i2 is not shown completely
 
         const secondPageResult = imageRow.nextPage();
         expect(secondPageResult.newImageIds).toEqual(['i3']);
-        expect(secondPageResult.scrollWidth).toBe(200);
+        expect(secondPageResult.positionLeft).toBe(-200);
         // Show i2 and i3; i3 is not shown completely
 
         const thirdPageResult = imageRow.nextPage();
         expect(thirdPageResult.newImageIds).toEqual([]);
-        expect(thirdPageResult.scrollWidth).toBe(250);
+        expect(thirdPageResult.positionLeft).toBe(-450);
         // Show i3 (no new images)
     });
 });

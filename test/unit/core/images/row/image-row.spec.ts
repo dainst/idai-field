542ea6c6101b4ef08533e1a7aa2fe3c760e962c4
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-01-29 10:28:37 +0100

    Extend test

--- test/unit/core/images/row/image-row.spec.ts
+++ test/unit/core/images/row/image-row.spec.ts
@@ -4,14 +4,13 @@ import {ImageRow} from '../../../../../app/core/images/row/image-row';
 
 describe('ImageRow', () => {
 
-    it('first page', () => {
+    it('show a page', () => {
 
         const imageDocuments = [
             {
                 resource: {
                     type: 'Drawing', id: 'i1', identifier: 'I1', width: 100,
-                    height: 100, relations: { depicts: []},
-                    shortDescription: 'S1', originalFilename: 'blub'
+                    height: 100, relations: { depicts: [] }
                 }
             }
         ] as unknown as Array<ImageDocument>;
@@ -24,31 +23,44 @@ describe('ImageRow', () => {
     });
 
 
-    it('second page', () => {
+    it('switch pages', () => {
 
         const imageDocuments = [
             {
                 resource: {
-                    type: 'Drawing', id: 'i1', identifier: 'I1', width: 200,
-                    height: 100, relations: { depicts: []},
-                    shortDescription: 'S1', originalFilename: 'blub1'
+                    type: 'Drawing', id: 'i1', identifier: 'I1', width: 200, height: 100,
+                    relations: { depicts: [] }
                 }
             },
             {
                 resource: {
-                    type: 'Drawing', id: 'i2', identifier: 'I2', width: 200,
-                    height: 100, relations: { depicts: []},
-                    shortDescription: 'S2', originalFilename: 'blub2'
+                    type: 'Drawing', id: 'i2', identifier: 'I2', width: 250, height: 100,
+                    relations: { depicts: [] }
+                }
+            },
+            {
+                resource: {
+                    type: 'Drawing', id: 'i3', identifier: 'I3', width: 150, height: 100,
+                    relations: { depicts: [] }
                 }
             }
         ] as unknown as Array<ImageDocument>;
 
-        const imageRow = new ImageRow(300, 100, 200, imageDocuments);
+        const imageRow = new ImageRow(300, 100, 300, imageDocuments);
 
         const firstPageResult = imageRow.nextPage();
         expect(firstPageResult.newImageIds).toEqual(['i1', 'i2']);
+        expect(firstPageResult.scrollWidth).toBe(0);
+        // Show i1 and i2; i2 is not shown completely
+
         const secondPageResult = imageRow.nextPage();
-        expect(secondPageResult.newImageIds).toEqual([]); // no new images
-        expect(secondPageResult.scrollWidth).toBe(200);   // show the second image of the first page as first image now
+        expect(secondPageResult.newImageIds).toEqual(['i3']);
+        expect(secondPageResult.scrollWidth).toBe(200);
+        // Show i2 and i3; i3 is not shown completely
+
+        const thirdPageResult = imageRow.nextPage();
+        expect(thirdPageResult.newImageIds).toEqual([]);
+        expect(thirdPageResult.scrollWidth).toBe(250);
+        // Show i3 (no new images)
     });
 });

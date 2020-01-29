d1e646ee970b2b6aa06cc6b3b68a3c00fc79a9a3
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-01-29 09:36:53 +0000

    Fix path of app.css target

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ gulp.task('convert-sass', () => {
             ], precision: 8
         }))
         .pipe(concat('app.css'))
-        .pipe(gulp.dest('app'));
+        .pipe(gulp.dest('app/components/'));
 });
 
 gulp.task('copy-fonts-convert-sass', ['convert-sass'], () => {

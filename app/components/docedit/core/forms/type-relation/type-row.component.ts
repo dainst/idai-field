4674c4fba7d722fcc5412050872f15f622eafd8a
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-01-29 22:29:17 +0000

    Show main image if present

--- app/components/docedit/core/forms/type-relation/type-row.component.ts
+++ app/components/docedit/core/forms/type-relation/type-row.component.ts
@@ -1,5 +1,8 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, Output} from '@angular/core';
 import {FieldDocument} from 'idai-components-2';
+import {ModelUtil} from '../../../../../core/model/model-util';
+import {ReadImagestore} from '../../../../../core/images/imagestore/read-imagestore';
+import {FieldReadDatastore} from '../../../../../core/datastore/field/field-read-datastore';
 
 
 @Component({
@@ -12,9 +15,26 @@ import {FieldDocument} from 'idai-components-2';
  *
  * @author Daniel de Oliveira
  */
-export class TypeRowComponent {
+export class TypeRowComponent implements OnChanges {
+
+    public mainThumbnailUrl: string|undefined;
 
     @Input() document: FieldDocument;
     @Input() imageIds: string[];
     @Output() onSelect: EventEmitter<void> = new EventEmitter<void>();
+
+    constructor(private imagestore: ReadImagestore) {}
+
+    async ngOnChanges() {
+
+        if (this.document) this.mainThumbnailUrl = await this.getMainThumbnailUrl(this.document);
+    }
+
+    private async getMainThumbnailUrl(document: FieldDocument): Promise<string|undefined> {
+
+        const mainImageId: string | undefined = ModelUtil.getMainImageId(document);
+        if (!mainImageId) return undefined;
+
+        return await this.imagestore.read(mainImageId, false, true);
+    }
 }

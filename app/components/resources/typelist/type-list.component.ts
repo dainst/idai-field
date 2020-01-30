9c77d9e905436fac93160943266fd6ade4f8e411
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-01-30 16:55:10 +0100

    Add context menu to type list

--- app/components/resources/typelist/type-list.component.ts
+++ app/components/resources/typelist/type-list.component.ts
@@ -4,12 +4,15 @@ import {BaseList} from '../base-list';
 import {ResourcesComponent} from '../resources.component';
 import {ViewFacade} from '../../../core/resources/view/view-facade';
 import {Loading} from '../../widgets/loading';
+import {ContextMenu} from '../widgets/context-menu';
+import {ContextMenuAction} from '../widgets/context-menu.component';
 
 
 @Component({
     selector: 'type-list',
     moduleId: module.id,
     templateUrl: './type-list.html',
+    host: { '(window:contextmenu)': 'handleClick($event, true)' }
 })
 /**
  * @author Daniel de Oliveira
@@ -19,10 +22,53 @@ export class TypeListComponent extends BaseList {
 
     @Input() documents: Array<FieldDocument>;
 
+    public contextMenu: ContextMenu = new ContextMenu();
+
+
     constructor(resourcesComponent: ResourcesComponent,
                 viewFacade: ViewFacade,
                 loading: Loading) {
 
         super(resourcesComponent, viewFacade, loading);
+
+        resourcesComponent.listenToClickEvents().subscribe(event => this.handleClick(event));
+    }
+
+
+    public async performContextMenuAction(action: ContextMenuAction) {
+
+        if (!this.contextMenu.document) return;
+        const document: FieldDocument = this.contextMenu.document;
+
+        this.contextMenu.close();
+
+        switch (action) {
+            case 'edit':
+                await this.resourcesComponent.editDocument(document);
+                break;
+            case 'delete':
+                await this.resourcesComponent.deleteDocument(document);
+                break;
+        }
+    }
+
+
+    public handleClick(event: any, rightClick: boolean = false) {
+
+        if (!this.contextMenu.position) return;
+
+        let target = event.target;
+        let inside: boolean = false;
+
+        do {
+            if (target.id === 'context-menu'
+                || (rightClick && target.id && target.id === 'type-list')) {
+                inside = true;
+                break;
+            }
+            target = target.parentNode;
+        } while (target);
+
+        if (!inside) this.contextMenu.close();
     }
 }

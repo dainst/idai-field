07b7b0469b3629dad9f6ba634af1b1cee54d5460
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-02-07 12:33:28 +0100

    Simplify

--- app/core/datastore/cached/get-sorted-ids.ts
+++ app/core/datastore/cached/get-sorted-ids.ts
@@ -1,6 +1,8 @@
-import {IndexItem, SimpleIndexItem} from '../index/index-item';
+import {on, is, isNot, undefinedOrEmpty, to} from 'tsfun';
 import {Query} from 'idai-components-2';
+import {IndexItem, SimpleIndexItem} from '../index/index-item';
 import {SortUtil} from '../../util/sort-util';
+import {ResourceId} from '../../constants';
 
 // @author Daniel de Oliveira
 // @author Thomas Kleinke
@@ -10,14 +12,13 @@ import {SortUtil} from '../../util/sort-util';
  * @param indexItems // TODO review typing: must be Array<IndexItem> if exactMatchFirst
  * @param query
  */
-export function getSortedIds(indexItems: Array<SimpleIndexItem>, query: Query): string[] {
+export function getSortedIds(indexItems: Array<SimpleIndexItem>, query: Query): Array<ResourceId> {
 
     indexItems = generateOrderedResultList(indexItems);
 
-    if (query.sort === 'exactMatchFirst' && query.q && query.q.length > 0) {
+    if (query.sort === 'exactMatchFirst' && isNot(undefinedOrEmpty)(query.q)) {
 
-        const exactMatch: SimpleIndexItem | undefined
-            = indexItems.find((indexItem: any) => indexItem['identifier'] === query.q);
+        const exactMatch = indexItems.find(on('identifier', is(query.q)));
 
         if (exactMatch) {
             indexItems.splice(indexItems.indexOf(exactMatch), 1);
@@ -25,7 +26,7 @@ export function getSortedIds(indexItems: Array<SimpleIndexItem>, query: Query): 
         }
     }
 
-    return indexItems.map(indexItem => indexItem.id);
+    return indexItems.map(to('id'));
 }
 
 

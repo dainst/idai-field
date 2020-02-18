1ca79ae147725b96f0f6a494f0151c4ab649c55d
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-02-18 12:04:47 +0100

    Switch rounding function in ImageWidthCalculator

--- app/core/images/row/image-width-calculator.ts
+++ app/core/images/row/image-width-calculator.ts
@@ -6,7 +6,7 @@ export module ImageWidthCalculator {
     export function computeWidth(imageWidth: number, imageHeight: number, targetHeight: number,
                                  maxWidth: number): number {
 
-        const targetWidth: number = Math.ceil(Math.min((targetHeight / imageHeight), 1) * imageWidth);
+        const targetWidth: number = Math.round(Math.min((targetHeight / imageHeight), 1) * imageWidth);
 
         return Math.min(targetWidth, maxWidth);
     }

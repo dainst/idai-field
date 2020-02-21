2095d430cbea28e7b1a88c286edb137fab9a496d
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-02-21 20:01:56 +0000

    Make separate test files for util functions

---
+++ test/unit/util/utils/into-obj.spec.ts
@@ -0,0 +1,49 @@
+import {intoObj} from '../../../../app/core/util/utils';
+
+
+describe('intoObj', () => {
+
+    it('intoObj - number key', () =>
+        expect(
+
+            intoObj('key', 'val')
+            ({}, {key: 2, val: 7}))
+
+            .toEqual({2: 7}));
+
+
+    it('intoObj - string key', () =>
+        expect(
+
+            intoObj('key', 'val')
+            ({}, {key: '2', val: 7}))
+
+            .toEqual({2: 7}));
+
+
+    it('intoObj - with reduce', () =>
+        expect(
+
+            [{key: 2, val: 7}, {key: 3, val: 8}]
+                .reduce(intoObj('key', 'val'), {}))
+
+            .toEqual({2: 7, 3: 8}));
+
+
+    it('intoObj - missing key', () =>
+        expect(
+
+            [{key: 2, val: 7}, {val: 8}]
+                .reduce(intoObj('key', 'val'), {}))
+
+            .toEqual({2: 7}));
+
+
+    it('intoObj - missing val', () =>
+        expect(
+
+            [{key: 2, val: 7}, {key: 8}]
+                .reduce(intoObj('key', 'val'), {}))
+
+            .toEqual({2: 7, 8: undefined}));
+});

cd2f518702d5c967ba49f75e582c6b6443b243b4
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-02-27 11:37:34 +0100

    Fix import

--- app/core/util/type-images-util.ts
+++ app/core/util/type-images-util.ts
@@ -1,11 +1,10 @@
 import {isDefined, filter} from 'tsfun';
 import {map as asyncMap, flow as asyncFlow} from 'tsfun/async';
-import {FieldDocument} from 'idai-components-2';
+import {FieldDocument, Query} from 'idai-components-2';
 import {FieldDocumentFindResult, FieldReadDatastore} from '../datastore/field/field-read-datastore';
 import {ResourceId} from '../constants';
 import {ModelUtil} from '../model/model-util';
 import {ImageRowItem, PLACEHOLDER} from '../../components/image/row/image-row.component';
-import {Query} from 'idai-components-2/index';
 
 
 /**

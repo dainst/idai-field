d9611df92a9b01f25db60284f4e33eaee766e337
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-02-28 20:57:15 +0000

    Remove unused test

--- test/unit/util/utils/take-or-make.spec.ts
+++ test/unit/util/utils/take-or-make.spec.ts
@@ -17,21 +17,4 @@ describe('takeOrMake', () => {
         takeOrMake(obj, 'a.b.c', []);
         expect(obj['a']['b']['c']).toEqual('a')
     });
-
-
-    // xit('reimplement takeOrMake', () => { TODO review, maybe remove
-    //
-    //     const o1: any = {a: {b: {c: 'd'}}};
-    //
-    //     const takeOrMake = <T>(o: T, path: string, alternative: any) =>
-    //         setOn(o, path)(getOn(path , alternative)(o));
-    //
-    //     takeOrMake(o1, 'a.b.c', undefined); // take
-    //     expect(o1['a']['b']['c']).toBe('d');
-    //
-    //     const o2: any = {a: {b: {c: undefined}}};
-    //
-    //     takeOrMake(o2, 'a.b.c', 'd'); // make
-    //     expect(o2['a']['b']['c']).toBe('d');
-    // });
 });

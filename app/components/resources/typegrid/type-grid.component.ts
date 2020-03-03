70d5a569abbb3a338b44473373aca6677b967024
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-03-03 12:25:51 +0100

    Add context menu to type grid

--- app/components/resources/typegrid/type-grid.component.ts
+++ app/components/resources/typegrid/type-grid.component.ts
@@ -13,12 +13,15 @@ import {ImageRowItem, PLACEHOLDER} from '../../image/row/image-row.component';
 import {ReadImagestore} from '../../../core/images/imagestore/read-imagestore';
 import {ImageReadDatastore} from '../../../core/datastore/field/image-read-datastore';
 import {ImageWidthCalculator} from '../../../core/images/row/image-width-calculator';
+import {ContextMenu} from '../widgets/context-menu';
+import {ContextMenuAction} from '../widgets/context-menu.component';
 
 
 @Component({
     selector: 'type-grid',
     moduleId: module.id,
-    templateUrl: './type-grid.html'
+    templateUrl: './type-grid.html',
+    host: { '(window:contextmenu)': 'handleClick($event, true)' }
 })
 /**
  * @author Thomas Kleinke
@@ -28,6 +31,7 @@ export class TypeGridComponent extends BaseList implements OnChanges {
     @Input() documents: Array<FieldDocument>;
 
     public images: { [resourceId: string]: Array<SafeResourceUrl> } = {};
+    public contextMenu: ContextMenu = new ContextMenu();
 
 
     constructor(private fieldDatastore: FieldReadDatastore,
@@ -38,6 +42,8 @@ export class TypeGridComponent extends BaseList implements OnChanges {
                 loading: Loading) {
 
         super(resourcesComponent, viewFacade, loading);
+
+        resourcesComponent.listenToClickEvents().subscribe(event => this.handleClick(event));
     }
 
 
@@ -56,6 +62,47 @@ export class TypeGridComponent extends BaseList implements OnChanges {
     public async open(document: FieldDocument) {
 
         await this.viewFacade.moveInto(document);
+    }
+
+
+    public async performContextMenuAction(action: ContextMenuAction) {
+
+        if (!this.contextMenu.document) return;
+        const document: FieldDocument = this.contextMenu.document;
+
+        this.contextMenu.close();
+
+        switch (action) {
+            case 'edit':
+                await this.resourcesComponent.editDocument(document);
+                break;
+            case 'move':
+                await this.resourcesComponent.moveDocument(document);
+                break;
+            case 'delete':
+                await this.resourcesComponent.deleteDocument(document);
+                break;
+        }
+    }
+
+
+    public handleClick(event: any, rightClick: boolean = false) {
+
+        if (!this.contextMenu.position) return;
+
+        let target = event.target;
+        let inside: boolean = false;
+
+        do {
+            if (target.id === 'context-menu'
+                || (rightClick && target.id && target.id.startsWith('type-grid-element'))) {
+                inside = true;
+                break;
+            }
+            target = target.parentNode;
+        } while (target);
+
+        if (!inside) this.contextMenu.close();
     }
 
 

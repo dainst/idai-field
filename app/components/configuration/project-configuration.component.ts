9a6d392cfd96cec70416aa678492cc9707b376f1
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-03-17 19:04:44 +0100

    Group fields in project conf component

--- app/components/configuration/project-configuration.component.ts
+++ app/components/configuration/project-configuration.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {ProjectConfiguration} from '../../core/configuration/project-configuration';
 import {IdaiType} from '../../core/configuration/model/idai-type';
+import {map, set, flow, to, on, is, filter} from 'tsfun';
+import {FieldDefinition} from '../../core/configuration/model/field-definition';
 
 @Component({
     moduleId: module.id,
@@ -13,6 +15,7 @@ export class ProjectConfigurationComponent {
 
     public typesTreeList: Array<IdaiType>;
     public selectedType: IdaiType;
+    public selectedGroup: string = 'stem';
 
 
     constructor(public projectConfiguration: ProjectConfiguration) {
@@ -24,10 +27,23 @@ export class ProjectConfigurationComponent {
 
 
     public selectType(type: IdaiType) {
-        
+
         this.selectedType = type;
     }
 
 
-    public getVisibleFields = (type: IdaiType) => type.fields.filter(field => field.visible);
+    public getGroups = (type: IdaiType): string[] =>
+        flow(
+            type.fields,
+            map(to(FieldDefinition.GROUP)),
+            set
+        );
+
+
+    public getVisibleFields = (type: IdaiType): FieldDefinition[] =>
+        flow(
+            type.fields,
+            filter(on(FieldDefinition.VISIBLE, is(true))),
+            filter(on(FieldDefinition.GROUP, is(this.selectedGroup)))
+        );
 }

6fa8018397f772e1b99476443fda9b45c1fbdaed
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-03-18 15:09:58 +0100

    Sort groups in project conf component

--- app/components/configuration/project-configuration.component.ts
+++ app/components/configuration/project-configuration.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {ProjectConfiguration} from '../../core/configuration/project-configuration';
 import {IdaiType} from '../../core/configuration/model/idai-type';
-import {map, set, flow, to, on, is, filter} from 'tsfun';
+import {map, set, flow, to, on, is, filter, intersect} from 'tsfun';
 import {FieldDefinition} from '../../core/configuration/model/field-definition';
 
 @Component({
@@ -15,29 +15,34 @@ export class ProjectConfigurationComponent {
 
     public typesTreeList: Array<IdaiType>;
     public selectedType: IdaiType;
-    public selectedGroup: string = 'stem';
-
+    public selectedGroup: string;
+    public availableGroups = [
+        'stem',
+        'identification',
+        undefined,
+        'child',
+        'dimension',
+        'position',
+        'time'
+    ];
 
     constructor(public projectConfiguration: ProjectConfiguration) {
 
         this.typesTreeList = projectConfiguration.getTypesList()
             .filter(type => !type.parentType);
-        this.selectedType = this.typesTreeList[0];
+        this.selectType(this.typesTreeList[0]);
     }
 
 
     public selectType(type: IdaiType) {
 
         this.selectedType = type;
+        this.selectedGroup = this.getGroups(type)[0];
     }
 
 
-    public getGroups = (type: IdaiType): string[] =>
-        flow(
-            type.fields,
-            map(to(FieldDefinition.GROUP)),
-            set
-        );
+    public getGroups = (type: IdaiType): any[] =>
+        intersect(this.availableGroups)(type.fields.map(to(FieldDefinition.GROUP)));
 
 
     public getVisibleFields = (type: IdaiType): FieldDefinition[] =>
@@ -46,4 +51,5 @@ export class ProjectConfigurationComponent {
             filter(on(FieldDefinition.VISIBLE, is(true))),
             filter(on(FieldDefinition.GROUP, is(this.selectedGroup)))
         );
+
 }

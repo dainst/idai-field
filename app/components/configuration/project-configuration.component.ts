d3e22dc6e74f6f18b7224483707ca6c489515f78
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-03-18 16:31:07 +0100

    Make sure identifier and shortDescription are visible

--- app/components/configuration/project-configuration.component.ts
+++ app/components/configuration/project-configuration.component.ts
@@ -1,8 +1,10 @@
 import {Component} from '@angular/core';
 import {ProjectConfiguration} from '../../core/configuration/project-configuration';
 import {IdaiType} from '../../core/configuration/model/idai-type';
-import {map, set, flow, to, on, is, filter, intersect} from 'tsfun';
+import {map, set, flow, to, on, is, filter, intersect, append, includedIn} from 'tsfun';
 import {FieldDefinition} from '../../core/configuration/model/field-definition';
+import { GroupUtil } from '../../core/configuration/group-util';
+import { debugId } from '../../core/util/utils';
 
 @Component({
     moduleId: module.id,
@@ -12,6 +14,8 @@ import {FieldDefinition} from '../../core/configuration/model/field-definition';
  * @author Sebastian Cuy
  */
 export class ProjectConfigurationComponent {
+
+    public OVERRIDE_VISIBLE_FIELDS = ['identifier', 'shortDescription'];
 
     public typesTreeList: Array<IdaiType>;
     public selectedType: IdaiType;
@@ -45,11 +49,14 @@ export class ProjectConfigurationComponent {
         intersect(this.availableGroups)(type.fields.map(to(FieldDefinition.GROUP)));
 
 
-    public getVisibleFields = (type: IdaiType): FieldDefinition[] =>
-        flow(
-            type.fields,
-            filter(on(FieldDefinition.VISIBLE, is(true))),
-            filter(on(FieldDefinition.GROUP, is(this.selectedGroup)))
-        );
+    public getVisibleFields(type: IdaiType): FieldDefinition[] {
+
+        const groupFields = type.fields.filter(on(FieldDefinition.GROUP, is(this.selectedGroup)));
+        let fields = groupFields
+            .filter(on(FieldDefinition.VISIBLE, is(true)))
+            .concat(groupFields.filter(on(FieldDefinition.NAME, includedIn(this.OVERRIDE_VISIBLE_FIELDS))));
+        GroupUtil.sortGroups(fields, this.selectedGroup);
+        return fields;
+    }
 
 }

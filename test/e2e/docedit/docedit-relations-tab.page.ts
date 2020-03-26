e76038c201be7abcd3640939aba05a8d93f2f2a6
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-03-26 10:55:45 +0100

    Adjust DocedtRelationsTabPage code style to common e2e test style

--- test/e2e/docedit/docedit-relations-tab.page.ts
+++ test/e2e/docedit/docedit-relations-tab.page.ts
@@ -1,9 +1,10 @@
 import {browser, protractor, element, by} from 'protractor';
-import {DoceditPage} from "./docedit.page";
+import {DoceditPage} from './docedit.page';
 
 const common = require('../common.js');
 const delays = require('../config/delays');
 const EC = protractor.ExpectedConditions;
+
 
 /**
  * @author Daniel de Oliveira
@@ -13,20 +14,20 @@ export class DoceditRelationsTabPage {
 
     // click
 
-    public static clickChooseRelationSuggestion = function(groupName, pickerIndex, suggestionIndex) {
+    public static clickChooseRelationSuggestion(groupName, pickerIndex, suggestionIndex) {
 
         browser.wait(EC.visibilityOf(element.all(by.css('.suggestion')).get(suggestionIndex)), delays.ECWaitTime);
         element(by.id(groupName)).all(by.css('.suggestion')).get(suggestionIndex).click();
     };
 
 
-    public static clickAddRelationForGroupWithIndex = function(groupName) {
+    public static clickAddRelationForGroupWithIndex(groupName) {
 
         element(by.id(groupName)).element(by.css('.circular-button.add-relation')).click();
     };
 
 
-    public static clickRelationDeleteButtonByIndices = function(groupName, pickerIndex = 0) {
+    public static clickRelationDeleteButtonByIndices(groupName, pickerIndex = 0) {
 
         return this.getRelationElByName(groupName, pickerIndex).element(by.css('.delete-relation')).click();
     };
@@ -34,7 +35,7 @@ export class DoceditRelationsTabPage {
 
     // get text
 
-    public static getRelationButtonText = function(groupName, pickerIndex = 0, relationIndex = 0) {
+    public static getRelationButtonText(groupName, pickerIndex = 0, relationIndex = 0) {
 
         DoceditPage.clickGotoTimeTab();
         return this.getRelationElByName(groupName, pickerIndex)
@@ -44,7 +45,7 @@ export class DoceditRelationsTabPage {
 
     // elements
 
-    public static getRelationElByName = function(groupName, pickerIndex) {
+    public static getRelationElByName(groupName, pickerIndex) {
 
         return element(by.id(groupName)).all(by.id('relation-picker')).get(pickerIndex);
     };
@@ -52,7 +53,7 @@ export class DoceditRelationsTabPage {
 
     // type in
 
-    public static typeInRelationByIndices = function(groupName, pickerIndex, input) {
+    public static typeInRelationByIndices(groupName, pickerIndex, input) {
 
         common.typeIn(element(by.id(groupName)).element(by.tagName('input')), input);
     };

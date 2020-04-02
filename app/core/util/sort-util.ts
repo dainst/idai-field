92562633c5f0536df5b1d41fa5a3309020d77bd2
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-04-02 14:02:10 +0100

    Convert to module

--- app/core/util/sort-util.ts
+++ app/core/util/sort-util.ts
@@ -3,7 +3,7 @@
  *
  * @author Sebastian Cuy
  */
-export class SortUtil {
+export module SortUtil {
 
     /**
      * Compares two string alphanumerically so that
@@ -13,10 +13,10 @@ export class SortUtil {
      * @param b another string
      * @returns {number} -1 if a < b, 1 if a > b, 0 if a == b
      */
-    public static alnumCompare(a: string, b: string): number {
+    export function alnumCompare(a: string, b: string): number {
 
-        let arrayA = SortUtil.makeAlNumArray(a);
-        let arrayB = SortUtil.makeAlNumArray(b);
+        let arrayA = makeAlNumArray(a);
+        let arrayB = makeAlNumArray(b);
 
         for (let i = 0; i < arrayA.length; i++) {
             // a is longer than b
@@ -57,7 +57,7 @@ export class SortUtil {
      * @param b
      * @returns {number}
      */
-    public static compare(a: any, b: any): number {
+    export function compare(a: any, b: any): number {
 
         if (a > b)
             return 1;
@@ -73,20 +73,18 @@ export class SortUtil {
      * @param compareFunction the compare function to wrap
      * @returns {(a:any, b:any)=>number} the new compare function
      */
-    public static compareDescending(compareFunction: Function) {
+    export function compareDescending(compareFunction: Function) {
 
-        return (a: any, b: any) => {
-            return compareFunction(a, b) * -1;
-        };
+        return (a: any, b: any) => compareFunction(a, b) * -1;
     }
 
 
     // split string and convert numbers to be able to sort alphabetic
     // and numeric parts of the string separately
-    private static makeAlNumArray(s: any) {
+    function makeAlNumArray(s: any) {
 
-        return s.split(/(\d+)/)
+        return s
+            .split(/(\d+)/)
             .map((s: any) => isNaN(s) ? s : parseInt(s));
     }
-
 }

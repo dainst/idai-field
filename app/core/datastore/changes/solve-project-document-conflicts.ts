322bc67d1767ed197a26d2876d6d9fd9def59cb3
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-04-30 12:53:33 +0200

    Adjust to code style rules

--- app/core/datastore/changes/solve-project-document-conflicts.ts
+++ app/core/datastore/changes/solve-project-document-conflicts.ts
@@ -2,7 +2,7 @@ import {assoc, assocOn, to, lookup, flow, map, filter, isDefined, union as tsfun
     compose, dissoc, append, Pair, left, right} from 'tsfun';
 import {Document, Resource} from 'idai-components-2';
 import {RevisionId} from '../../constants';
-import {dissocIndices, last2, replaceLastPair, sortRevisionsByLastModified,} from '../helpers';
+import {dissocIndices, last2, replaceLastPair, sortRevisionsByLastModified} from '../helpers';
 import RESOURCE = Document.RESOURCE;
 import {clone} from '../../util/object-util';
 import {withDissoc} from '../../util/utils';

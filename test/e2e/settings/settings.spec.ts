7053dc940b042c1386be4c5f441083f8f239d801
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-05-20 15:28:00 +0200

    Remove settings e2e test

--- test/e2e/settings/settings.spec.ts
+++ test/e2e/settings/settings.spec.ts
@@ -37,29 +37,6 @@ describe('settings --', function() {
     });
 
 
-    xit('save syncing settings to config file and load them after restart', done => {
-
-        common.typeIn(SettingsPage.getUserNameInput(), 'settings_test_user');
-        SettingsPage.clickSaveSettingsButton();
-        NavbarPage.clickCloseNonResourcesTab();
-
-        NavbarPage.clickTab('project')
-            //.then(() => {
-                // browser.sleep(5000);
-                //return SettingsPage.get();
-            //})
-            .then(() => browser.sleep(2000))
-            .then(() => SettingsPage.getUserName())
-            .then(username => {
-                expect(username).toEqual('settings_test_user');
-                done();
-            }).catch(err => {
-                fail(err);
-                done();
-            });
-    });
-
-
     it('show warnings if an invalid imagestore path is set', () => {
 
         common.typeIn(SettingsPage.getImagestorePathInput(), '/invalid/path/to/imagestore');

6bb79f000210ac133d1f53d7b83e90c9cda2d936
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-05-26 11:54:04 +0200

    Add scss files to Angular scripts instead of compiling them via Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,6 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 
-// compile sass and concatenate to single css file in build dir
-gulp.task('convert-sass', () => {
-
-    return gulp.src([
-        'src/app/components/app.scss',
-        'node_modules/leaflet/dist/leaflet.css',
-        'node_modules/Leaflet.vector-markers/dist/leaflet-vector-markers.css',
-        'node_modules/leaflet.pm/dist/leaflet.pm.css'
-    ])
-        .pipe(sass({
-            includePaths: [
-                'node_modules/roboto-fontface/css/roboto/sass',
-                'node_modules/bootstrap/scss',
-                'node_modules/@mdi/font/scss/'
-            ], precision: 8
-        }))
-        .pipe(concat('app.css'))
-        .pipe(gulp.dest('src/'));
-});
 
 gulp.task('copy-fonts-convert-sass', gulp.series('convert-sass', () => {
 

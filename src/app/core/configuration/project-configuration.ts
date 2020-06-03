cd1e5e95fbc00497532e83ab065c3f82b6b39c94
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-06-03 11:50:41 +0100

    Remove todo

--- src/app/core/configuration/project-configuration.ts
+++ src/app/core/configuration/project-configuration.ts
@@ -61,7 +61,6 @@ export class ProjectConfiguration {
     }
 
 
-    // TODO introduce getCategory(name: Name) instead of accessing it by key after calling getCategoriesMap
     public getCategoriesMap(): Map<Category> {
 
         return this.categoriesMap;

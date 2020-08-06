b50b996e4e88222a437dcaa15fc03d9ba86f095f
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-08-06 17:27:31 +0200

    Remove subfield conversion

--- src/app/core/datastore/field/migration-map.ts
+++ src/app/core/datastore/field/migration-map.ts
@@ -2,10 +2,6 @@ export const migrationMap: { [oldName: string]: string } = {
     'type': 'category'
 };
 
-export const subFieldsMigrationMap: { [oldName: string]: string } = {
-
-};
-
 export const singleToMultipleValuesFieldNames: string[] = [
     'processor',
     'supervisor',
--- src/app/core/datastore/field/migrator.ts
+++ src/app/core/datastore/field/migrator.ts
@@ -1,6 +1,6 @@
 import {isObject, flow} from 'tsfun';
 import {Document, ValOptionalEndVal} from 'idai-components-2';
-import {migrationMap, subFieldsMigrationMap, singleToMultipleValuesFieldNames} from './migration-map';
+import {migrationMap, singleToMultipleValuesFieldNames} from './migration-map';
 
 
 /**
@@ -51,42 +51,13 @@ export module Migrator {
         Object.keys(document.resource).forEach((fieldName: string) => {
            const newFieldName: string = migrationMap[fieldName] ? migrationMap[fieldName] : fieldName;
 
-           let field: any = document.resource[fieldName];
-           if (Array.isArray(field) && (field.some(isObject))) field = migrateArrayField(field);
-
-           resource[newFieldName] = field;
+           resource[newFieldName] = document.resource[fieldName];
+           // TODO no deletion of original field?
         });
 
         document.resource = resource;
 
         return document;
-    }
-
-
-    function migrateArrayField(arrayField: any[]): any[] {
-
-        const result: any[] = [];
-
-        arrayField.forEach(entry => {
-            result.push(isObject(entry) ? migrateSubFieldNames(entry) : entry);
-        });
-
-        return result;
-    }
-
-
-    function migrateSubFieldNames(object: any): any {
-
-        const result: any = {};
-
-        Object.keys(object).forEach((fieldName: string) => {
-            const newFieldName: string = subFieldsMigrationMap[fieldName]
-                ? subFieldsMigrationMap[fieldName]
-                : fieldName;
-            result[newFieldName] = object[fieldName];
-        });
-
-        return result;
     }
 
 

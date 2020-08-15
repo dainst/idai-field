2d76d5cc48fe6390bd59b8b2615f564592e7b4e1
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-08-15 12:25:38 +0200

    Remove part of apidoc

--- src/app/core/import/import/process/process-relations.ts
+++ src/app/core/import/import/process/process-relations.ts
@@ -38,19 +38,8 @@ import RECORDED_IN = HierarchicalRelations.RECORDEDIN;
  *
  * LIES_WITHIN gets replaced by or joined by RECORDED_IN relations.
  *
- * @param documents get modified in place (document.resource.relations)
- * @param validator
- * @param operationCategoryNames
- * @param inverseRelationsMap
- * @param get
- * @param mergeMode
- * @param permitDeletions
- * @param mainTypeDocumentId
- *
  * @author Daniel de Oliveira
  * @author Thomas Kleinke
- *
- *
  *
  * TODO migrated doc, review:
  *
@@ -60,53 +49,6 @@ import RECORDED_IN = HierarchicalRelations.RECORDEDIN;
  * This function takes relations in the form, that only liesWithin is defined and never isRecordedIn.
  * isRecordedIn gets inferred. This especially is true in cases where a top level item references
  * its operation with liesWithin, which gets resolved to an empty liesWithin and a isRecordedIn in its place.
- *
- * -------------------------------------------------------
- * @returns an array with 3 entries
- *   [
- *     the documents, prepared such that database updates can be performed,
- *     related documents, with adjusted relations so that the database will be consistent after update,
- *     ImportErrors, if any
- *   ]
- *
- *   Possibly occuring ImportErrors:
- *
- *   [MUST_LIE_WITHIN_OTHER_NON_OPERATON_RESOURCE, category, resourceIdentifier]
- *     - if a resource of
- *       a defined builtin category should be placed inside another resource of a legal LIES_WITHIN range category, but is placed
- *       directly below an operation.
- *
- *   [BAD_INTERRELATION, sourceId]
- *     - if opposing relations are pointing to the same resource.
- *       For example IS_BEFORE and IS_AFTER pointing both from document '1' to '2'.
- *     - if mutually exluding relations are pointing to the same resource.
- *       For example IS_CONTEMPORARY_WITH and IS_AFTER both from document '1' to '2'.
- *
- *   [TARGET_CATEGORY_RANGE_MISMATCH, resourceIdentifier, relationName, relationTargetCategory]
- *     - if a resource points to another resource, however, the specified relation is not allowed between the
- *       categories of the resources.
- *
- *   [PARENT_ASSIGNMENT_TO_OPERATIONS_NOT_ALLOWED]
- *     - if operationId is not '' and
- *       a resource references an operation as parent.
- *
- *   [LIES_WITHIN_TARGET_NOT_MATCHES_ON_IS_RECORDED_IN, resourceIdentifier]
- *     - if the inferredRecordedIn
- *       differs from a possibly specified recordedIn.
- *
- *   [EXEC_MISSING_RELATION_TARGET, targetId]
- *
- * [CATEGORY_CANNOT_BE_CHANGED, identifier]
- *   - if it is tried to change the category of a resource
- *
- * [EMPTY_SLOTS_IN_ARRAYS_FORBIDDEN, identifier]
- *   - if deletion would result in empty array slots
- *
- * [ARRAY_OF_HETEROGENEOUS_TYPES, identifier]
- *
- * [EMPTY_RELATION, resourceId]
- *   - if relations empty for some relation is empty.
- *     For example relations: {isAbove: []}
  */
 export async function processRelations(documents: Array<Document>, validator: ImportValidator,
                                        operationCategoryNames: string[],

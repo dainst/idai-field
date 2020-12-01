e5ae203f7cdff56f051bf91a102ba5ab4508d54b
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-12-01 17:04:39 +0000

    Simplify resource-deletion; comment out likely obsolete code

--- src/app/components/resources/deletion/resource-deletion.ts
+++ src/app/components/resources/deletion/resource-deletion.ts
@@ -3,12 +3,10 @@ import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 import {FieldDocument} from 'idai-components-2';
 import {DeleteModalComponent} from './delete-modal.component';
 import {RelationsManager} from '../../../core/model/relations-manager';
-import {M} from '../../messages/m';
 import {DeletionInProgressModalComponent} from './deletion-in-progress-modal.component';
-import {Imagestore} from '../../../core/images/imagestore/imagestore';
 import {ProjectConfiguration} from '../../../core/configuration/project-configuration';
-import {DatastoreErrors} from '../../../core/datastore/model/datastore-errors';
 import {ImageRelationsManager} from '../../../core/model/image-relations-manager';
+import {ProjectCategories} from '../../../core/configuration/project-categories';
 
 
 /**
@@ -21,7 +19,6 @@ export class ResourceDeletion {
     constructor(private modalService: NgbModal,
                 private relationsManager: RelationsManager,
                 private imageRelationsManager: ImageRelationsManager,
-                private imagestore: Imagestore,
                 private projectConfiguration: ProjectConfiguration) {}
 
 
@@ -43,47 +40,41 @@ export class ResourceDeletion {
 
 
     // TODO we could double check that all documents have document.project
-    // TODO review deletion of Type resources with children
     // TODO write apidoc for document.project
     private async performDeletion(document: FieldDocument, deleteRelatedImages: boolean) {
 
-        if (document.resource.category === 'TypeCatalog' || document.resource.category === 'Type') {
+         if (ProjectCategories.getTypeCategoryNames().includes(document.resource.category)
+            && (document.resource.project !== undefined || deleteRelatedImages)) {
 
-            if (document.resource.project !== undefined || deleteRelatedImages) {
-                await this.imageRelationsManager.remove(document);
-            } else {
-                await this.relationsManager.remove(document);
-            }
-
+             await this.imageRelationsManager.remove(document);
         } else {
-            await this.deleteImageWithImageStore(document);
-            await this.deleteWithPersistenceManager(document);
+             await this.relationsManager.remove(document);
         }
     }
 
 
-    private async deleteImageWithImageStore(document: FieldDocument) {
-
-        if (!this.projectConfiguration.isSubcategory(document.resource.category, 'Image')) {
-            return undefined;
-        }
-
-        if (!this.imagestore.getPath()) throw [M.IMAGESTORE_ERROR_INVALID_PATH_DELETE];
-        try {
-            await this.imagestore.remove(document.resource.id);
-        } catch {
-            throw [M.IMAGESTORE_ERROR_DELETE, document.resource.id];
-        }
-    }
-
-
-    private async deleteWithPersistenceManager(document: FieldDocument) {
-
-        try {
-            await this.relationsManager.remove(document);
-        } catch (removeError) {
-            console.error('removeWithPersistenceManager', removeError);
-            if (removeError !== DatastoreErrors.DOCUMENT_NOT_FOUND) throw [M.DOCEDIT_ERROR_DELETE];
-        }
-    }
+    // private async deleteImageWithImageStore(document: FieldDocument) {
+    //
+    //     if (!this.projectConfiguration.isSubcategory(document.resource.category, 'Image')) {
+    //         return undefined;
+    //     }
+    //
+    //     if (!this.imagestore.getPath()) throw [M.IMAGESTORE_ERROR_INVALID_PATH_DELETE];
+    //     try {
+    //         await this.imagestore.remove(document.resource.id);
+    //     } catch {
+    //         throw [M.IMAGESTORE_ERROR_DELETE, document.resource.id];
+    //     }
+    // }
+    //
+    //
+    // private async deleteWithPersistenceManager(document: FieldDocument) {
+    //
+    //     try {
+    //         await this.relationsManager.remove(document);
+    //     } catch (removeError) {
+    //         console.error('removeWithPersistenceManager', removeError);
+    //         if (removeError !== DatastoreErrors.DOCUMENT_NOT_FOUND) throw [M.DOCEDIT_ERROR_DELETE];
+    //     }
+    // }
 }

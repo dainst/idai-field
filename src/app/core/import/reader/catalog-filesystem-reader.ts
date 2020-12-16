ebdc974902281f739d75067df7819b27f15dcc47
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-12-16 18:37:32 +0100

    Use appData dir to place tempdir for catalog import

--- src/app/core/import/reader/catalog-filesystem-reader.ts
+++ src/app/core/import/reader/catalog-filesystem-reader.ts
@@ -4,7 +4,7 @@ import {Settings} from '../../settings/settings';
 
 const fs = typeof window !== 'undefined' ? window.require('fs') : require('fs');
 const extract = typeof window !== 'undefined' ? window.require('extract-zip') : require('extract-zip');
-
+const remote = typeof window !== 'undefined' ? window.require('electron').remote : require('electron').remote;
 
 /**
  * @author Daniel de Oliveira
@@ -23,31 +23,38 @@ export class CatalogFilesystemReader implements Reader {
 
         return new Promise(async (resolve, reject) => {
 
+            // TODO review duplication
+            const tmpBaseDir = remote.app.getPath('appData') + '/' + remote.app.getName() + '/temp/';
+            const tmpDir = tmpBaseDir + 'catalog-import/';
+            const imgDir = tmpDir + 'images/';
+            fs.rmdirSync(tmpDir, { recursive: true });
+            fs.mkdirSync(imgDir, { recursive: true });
+
             const basePath = this.file.path
                 .slice(0, this.file.path.lastIndexOf('.')) + '/'; // TODO review code duplication with catalog exporter
 
             try {
 
-                const outputDir = this.file.path
-                    .slice(0, this.file.path.lastIndexOf('.'));
-                await extract(this.file.path, { dir: outputDir });
+                await extract(this.file.path, { dir: tmpDir });
 
-                const imagesFolder = fs.readdirSync(basePath + 'images');
-                const data = fs.readFileSync(basePath + 'catalog.jsonl', 'utf-8');
+                const imagesFolder = fs.readdirSync(imgDir);
+                const data = fs.readFileSync(tmpDir + 'catalog.jsonl', 'utf-8');
 
-                for (let file of imagesFolder) {
-                    const source = basePath + 'images/' + file;
+                for (let imageFile of imagesFolder) {
+                    const source = imgDir + imageFile;
                     const target = this.settings.imagestorePath
                         + this.settings.selectedProject
                         + '/';
                     if (!fs.existsSync(target)) fs.mkdirSync(target);
-                    fs.copyFileSync(source, target + file);
+                    fs.copyFileSync(source, target + imageFile);
                 }
 
                 resolve(data);
             } catch (err) {
                 console.log("err", err)
                 reject([ReaderErrors.SHAPEFILE_GENERIC]); // TODO use other error
+            } finally {
+                fs.rmdirSync(tmpDir, { recursive: true });
             }
         });
     }

8d2ac8dbda1384b16f469e6e4ddb85ee5ad0ea64
Thomas Kleinke <thomas.kleinke@dainst.de>
2021-01-13 11:15:05 +0100

    Remove 'idai-components-2' from string values of DatastoreErrors

--- src/app/core/datastore/model/datastore-errors.ts
+++ src/app/core/datastore/model/datastore-errors.ts
@@ -3,11 +3,11 @@
  */
 export class DatastoreErrors {
 
-    public static INVALID_DOCUMENT: string = 'idai-components-2/datastore/invaliddocument';
-    public static GENERIC_ERROR: string = 'idai-components-2/datastore/genericerror';
-    public static DOCUMENT_RESOURCE_ID_EXISTS: string = 'idai-components-2/datastore/documentresourceidexists';
-    public static DOCUMENT_NO_RESOURCE_ID: string = 'idai-components-2/datastore/documentnoresourceid';
-    public static DOCUMENT_NOT_FOUND: string = 'idai-components-2/datastore/documentnotfound';
-    public static SAVE_CONFLICT: string = 'idai-components-2/datastore/saveconflict';
-    public static REMOVE_REVISIONS_ERROR: string = 'idai-components-2/datastore/removerevisionserror';
+    public static INVALID_DOCUMENT: string = 'datastore/invaliddocument';
+    public static GENERIC_ERROR: string = 'datastore/genericerror';
+    public static DOCUMENT_RESOURCE_ID_EXISTS: string = 'datastore/documentresourceidexists';
+    public static DOCUMENT_NO_RESOURCE_ID: string = 'datastore/documentnoresourceid';
+    public static DOCUMENT_NOT_FOUND: string = 'datastore/documentnotfound';
+    public static SAVE_CONFLICT: string = 'datastore/saveconflict';
+    public static REMOVE_REVISIONS_ERROR: string = 'datastore/removerevisionserror';
 }

110df0d5d649d41d22c7e3294f0444b293d045df
Thomas Kleinke <thomas.kleinke@dainst.de>
2021-01-14 15:41:44 +0100

    Remove todo

--- src/app/core/export/catalog/get-export-documents.ts
+++ src/app/core/export/catalog/get-export-documents.ts
@@ -23,7 +23,6 @@ export async function getExportDocuments(datastore: DocumentReadDatastore,
     const linkedImages = clone(await imageRelationsManager.getLinkedImages(catalogAndTypes));
     const exclusivelyLinkedImages = await imageRelationsManager.getLinkedImages(catalogAndTypes, true);
 
-    // TODO Check if this is really necessary
     if (linkedImages.length !== exclusivelyLinkedImages.length) {
 
         const diff = subtract(on(RESOURCE_ID_PATH), exclusivelyLinkedImages)(linkedImages);

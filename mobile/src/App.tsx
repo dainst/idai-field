4590033b7bde5537bbd5398293e66df15e897b33
Sebastian Cuy <sebastian.cuy@dainst.de>
2021-03-19 13:12:33 +0100

    Initial proof-of-concept for mobile app

---
+++ mobile/src/App.tsx
@@ -0,0 +1,169 @@
+import { IonApp, IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonMenu, IonMenuToggle, IonModal, IonSplitPane, IonTitle, IonToolbar } from '@ionic/react';
+import { menu, refresh, settings } from 'ionicons/icons';
+import { Map } from 'ol';
+import olms from 'ol-mapbox-style';
+import React, { CSSProperties, useEffect, useState } from 'react';
+import { listOperations, setupDB, setupReplication } from './pouchdb-service';
+
+
+const MAPBOX_KEY = 'pk.eyJ1Ijoic2ViYXN0aWFuY3V5IiwiYSI6ImNrOTQxZjA4MzAxaGIzZnBwZzZ4c21idHIifQ._2-exYw4CZRjn9WoLx8i1A';
+
+
+function App() {
+
+  const [db, setDb] = useState<PouchDB.Database>();
+  const [syncStatus, setSyncStatus] = useState<string>('db loading');
+  const [operations, setOperations] = useState<any[]>();
+
+  const [dbName, setDbName] = useState<string>('test');
+  const [remoteUser, setRemoteUser] = useState<string>('');
+  const [remotePassword, setRemotePassword] = useState<string>('');
+
+  const [tmpDbName, setTmpDbName] = useState<string>('test');
+  const [tmpRemoteUser, setTmpRemoteUser] = useState<string>('');
+  const [tmpRemotePassword, setTmpRemotePassword] = useState<string>('');
+
+  const [showSettings, setShowSettings] = useState<boolean>(false);
+
+  useEffect(() => {
+
+    createMap();
+  }, []);
+
+  useEffect(() => {
+    
+    setSyncStatus('db loading');
+
+    setupDB(dbName)
+      .then((db) => {
+        setDb(db);
+        loadOperations(db);
+      })
+      .then(() => setSyncStatus('unsynced'))
+      .catch(console.error);
+  }, [dbName]);
+
+  useEffect(() => {
+
+    if (db && remoteUser) {
+      setSyncStatus('syncing');
+      setupReplication(db, remoteUser, remotePassword)
+        .then(() => setSyncStatus('synced'))
+        .then(() => loadOperations(db))
+        .catch(() => setSyncStatus('sync error'));
+    }
+  }, [db, remoteUser, remotePassword])
+
+  const loadOperations = (db?: PouchDB.Database) => {
+
+    if (db) {
+      setOperations([]);
+      listOperations(db).then(setOperations);
+    }
+  }
+
+  const settingsSaved = () => {
+
+    setDbName(tmpDbName);
+    setRemoteUser(tmpRemoteUser);
+    setRemotePassword(tmpRemotePassword);
+    setShowSettings(false);
+  }
+
+  const renderToolbar = () => 
+    <IonToolbar>
+      <IonTitle>
+        Status: { syncStatus }
+      </IonTitle>
+      <IonButtons slot="primary">
+        <IonButton onClick={ () => loadOperations(db) }>
+          <IonIcon icon={ refresh } />
+        </IonButton>
+      </IonButtons>
+    </IonToolbar>;
+
+  return <IonApp>
+    <IonSplitPane contentId="main-content">
+
+      <IonModal isOpen={ showSettings }>
+        <IonList>
+          <IonItem>
+            <IonLabel>Projekt</IonLabel>
+            <IonInput value={ tmpDbName } onIonChange={ (e) => setTmpDbName(e.detail.value!)}></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Nutzername</IonLabel>
+            <IonInput value={ tmpRemoteUser } onIonChange={ (e) => setTmpRemoteUser(e.detail.value!)}></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Passwort</IonLabel>
+            <IonInput type="password" value={ tmpRemotePassword } onIonChange={ (e) => setTmpRemotePassword(e.detail.value!)}></IonInput>
+          </IonItem>
+        </IonList>
+        <IonButton onClick={ settingsSaved }>
+          Speichern
+        </IonButton>
+      </IonModal>
+
+      <IonMenu contentId="main-content">
+        <IonHeader>
+          { renderToolbar() }
+        </IonHeader>
+        <IonContent>
+          { operations?.length ? renderOperations(operations) : 'No operations found' }
+        </IonContent>
+      </IonMenu>
+
+      <div className="ion-page" id="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonMenuToggle>
+                <IonButton>
+                  <IonIcon slot="icon-only" icon={ menu }></IonIcon>
+                </IonButton>
+              </IonMenuToggle>
+            </IonButtons>
+            <IonTitle>{ dbName }</IonTitle>
+            <IonButtons slot="primary">
+              <IonButton onClick={ () => setShowSettings(true) }>
+                <IonIcon icon={ settings } />
+              </IonButton>
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          <div id="ol-map" style={ mapStyle } />
+        </IonContent>
+      </div>
+
+    </IonSplitPane>
+  </IonApp>;
+}
+
+
+const createMap = (): Map => 
+  olms('ol-map', 'https://api.mapbox.com/styles/v1/sebastiancuy/ckff2undp0v1o19mhucq9oycb?access_token=' + MAPBOX_KEY);
+
+
+const renderOperations = (operations: any[]) =>
+  <IonList>
+    { operations.map(renderOperation) }
+  </IonList>
+
+
+const renderOperation = (operation: any) => 
+  <IonItem key={ operation.resource.id }>
+    <IonLabel>
+      { operation.resource.identifier } - { operation.resource.shortDescription }
+    </IonLabel>
+  </IonItem>;
+
+
+const mapStyle: CSSProperties = {
+  width: '100%',
+  height: '100%'
+};
+
+
+export default App;

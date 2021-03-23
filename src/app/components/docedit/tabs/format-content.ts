9ee29db8ea9a53466d1cbfe67a9161c62830a61c
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-03-23 15:45:22 +0100

    Finish format-content

--- src/app/components/docedit/tabs/format-content.ts
+++ src/app/components/docedit/tabs/format-content.ts
@@ -1,24 +1,28 @@
 import { Dating, Dimension, Literature, OptionalRange, Resource } from 'idai-components-2';
 import { FieldDefinition } from 'src/app/core/configuration/model/field-definition';
-import { flow, identity, isArray, isString } from 'tsfun';
+import { flow, identity, isArray, isObject, isString } from 'tsfun';
 
 export type InnerHTML = string;
 
 
 /**
  * Right now, there is no translation of field entries. // TODO needed?
- * 
+ *
  * @author Daniel de Oliveira
  */
 export function formatContent(resource: Resource, field: any): InnerHTML {
 
     const fieldContent = resource[field.name];
 
-    return !isArray(fieldContent) 
-        ? JSON.stringify(fieldContent) /* TODO review possible object types */
-        : flow(fieldContent, 
-            convertArray(field.inputType), 
-            formatArray);
+    return isArray(fieldContent)
+        ? flow(fieldContent,
+            convertArray(field.inputType),
+            formatArray)
+        : isObject(fieldContent)
+        ? flow(fieldContent,
+            convertObject(field.inputType),
+            formatObject)
+        : fieldContent;
 }
 
 
@@ -34,22 +38,36 @@ function formatArray(fieldContent: Array<string>): InnerHTML {
 }
 
 
-const convertArray = (inputType?: FieldDefinition.InputType) => 
+function formatObject(fieldContent: string): InnerHTML {
+
+    return fieldContent; // currently not special handling
+}
+
+
+const convertObject = (inputType?: FieldDefinition.InputType) =>
+                      (fieldContent: any) => {
+
+    if (inputType === 'dropdownRange' && OptionalRange.buildIsOptionalRange(isString)(fieldContent)) {
+        return OptionalRange.generateLabel(fieldContent, identity, identity);
+    }
+    return JSON.stringify(fieldContent);
+}
+
+
+const convertArray = (inputType?: FieldDefinition.InputType) =>
                      (fieldContent: Array<any>): Array<string> => {
+
 
     return fieldContent.map(element => {
 
         if (inputType === 'dimension' && Dimension.isDimension(element)) {
-            return Dimension.generateLabel(element, identity, identity /* TODO review */);
+            return Dimension.generateLabel(element, identity, identity);
         }
         else if (inputType === 'dating' && Dating.isDating(element)) {
             return Dating.generateLabel(element, identity);
         }
-        else if (inputType === 'dropdownRange' && OptionalRange.buildIsOptionalRange(isString)(element)) {
-            return OptionalRange.generateLabel(element, identity, identity /* TODO review */);
-        }
         else if (inputType === 'literature' && Literature.isLiterature(element)) {
-            return Literature.generateLabel(element, identity /*, zenonId? */)
+            return Literature.generateLabel(element, identity)
         }
         else return JSON.stringify(element);
     });

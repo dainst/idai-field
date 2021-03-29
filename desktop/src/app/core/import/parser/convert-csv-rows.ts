aea22b2e907c0ed5eace54b429dc5f5dc9b7e0a1
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-03-29 14:01:26 +0200

    Rename var

--- desktop/src/app/core/import/parser/convert-csv-rows.ts
+++ desktop/src/app/core/import/parser/convert-csv-rows.ts
@@ -46,7 +46,7 @@ export function convertCsvRows(separator: string) {
         assertHeadingsDoNotContainIncompleteArrays(headings);
         assertRowsAndHeadingLengthsMatch(headings, rows);
 
-        return rows.map(_ => _.reduce(insertFieldIntoDocument(headings), {}));
+        return rows.map(row => row.reduce(insertFieldIntoDocument(headings), {}));
     }
 }
 

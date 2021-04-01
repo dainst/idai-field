7230fbe44ea9b120b7f475dce3ae8795535d4c67
Michael <michael.kihm@web.de>
2021-04-01 23:48:40 +0200

    Added react-native-paper

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,15 @@
 import React, { ReactElement } from 'react';
 import { enableScreens } from 'react-native-screens';
 import StackNavigator from './navigation/StackNavigator';
-
+import { Provider as PaperProvider } from 'react-native-paper';
 
 enableScreens();
 
 export default function App(): ReactElement {
   return (
-      <StackNavigator />
+    <PaperProvider>
+        <StackNavigator />
+    </PaperProvider>
   );
 }
 

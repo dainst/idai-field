7bd6e2f7e67276f58023a4a77f99e7ff3131c2d9
Michael <michael.kihm@web.de>
2021-04-01 15:42:38 +0200

    Added stack native navigator

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { enableScreens } from 'react-native-screens';
+import Home from './screens/Home';
+
+const Stack = createStackNavigator();
+
+enableScreens();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+          <Stack.Navigator>
+              <Stack.Screen name="Home" component={Home} />
+          </Stack.Navigator>
+      </NavigationContainer>  
   );
 }
 
---
+++ mobile/screens/Home.tsx
@@ -0,0 +1,24 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+// create a component
+const Home: React.FC = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Home</Text>
+        </View>
+    );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+
+//make this component available to the app
+export default Home;

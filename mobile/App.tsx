f8bea88d68adffd180e5c40b523576cbf75064be
Michael <michael.kihm@web.de>
2021-04-06 07:43:48 +0200

    Added native-base

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,33 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import { enableScreens } from 'react-native-screens';
 import StackNavigator from './navigation/StackNavigator';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 
+
+const fetchFonts = () => {
+    return Font.loadAsync({
+        Roboto: require('native-base/Fonts/Roboto.ttf'),
+        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+        ...Ionicons.font,
+    });
+};
 
 enableScreens();
 
 export default function App(): ReactElement {
-  return (
-      <StackNavigator />
-  );
-}
 
+    const [fontLoaded, setFontLoaded] = useState(false);
+
+    if(!fontLoaded){
+        return <AppLoading
+                    startAsync={ fetchFonts }
+                    onFinish={ () => setFontLoaded(true) }
+                    onError={ (err) => console.log(err) } />;
+    }
+
+    return (
+        <StackNavigator />
+    );
+}

5b724fd5b773503fc446eadb0068599ee7293e61
Michael <michael.kihm@web.de>
2021-04-09 09:11:36 +0200

    Changed header title

--- mobile/src/navigation/HomeStackNavigator/HomeStackNavigator.tsx
+++ mobile/src/navigation/HomeStackNavigator/HomeStackNavigator.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../../screens/HomeScreen';
 import { headerBackgroundColor } from '../../constants/colors';
 import { HomeStackParamList } from './HomeStackParamList';
+import PouchDbContext from '../../data/pouchdb/pouch-context';
 
 const Stack = createStackNavigator<HomeStackParamList>();
 
@@ -10,19 +11,26 @@ const commonHeaderStyle = {
     backgroundColor: headerBackgroundColor
 };
 
-const HomeStackNavigator = (): JSX.Element => (
-    <Stack.Navigator
-        screenOptions={ {
-            headerStyle: commonHeaderStyle,
-            headerTintColor: 'white',
-        } }>
-        <Stack.Screen
-            name="Home"
-            component={ HomeScreen }
-            options={
-                { title: 'iDAI field mobile' }
-            } />
-    </Stack.Navigator>
-);
+const HomeStackNavigator = (): JSX.Element => {
+
+    const pouchCtx = useContext(PouchDbContext);
+
+    return (
+        <Stack.Navigator
+            screenOptions={ {
+                headerStyle: commonHeaderStyle,
+                headerTintColor: 'white',
+            } }>
+            <Stack.Screen
+                name="Home"
+                component={ HomeScreen }
+                options={ {
+                    headerTitle: pouchCtx.dbName ?
+                        `Connected to ${pouchCtx.dbName}` :
+                        'iDAIfield mobile'
+                } } />
+        </Stack.Navigator>
+    );
+};
 
 export default HomeStackNavigator;

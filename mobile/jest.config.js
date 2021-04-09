b21dcfd93ddebfa68e24f1aebd4c8697eb4ae74d
Sebastian Cuy <sebastian.cuy@dainst.de>
2021-04-09 15:17:12 +0200

    Fix jest config

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,27 +1,9 @@
 module.exports = {
     preset: 'jest-expo',
-    globals: {
-        'ts-jest': {
-          tsconfig: {
-            jsx: 'react',
-          },
-        },
-      },
-      transform: {
-        '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
-        '^.+\\.tsx?$': 'ts-jest',
-      },
-      testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
-      collectCoverageFrom: [
-        '**/*.{ts,tsx}',
-        '!**/coverage/**',
-        '!**/node_modules/**',
-        '!**/babel.config.js',
-        '!**/jest.setup.js',
-      ],
-      moduleFileExtensions: ['js', 'ts', 'tsx'],
-      transformIgnorePatterns: [
-        'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|sentry-expo|native-base)',
-      ],
-      coverageReporters: ['json-summary', 'text', 'lcov'],
+    moduleFileExtensions: ['js', 'ts', 'tsx'],
+    transformIgnorePatterns: [
+      'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|sentry-expo|native-base)',
+      '../core/dist/src'
+    ],
+    coverageReporters: ['json-summary', 'text', 'lcov'],
 };

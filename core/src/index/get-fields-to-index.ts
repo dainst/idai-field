833ee9ec0195607d5c4f955a57db1f9034acc304
Sebastian Cuy <sebastian.cuy@dainst.de>
2021-04-09 20:31:12 +0200

    Make sure default fields are always indexed

--- core/src/index/get-fields-to-index.ts
+++ core/src/index/get-fields-to-index.ts
@@ -1,4 +1,4 @@
-import {Map} from 'tsfun';
+import { Map } from 'tsfun';
 import { Category } from '../model/category';
 
 
@@ -7,10 +7,10 @@ const defaultFieldsToIndex = ['identifier', 'shortDescription'];
 
 export function getFieldsToIndex(categoriesMap: Map<Category>, categoryName: string): string[] {
 
-    return !categoriesMap[categoryName]
+    const fields = !categoriesMap[categoryName]
         ? []
         : Category.getFields(categoriesMap[categoryName])
             .filter(field => field.fulltextIndexed)
-            .map(field => field.name)
-            .concat(defaultFieldsToIndex);
+            .map(field => field.name);
+    return fields.concat(defaultFieldsToIndex);
 }

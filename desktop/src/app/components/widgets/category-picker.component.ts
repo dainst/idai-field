a93e65fc1364d0e272f2b583429062978b597793
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-04-14 00:45:09 +0200

    Do todos

--- desktop/src/app/components/widgets/category-picker.component.ts
+++ desktop/src/app/components/widgets/category-picker.component.ts
@@ -73,6 +73,6 @@ export class CategoryPickerComponent implements OnChanges {
     public hasCustomFields: Predicate<Category> = compose(
         Category.getFields,
         map(to(FieldDefinition.SOURCE)),
-        any(is(FieldDefinition.Source.CUSTOM)) as any /* TODO review any */
+        any(is(FieldDefinition.Source.CUSTOM))
     );
 }

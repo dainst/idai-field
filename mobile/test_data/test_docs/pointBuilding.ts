eb7fd924ea6a356965f7703646a2426bd2ee17fb
Michael <michael.kihm@web.de>
2021-04-16 18:27:52 +0200

    Add test data

---
+++ mobile/test_data/test_docs/pointBuilding.ts
@@ -0,0 +1,31 @@
+import { Document } from 'idai-field-core';
+
+export const pointBuilding: Document = {
+    _id: 'd49fb68c-e571-49e2-53cb-cf69780a1a0c',
+    resource: {
+      relations: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          27.18918800354004,
+          39.14124870300293
+        ]
+      },
+      identifier: 'pointBuilding',
+      id: 'd49fb68c-e571-49e2-53cb-cf69780a1a0c',
+      shortDescription: 'pointBuilding',
+      type: 'Building',
+      category: 'Building'
+    },
+    created: {
+      user: 'mkihm',
+      date: new Date('2021-04-16T12:54:14.167Z')
+    },
+    modified: [
+      {
+        user: 'mkihm',
+        date: new Date('2021-04-16T12:56:29.626Z')
+      }
+    ],
+    _rev: '2-3d0abffe2fda44e48baa62302568abd3'
+  };

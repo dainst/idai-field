3f7111ececec08170a7dd80c74e4d9a08305eacb
Thomas Kleinke <thomas.kleinke@dainst.de>
2021-04-16 11:03:32 +0200

    Rewrite image overview upload e2e tests

--- desktop/test/e2e/images/image-overview-upload.spec.ts
+++ desktop/test/e2e/images/image-overview-upload.spec.ts
@@ -1,55 +1,63 @@
-import {browser, protractor} from 'protractor';
-import {ImageOverviewPage} from './image-overview.page';
-import {NavbarPage} from '../navbar.page';
-import {MenuPage} from '../menu.page';
+import { navigateTo, resetApp, start, stop, waitForExist } from '../app';
+import { ImageOverviewPage } from './image-overview.page';
+import { NavbarPage } from '../navbar.page';
 
 const path = require('path');
-
-const EC = protractor.ExpectedConditions;
-const delays = require('../delays');
-const common = require('../common');
 
 
 describe('images/upload --', () => {
 
     // image is already present in mediastore folder since uploading does not work in HttpMediastore
-    const imageUploadFileName: string = 'Aldrin_Apollo_11.jpg';
+    const imageFileName: string = 'Aldrin_Apollo_11.jpg';
 
 
-    const uploadImage = () => {
+    beforeAll(async done => {
 
-        ImageOverviewPage.clickUploadArea();
-        ImageOverviewPage.uploadImage(path.resolve(__dirname, '../../../../test-data/' + imageUploadFileName));
-        ImageOverviewPage.chooseImageSubcategory(0);
+        await start();
+        done();
+    });
+
+
+    beforeEach(async done => {
+
+        await navigateTo('settings');
+        await resetApp();
+        await navigateTo('images');
+        await ImageOverviewPage.waitForCells();
+        done();
+    });
+
+
+    afterAll(async done => {
+
+        await stop();
+        done();
+    });
+
+
+    const uploadImage = async () => {
+
+        await ImageOverviewPage.uploadImage(path.resolve(__dirname, '../../../../../test-data/' + imageFileName));
+        await ImageOverviewPage.chooseImageSubcategory(0);
     };
 
 
-    beforeEach(() => {
+    it('image upload should create a JSON document, which in turn gets displayed in the grid', async done => {
 
-        browser.sleep(1500);
-
-        MenuPage.navigateToSettings();
-        browser.sleep(1)
-            .then(() => common.resetApp());
-        MenuPage.navigateToImages();
-        ImageOverviewPage.waitForCells();
+        await uploadImage();
+        await waitForExist(await ImageOverviewPage.getCellByIdentifier(imageFileName));
+        
+        done();
     });
 
 
-    it('image upload should create a JSON document, which in turn gets displayed in the grid', () => {
+    it('do not allow uploading an image with a duplicate filename', async done => {
 
-        uploadImage();
-        browser.wait(EC.presenceOf(ImageOverviewPage.getCellByIdentifier(imageUploadFileName)),
-            delays.ECWaitTime);
-    });
+        await uploadImage();
+        await waitForExist(await ImageOverviewPage.getCellByIdentifier(imageFileName));
+        await uploadImage();
+        await NavbarPage.awaitAlert('Ein Bild mit dem gleichen Dateinamen existiert bereits', false);
 
-
-    it('do not allow uploading an image with a duplicate filename', () => {
-
-        uploadImage();
-        browser.wait(EC.presenceOf(ImageOverviewPage.getCellByIdentifier(imageUploadFileName)),
-            delays.ECWaitTime);
-        uploadImage();
-        NavbarPage.awaitAlert('Ein Bild mit dem gleichen Dateinamen existiert bereits', false);
+        done();
     });
 });

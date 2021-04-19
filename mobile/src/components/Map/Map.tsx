dacb23bcd6d193426d81621b49c8bcd3c374abbc
Michael <michael.kihm@web.de>
2021-04-19 15:12:58 +0200

    First map version

--- mobile/src/components/Map/Map.tsx
+++ mobile/src/components/Map/Map.tsx
@@ -1,31 +1,88 @@
-import { Document } from 'idai-field-core';
-import { Box, HStack, Text } from 'native-base';
-import React from 'react';
+import { Document, FieldGeometry } from 'idai-field-core';
+import { Box, Text } from 'native-base';
+import React, { ReactElement } from 'react';
 import { StyleSheet } from 'react-native';
-
+import Svg, { Circle, G } from 'react-native-svg';
+import { GeoLineString, GeoMultiLineString, GeoMultiPoint, GeoMultiPolygon, GeoPoint, GeoPolygon } from './geo-svg';
+import { defineViewBox, getViewBoxHeight } from './view-box-utils';
 interface MapProps {
     geoDocuments: Document[]
 }
+const Map: React.FC<MapProps> = ({ geoDocuments }) => {
 
-const Map: React.FC<MapProps> = ({ documents }) => {
-
+    const viewBox = defineViewBox(geoDocuments);
+  
     return (
-        <Box style={ styles.card }>
-            {geoDocuments && geoDocuments.map(document => (
-                <HStack key={ document._id } space={ 2 }>
-                    <Text>{document.resource.id}</Text>
-                    <Text>{document.resource.shortDescription}</Text>
-                </HStack>
-            ))}
+        <Box style={ styles.container }>
+            {geoDocuments ?
+                <Svg style={ styles.svg } viewBox={ viewBox } preserveAspectRatio="none">
+                    {geoDocuments.map(doc =>(
+                        <G key={ doc._id }>
+                            {renderGeoSvgElement(doc, viewBox)}
+                        </G>))
+                    }
+                </Svg> :
+                <Text>No docs available</Text>
+            }
         </Box>
     );
 };
 
 const styles = StyleSheet.create({
-    card: {
-        width: '90%',
-        alignItems: 'center'
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
     },
+    svg: {
+        height: 500,
+        width: 500,
+        borderWidth: 1,
+        borderColor: 'black'
+    }
 });
+
+const renderGeoSvgElement = (document: Document, viewBox: string): ReactElement => {
+    
+    const viewBoxHeight = getViewBoxHeight(viewBox);
+    const geometry: FieldGeometry = document.resource.geometry;
+
+    switch(geometry.type){
+        case('Polygon'):
+            return <GeoPolygon
+                coordinates={ geometry.coordinates }
+                fill="blue"
+                viewBoxHeight={ viewBoxHeight } />;
+        case('MultiPolygon'):
+            return <GeoMultiPolygon
+                coordinates={ geometry.coordinates }
+                fill="red"
+                viewBoxHeight={ viewBoxHeight } />;
+        case('LineString'):
+            return <GeoLineString
+                coordinates={ geometry.coordinates }
+                stroke="green"
+                viewBoxHeight={ viewBoxHeight } />;
+        case('MultiLineString'):
+            return <GeoMultiLineString
+                coordinates={ geometry.coordinates }
+                stroke="green"
+                viewBoxHeight={ viewBoxHeight } />;
+        case('Point'):
+            return <GeoPoint
+                point={ geometry.coordinates }
+                fill="black"
+                viewBoxHeight={ viewBoxHeight } />;
+        case('MultiPoint'):
+            return <GeoMultiPoint
+                points={ geometry.coordinates }
+                fill="yellow"
+                viewBoxHeight={ viewBoxHeight } />;
+        default:
+            return <Circle />;
+
+    }
+};
+
 
 export default Map;

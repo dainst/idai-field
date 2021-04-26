a5721e01672baf1ed73cc744a2d26bdd86502231
Michael <michael.kihm@web.de>
2021-04-26 11:46:06 +0200

    Svg scaling by pinching

--- mobile/src/components/SvgMap/SvgMap.tsx
+++ mobile/src/components/SvgMap/SvgMap.tsx
@@ -1,60 +1,104 @@
 import React, { useRef } from 'react';
-import { Animated, Dimensions, PanResponder, StyleSheet } from 'react-native';
+import { Animated, Dimensions, StyleSheet } from 'react-native';
+import {
+    PanGestureHandler, PanGestureHandlerStateChangeEvent, PinchGestureHandler,
+    PinchGestureHandlerStateChangeEvent, State
+} from 'react-native-gesture-handler';
 import Svg, { G, SvgProps } from 'react-native-svg';
 
 
 const SvgMap: React.FC<SvgProps> = ( props ) => {
 
-    const pan = useRef<Animated.ValueXY>(new Animated.ValueXY()).current;
+    // pan & translation
+    const translateX = useRef<Animated.Value>(new Animated.Value(0)).current;
+    const translateY = useRef<Animated.Value>(new Animated.Value(0)).current;
+    const lastOffset = { x: 0, y: 0 };
+
+    // scaling
+    const baseScale = useRef<Animated.Value>(new Animated.Value(1)).current;
+    const pinchScale = useRef<Animated.Value>(new Animated.Value(1)).current;
+    const scale = Animated.multiply(baseScale, pinchScale);
+    let lastScale = 1;
+
     const AG = Animated.createAnimatedComponent(G);
+    const AnimatedSvg = Animated.createAnimatedComponent(Svg);
     const { width, height } = Dimensions.get('screen');
 
-    const panResponder = useRef(
-        PanResponder.create({
-            onMoveShouldSetPanResponder: () => true,
-            onPanResponderGrant: () => {
-                pan.setOffset({
-                    x: pan.x._value,
-                    y: pan.y._value
-                });
+    // pan & translation events
+    const onGestureEvent = Animated.event(
+        [
+          {
+            nativeEvent: {
+              translationX: translateX,
+              translationY: translateY,
             },
-            onPanResponderMove: (event, gesture) => {
-                pan.x.setValue(gesture.dx * 100 / width);
-                pan.y.setValue(gesture.dy * 100 / height);
-            },
-            onPanResponderRelease: () => {
-                pan.flattenOffset();
-            }
-        })
-    ).current;
+          },
+        ],
+        { useNativeDriver: false }
+    );
     
 
+    const onHandlerStateChange = (event: PanGestureHandlerStateChangeEvent) => {
+
+        if (event.nativeEvent.oldState === State.ACTIVE) {
+            lastOffset.x += event.nativeEvent.translationX;
+            lastOffset.y += event.nativeEvent.translationY;
+            translateX.setOffset(lastOffset.x);
+            translateX.setValue(0);
+            translateY.setOffset(lastOffset.y);
+            translateY.setValue(0);
+        }
+    };
+
+    
+    // pinch & scaling events
+    const onPinchGestureEvent = Animated.event(
+        [{ nativeEvent: { scale: pinchScale } }],
+        { useNativeDriver: false }
+    );
+
+
+    const onPinchHandlerStateChange = (event: PinchGestureHandlerStateChangeEvent) => {
+        
+        if (event.nativeEvent.oldState === State.ACTIVE) {
+            lastScale *= event.nativeEvent.scale;
+            baseScale.setValue(lastScale);
+            pinchScale.setValue(1);
+        }
+      };
+
+    
     const animatedStyle = {
         transform: [
           {
-            translateY: pan.y
+            translateY: Animated.multiply(translateY, 100 / height)
           },
           {
-            scaleX: 1
+            scale: scale
           },
           {
-            scaleY: 1
-          },
-          {
-            translateX: pan.x
+            translateX: Animated.multiply(translateX, 100 / width)
           }
         ]
     };
 
+
     return (
-        <Animated.View style={ styles.animatedViewStyle } { ...panResponder.panHandlers }>
-            <Svg viewBox={ props.viewBox } >
-                <AG style={ animatedStyle } >
-                    {props.children}
-                </AG>
-            </Svg>
-        </Animated.View>
-    );
+        <PanGestureHandler
+            onGestureEvent={ onGestureEvent }
+            onHandlerStateChange={ onHandlerStateChange }>
+            <PinchGestureHandler
+                onGestureEvent={ onPinchGestureEvent }
+                onHandlerStateChange={ onPinchHandlerStateChange }>
+                <Animated.View style={ styles.animatedViewStyle } >
+                    <AnimatedSvg viewBox={ props.viewBox } >
+                        <AG style={ animatedStyle } >
+                            {props.children}
+                        </AG>
+                    </AnimatedSvg>
+                </Animated.View>
+            </PinchGestureHandler>
+        </PanGestureHandler>);
 };
 
 

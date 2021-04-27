af9befd753bf4b4d6ee3e9fa96b1e767e0602b3a
Sebastian Cuy <sebastian.cuy@dainst.de>
2021-04-27 17:26:55 +0200

    Implement simple project selector on HomeScreen

--- mobile/src/screens/HomeScreen.tsx
+++ mobile/src/screens/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import { StackNavigationProp } from '@react-navigation/stack';
 import { AppStackParamList } from 'mobile/App';
-import { Button, Center } from 'native-base';
-import React, { SetStateAction, useCallback } from 'react';
+import { Button, Center, Column, Icon, IconButton, Row, Select, View } from 'native-base';
+import React, { SetStateAction, useCallback, useState } from 'react';
 import { update } from 'tsfun';
 import { Settings } from '../model/settings';
 
@@ -14,6 +14,7 @@ interface HomeScreenProps {
 
 const HomeScreen: React.FC<HomeScreenProps> = ({ navigation, setSettings }) => {
 
+    const [selectedProject, setSelectedProject] = useState<string>('');
 
     const openProject = useCallback((project: string) => {
 
@@ -23,11 +24,50 @@ const HomeScreen: React.FC<HomeScreenProps> = ({ navigation, setSettings }) => {
 
 
     return (
-        <Center flex={ 1 } safeArea>
-            <Button size="lg" colorScheme="lightBlue" onPress={ () => openProject('test467') }>
-                Open test project
-            </Button>
-        </Center>
+        <View flex={ 1 } safeArea>
+            <Center flex={ 1 }>
+                <Column space={ 3 }>
+                    <Center rounded="lg" p={ 5 } bg="gray.200">
+                        <Row space={ 3 }>
+                            <Center flex={ 1 } _text={ { fontWeight: 'bold' } }>
+                                Open existing project:
+                            </Center>
+                            <Select
+                                selectedValue={ selectedProject }
+                                minWidth={ 200 }
+                                onValueChange={ setSelectedProject }
+                            >
+                                <Select.Item label="test467" value="test467" />
+                                <Select.Item label="meninx-project" value="meninx-project" />
+                                <Select.Item label="uruk" value="uruk" />
+                                <Select.Item label="asdf" value="asdf" />
+                            </Select>
+                            <Center flex={ 1 }>
+                                <IconButton
+                                    colorScheme="blue"
+                                    variant="solid"
+                                    icon={ <Icon name="folder-open" size={ 6 } type="Ionicons" color="white" /> }
+                                    onPress={ () => openProject(selectedProject) }
+                                />
+                            </Center>
+                        </Row>
+                    </Center>
+                    <Button
+                        colorScheme="green"
+                        startIcon={ <Icon name="add-circle" size={ 6 } type="Ionicons" color="white" /> }
+                    >
+                        Create new project
+                    </Button>
+                    <Button
+                        colorScheme="yellow"
+                        startIcon={ <Icon name="folder-open" size={ 6 } type="Ionicons" /> }
+                        onPress={ () => openProject('test') }
+                    >
+                        Open test project
+                    </Button>
+                </Column>
+            </Center>
+        </View>
     );
 };
 

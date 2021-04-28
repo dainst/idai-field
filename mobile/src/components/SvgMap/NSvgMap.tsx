cfa35678cb0d160769d4e197018415d1994d65db
Michael <michael.kihm@web.de>
2021-04-28 12:56:46 +0200

    Translate width fixed window size

--- mobile/src/components/SvgMap/NSvgMap.tsx
+++ mobile/src/components/SvgMap/NSvgMap.tsx
@@ -1,63 +1,143 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import {
-    Animated, Dimensions
+    Animated, GestureResponderEvent, PanResponder, PanResponderGestureState, StyleSheet
 } from 'react-native';
 import Svg, { G, SvgProps } from 'react-native-svg';
 import { getViewPortTransform } from './viewbox-utils';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const no = () => {};
+const yes = () => true;
 
 
 const NSvgMap: React.FC<SvgProps> = ( props ) => {
 
     //absolute positions
-    const [left, setLeft] = useState<number>(0);
-    const [top, setTop] = useState<number>(0);
-    const [zoom, setZoom] = useState<number>(1);
+    const left = useRef<Animated.Value>(new Animated.Value(0)).current;
+    const top = useRef<Animated.Value>(new Animated.Value(0)).current;
+    const zoom = useRef<Animated.Value>(new Animated.Value(1)).current;
+
 
     //viewbox transform only set at beginning
-    const [translateX, setTranslateX] = useState<number>(0);
-    const [translateY, setTranslateY] = useState<number>(0);
-    const [scaleX, setScaleX] = useState<number>(1);
-    const [scaleY, setScaleY] = useState<number>(1);
+    const translateX = useRef<Animated.Value>(new Animated.Value(0)).current;
+    const translateY = useRef<Animated.Value>(new Animated.Value(0)).current;
+    const scaleX = useRef<Animated.Value>(new Animated.Value(0)).current;
+    const scaleY = useRef<Animated.Value>(new Animated.Value(0)).current;
+
 
     //boolean to init gestures
-    const [isZooming, setIsZooming] = useState<boolean>(true);
-    const [isMoving, setIsMoving] = useState<boolean>(false);
+    const isZooming = useRef<Animated.Value>(new Animated.Value(0)).current;
+    const isMoving = useRef<Animated.Value>(new Animated.Value(0)).current;
 
     //reference values set at the beginning of the gesture
-    const [initialX, setInitialX] = useState<number>(0);
-    const [initialY, setInitialY] = useState<number>(0);
-    const [initialDistance, setInitialDistance] = useState<number>(1);
+    const initialX = useRef<Animated.Value>(new Animated.Value(0)).current;
+    const initialY = useRef<Animated.Value>(new Animated.Value(0)).current;
+    const initialLeft = useRef<Animated.Value>(new Animated.Value(0)).current;
+    const initialTop = useRef<Animated.Value>(new Animated.Value(0)).current;
 
-    const { width, height } = Dimensions.get('screen');
 
-    useState(()=> {
+    // const { width, height } = Dimensions.get('window');
+    // console.log(width, height);
+    const width = 1000;
+    const height = 1000;
+    const moveThreshold = 5;
+    const AG = Animated.createAnimatedComponent(G);
+
+    useEffect(() => {
         const transforms = getViewPortTransform(props.viewBox, props.preserveAspectRatio, width, height);
-        setTranslateX(transforms.translateX);
-        setTranslateY(transforms.translateY);
-        setScaleX(transforms.scaleX);
-        setScaleY(transforms.scaleY);
+        translateX.setValue(transforms.translateX);
+        translateY.setValue(transforms.translateY);
+        scaleY.setValue(transforms.scaleY);
+        scaleX.setValue(transforms.scaleX);
     });
+    
+   
+    const shouldRespond = (e: GestureResponderEvent, gestureState: PanResponderGestureState):boolean =>
+        e.nativeEvent.touches.length === 2 ||
+        Math.pow(gestureState.dx,2) + Math.pow(gestureState.dy,2) >= moveThreshold;
+
+
+    const panResponder = useRef(PanResponder.create({
+        onPanResponderGrant: no,
+        onPanResponderTerminate: no,
+        onShouldBlockNativeResponder: yes,
+        onPanResponderTerminationRequest: yes,
+        onMoveShouldSetPanResponder: shouldRespond,
+        onStartShouldSetPanResponder: shouldRespond,
+        onMoveShouldSetPanResponderCapture: shouldRespond,
+        onStartShouldSetPanResponderCapture: shouldRespond,
+        onPanResponderMove: e => {
+            const { nativeEvent: { touches } } = e;
+            if(touches.length === 1){
+                const [{ pageX, pageY }] = touches;
+                touchHandler(pageX, pageY);
+            } else return;
+            e.preventDefault();
+        },
+        onPanResponderRelease: () => {
+            isMoving.setValue(0);
+            isZooming.setValue(0);
+        }
+    })).current;
+
+    
+    const touchHandler = (x: number, y: number): void => {
+
+        if(!isMoving._value || isZooming._value){
+            isMoving.setValue(1);
+            isZooming.setValue(0);
+            initialLeft.setValue(left._value);
+            initialTop.setValue(top._value);
+            initialX.setValue(x);
+            initialY.setValue(y);
+        } else {
+            //console.log('inside handler');
+            const dx = x - initialX._value;
+            const dy = y - initialY._value;
+
+            left.setValue(initialLeft._value + dx);
+            top.setValue(initialTop._value + dy );
+        }
+    };
+
+    const animatedStyle = {
+        transform: [
+          {
+            translateY: Animated.add(top, Animated.multiply(zoom, translateY))
+          },
+          {
+            scaleX: Animated.multiply(scaleX, zoom)
+          },
+          {
+            scaleY: Animated.multiply(scaleY, zoom)
+          },
+          {
+            translateX: Animated.add(left, Animated.multiply(zoom, translateX))
+          }
+        ]
+    };
+    
 
     return (
-        <Animated.View style={ props.style }>
-            <Svg height={ height } width={ width }>
-                <G { ...getZoomTransform(left, top, zoom, scaleX, scaleY, translateX, translateY) }>
+        <Animated.View style={ props.style } { ...panResponder.panHandlers } >
+            <Svg >
+                {/* <AG transform={ getZoomTransform(left._value, top._value, zoom._value, scaleX._value, scaleY._value, translateX._value, translateY._value) }> */}
+                <AG style={ animatedStyle }>
                     {props.children}
-                </G>
+                </AG>
             </Svg>
         </Animated.View>
     );
 };
 
+const styles = StyleSheet.create({
+    box: {
+        backgroundColor: '#61dafb',
+        width: 80,
+        height: 80,
+        borderRadius: 4,
+      },
+});
 
-const getZoomTransform = (left: number,top: number,zoom: number,scaleX: number,scaleY:
-                        number,translateX: number,translateY: number) => {
-        return {
-            translateX: left + zoom * translateX,
-            translateY: top + zoom * translateY,
-            scaleX: zoom * scaleX,
-            scaleY: zoom * scaleY,
-        };
-};
 
 export default NSvgMap;

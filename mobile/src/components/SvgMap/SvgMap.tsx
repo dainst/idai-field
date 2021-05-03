298082a7fdd45737d35d0532cd18bf53c181fe70
Michael <michael.kihm@web.de>
2021-05-03 08:35:59 +0200

    Add useRef for initial values

--- mobile/src/components/SvgMap/SvgMap.tsx
+++ mobile/src/components/SvgMap/SvgMap.tsx
@@ -11,6 +11,11 @@ import { getViewPortTransform } from './viewbox-utils/viewbox-utils';
 const no = () => {};
 const yes = () => true;
 
+interface Coordinate {
+    x: number;
+    y: number;
+}
+
 
 const SvgMap: React.FC<SvgProps> = ( props ) => {
 
@@ -28,16 +33,15 @@ const SvgMap: React.FC<SvgProps> = ( props ) => {
 
 
     //boolean to init gestures
-    const isZooming = useRef<Animated.Value>(new Animated.Value(0)).current;
-    const isMoving = useRef<Animated.Value>(new Animated.Value(0)).current;
+    const isZooming = useRef<boolean>(false);
+    const isMoving = useRef<boolean>(false);
 
     //reference values set at the beginning of the gesture
-    const initialX = useRef<Animated.Value>(new Animated.Value(0)).current;
-    const initialY = useRef<Animated.Value>(new Animated.Value(0)).current;
-    const initialLeft = useRef<Animated.Value>(new Animated.Value(0)).current;
-    const initialTop = useRef<Animated.Value>(new Animated.Value(0)).current;
-    const initialZoom = useRef<Animated.Value>(new Animated.Value(0)).current;
-    const initialDistance = useRef<Animated.Value>(new Animated.Value(0)).current;
+    const initialTouch = useRef<Coordinate>({ x:0, y:0 });
+    const initialLeft = useRef<number>(0);
+    const initialTop = useRef<number>(0);
+    const initialZoom = useRef<number>(0);
+    const initialDistance = useRef<number>(0);
 
     const moveThreshold = 5;
     const AG = Animated.createAnimatedComponent(G);
@@ -80,27 +84,26 @@ const SvgMap: React.FC<SvgProps> = ( props ) => {
             e.preventDefault();
         },
         onPanResponderRelease: () => {
-            isMoving.setValue(0);
-            isZooming.setValue(0);
+            isMoving.current = false;
+            isZooming.current = false;
         }
     })).current;
 
     
     const touchHandler = (x: number, y: number): void => {
 
-        if(!(isMoving as any)._value || (isZooming as any)._value){
-            isMoving.setValue(1);
-            isZooming.setValue(0);
-            initialLeft.setValue((left as any)._value);
-            initialTop.setValue((top as any)._value);
-            initialX.setValue(x);
-            initialY.setValue(y);
+        if(!isMoving.current || isZooming.current){
+            isMoving.current = true;
+            isZooming.current = false;
+            initialLeft.current = (left as any)._value;
+            initialTop.current = (top as any)._value;
+            initialTouch.current = { x, y };
         } else {
-            const dx = Animated.subtract(x,initialX);
-            const dy = Animated.subtract(y,initialY);
+            const dx = Animated.subtract(x,initialTouch.current.x);
+            const dy = Animated.subtract(y,initialTouch.current.y);
 
-            left.setValue((Animated.add(initialLeft, dx) as any).__getValue());
-            top.setValue((Animated.add(initialTop, dy) as any).__getValue());
+            left.setValue((Animated.add(initialLeft.current, dx) as any).__getValue());
+            top.setValue((Animated.add(initialTop.current, dy) as any).__getValue());
         }
     };
 
@@ -108,22 +111,21 @@ const SvgMap: React.FC<SvgProps> = ( props ) => {
         const distance = calcDistance(x1, y1, x2, y2);
         const { x, y } = calcCenter(x1, y1, x2, y2);
 
-        if(!(isZooming as any)._value){
-            isZooming.setValue(1);
-            initialX.setValue(x);
-            initialY.setValue(y);
-            initialTop.setValue((top as any)._value);
-            initialLeft.setValue((left as any)._value);
-            initialZoom.setValue((zoom as any)._value);
-            initialDistance.setValue(distance);
+        if(!isZooming.current){
+            isZooming.current = true;
+            initialTouch.current = { x,y };
+            initialTop.current = (top as any)._value;
+            initialLeft.current = (left as any)._value;
+            initialZoom.current = (zoom as any)._value;
+            initialDistance.current = distance;
         } else {
-            const touchZoom = distance / (initialDistance as any)._value;
-            const dx = x - (initialX as any)._value;
-            const dy = y - (initialY as any)._value;
+            const touchZoom = distance / initialDistance.current;
+            const dx = x - initialTouch.current.x;
+            const dy = y - initialTouch.current.y;
 
-            left.setValue( ((initialLeft as any)._value + dx - x) * touchZoom + x);
-            top.setValue ( ((initialTop as any)._value + dy - y) * touchZoom + y);
-            zoom.setValue ( (initialZoom as any)._value * touchZoom);
+            left.setValue( (initialLeft.current + dx - x) * touchZoom + x);
+            top.setValue ( (initialTop.current + dy - y) * touchZoom + y);
+            zoom.setValue ( initialZoom.current * touchZoom);
         }
     };
 

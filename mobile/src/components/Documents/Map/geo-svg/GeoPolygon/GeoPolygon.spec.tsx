d6669f7277dd38b62c19b6798fa0072725120270
Michael <michael.kihm@web.de>
2021-05-05 08:25:58 +0200

    Move GeoPolygon component and test to separate folder

--- mobile/src/components/Documents/Map/geo-svg/GeoPolygon/GeoPolygon.spec.tsx
+++ mobile/src/components/Documents/Map/geo-svg/GeoPolygon/GeoPolygon.spec.tsx
@@ -2,7 +2,7 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import { Path } from 'react-native-svg';
-import { GeoElementsCommonProps } from './common-props';
+import { GeoElementsCommonProps } from '../common-props';
 import { GeoPolygon } from './GeoPolygon';
 
 
--- mobile/src/components/Documents/Map/geo-svg/GeoPolygon/GeoPolygon.tsx
+++ mobile/src/components/Documents/Map/geo-svg/GeoPolygon/GeoPolygon.tsx
@@ -1,8 +1,8 @@
 import { Position } from 'geojson';
 import React from 'react';
 import { CommonPathProps, Path } from 'react-native-svg';
-import { polygonToPath } from '../geojson-path/geojson-svg-path';
-import { GeoElementsCommonProps } from './common-props';
+import { polygonToPath } from '../../geojson-path/geojson-svg-path';
+import { GeoElementsCommonProps } from '../common-props';
 
 interface GeoPolygonProps extends CommonPathProps, GeoElementsCommonProps {}
 

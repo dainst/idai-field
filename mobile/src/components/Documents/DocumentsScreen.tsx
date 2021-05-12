d61f3ea924805c5940a19ed46a9a1a8eb0b04433
Sebastian Cuy <sebastian.cuy@dainst.de>
2021-05-12 09:00:47 +0200

    Move hooks to DocumentScreen

--- mobile/src/components/Documents/DocumentsScreen.tsx
+++ mobile/src/components/Documents/DocumentsScreen.tsx
@@ -1,21 +1,53 @@
-import { ProjectConfiguration, SyncStatus } from 'idai-field-core';
 import React from 'react';
-import { ProjectSettings } from '../../models/preferences';
-import { DocumentRepository } from '../../repositories/document-repository';
+import useConfiguration from '../../hooks/use-configuration';
+import usePouchdbManager from '../../hooks/use-pouchdb-manager';
+import useRepository from '../../hooks/use-repository';
+import useSync from '../../hooks/use-sync';
+import { Preferences, ProjectSettings } from '../../models/preferences';
 import DocumentsContainer from './DocumentsContainer';
 
 
 interface DocumentsScreenProps {
-    repository?: DocumentRepository;
-    syncStatus: SyncStatus;
-    projectSettings: ProjectSettings;
-    config?: ProjectConfiguration;
+    preferences: Preferences;
     setProjectSettings: (projectSettings: ProjectSettings) => void;
 }
 
 
-const DocumentsScreen: React.FC<DocumentsScreenProps> = ({ repository, config, ...props }) =>
-    (repository && config) ? <DocumentsContainer { ... { ...props, config, repository } } /> : null;
+const DocumentsScreen: React.FC<DocumentsScreenProps> = ({ preferences, setProjectSettings }) => {
+
+    const pouchdbManager = usePouchdbManager(preferences.currentProject);
+
+    const config = useConfiguration(
+        preferences.currentProject,
+        preferences.languages,
+        preferences.username,
+        pouchdbManager,
+    );
+
+    const repository = useRepository(
+        preferences.currentProject,
+        preferences.username,
+        config?.getCategoryForest() || [],
+        pouchdbManager,
+    );
+
+    const syncStatus = useSync(
+        preferences.currentProject,
+        preferences.projects[preferences.currentProject],
+        repository,
+        pouchdbManager
+    );
+
+    return (repository && config)
+        ? <DocumentsContainer
+            projectSettings={ preferences.projects[preferences.currentProject] }
+            setProjectSettings={ setProjectSettings }
+            config={ config }
+            repository={ repository }
+            syncStatus={ syncStatus }
+        />
+        : null;
+};
 
 
 export default DocumentsScreen;

5989edc692242cb35696cee39ed4fa6cd5f5556a
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-05-12 12:07:53 +0200

    Show main image badge and refresh after setting main img

--- desktop/src/app/components/viewmodal/image/image-view-modal.component.ts
+++ desktop/src/app/components/viewmodal/image/image-view-modal.component.ts
@@ -91,9 +91,9 @@ export class ImageViewModalComponent extends ViewModalComponent {
             this.linkedDocument.resource.relations[Relations.Image.ISDEPICTEDIN].filter(targetId => {
                 return targetId !== mainImageId;
             })
-        ); // TODO persist!
+        );
 
-        // this.loadImages(); TODO
+        this.loadImages();
     }
 
 

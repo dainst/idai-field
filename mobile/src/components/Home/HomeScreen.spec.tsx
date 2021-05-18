679c24f3e7022ea15eabf626806d99fefb859051
Sebastian Cuy <sebastian.cuy@dainst.de>
2021-05-18 08:56:21 +0200

    Add addtional test for HomeScreen

--- mobile/src/components/Home/HomeScreen.spec.tsx
+++ mobile/src/components/Home/HomeScreen.spec.tsx
@@ -5,23 +5,35 @@ import HomeScreen from './HomeScreen';
 describe('HomeScreen', () => {
 
     it('displays warning if username is not set', () => {
-        const props = {
-                preferences: {
-                username: '',
-                languages: [],
-                currentProject: '',
-                recentProjects: [],
-                projects: {}
-            },
-            navigation: {},
-            setCurrentProject: (_: string) => { return; },
-            deleteProject: (_: string) => { return; },
-            navigate: (_: string) => { return; }
-        };
-        const { getByText } = render(<HomeScreen { ... props } />);
+        
+        const { getByText } = render(<HomeScreen { ... mockProps() } />);
         const warning = getByText('Make sure to set your name!');
 
         expect(warning).toBeTruthy();
     });
 
+    it('does not display warning if username is set', () => {
+
+        const props = mockProps();
+        props.preferences.username = 'Test User';
+        const { queryByText } = render(<HomeScreen { ... props } />);
+        const warning = queryByText('Make sure to set your name!');
+
+        expect(warning).toBeNull();
+    });
+
+});
+
+
+const mockProps = () => ({
+    preferences: {
+        username: '',
+        languages: [],
+        currentProject: '',
+        recentProjects: [],
+        projects: {}
+    },
+    setCurrentProject: (_: string) => { return; },
+    deleteProject: (_: string) => { return; },
+    navigate: (_: string) => { return; }
 });

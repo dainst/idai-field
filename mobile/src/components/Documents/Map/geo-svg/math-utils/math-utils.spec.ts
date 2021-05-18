ba76d019640b855f5a7ac3d018c074f746d24d3c
Michael <michael.kihm@web.de>
2021-05-18 15:38:13 +0200

    Add area computation for GeoJSON elements

---
+++ mobile/src/components/Documents/Map/geo-svg/math-utils/math-utils.spec.ts
@@ -0,0 +1,26 @@
+import { pointRadius } from '../constants';
+import { pointArea, polygonArea } from './math-utils';
+
+describe('geo-svg/math-utils', () => {
+
+    it('should calculate the point area correctly', () => {
+        
+        expect(pointArea()).toBe(Math.PI * Math.pow(pointRadius,2));
+    });
+
+    it('should calculate the area of a polygon regarding https://en.wikipedia.org/wiki/Polygon#Area', () => {
+
+        const polygon = [
+            [[2,11],[17,13],[11,2],[2,2]]
+        ];
+        expect(polygonArea(polygon)).toBe(117);
+    });
+
+    it('should calculate the area of a polygon with holes', () => {
+        const polygon = [
+            [[2,2],[2,5],[5,9],[8,7],[9,4],[5,2]],//32.5
+            [[4,2],[4,5],[5,6],[6,5],[5,4],[6,3]]//5
+        ];
+        expect(polygonArea(polygon)).toBe(32.5 - 5);
+    });
+});
---
+++ mobile/src/components/Documents/Map/geo-svg/math-utils/math-utils.ts
@@ -0,0 +1,37 @@
+import { Position } from 'geojson';
+import { pointRadius } from '../constants';
+
+/**
+ * polygonArea
+ * Exterior ring coordinates need to be converted to 'cut' holes in polygon
+ * with inner ring coordinates (https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/fill-rule).
+ * Therefore, outer ring coordinates are the area of the polygon. The inner ring coords represent holes and
+ * have therefore be subtracted from the area formed by the outer ring coords
+ * @param polygon GeoJSON polygon
+ * @returns area of the polygon
+ */
+export const polygonArea = (polygon: Position[][]): number => {
+    
+    const outerArea = lineStringArea(polygon[0]);
+    let innerAreas = 0;
+    for(let i = 1; i < polygon.length; i++){
+        innerAreas += lineStringArea(polygon[i]);
+    }
+    return outerArea - innerAreas;
+};
+
+const lineStringArea = (lineString: Position[]) => {
+    
+    let sum = 0;
+    const lineStringLength = lineString.length;
+    for(let i = 0; i < lineStringLength; i++){
+        const [x_i, y_i] = lineString[i];
+        const [x_i1, y_i1] = lineString[(i + 1) % lineStringLength];
+        sum += x_i * y_i1 - x_i1 * y_i;
+    }
+    return Math.abs(sum) / 2;
+};
+
+export const pointArea = (): number => Math.PI * Math.pow(pointRadius,2);
+
+export const lineArea = (): 0 => 0;

c8c0b0cfa7553da17c10222c0960a09e17baabd1
Sebastian Cuy <sebastian.cuy@dainst.de>
2021-05-20 11:21:06 +0200

    Add CategoryIcon component

---
+++ mobile/src/components/common/CategoryIcon.tsx
@@ -0,0 +1,37 @@
+import { Document, ProjectConfiguration } from 'idai-field-core';
+import React, { ReactElement } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+interface CategoryIconProps {
+    config: ProjectConfiguration;
+    document: Document;
+    size: number;
+}
+
+
+const CategoryIcon = ({ config, document, size }: CategoryIconProps): ReactElement => {
+
+    const styles = getStyles(config, document, size);
+
+    return <View style={ styles.container }>
+        <Text style={ styles.text }>{ document.resource.category[0].toUpperCase() }</Text>
+    </View>;
+};
+
+export default CategoryIcon;
+
+
+const getStyles = (config: ProjectConfiguration, document: Document, size: number) => StyleSheet.create({
+    container: {
+        height: size,
+        width: size,
+        backgroundColor: config.getColorForCategory(document.resource.category),
+        borderRadius: size / 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: config.getTextColorForCategory(document.resource.category),
+    },
+});

36c10cf52faa68a60c50423447220bf9eb006cf7
Sebastian Cuy <sebastian.cuy@dainst.de>
2021-05-20 10:26:57 +0200

    Remove native base from SyncSettingsButton

--- mobile/src/components/Documents/Sync/SyncSettingsButton.tsx
+++ mobile/src/components/Documents/Sync/SyncSettingsButton.tsx
@@ -1,7 +1,8 @@
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import { SyncStatus } from 'idai-field-core';
-import { Icon, IconButton } from 'native-base';
 import React, { useState } from 'react';
 import { ProjectSettings } from '../../../models/preferences';
+import Button from '../../common/Button';
 import SyncSettingsModal from './SyncSettingsModal';
 
 
@@ -32,9 +33,9 @@ const SyncSettingsButton: React.FC<SyncSettingsButtonProps> = ({
             onClose={ () => setShowSettings(false) }
         />
 
-        <IconButton
-            variant="ghost"
-            icon={ getSyncStatusIcon(status) }
+        <Button
+            variant="transparent"
+            icon={ React.cloneElement(getSyncStatusIcon(status), { size: 18 }) }
             onPress={ () => setShowSettings(true) } />
 
     </>);
@@ -45,15 +46,15 @@ const getSyncStatusIcon = (syncStatus: SyncStatus) => {
     
     switch (syncStatus) {
         case SyncStatus.Offline:
-            return <Icon type="MaterialIcons" name="cloud-off" />;
+            return <MaterialIcons name="cloud-off" />;
         case SyncStatus.Pulling:
-            return <Icon type="MaterialIcons" name="cloud-download" />;
+            return <MaterialIcons name="cloud-download" />;
         case SyncStatus.Pushing:
-            return <Icon type="MaterialIcons" name="cloud-upload" />;
+            return <MaterialIcons name="cloud-upload" />;
         case SyncStatus.InSync:
-            return <Icon type="MaterialCommunityIcons" name="cloud-check" />;
+            return <MaterialCommunityIcons name="cloud-check" />;
         default:
-            return <Icon type="MaterialCommunityIcons" name="cloud-alert" />;
+            return <MaterialCommunityIcons name="cloud-alert" />;
     }
 };
 

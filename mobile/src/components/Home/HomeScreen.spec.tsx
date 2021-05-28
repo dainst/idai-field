9f6fe9733c416c120091b352c78118dce4bed557
Sebastian Cuy <sebastian.cuy@dainst.de>
2021-05-28 15:27:43 +0200

    Add additional tests for HomeSreen

--- mobile/src/components/Home/HomeScreen.spec.tsx
+++ mobile/src/components/Home/HomeScreen.spec.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Preferences } from '../../models/preferences';
 import { render } from '../../utils/test-utils';
 import HomeScreen from './HomeScreen';
 
@@ -21,6 +22,24 @@ describe('HomeScreen', () => {
 
         expect(warning).toBeNull();
     });
+    
+    it('does not display picker when no projects are present', () => {
+        
+        const { queryByText } = render(<HomeScreen { ... mockProps() } />);
+        const warning = queryByText('Open existing project:');
+
+        expect(warning).toBeNull();
+    });
+    
+    it('displays picker when no projects are present', () => {
+        
+        const props = mockProps();
+        props.preferences.recentProjects = ['project-1', 'project-2'];
+        const { getByText } = render(<HomeScreen { ... props } />);
+        const warning = getByText('Open existing project:');
+
+        expect(warning).toBeTruthy();
+    });
 
 });
 
@@ -32,7 +51,7 @@ const mockProps = () => ({
         currentProject: '',
         recentProjects: [],
         projects: {}
-    },
+    } as Preferences,
     setCurrentProject: (_: string) => { return; },
     deleteProject: (_: string) => { return; },
     navigate: (_: string) => { return; }

71960d6dfdaffc14e2939b484bf9c5a51b9025cf
Michael <michael.kihm@web.de>
2021-05-31 09:22:11 +0200

    Setup useMapData added and tested

---
+++ mobile/src/hooks/use-mapdata.spec.ts
@@ -0,0 +1,63 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { identityMatrix4 } from 'react-native-redash';
+import { mocked } from 'ts-jest/utils';
+import { bu1 } from '../../test_data/test_docs/bu1';
+import { lineBuilding } from '../../test_data/test_docs/lineBuilding';
+import { multiPolyTrench } from '../../test_data/test_docs/multiPolyTrench';
+import { pointBuilding } from '../../test_data/test_docs/pointBuilding';
+import { r1 } from '../../test_data/test_docs/r1';
+import { si1 } from '../../test_data/test_docs/si1';
+import { DocumentRepository } from '../repositories/document-repository';
+import useMapData from './use-mapdata';
+
+
+const bu1Id = bu1.resource.id;
+const lineBuildingId = lineBuilding.resource.id;
+const multiPolyTrenchId = multiPolyTrench.resource.id;
+const pointBuildingId = pointBuilding.resource.id;
+const r1Id = r1.resource.id;
+const si1Id = si1.resource.id;
+
+jest.mock('../repositories/document-repository');
+
+
+describe('useMapData',() => {
+
+    const MockedDocumentRepository = mocked(DocumentRepository, true);
+
+    // eslint-disable-next-line max-len
+    const hook = () => renderHook(({ repository, viewPort, selectedDocIds }) => useMapData(repository, viewPort, selectedDocIds), {
+        initialProps: {
+            repository: new MockedDocumentRepository(),
+            viewPort: { x:0, y:0, width: 500, height: 12 },
+            selectedDocIds: [bu1Id, si1Id]
+        } });
+
+
+    it('should setup the hook correctly',async () => {
+        const { result, waitForNextUpdate } = hook();
+        await waitForNextUpdate();
+        
+        const [docIds, geoMap, transformMatrix] = result.current;
+        
+        //assert docIds
+        const expectedDocOrdering = [multiPolyTrenchId, si1Id, bu1Id, r1Id, pointBuildingId, lineBuildingId];
+        expect(docIds?.length).toEqual(expectedDocOrdering.length);
+        expect(docIds).toEqual(expectedDocOrdering);
+        
+        //assert transformation matrix
+        expect(transformMatrix).not.toEqual(identityMatrix4);
+
+        //assert geoMap
+        let docCnt = 0;
+        geoMap?.forEach((value, key) => {
+            if(key === bu1Id || key === si1Id) expect(value.isSelected).toEqual(true);
+            else expect(value.isSelected).toBeUndefined();
+
+            docCnt += 1;
+        });
+        expect(docCnt).toBe(docIds?.length);
+        
+    });
+});
+
--- mobile/src/hooks/use-mapdata.ts
+++ mobile/src/hooks/use-mapdata.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { Document, Query } from 'idai-field-core';
-import { useEffect, useRef, useState } from 'react';
-import { Matrix4 } from 'react-native-redash';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { identityMatrix4, Matrix4 } from 'react-native-redash';
 import {
     GeometryBoundings,
     getGeometryBoundings,
@@ -16,27 +16,39 @@ import usePrevious from './use-previous';
 const useMapData = (repository: DocumentRepository, viewPort: ViewPort, selectedDocIds: string[]):
     [string[] | undefined, GeoMap | null, Matrix4 | undefined] => {
     
-    const [transformMatrix, setTransformMatrix] = useState<Matrix4>();
+    const [transformMatrix, setTransformMatrix] = useState<Matrix4>(identityMatrix4);
     const [geoDocuments, setGeoDocuments] = useState<Document[]>();
     const [geometryBoundings, setGeometryBoundings] = useState<GeometryBoundings | null>(null);
 
-    const documentsGeoMap = useRef<GeoMap | null>(null);
+    const [documentsGeoMap, setDocumentsGeoMap] = useState<GeoMap | null>(null);
     const [docIds, setDocIds] = useState<string[]>();
 
-    const searchQuery: Query = {
+    const searchQuery: Query = useMemo(() => ({
         q: '*',
         constraints: { 'geometry:exist': 'KNOWN' }
-    };
+    }),[]);
 
     const previousSelectedDocIds = usePrevious(selectedDocIds);
+
+    const sortDocIdsByArea = useCallback(() => {
+
+        if(documentsGeoMap){
+            return Array.from(documentsGeoMap?.keys()).sort((a,b) => {
+                if(documentsGeoMap!.get(a)!.area > documentsGeoMap!.get(b)!.area) return -1;
+                else if(documentsGeoMap!.get(a)!.area < documentsGeoMap!.get(b)!.area) return 1;
+                else return 0;
+            });
+        }
+    },[documentsGeoMap]);
     
     useEffect(() => {
+
         repository.find(searchQuery)
             .then(result => {
                 setGeoDocuments(result.documents);
                 setGeometryBoundings(getGeometryBoundings(result.documents));
             });
-    });
+    },[searchQuery, repository]);
 
 
     useEffect(() => {
@@ -47,41 +59,33 @@ const useMapData = (repository: DocumentRepository, viewPort: ViewPort, selected
 
     useEffect(() => {
 
-        documentsGeoMap.current = setupGeoMap(geoDocuments, transformMatrix);
-        setDocIds(sortDocIdsByArea());
+        
+        setDocumentsGeoMap(setupGeoMap(geoDocuments, transformMatrix));
+
     },[geoDocuments, transformMatrix]);
 
     useEffect(() => {
 
         //set previously selected docs as not selected
-        if(previousSelectedDocIds){
-            for(const id of previousSelectedDocIds){
-                documentsGeoMap.current!.get(id)!.isSelected = false;
+        if(documentsGeoMap){
+            if(previousSelectedDocIds){
+                for(const id of previousSelectedDocIds){
+                    documentsGeoMap.get(id)!.isSelected = false;
+                }
             }
+
+            for(const id of selectedDocIds){
+                documentsGeoMap.get(id)!.isSelected = true;
+            }
+            setDocIds(sortDocIdsByArea());
         }
 
-        for(const id of selectedDocIds){
-            documentsGeoMap.current!.get(id)!.isSelected = true;
-        }
-        setDocIds(sortDocIdsByArea());
-
-    }, [previousSelectedDocIds, selectedDocIds]);
-
-
-    const sortDocIdsByArea = () => {
-
-        if(documentsGeoMap.current){
-            return Array.from(documentsGeoMap.current?.keys()).sort((a,b) => {
-                if(documentsGeoMap.current!.get(a)!.area > documentsGeoMap.current!.get(b)!.area) return -1;
-                else if(documentsGeoMap.current!.get(a)!.area < documentsGeoMap.current!.get(b)!.area) return 1;
-                else return 0;
-            });
-        }
-    };
+    }, [previousSelectedDocIds, selectedDocIds, documentsGeoMap, sortDocIdsByArea]);
+   
 
     return [
         docIds,
-        documentsGeoMap.current,
+        documentsGeoMap,
         transformMatrix
     ];
     

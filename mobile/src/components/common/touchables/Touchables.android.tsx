a53772c4049509f9bccf10db521a21a395a685dc
Sebastian Cuy <sebastian.cuy@dainst.de>
2021-06-04 15:27:48 +0200

    Fix TouchableOpacity styles on android

--- mobile/src/components/common/touchables/Touchables.android.tsx
+++ mobile/src/components/common/touchables/Touchables.android.tsx
@@ -1,5 +1,18 @@
-export {
-    TouchableHighlight,
-    TouchableOpacity,
+import React, { ReactElement } from 'react';
+import { TouchableOpacityProps } from 'react-native';
+import {
+    TouchableHighlight, TouchableOpacity as GHTouchableOpacity,
     TouchableWithoutFeedback
 } from 'react-native-gesture-handler';
+
+
+const TouchableOpacity = (props: TouchableOpacityProps): ReactElement => {
+
+    return <GHTouchableOpacity { ... props } containerStyle={ props.style } style={ [] } />;
+};
+
+export {
+    TouchableOpacity,
+    TouchableHighlight,
+    TouchableWithoutFeedback
+};

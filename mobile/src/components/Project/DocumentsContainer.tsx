36efed31bb5cb2f2c79b585de75428d3186bf781
Sebastian Cuy <sebastian.cuy@dainst.de>
2021-06-04 11:21:18 +0200

    Add useProjectData hook

--- mobile/src/components/Project/DocumentsContainer.tsx
+++ mobile/src/components/Project/DocumentsContainer.tsx
@@ -1,10 +1,10 @@
 import { createDrawerNavigator, DrawerNavigationProp } from '@react-navigation/drawer';
 import { RouteProp } from '@react-navigation/native';
-import { Document, ProjectCategories, ProjectConfiguration, Query, SyncStatus } from 'idai-field-core';
-import React, { useEffect, useState } from 'react';
-import { dropRight, last } from 'tsfun';
+import { Document, ProjectConfiguration, SyncStatus } from 'idai-field-core';
+import React, { useState } from 'react';
+import { last } from 'tsfun';
 import useOrientation from '../../hooks/use-orientation';
-import useSearch from '../../hooks/use-search';
+import useProjectData from '../../hooks/use-project-data';
 import { ProjectSettings } from '../../models/preferences';
 import { DocumentRepository } from '../../repositories/document-repository';
 import DocumentDetails from './DocumentDetails';
@@ -49,41 +49,9 @@ const DocumentsContainer: React.FC<DocumentsContainerProps> = ({
     setProjectSettings
 }) => {
 
-    const [query, setQuery] = useState<Query>({
-        categories: ProjectCategories.getOperationCategoryNames(config.getCategoryForest()),
-        constraints: {}
-    });
-    const documents = useSearch(repository, query);
-
     const [q, setQ] = useState<string>('');
-
-    const [hierarchyPath, setHierarchyPath] = useState<Document[]>([]);
-    
     const orientation = useOrientation();
-
-    useEffect(() => {
-
-        const operationCategories = ProjectCategories.getOperationCategoryNames(config.getCategoryForest());
-        const concreteCategories = ProjectCategories.getConcreteFieldCategoryNames(config.getCategoryForest());
-        
-        if (q) {
-            setQuery({ q, categories: concreteCategories });
-        } else {
-            const currentParent = last(hierarchyPath);
-            if (currentParent) {
-                if (operationCategories.includes(currentParent.resource.category)) {
-                    setQuery({ constraints: {
-                        'isRecordedIn:contain': currentParent.resource.id,
-                        'liesWithin:exist': 'UNKNOWN'
-                    } });
-                } else {
-                    setQuery({ constraints: { 'liesWithin:contain': currentParent.resource.id } });
-                }
-            } else {
-                setQuery({ categories: operationCategories });
-            }
-        }
-    }, [config, q, hierarchyPath]);
+    const { documents, hierarchyPath, pushToHierarchy, popFromHierarchy } = useProjectData(config, repository, q);
 
     const onDocumentSelected = (doc: Document, navigation: DrawerNavigation) => {
     
@@ -105,8 +73,8 @@ const DocumentsContainer: React.FC<DocumentsContainerProps> = ({
                     onDocumentSelected={ doc => onDocumentSelected(doc, navigation) }
                     onHomeButtonPressed={ () => navigation.navigate('HomeScreen') }
                     onSettingsButtonPressed={ () => navigation.navigate('SettingsScreen') }
-                    onParentSelected={ doc => setHierarchyPath(old => [...old, doc]) }
-                    onHierarchyBack={ () => setHierarchyPath(old => dropRight(1, old)) }
+                    onParentSelected={ pushToHierarchy }
+                    onHierarchyBack={ popFromHierarchy }
                 />;
             } }
         >

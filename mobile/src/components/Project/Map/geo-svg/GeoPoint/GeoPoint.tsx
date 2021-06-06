07b9340bcbf8db964a61e82d7eab81ebba35a2ce
Michael <michael.kihm@web.de>
2021-06-06 18:07:13 +0200

    GeoPoint don't increase size by zooming

--- mobile/src/components/Project/Map/geo-svg/GeoPoint/GeoPoint.tsx
+++ mobile/src/components/Project/Map/geo-svg/GeoPoint/GeoPoint.tsx
@@ -1,6 +1,7 @@
 import { Position } from 'geojson';
 import React from 'react';
-import { CircleProps } from 'react-native-svg';
+import { Animated } from 'react-native';
+import { CircleProps, NumberProp } from 'react-native-svg';
 import { ACircle, correctStrokeZooming, GeoElementsCommonProps } from '../common-props';
 import { pointRadius } from '../constants';
 
@@ -16,6 +17,9 @@ export const GeoPoint: React.FC<GeoPointProps> = (props) => {
             cx={ x }
             cy={ y }
             strokeWidth={ correctStrokeZooming(props.zoom, props.strokeWidth) }
-            r={ pointRadius } />
+            r={ correctRadiusZooming(props.zoom, pointRadius) } />
     );
 };
+
+const correctRadiusZooming = (zoom: Animated.Value, r?: NumberProp): Animated.AnimatedDivision =>
+    Animated.divide(r || 1, zoom);

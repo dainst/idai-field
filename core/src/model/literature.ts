76982195f0462537deca7755a3b0c0a8e024af59
Thomas Kleinke <thomas.kleinke@dainst.de>
2021-06-07 11:39:07 +0200

    Add DOI to input type literature

--- core/src/model/literature.ts
+++ core/src/model/literature.ts
@@ -7,6 +7,7 @@ export interface Literature {
 
     quotation: string;
     zenonId?: string;
+    doi?: string;
     page?: string;
     figure?: string;
 }
@@ -16,13 +17,14 @@ export module Literature {
 
     export const QUOTATION = 'quotation';
     export const ZENON_ID = 'zenonId';
+    export const DOI = 'doi';
     export const PAGE = 'page';
     export const FIGURE = 'figure';
 
-    const VALID_FIELDS = [QUOTATION, ZENON_ID, PAGE, FIGURE];
+    const VALID_FIELDS = [QUOTATION, ZENON_ID, DOI, PAGE, FIGURE];
 
 
-    export type Translations = 'zenonId'|'page'|'figure';
+    export type Translations = 'zenonId'|'doi'|'page'|'figure';
 
 
     export function isLiterature(literature: any): literature is Literature {
@@ -33,6 +35,7 @@ export module Literature {
         }
         if (!isString(literature.quotation)) return false;
         if (literature.zenonId && !isString(literature.zenonId)) return false;
+        if (literature.doi && !isString(literature.doi)) return false;
         if (literature.page && !isString(literature.page)) return false;
         if (literature.figure && !isString(literature.figure)) return false;
         return true;
@@ -47,7 +50,8 @@ export module Literature {
 
     export function generateLabel(literature: Literature, 
                                   getTranslation: (term: Literature.Translations) => string,
-                                  includeZenonId: boolean = true): string {
+                                  includeZenonId: boolean = true,
+                                  includeDoi: boolean = true): string {
 
         if (isValid(literature)) {
 
@@ -55,6 +59,9 @@ export module Literature {
     
             if (includeZenonId && literature.zenonId) {
                 additionalInformation.push(getTranslation('zenonId') + ': ' + literature.zenonId);
+            }
+            if (includeDoi && literature.doi) {
+                additionalInformation.push(getTranslation('doi') + ': ' + literature.doi);
             }
             if (literature.page) {
                 additionalInformation.push(getTranslation('page') + ' ' + literature.page);

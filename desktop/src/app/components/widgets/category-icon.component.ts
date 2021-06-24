2f08d1a756aa5570f319b58a8b418849fcc8351b
Thomas Kleinke <thomas.kleinke@dainst.de>
2021-06-24 18:32:15 +0200

    Update category icons immediately after changing category color

--- desktop/src/app/components/widgets/category-icon.component.ts
+++ desktop/src/app/components/widgets/category-icon.component.ts
@@ -1,12 +1,13 @@
-import {Component, OnChanges, Input} from '@angular/core';
-import {ProjectConfiguration} from 'idai-field-core';
+import { Component, OnChanges, Input } from '@angular/core';
+import { isString } from 'tsfun';
+import { Category, ProjectConfiguration } from 'idai-field-core';
 
 
 @Component({
-  selector: 'category-icon',
-  template: '<div class="category-icon" [style.width]="pxSize" [style.height]="pxSize" [style.font-size]="pxSize" [style.line-height]="pxSize" [style.background-color]="color">' +
-    '<span class="character" [style.color]="textColor">{{character}}</span>' +
-  '</div>'
+    selector: 'category-icon',
+    template: '<div class="category-icon" [style.width]="pxSize" [style.height]="pxSize" [style.font-size]="pxSize" [style.line-height]="pxSize" [style.background-color]="color">' +
+        '<span class="character" [style.color]="textColor">{{character}}</span>' +
+    '</div>'
 })
 
 /**
@@ -15,7 +16,7 @@ import {ProjectConfiguration} from 'idai-field-core';
 export class CategoryIconComponent implements OnChanges {
 
     @Input() size: number;
-    @Input() category: string;
+    @Input() category: string|Category;
 
     public character: string;
     public color: string;
@@ -28,8 +29,8 @@ export class CategoryIconComponent implements OnChanges {
 
     ngOnChanges() {
 
-        this.character = this.projectConfiguration.getLabelForCategory(this.category).substr(0, 1);
-        this.color = this.projectConfiguration.getColorForCategory(this.category);
+        this.character = this.projectConfiguration.getLabelForCategory(this.getCategoryName()).substr(0, 1);
+        this.color = this.projectConfiguration.getColorForCategory(this.getCategoryName());
         this.textColor = CategoryIconComponent.isColorTooBright(this.color) ? 'black' : 'white';
         this.pxSize = this.size + 'px';
     }
@@ -44,5 +45,11 @@ export class CategoryIconComponent implements OnChanges {
         let b = (rgb >>  0) & 0xff;  // extract blue
         let luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
         return luma > 200;
+    }
+
+
+    private getCategoryName(): string {
+
+        return isString(this.category) ? this.category : this.category.name;
     }
 }

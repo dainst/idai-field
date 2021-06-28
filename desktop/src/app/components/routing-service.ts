10e81646c99accaab59a42b97c163b71e498259a
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-06-28 22:26:39 +0200

    Allow to edit project images via extra button
    Thereby cleaning up what would have been a circular dependency. The routing service should not be able by itself to open a modal, because the modal belongs to the components layer, which is higher than the service layer.

--- desktop/src/app/components/routing-service.ts
+++ desktop/src/app/components/routing-service.ts
@@ -1,16 +1,12 @@
+import { Observable, Observer } from 'rxjs';
 import { Location } from '@angular/common';
 import { Injectable } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DatastoreErrors } from 'idai-field-core';
-import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { Document } from 'idai-field-core';
-import { Observable, Observer } from 'rxjs';
 import { ProjectCategories } from 'idai-field-core';
 import { ProjectConfiguration } from 'idai-field-core';
 import { ViewFacade } from '../core/resources/view/view-facade';
-import { SettingsProvider } from '../core/settings/settings-provider';
-import { MenuContext, MenuService } from './menu-service';
-import { ProjectsModalComponent } from './navbar/projects-modal.component';
 
 
 @Injectable()
@@ -31,10 +27,7 @@ export class RoutingService {
     constructor(private router: Router,
                 private viewFacade: ViewFacade,
                 private location: Location,
-                private projectConfiguration: ProjectConfiguration,
-                private modalService: NgbModal,
-                private settingsProvider: SettingsProvider,
-                private menuService: MenuService) {}
+                private projectConfiguration: ProjectConfiguration) {}
 
 
     // For ResourcesComponent
@@ -57,9 +50,7 @@ export class RoutingService {
 
         if (comingFromOutsideResourcesComponent) this.currentRoute = undefined;
 
-        if (documentToSelect.resource.category === 'Project') {
-            return this.openProjectsModal();
-        } else if (this.projectConfiguration.isSubcategory(documentToSelect.resource.category, 'Image')) {
+        if (this.projectConfiguration.isSubcategory(documentToSelect.resource.category, 'Image')) {
             await this.jumpToImageCategoryResource(documentToSelect, comingFromOutsideResourcesComponent);
         } else {
             await this.jumpToFieldCategoryResource(documentToSelect, comingFromOutsideResourcesComponent);
@@ -84,24 +75,6 @@ export class RoutingService {
             return this.router.navigate(
                 ['resources', viewName, document.resource.id, 'edit', 'conflicts']
             );
-        }
-    }
-
-
-    public async openProjectsModal(openConflictResolver: boolean = false) {
-
-        this.menuService.setContext(MenuContext.PROJECTS);
-
-        const ref: NgbModalRef = this.modalService.open(ProjectsModalComponent, { keyboard: false });
-        ref.componentInstance.selectedProject = this.settingsProvider.getSettings().selectedProject;
-        ref.componentInstance.openConflictResolver = openConflictResolver;
-
-        try {
-            await ref.result;
-        } catch(err) {
-            // Projects modal has been canceled
-        } finally {
-            this.menuService.setContext(MenuContext.DEFAULT);
         }
     }
 

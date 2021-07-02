9c89e8b1cf6815e5267c29cbc5033284afe7e92c
Thomas Kleinke <thomas.kleinke@dainst.de>
2021-07-02 15:31:06 +0200

    Fix drag & drop for newly created groups

--- desktop/src/app/core/configuration/configuration-util.ts
+++ desktop/src/app/core/configuration/configuration-util.ts
@@ -1,5 +1,5 @@
-import { Category, CustomCategoryDefinition, FieldDefinition, FieldResource, Resource } from 'idai-field-core';
-import { GroupDefinition } from 'idai-field-core';
+import { Category, CustomCategoryDefinition, FieldDefinition, FieldResource, Resource,
+    GroupDefinition } from 'idai-field-core';
 
 
 export const OVERRIDE_VISIBLE_FIELDS = [Resource.IDENTIFIER, FieldResource.SHORTDESCRIPTION];
@@ -23,14 +23,12 @@ export module ConfigurationUtil {
                                               permanentlyHiddenFields: string[]): Array<GroupDefinition> {
 
         return category.groups.reduce((result, group) => {
-            if (group.fields.length > 0) {
-                result.push({
-                    name: group.name,
-                    fields: group.fields
-                        .filter(field => !permanentlyHiddenFields.includes(field.name))
-                        .map(field => field.name)
-                });
-            }
+            result.push({
+                name: group.name,
+                fields: group.fields
+                    .filter(field => !permanentlyHiddenFields.includes(field.name))
+                    .map(field => field.name)
+            });
             return result;
         }, []);
     }

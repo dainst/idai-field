c6ad2e5461fc7fe7d88c17cdc57b600626fcdf62
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-07-07 23:14:11 +0200

    Bring category preview in place

---
+++ desktop/src/app/components/configuration/add/category-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Category } from 'idai-field-core';
+
+
+@Component({
+    selector: 'category-preview',
+    templateUrl: './category-preview.html'
+})
+/**
+ * @author Daniel de Oliveira
+ */
+export class CategoryPreviewComponent {
+
+    @Input() category: Category;
+}
--- desktop/src/app/components/configuration/configuration.module.ts
+++ desktop/src/app/components/configuration/configuration.module.ts
@@ -14,6 +14,7 @@ import { AddFieldModalComponent } from './add/add-field-modal.component';
 import { FieldEditorModalComponent } from './editor/field-editor-modal.component';
 import { CategoryEditorModalComponent } from './editor/category-editor-modal.component';
 import { AddCategoryModalComponent } from './add/add-category-modal.component';
+import { CategoryPreviewComponent } from './add/category-preview.component';
 import { LinkLibraryCategoryModalComponent } from './add/link-library-category-modal.component';
 import { ConfigurationFieldDragElement } from './configuration-field-drag-element.component';
 import { AddGroupModalComponent } from './add/add-group-modal.component';
@@ -41,6 +42,7 @@ import { DeleteGroupModalComponent } from './delete/delete-group-modal.component
         AddFieldModalComponent,
         AddGroupModalComponent,
         AddCategoryModalComponent,
+        CategoryPreviewComponent,
         LinkLibraryCategoryModalComponent,
         FieldEditorModalComponent,
         GroupEditorModalComponent,

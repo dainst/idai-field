b65c84e27efa82eacb4fb23578e855db52e99bd1
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-07-14 12:56:40 +0200

    Reactivate basic configuration index test

--- desktop/test/unit/core/configuration/configuration-index.spec.ts
+++ desktop/test/unit/core/configuration/configuration-index.spec.ts
@@ -5,6 +5,19 @@ describe('ConfigurationIndex', () => {
 
     it('base case', () => {
 
-        // expect(ConfigurationIndex.find({}, '')).toEqual('abcdef');
+        const categories = [
+            {
+                name: 'a:default',
+                label: { de: 'A' },
+                defaultLabel: { de: 'A' },
+                parentCategory: {
+                    name: 'a:parent'
+                }
+            }
+        ]
+        const index = ConfigurationIndex.create(categories as any);
+
+        const result = ConfigurationIndex.find(index, '', 'a:parent');
+        expect(result[0].name).toEqual('a:default');
     })
 });

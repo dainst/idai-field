8342b5ca4ad1144fe8b7b0a77cdce3a74116109d
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-07-17 17:14:32 +0200

    Let relations manager fetch descendants of places

--- desktop/test/unit/core/model/relations-manager.spec.ts
+++ desktop/test/unit/core/model/relations-manager.spec.ts
@@ -1,6 +1,5 @@
 import {Document} from 'idai-field-core';
 import {RelationsManager} from '../../../../src/app/core/model/relations-manager';
-import {ObjectUtils} from 'idai-field-core';
 import {ProjectConfiguration} from '../../../../src/app/core/configuration/project-configuration';
 
 

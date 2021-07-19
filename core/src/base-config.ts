78171a126b0d91c00f960bfff9d990e12a565621
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-07-19 22:29:59 +0200

    Add todo

--- core/src/base-config.ts
+++ core/src/base-config.ts
@@ -1,8 +1,10 @@
 export const basicIndexConfiguration = {
     'identifier:match': { path: 'resource.identifier', pathArray: ['resource', 'identifier'], type: 'match' },
     'id:match': { path: 'resource.id', pathArray: ['resource', 'id'], type: 'match' },
+    // TODO remove and just use isChildOf
     'isRecordedIn:contain': { path: 'resource.relations.isRecordedIn', pathArray: ['resource', 'relations', 'isRecordedIn'], type: 'contain' },
     'liesWithin:contain': { path: 'resource.relations.liesWithin', pathArray: ['resource', 'relations', 'liesWithin'], type: 'contain', recursivelySearchable: true },
     'liesWithin:exist': { path: 'resource.relations.liesWithin', pathArray: ['resource', 'relations', 'liesWithin'], type: 'exist' },
+    // -
     'isChildOf:contain': { path: 'resource.relations.isChildOf', pathArray: ['resource', 'relations', 'isChildOf'], type: 'contain', recursivelySearchable: true }
 };

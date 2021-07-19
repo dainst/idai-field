1261512d2e0b8893ca5733a535898abb0224d39a
Thomas Kleinke <thomas.kleinke@dainst.de>
2021-07-19 11:17:51 +0200

    Add confirmation dialog to delete modal for categories with custom fields

--- desktop/src/app/components/configuration/delete/delete-category-modal.component.ts
+++ desktop/src/app/components/configuration/delete/delete-category-modal.component.ts
@@ -12,19 +12,17 @@ import { Category } from 'idai-field-core';
 export class DeleteCategoryModalComponent {
 
     public category: Category;
+    public confirmDeletionCategoryName: string;
 
 
     constructor(public activeModal: NgbActiveModal) {}
 
 
-    public confirmDeletion() {
+    public hasCustomFields = (): boolean => Category.hasCustomFields(this.category);
 
-        this.activeModal.close();
-    }
+    public confirmDeletion = () => this.checkConfirmDeletionCategoryName() && this.activeModal.close();
+    
+    public cancel = () => this.activeModal.dismiss('cancel');
 
-
-    public cancel() {
-
-        this.activeModal.dismiss('cancel');
-    }
+    public checkConfirmDeletionCategoryName = () => this.confirmDeletionCategoryName === this.category.name;
 }

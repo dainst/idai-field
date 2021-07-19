01510462db90cd174c90f9d8ad7f41b94eaf4416
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-07-19 16:12:56 +0200

    Add interface export context

--- desktop/src/app/components/export/export.component.ts
+++ desktop/src/app/components/export/export.component.ts
@@ -102,7 +102,7 @@ export class ExportComponent implements OnInit {
         this.categoryCounts = await ExportRunner.determineCategoryCounts(
             this.get,
             this.find,
-            this.getOperationIdForMode(),
+            this.getExportContext(),
             Tree.flatten(this.projectConfiguration.getCategories())
         );
 
@@ -128,7 +128,7 @@ export class ExportComponent implements OnInit {
     }
 
 
-    private getOperationIdForMode() {
+    private getExportContext(): ExportRunner.ExportContext {
 
         return this.csvExportMode === 'complete' ? this.selectedOperationOrPlaceId : undefined;
     }
@@ -216,7 +216,7 @@ export class ExportComponent implements OnInit {
                 this.get,
                 this.find,
                 (async resourceId => (await this.datastore.get(resourceId)).resource.identifier),
-                this.getOperationIdForMode(),
+                this.getExportContext(),
                 this.selectedCategory,
                 this.projectConfiguration
                     .getRelationsForDomainCategory(this.selectedCategory.name)

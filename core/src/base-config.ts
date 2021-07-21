2523e5eef848e9ecd1cfe56c7aa7a013c2e3d1ea
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-07-21 14:08:42 +0200

    Remove todo

--- core/src/base-config.ts
+++ core/src/base-config.ts
@@ -1,7 +1,6 @@
 import {Query} from './model/query';
 import {Resource} from './model/resource';
 
-// TODO allow to also pass Document
 export const childrenOf = (id: Resource.Id): Query => ({
     constraints: {
         'isChildOf:contain': {

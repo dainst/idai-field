3c7a19e82d499f444990c407004cf3aa7a74f37d
Thomas Kleinke <thomas.kleinke@dainst.de>
2021-07-21 11:07:23 +0200

    Delete unused type ConfigurationChange

--- desktop/src/app/core/configuration/configuration-change.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigurationDocument, ProjectConfiguration } from 'idai-field-core';
-
-
-export type ConfigurationChange = {
-
-    newCustomConfigurationDocument: ConfigurationDocument;
-    newProjectConfiguration: ProjectConfiguration;
-}

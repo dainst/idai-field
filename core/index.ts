5a4f86096b4fe440e76522c828d7908244baa8b1
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-07-28 19:55:40 +0200

    Move sync into datastore

--- core/index.ts
+++ core/index.ts
@@ -2,7 +2,6 @@ export * from './src/constants';
 export * from './src/datastore';
 export * from './src/index';
 export * from './src/model';
-export * from './src/sync';
 export * from './src/tools';
 export * from './src/base-config';
 export * from './test';
--- core/src/datastore/pouchdb/index.ts
+++ core/src/datastore/pouchdb/index.ts
@@ -2,4 +2,6 @@ export { ChangeHistoryMerge } from './change-history-merge';
 export { IdGenerator } from './id-generator';
 export { PouchdbDatastore } from './pouchdb-datastore';
 export { PouchdbManager } from './pouchdb-manager';
+export { SyncService } from './sync-service';
+export { SyncProcess, SyncStatus } from './sync-process';
 export * from './types';

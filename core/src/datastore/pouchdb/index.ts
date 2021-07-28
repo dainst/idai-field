fc02d239d91d55418ec95e22956c2898ba0b491e
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-07-28 20:10:29 +0200

    Merge artifacts

--- core/src/datastore/pouchdb/index.ts
+++ core/src/datastore/pouchdb/index.ts
@@ -2,6 +2,5 @@ export { ChangeHistoryMerge } from './change-history-merge';
 export { IdGenerator } from './id-generator';
 export { PouchdbDatastore } from './pouchdb-datastore';
 export { PouchdbManager } from './pouchdb-manager';
-export { SyncService } from './sync-service';
-export { SyncProcess, SyncStatus } from './sync-process';
+export { SyncProcess, SyncStatus, SyncService } from './sync-service';
 export * from './types';

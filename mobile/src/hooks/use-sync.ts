70a94b96075a5468e9c4711f89e3468477ec0ab4
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-07-30 15:39:06 +0200

    Fix and symplify sync

--- mobile/src/hooks/use-sync.ts
+++ mobile/src/hooks/use-sync.ts
@@ -50,28 +50,29 @@ const setupSync = async (
 ): Promise<[Subscription] | undefined> => {
 
     if (connected && url && repository.syncService) {
-
         repository.syncService.init(url, project, password);
-        repository.syncService.startSync((doc: Document) => !['Image', 'Photo', 'Drawing'].includes(doc.resource.type));
-
-        // const syncProcess = await syncService.setupSync(url, project, password);
-// 
-        const statusSubscription = repository.syncService
-            .statusNotifications().subscribe({ next: status => {
-                console.log("status", status);
-                setStatus(status);
-            }});
-        // TODO make error subscription, too
-
-        // const subscription = syncProcess.observer.subscribe({
-            // next: setStatus,
-            // error: err => {
-                // showToast(ToastType.Error,`Error while syncing ${err}`);
-                // console.error('Error while syncing', err);
-                // setStatus(err);
-            // }
-        // });
-        return [statusSubscription];
+        try {
+            const statusSubscription = repository.syncService
+                .statusNotifications().subscribe(status => setStatus(status));
+            // TODO make error subscription, too; see below
+            // TODO reactivate showToast
+            // const subscription = syncProcess.observer.subscribe({
+                // next: setStatus,
+                // error: err => {
+                    // showToast(ToastType.Error,`Error while syncing ${err}`);
+                    // console.error('Error while syncing', err);
+                    // setStatus(err);
+                // }
+            // });
+            // return [statusSubscription];
+            repository.syncService
+                .startSync((doc: Document) => !['Image', 'Photo', 'Drawing'].includes(doc.resource.type))
+                .subscribe(); // TODO we should not need to subscribe here; jsut call startSync and be done, because we already subscribed to statusNotifications
+            return [statusSubscription];
+        } catch (e) {
+            console.log('error in setupSync', e);
+            return undefined;
+        }
     }
 };
 

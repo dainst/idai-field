f1bc6411bbb348dddd5161a31ac6ff8569f23aac
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-08-02 19:54:22 +0200

    Allow to link a map layer to multiple operations

--- desktop/test/unit/core/subsystem/docedit/widgets/get-image-suggestions.spec.ts
+++ desktop/test/unit/core/subsystem/docedit/widgets/get-image-suggestions.spec.ts
@@ -1,5 +1,5 @@
 import PouchDB =  require('pouchdb-node');
-import { Document } from 'idai-field-core';
+import { Datastore, Document } from 'idai-field-core';
 import { update } from 'tsfun';
 import { getImageSuggestions } from '../../../../../../src/app/core/docedit/widgets/get-image-suggestions';
 import { createApp, setupSyncTestDb } from '../../subsystem-helper';
@@ -7,9 +7,16 @@ import { createApp, setupSyncTestDb } from '../../subsystem-helper';
 
 describe('subsystem/getImageSuggestions', () => {
 
-    let datastore;
+    let datastore: Datastore;
 
-    const doc = { resource: { id: '1', identifier: 'One', category: 'Image', relations: {}}, project: undefined }
+    const doc = { resource: { id: '1', identifier: 'One', category: 'Image', relations: {}, georeference: true }, project: undefined }
+
+    const query = {
+        q: '',
+        categories: ['Image'],
+        offset: 0,
+        limit: 1
+    };
 
     beforeEach(async done => {
 
@@ -23,18 +30,13 @@ describe('subsystem/getImageSuggestions', () => {
 
     it('getImageSuggestions', async done => {
 
-        await datastore.create(doc, 'test')
+        await datastore.create(doc);
 
         const [,[[image], totalCount]] = await getImageSuggestions(
             datastore,
             { resource: {} } as Document,
             'depicts',
-            {
-                q: '',
-                categories: ['Image'],
-                offset: 0,
-                limit: 1
-            }
+            query
         );
         expect(image.resource.id).toBe('1');
         expect(totalCount).toBe(1);
@@ -44,18 +46,13 @@ describe('subsystem/getImageSuggestions', () => {
 
     it('exclude an already linked image from suggestions', async done => {
 
-        await datastore.create(update(['resource', 'relations', 'depicts'], ['2'], doc), 'test')
+        await datastore.create(update(['resource', 'relations', 'depicts'], ['2'], doc));
 
         const [,[images, totalCount]] = await getImageSuggestions(
             datastore,
             { resource: { id: '2' } } as Document,
             'depicts',
-            {
-                q: '',
-                categories: ['Image'],
-                offset: 0,
-                limit: 1
-            }
+            query
         );
         expect(images.length).toBe(0);
         expect(totalCount).toBe(0);
@@ -65,21 +62,112 @@ describe('subsystem/getImageSuggestions', () => {
 
     it('exclude documents from other projects', async done => {
 
-        await datastore.create(update('project', 'other', doc), 'test')
+        await datastore.create(update('project', 'other', doc));
 
         const [,[images, totalCount]] = await getImageSuggestions(
             datastore,
             { resource: {} } as Document,
             'depicts',
-            {
-                q: '',
-                categories: ['Image'],
-                offset: 0,
-                limit: 1
-            }
+            query
         );
         expect(images.length).toBe(0);
         expect(totalCount).toBe(0);
+        done();
+    });
+
+
+    it('georeference', async done => {
+
+        await datastore.create(doc);
+
+        const [,[images, totalCount]] = await getImageSuggestions(
+            datastore,
+            { resource: { id: '2', relations: {} } } as Document,
+            'layers',
+            query
+        );
+        expect(images.length).toBe(1);
+        expect(totalCount).toBe(1);
+        done();
+    });
+
+
+    it('exclude if try to assign to non-project but already assigned to project document', async done => {
+
+        await datastore.create(update(['resource', 'relations', 'isMapLayerOf'], ['project'], doc));
+
+        const [,[images, totalCount]] = await getImageSuggestions(
+            datastore,
+            { resource: { id: '2', relations: {} } } as Document,
+            'layers',
+            query
+        );
+        expect(images.length).toBe(0);
+        expect(totalCount).toBe(0);
+        done();
+    });
+
+
+    it('exclude if try to assign to project but already assigned to non-project document', async done => {
+
+        await datastore.create(update(['resource', 'relations', 'isMapLayerOf'], ['3'], doc));
+
+        const [,[images, totalCount]] = await getImageSuggestions(
+            datastore,
+            { resource: { category: 'Project' , id: '2', relations: {} } } as Document,
+            'layers',
+            query
+        );
+        expect(images.length).toBe(0);
+        expect(totalCount).toBe(0);
+        done();
+    });
+
+
+    it('exclude if try to assign to project but already assigned to project document', async done => {
+
+        await datastore.create(update(['resource', 'relations', 'isMapLayerOf'], ['project'], doc));
+
+        const [,[images, totalCount]] = await getImageSuggestions(
+            datastore,
+            { resource: { category: 'Project', relations: {} } } as Document,
+            'layers',
+            query
+        );
+        expect(images.length).toBe(0);
+        expect(totalCount).toBe(0);
+        done();
+    });
+
+
+    it('exclude if try to assign to non-project but already assigned to the document', async done => {
+
+        await datastore.create(update(['resource', 'relations', 'isMapLayerOf'], ['2'], doc));
+
+        const [,[images, totalCount]] = await getImageSuggestions(
+            datastore,
+            { resource: { id: '2', relations: {} } } as Document,
+            'layers',
+            query
+        );
+        expect(images.length).toBe(0);
+        expect(totalCount).toBe(0);
+        done();
+    });
+
+
+    it('do not exclude if try to assign to non-project but already assigned to the document', async done => {
+
+        await datastore.create(update(['resource', 'relations', 'isMapLayerOf'], ['3'], doc));
+
+        const [,[images, totalCount]] = await getImageSuggestions(
+            datastore,
+            { resource: { id: '2', relations: {} } } as Document,
+            'layers',
+            query
+        );
+        expect(images.length).toBe(1);
+        expect(totalCount).toBe(1);
         done();
     });
 });

c4311d87d0c71ab0d39d860169fd300ca3a3cdc2
Michael <michael.kihm@web.de>
2021-08-10 10:43:48 +0200

    Add DocumentForm component to DocumentEdit

--- mobile/src/components/Project/DocumentEdit.tsx
+++ mobile/src/components/Project/DocumentEdit.tsx
@@ -1,8 +1,16 @@
+import { Ionicons } from '@expo/vector-icons';
 import { DrawerNavigationProp } from '@react-navigation/drawer';
-import React from 'react';
-import { StyleSheet, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Category, Resource } from 'idai-field-core';
+import React, { useContext, useEffect, useState } from 'react';
+import { Keyboard } from 'react-native';
+import { ConfigurationContext } from '../../contexts/configuration-context';
+import LabelsContext from '../../contexts/labels/labels-context';
+import useDocument from '../../hooks/use-document';
+import useToast from '../../hooks/use-toast';
 import { DocumentRepository } from '../../repositories/document-repository';
+import Button from '../common/Button';
+import DocumentForm from '../common/forms/DocumentForm';
+import { ToastType } from '../common/Toast/ToastProvider';
 import { DocumentsContainerDrawerParamList } from './DocumentsContainer';
 
 type DocumentEditNav = DrawerNavigationProp<DocumentsContainerDrawerParamList, 'DocumentEdit'>;
@@ -15,20 +23,64 @@ interface DocumentEditProps {
 }
 
 const DocumentEdit: React.FC<DocumentEditProps> = ({ repository, navigation, docId, categoryName }) => {
+
+    const config = useContext(ConfigurationContext);
+    const [category, setCategory] = useState<Category>();
+    const [resource, setResource] = useState<Resource>();
+
+    const { labels } = useContext(LabelsContext);
+    const document = useDocument(repository,docId);
+    const { showToast } = useToast();
+
+    useEffect(() => setCategory(config.getCategory(categoryName)),[config, categoryName]);
+    
+    useEffect(() => {
+
+        if(document) setResource(document.resource);
+    },[document]);
+
+    const onReturn = () => navigation.navigate('DocumentsMap',{});
+
+    const editDocument = () => {
+
+        if(document && resource){
+            repository.update({ ...document, resource }).then(doc => {
+                showToast(ToastType.Success, `Edited ${doc.resource.identifier}`);
+                navigation.navigate('DocumentsMap', { highlightedDocId: doc.resource.id });
+            }).catch(err => {
+                Keyboard.dismiss();
+                showToast(ToastType.Error, `Could not update ${document.resource.identifier}: ${err}`);
+            });
+        }
+    };
+        
+    
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const updateResource = (key: string, value: any) => {
+        setResource(oldResource => oldResource && { ...oldResource, [key]: value });
+    };
+        
+    
+    if(!category || !labels || !document || !resource) return null;
+
     return (
-        <SafeAreaView style={ styles.container }>
-            <Text>Lets Edit {docId} with cat {categoryName}</Text>
-        </SafeAreaView>
+        <DocumentForm
+            titleBarRight={
+                <Button
+                    variant="primary"
+                    onPress={ editDocument }
+                    title="Edit"
+                    icon={ <Ionicons name="create-outline" size={ 18 } /> }
+                />
+            }
+            category={ category }
+            headerText={ `Edit ${labels.get(category)} ${ document.resource.identifier }` }
+            returnBtnHandler={ onReturn }
+            resource={ resource }
+            updateFunction={ updateResource }
+        />
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-});
 
 
 export default DocumentEdit;

1a03fb541eaf4b0b9afeb2b22e07cfa1c3050582
Michael <michael.kihm@web.de>
2021-08-12 19:38:03 +0200

    Add Dimension[] to type currentValue

--- mobile/src/components/common/forms/common-props.ts
+++ mobile/src/components/common/forms/common-props.ts
@@ -1,8 +1,8 @@
-import { Field } from 'idai-field-core';
+import { Dimension, Field } from 'idai-field-core';
 
 export interface FieldBaseProps {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     setFunction: (key: string, value: any) => void;
     field: Field;
-    currentValue?: string | number | string[] | boolean;
+    currentValue?: string | number | string[] | boolean | Dimension[];
 }

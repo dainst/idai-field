5d5edca50e3174fff501dfc27d85c139c19aebdf
Michael <michael.kihm@web.de>
2021-08-17 11:31:42 +0200

    Update mock of react-native-async-storage

--- mobile/jest.setup.js
+++ mobile/jest.setup.js
@@ -1,8 +1,6 @@
-import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 // see https://dev.to/mrmurphy/animating-text-content-with-redash-and-reanimated-2-3cmg
 import 'react-native-reanimated';
 
-jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');

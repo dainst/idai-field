9c48586cef73e6f7ff7f9c014cd383566b435c2e
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-09-01 21:42:25 +0200

    Extract functions

--- web/api/lib/worker/services/idaifield_db.ex
+++ web/api/lib/worker/services/idaifield_db.ex
@@ -8,12 +8,9 @@ defmodule Api.Worker.Services.IdaiFieldDb do
   """
   def get_doc(db), do: fn id -> get_doc(db, id) end
   def get_doc(db, id) do
-    auth = [hackney: [basic_auth: {Api.Core.Config.get(:couchdb_user), Api.Core.Config.get(:couchdb_password)}]]
+    {auth, url} = get_connection_data()
 
-    result = HTTPoison.get("#{Api.Core.Config.get(:couchdb_url)}/#{db}/#{id}", %{}, auth)
-             |> ResultHandler.handle_result
-
-    case result do
+    case send_request("#{url}/#{db}/#{id}", auth) do
       document = %{ "resource" => _resource } ->
         CorePropertiesAtomizing.format_document document
       nil ->
@@ -25,18 +22,27 @@ defmodule Api.Worker.Services.IdaiFieldDb do
   end
 
   def fetch_changes db do
+    {auth, url} = get_connection_data()
 
-    user = Api.Core.Config.get(:couchdb_user)
-    pass = Api.Core.Config.get(:couchdb_password)
-    auth = [hackney: [basic_auth: {user, pass}]]
-    url = Api.Core.Config.get(:couchdb_url)
-    full_url = "#{url}/#{db}/_all_docs?include_docs=true"
-
-    full_url
-    |> HTTPoison.get(%{}, auth)
-    |> ResultHandler.handle_result
+    "#{url}/#{db}/_all_docs?include_docs=true"
+    |> send_request(auth)
     |> get_in(["rows"])
     |> CorePropertiesAtomizing.format_changes
     |> update_in([Access.all(), :doc], &(Map.drop(&1, [:_id, :_rev, :_attachments])))
+  end
+
+  defp send_request full_url, auth do
+    full_url
+    |> HTTPoison.get(%{}, auth)
+    |> ResultHandler.handle_result
+  end
+
+  defp get_connection_data do
+    {
+      [hackney: [basic_auth: {
+            Api.Core.Config.get(:couchdb_user),
+            Api.Core.Config.get(:couchdb_password)}]],
+      Api.Core.Config.get(:couchdb_url)
+    }
   end
 end

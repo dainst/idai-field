f89fb3f3a154296d9032052fcb1772c9b718014c
Thomas Kleinke <thomas.kleinke@dainst.de>
2021-09-20 14:58:48 +0200

    Adjust settings in subsystem helper

--- desktop/test/unit/core/subsystem/subsystem-helper.ts
+++ desktop/test/unit/core/subsystem/subsystem-helper.ts
@@ -56,14 +56,9 @@ export async function setupSettingsService(pouchdbmanager, pouchdbserver, projec
     const settings = await settingsService.updateSettings({
         languages: ['de', 'en'],
         isAutoUpdateActive: false,
-        isSyncActive: false,
         remoteSites: [],
         hostPassword: '',
-        syncTarget: new class implements SyncTarget {
-            address: string = 'http://localhost:3003/';
-            password: string;
-            username: string;
-        },
+        syncTargets: {},
         dbs: [projectName],
         selectedProject: '',
         imagestorePath: process.cwd() + '/test/test-temp/imagestore',

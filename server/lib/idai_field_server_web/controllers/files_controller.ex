40b4970a899ac946832f077592f7271272682ebc
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-10-06 20:40:22 +0200

    Put is_authorized? function in place

--- server/lib/idai_field_server_web/controllers/files_controller.ex
+++ server/lib/idai_field_server_web/controllers/files_controller.ex
@@ -8,62 +8,68 @@ defmodule IdaiFieldServerWeb.FilesController do
   @files_root "./#{@files_folder_name}"
 
   def download %{ query_params: query_params} = conn, params do
-
-    { _email, _password } = Plug.BasicAuth.parse_basic_auth(conn)
-    # IdaiFieldServer.Accounts.get_project_by_email_and_password(email, password)
-
-    filepath = get_filepath conn
-
-    if not File.exists? filepath do
-      json conn, %{ status: :not_found, path: filepath }
+    unless is_authorized? conn do # TODO consider using a plug
+      json conn, %{ status: :unauthorized }
     else
-      if File.dir? filepath do
-        json conn, %{ project: get_project(conn), files: get_files(filepath) }
+      filepath = get_filepath conn
+
+      if not File.exists? filepath do
+        json conn, %{ status: :not_found, path: filepath }
       else
-        send_download conn, {:file, filepath}
+        if File.dir? filepath do
+          json conn, %{ project: get_project(conn), files: get_files(filepath) }
+        else
+          send_download conn, {:file, filepath}
+        end
       end
     end
   end
 
   def upload %{ params: %{ "project" => project, "filepath" => filepath }} = conn, params do
+    unless is_authorized? conn do
+      json conn, %{ status: :unauthorized}
+    else
+      if length(filepath) > 0 do
+        fp = filepath |> Enum.reverse |> tl() |> Enum.reverse
+        File.mkdir_p! "#{@files_root}/#{project}/#{fp}"
+      end
 
-    { _email, _password } = Plug.BasicAuth.parse_basic_auth(conn)
-    # IdaiFieldServer.Accounts.get_project_by_email_and_password(email, password)
+      filepath = Enum.join filepath, "/"
 
-    if length(filepath) > 0 do
-      fp = filepath |> Enum.reverse |> tl() |> Enum.reverse
-      File.mkdir_p! "#{@files_root}/#{project}/#{fp}"
+      {:ok, body, conn} = Plug.Conn.read_body conn
+
+      {:ok, file} = File.open "#{@files_root}/#{project}/#{filepath}", [:write]
+      IO.binwrite file, body
+      File.close file
+
+      json conn, %{ status: :ok }
     end
-
-    filepath = Enum.join filepath, "/"
-
-    {:ok, body, conn} = Plug.Conn.read_body conn
-
-    {:ok, file} = File.open "#{@files_root}/#{project}/#{filepath}", [:write]
-    IO.binwrite file, body
-    File.close file
-
-    json conn, %{ status: :ok }
   end
 
-  def delete conn, params do
-
-    { _email, _password } = Plug.BasicAuth.parse_basic_auth(conn)
-    # IdaiFieldServer.Accounts.get_project_by_email_and_password(email, password)
-
-    filepath = get_filepath conn
-
-    if File.exists?(filepath) and not File.dir?(filepath) do
-      File.rm! filepath
-      json conn, %{ status: :deleted }
+  def delete conn, _params do
+    unless is_authorized? conn do
+      json conn, %{ status: :unauthorized }
     else
-      json conn, %{ status: :notfound }
+      filepath = get_filepath conn
+      if File.exists?(filepath) and not File.dir?(filepath) do
+        File.rm! filepath
+        json conn, %{ status: :deleted }
+      else
+        json conn, %{ status: :notfound }
+      end
     end
   end
 
   defp get_filepath %{ params: %{ "project" => project, "filepath" => filepath }} do
     filepath_string = Enum.join filepath, "/"
     "#{@files_root}/#{project}/#{filepath_string}"
+  end
+
+  defp is_authorized? conn do
+    project = get_project conn
+    { user, password } = Plug.BasicAuth.parse_basic_auth(conn)
+    # IdaiFieldServer.Accounts.get_project_by_email_and_password(email, password)
+    project == user and user == password # TODO implement proper authorization
   end
 
   defp get_project %{ params: %{ "project" => project }} do

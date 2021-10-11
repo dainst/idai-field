3f616c8f127e1504c4ae1ca6154c3d4e522e3181
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-10-11 23:25:44 +0200

    Extract functions

--- desktop/src/app/services/imagestore/image-changes-stream.ts
+++ desktop/src/app/services/imagestore/image-changes-stream.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { ChangesStream, Named, ProjectConfiguration } from 'idai-field-core';
+import { ChangesStream, Named, ProjectConfiguration, Document } from 'idai-field-core';
+import { Settings } from '../settings/settings';
 import { SettingsProvider } from '../settings/settings-provider';
 
 const fs = typeof window !== 'undefined' ? window.require('fs') : require('fs');
@@ -16,35 +17,53 @@ export class ImageChangesStream {
                 settingsProvider: SettingsProvider,
                 projectConfiguration: ProjectConfiguration) {
 
-        const settings = settingsProvider.getSettings();
-        const imagestorePath = settings.imagestorePath;
-        const project = settings.selectedProject;
-        const syncSource = settings.syncTargets[project];
-        const syncUrl = 'http://' + project + ':' + syncSource.password
-            + '@' + syncSource.address + '/files/' + project + '/';
+        const [syncUrl, imagestoreProjectPath] =
+            ImageChangesStream.extract(settingsProvider.getSettings());
 
         changesStream.remoteChangesNotifications().subscribe(next => {
-
             if (!projectConfiguration.getImageCategories().map(Named.toName)
                 .includes(next.resource.category)) return;
 
-            const hiresPath = imagestorePath + project + '/' + next.resource.id;
-            const loresPath = imagestorePath + project + '/thumbs/' + next.resource.id;
-
-            if (!fs.existsSync(hiresPath)) {
-                console.log('hires image does not yet exist', hiresPath);
-                // TODO fetch from remote
-                http.get(syncUrl, (res: any) => {
-                    // TODO impl
-                });
-            }
-            if (!fs.existsSync(loresPath)) {
-                console.log('lores image does not yet exist', loresPath);
-                // TODO fetch from remote
-                http.get(syncUrl, (res: any) => {
-                    // TODO impl
-                });
-            }
+            ImageChangesStream.go(syncUrl, imagestoreProjectPath, next);
         });
+    }
+
+
+    private static go(syncUrl: string, imagestoreProjectPath: string, next: Document) {
+
+        const hiresPath = imagestoreProjectPath + next.resource.id;
+        const loresPath = imagestoreProjectPath + 'thumbs/' + next.resource.id;
+
+        if (!fs.existsSync(hiresPath)) {
+            console.log('hires image does not yet exist', hiresPath);
+            // TODO fetch from remote
+            http.get(syncUrl, (res: any) => {
+                // TODO impl
+            });
+        }
+        if (!fs.existsSync(loresPath)) {
+            console.log('lores image does not yet exist', loresPath);
+            // TODO fetch from remote
+            http.get(syncUrl, (res: any) => {
+                // TODO impl
+            });
+        }
+    }
+
+
+    private static extract(settings: Settings) {
+
+        const project = settings.selectedProject;
+        const syncSource = settings.syncTargets[project];
+
+        const address = 'localhost'; // TODO should be derived from syncSource.address
+        const protocol = 'http'; // TODO be able to deal with both http and https
+        const syncUrl = protocol + '://' + project + ':' + syncSource.password
+            + '@' + address + '/files/' + project + '/';
+
+        const imagestorePath = settings.imagestorePath;
+        const imagestoreProjectPath = imagestorePath + project + '/';
+
+        return [syncUrl, imagestoreProjectPath];
     }
 }

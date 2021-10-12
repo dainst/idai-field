7083fd032a030d1bebbac31f37811ee00c8d3c92
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-10-12 23:44:15 +0200

    Simplify

--- desktop/src/app/services/imagestore/fs-adapter.ts
+++ desktop/src/app/services/imagestore/fs-adapter.ts
@@ -33,16 +33,14 @@ export class FsAdapter {
     // see https://stackoverflow.com/a/16684530
     public listFiles(dir) {
 
-        const self = this;
-
         var results = [];
         var list = fs.readdirSync(dir);
-        list.forEach(function(file) {
+        list.forEach(file => {
             file = dir + '/' + file;
             var stat = fs.statSync(file);
             if (stat && stat.isDirectory()) {
                 /* Recurse into a subdirectory */
-                results = results.concat(self.listFiles(file));
+                results = results.concat(this.listFiles(file));
             } else {
                 /* Is a file */
                 results.push(file);

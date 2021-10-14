afb9170332ca816fa5f60c5976782e3dd69fd0be
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-10-14 23:48:52 +0200

    Make Desktop download page the landing page

--- server/lib/idai_field_server_web/controllers/page_controller.ex
+++ server/lib/idai_field_server_web/controllers/page_controller.ex
@@ -2,6 +2,15 @@ defmodule IdaiFieldServerWeb.PageController do
   use IdaiFieldServerWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+
+    url = "https://api.github.com/repos/dainst/idai-field/releases/latest"
+    headers = [{"Accept", "application/vnd.github.v3+json"}]
+    {:ok, response} = HTTPoison.get url, headers, []
+    answer = Poison.decode! response.body
+
+    tag_name = answer["tag_name"]
+    tag_name = String.slice tag_name, 1..-1
+
+    render(conn, "index.html", latest_tag: tag_name)
   end
 end

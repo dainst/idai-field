1b3f234d91d52c16c5502178d760b14b0b5f6d4d
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-10-14 21:31:21 +0200

    Simplify

--- server/lib/idai_field_server/couchdb_connection.ex
+++ server/lib/idai_field_server/couchdb_connection.ex
@@ -3,12 +3,11 @@ defmodule IdaiFieldServer.CouchdbConnection do
   alias IdaiFieldServer.Repo
 
   def anon_post url, data do # TODO reuse_admin_post_put
-    couchdb_path = get_couchdb_path()
+    url = get_couchdb_path url
     password = get_password()
     headers = [{"Content-type", "application/json"}]
     options = []
     body = Poison.encode! data
-    url = "http://#{couchdb_path}/#{url}"
     {:ok, response} = HTTPoison.post url, body, headers, options
     Poison.decode! response.body
   end
@@ -22,29 +21,25 @@ defmodule IdaiFieldServer.CouchdbConnection do
   end
 
   def admin_post_put method, url, data do # TODO make private
-    couchdb_path = get_couchdb_path()
+    url = get_couchdb_path url
     password = get_password()
     headers = [{"Content-type", "application/json"}]
     options = [hackney: [basic_auth: {"admin", password}]]
     body = Poison.encode! data
-    url = "http://#{couchdb_path}/#{url}"
     {:ok, response} = method.(url, body, headers, options)
     Poison.decode! response.body
   end
 
   def admin_delete url do
-    couchdb_path = get_couchdb_path()
+    url = get_couchdb_path url
     password = get_password()
     options = [hackney: [basic_auth: {"admin", password}]]
-    url = "http://#{couchdb_path}/#{url}"
     {:ok, response} = HTTPoison.delete url, %{}, options
   end
 
   def admin_get url do
-    couchdb_path = get_couchdb_path()
+    url = get_couchdb_path url
     password = get_password()
-
-    url = "http://#{couchdb_path}/#{url}"
     options = [hackney: [basic_auth: {"admin", password}]]
     {:ok, response} = HTTPoison.get url, %{}, options
     Poison.decode! response.body
@@ -57,9 +52,9 @@ defmodule IdaiFieldServer.CouchdbConnection do
   end
 
   # TODO dedup, see files_controller
-  defp get_couchdb_path do
+  defp get_couchdb_path url do
     repo_env = Application.fetch_env! :idai_field_server, Repo
     repo_env = Enum.into repo_env, %{}
-    repo_env.couchdb
+    "http://#{repo_env.couchdb}/#{url}"
   end
 end

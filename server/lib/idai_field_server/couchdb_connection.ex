175fbfe5a17d52ad776966b7cf06971487520106
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-10-14 21:36:00 +0200

    Reuse function and make private

--- server/lib/idai_field_server/couchdb_connection.ex
+++ server/lib/idai_field_server/couchdb_connection.ex
@@ -2,32 +2,16 @@ defmodule IdaiFieldServer.CouchdbConnection do
 
   alias IdaiFieldServer.Repo
 
-  def anon_post url, data do # TODO reuse_admin_post_put
-    url = get_couchdb_path url
-    password = get_password()
-    headers = [{"Content-type", "application/json"}]
-    options = []
-    body = Poison.encode! data
-    {:ok, response} = HTTPoison.post url, body, headers, options
-    Poison.decode! response.body
+  def anon_post url, data do
+    post_put &HTTPoison.post/4, url, data, false
   end
 
   def admin_post url, data do
-    admin_post_put &HTTPoison.post/4, url, data
+    post_put &HTTPoison.post/4, url, data, true
   end
 
   def admin_put url, data do
-    admin_post_put &HTTPoison.put/4, url, data
-  end
-
-  def admin_post_put method, url, data do # TODO make private
-    url = get_couchdb_path url
-    password = get_password()
-    headers = [{"Content-type", "application/json"}]
-    options = [hackney: [basic_auth: {"admin", password}]]
-    body = Poison.encode! data
-    {:ok, response} = method.(url, body, headers, options)
-    Poison.decode! response.body
+    post_put &HTTPoison.put/4, url, data, true
   end
 
   def admin_delete url do
@@ -42,6 +26,16 @@ defmodule IdaiFieldServer.CouchdbConnection do
     password = get_password()
     options = [hackney: [basic_auth: {"admin", password}]]
     {:ok, response} = HTTPoison.get url, %{}, options
+    Poison.decode! response.body
+  end
+
+  defp post_put method, url, data, as_admin do
+    url = get_couchdb_path url
+    password = get_password()
+    headers = [{"Content-type", "application/json"}]
+    options = if as_admin do [hackney: [basic_auth: {"admin", password}]] else [] end
+    body = Poison.encode! data
+    {:ok, response} = method.(url, body, headers, options)
     Poison.decode! response.body
   end
 

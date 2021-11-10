b42253e87b2a5129d6d8141418ccc656f4b8eb36
Michael <michael.kihm@web.de>
2021-11-10 10:12:22 +0100

    Export getLayerCoordinates in index.ts

--- mobile/src/components/Project/Map/GLMap/cs-transform/index.ts
+++ mobile/src/components/Project/Map/GLMap/cs-transform/index.ts
@@ -1,7 +1,9 @@
 import { defineWorldCoordinateSystem, WORLD_CS_HEIGHT, WORLD_CS_WIDTH } from './constants';
 import { getDocumentToWorldTransform } from './document-to-world/doc2world-trans-matrix';
 import { getDocumentToWorldTransformMatrix } from './document-to-world/document-to-world-transformation';
-import { getGeometryBoundings, getMinMaxGeometryCoords } from './document-to-world/utils/cs-transform-utils';
+import {
+    getGeometryBoundings, getLayerCoordinates, getMinMaxGeometryCoords
+} from './document-to-world/utils/cs-transform-utils';
 import { matrixInverse4, processTransform2d } from './matrix-utils/matrix-utils';
 import { getScreenToWorldTransformationMatrix } from './screen-to-world/screen-to-world-transformation';
 import { GeometryBoundings, Transformation } from './types';
@@ -19,4 +21,5 @@ export {
     getGeometryBoundings,
     Transformation,
     getDocumentToWorldTransform,
+    getLayerCoordinates
 };

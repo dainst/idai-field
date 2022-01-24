56a3cf7863f612cf575bd02c955d7aae22113f4c
Thomas Kleinke <thomas.kleinke@dainst.de>
2022-01-24 17:26:41 +0100

    Show value description as tooltip in valuelist view

--- desktop/src/app/components/configuration/valuelist-view.component.ts
+++ desktop/src/app/components/configuration/valuelist-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { clone, Map } from 'tsfun';
 import { Labels, Valuelist } from 'idai-field-core';
 
 
@@ -10,20 +11,39 @@ import { Labels, Valuelist } from 'idai-field-core';
  *  @author Sebastian Cuy
  *  @author Thomas Kleinke
  */
-export class ValuelistViewComponent {
+export class ValuelistViewComponent implements OnChanges {
 
     @Input() valuelist: Valuelist;
     @Input() showDescription: boolean = true;
     @Input() largeList: boolean;
 
+    public valueLabels: Map<string> = {};
+    public valueDescriptions: Map<string> = {};
+
 
     constructor(private labels: Labels) {}
+
+
+    ngOnChanges() {
+        
+        Object.keys(this.valuelist.values).forEach(valueId => {
+            const { label, description } = this.getLabelAndDescription(valueId);
+            this.valueLabels[valueId] = label;
+            this.valueDescriptions[valueId] = description;
+        });
+    }
     
 
     public getValuelistDescription = (valuelist: Valuelist) => this.labels.getDescription(valuelist);
 
     public getValues = (valuelist: Valuelist) => this.labels.orderKeysByLabels(valuelist);
 
-    public getValueLabel = (valuelist: Valuelist, valueId: string) =>
-        this.labels.getValueLabel(valuelist, valueId);
+
+    private getLabelAndDescription(valueId: string): { label: string, description?: string } {
+
+        const value: any = clone(this.valuelist.values[valueId]);
+        value.name = valueId;
+
+        return this.labels.getLabelAndDescription(value);
+    }
 }

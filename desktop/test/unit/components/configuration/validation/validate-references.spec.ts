71659ebca1203b9af8247653f2e4b2fc37728ec3
Thomas Kleinke <thomas.kleinke@dainst.de>
2022-01-26 10:59:30 +0100

    Add unit test for reference validation

---
+++ desktop/test/unit/components/configuration/validation/validate-references.spec.ts
@@ -0,0 +1,29 @@
+import { validateReferences } from "../../../../../src/app/components/configuration/validation/validate-references";
+
+describe('validate references', () => {
+
+    it('perform validation', () => {
+        
+        try {
+            validateReferences(['http://www.example.de']);
+        } catch(err) {
+            fail();
+        }
+
+        try {
+            validateReferences(['https://www.example.de']);
+        } catch(err) {
+            fail();
+        }
+
+        try {
+            validateReferences(['www.example.de']);
+            fail();
+        } catch(err) {}
+
+        try {
+            validateReferences(['abc']);
+            fail();
+        } catch(err) {}
+    });
+});

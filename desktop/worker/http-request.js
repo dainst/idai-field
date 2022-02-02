cf5ebc268227319f30c1fd345b6985496b8f8129
Simon Hohl <mail@dersmon.net>
2022-02-02 16:42:38 +0100

    Fix child process deserialization for Buffer

--- desktop/worker/http-request.js
+++ desktop/worker/http-request.js
@@ -9,7 +9,11 @@ process.on('message', (params) => {
     if (!('maxBodyLength' in params)) {
         params.maxBodyLength = Infinity;
     }
-    
+
+    if ('data' in params && 'type' in params.data && 'data' in params.data) {
+        params.data = Buffer.from(params.data.data)
+    }
+
     axios(params).then(function (response) {
         process.send({data: response.data, status: response.status});
     }).catch(function (error) {

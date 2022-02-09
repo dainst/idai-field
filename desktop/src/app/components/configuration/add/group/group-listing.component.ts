c90c2a2a3cb9080f6ffdd42ee2b45327ee494391
Thomas Kleinke <thomas.kleinke@dainst.de>
2022-02-09 17:16:54 +0100

    Prevent creation of duplicate groups

--- desktop/src/app/components/configuration/add/group/group-listing.component.ts
+++ desktop/src/app/components/configuration/add/group/group-listing.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { Labels } from 'idai-field-core';
+import { to } from 'tsfun';
+import { CategoryForm, Labels, Named } from 'idai-field-core';
 import { getSearchResultLabel } from '../getSearchResultLabel';
 import { GroupEntry } from '../../../../services/configuration/index/group-index';
 
@@ -14,6 +15,7 @@ import { GroupEntry } from '../../../../services/configuration/index/group-index
 export class GroupListingComponent {
 
     @Input() groups: Array<GroupEntry> = [];
+    @Input() category: CategoryForm;
     @Input() emptyGroup: GroupEntry|undefined;
     @Input() selectedGroup: GroupEntry;
     @Input() searchTerm: string = '';
@@ -29,7 +31,8 @@ export class GroupListingComponent {
     public getLabel = (value: any) => this.labels.get(value);
 
     public isNewGroupOptionShown = (): boolean => this.emptyGroup !== undefined
-        && !this.groups.map(group => group.name).includes(this.searchTerm);
+        && !this.groups.map(group => group.name).includes(this.searchTerm)
+        && !this.category.groups.map(to(Named.NAME)).includes(this.searchTerm);
 
     public getSearchResultLabel = (group: GroupEntry) => getSearchResultLabel(group, this.searchTerm, this.getLabel);
 }

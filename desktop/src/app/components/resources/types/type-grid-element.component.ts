f610865680d00d8e102359a25cbf3651a5469234
Simon Hohl <mail@dersmon.net>
2022-02-15 21:49:52 +0100

    Remove deprecated comments

--- desktop/src/app/components/resources/types/type-grid-element.component.ts
+++ desktop/src/app/components/resources/types/type-grid-element.component.ts
@@ -1,6 +1,5 @@
 import {Input, Component, OnChanges, SimpleChanges} from '@angular/core';
 import {SafeResourceUrl} from '@angular/platform-browser';
-import {ImageUrlMaker} from '../../../services/imagestore/image-url-maker';
 import { FieldDocument } from 'idai-field-core';
 
 @Component({
@@ -11,36 +10,11 @@ import { FieldDocument } from 'idai-field-core';
  * @author Thomas Kleinke
  * @author Sebastian Cuy
  */
-export class TypeGridElementComponent/* implements OnChanges */{
+export class TypeGridElementComponent {
 
     @Input() document: FieldDocument;
     @Input() images: Array<SafeResourceUrl>;
     @Input() subtype?: FieldDocument;
 
-    // public imageUrls: Array<SafeResourceUrl> = [];
-
-
-    constructor(private imageUrlMaker: ImageUrlMaker) {}
-
-
-    // async ngOnChanges(changes: SimpleChanges) {
-
-    //     if (changes['document'] || changes['images']) await this.loadImages();
-    // }
-
-
-    // private async loadImages() {
-
-    //     for(const url in this.images){
-    //         await url;
-
-    //     }
-
-    //     // if (!this.images) return;
-
-    //     // for (let blob of this.images) {
-    //     //     const url = this.blobMaker.makeBlob(blob);
-    //     //     this.imageUrls.push(url.safeResourceUrl);
-    //     // }
-    // }
+    constructor() {}
 }

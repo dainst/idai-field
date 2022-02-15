17a54420365c279f43ae4956977187d944cc4b07
Simon Hohl <mail@dersmon.net>
2022-02-15 10:28:20 +0100

    Set host and port in runtime script

--- server/config/runtime.exs
+++ server/config/runtime.exs
@@ -24,14 +24,14 @@ if config_env() == :prod do
   host = System.get_env("HOST")
 
   config :field_hub, FieldHubWeb.Endpoint,
-    # url: [host: host, port: 443],
+    url: [host: host, port: 443],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      port: 4000
     ],
     secret_key_base: secret_key_base
 

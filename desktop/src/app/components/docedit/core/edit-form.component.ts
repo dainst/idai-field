4919c2fa7fb179dc1918fd1ac36a8db619f51dda
Thomas Kleinke <thomas.kleinke@dainst.de>
2022-02-16 17:27:12 +0100

    Remove unused dependency

--- desktop/src/app/components/docedit/core/edit-form.component.ts
+++ desktop/src/app/components/docedit/core/edit-form.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, ElementRef, Input, OnChanges, ViewChild } from '@angular/core';
 import { I18n } from '@ngx-translate/i18n-polyfill';
 import { isUndefinedOrEmpty, clone } from 'tsfun';
-import { Document, ProjectConfiguration, Field, Group, Labels, Named } from 'idai-field-core';
+import { Document, Field, Group, Labels } from 'idai-field-core';
 
 
 @Component({
@@ -33,7 +33,6 @@ export class EditFormComponent implements AfterViewInit, OnChanges {
 
     constructor(private elementRef: ElementRef,
                 private i18n: I18n,
-                private projectConfiguration: ProjectConfiguration,
                 private labels: Labels) {}
 
 

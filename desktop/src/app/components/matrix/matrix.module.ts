d8512dcea67ec3d023e8e22a9b722cfba59346af
Thomas Kleinke <thomas.kleinke@dainst.de>
2022-02-23 11:47:30 +0100

    Clean up imports

--- desktop/src/app/components/matrix/matrix.module.ts
+++ desktop/src/app/components/matrix/matrix.module.ts
@@ -1,13 +1,14 @@
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {MatrixViewComponent} from './matrix-view.component';
-import {GraphComponent} from './graph.component';
-import {MatrixState} from './matrix-state';
-import {WidgetsModule} from '../widgets/widgets.module';
-import {MatrixOptionsMenuComponent} from './matrix-options-menu.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatrixViewComponent } from './matrix-view.component';
+import { GraphComponent } from './graph.component';
+import { MatrixState } from './matrix-state';
+import { WidgetsModule } from '../widgets/widgets.module';
+import { MatrixOptionsMenuComponent } from './matrix-options-menu.component';
+
 
 @NgModule({
     imports: [
@@ -27,4 +28,4 @@ import {MatrixOptionsMenuComponent} from './matrix-options-menu.component';
     ]
 })
 
-export class MatrixModule { }
+export class MatrixModule {}

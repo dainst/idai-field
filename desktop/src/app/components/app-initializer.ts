c3cd09d6489a36b321351211c80e5bee9ce128e8
Simon Hohl <mail@dersmon.net>
2022-02-28 20:40:31 +0100

    Move thumbnail migration to its own file

--- desktop/src/app/components/app-initializer.ts
+++ desktop/src/app/components/app-initializer.ts
@@ -8,7 +8,6 @@ import {
     FulltextIndex,
     getConfigurationName,
     ImageStore,
-    ImageVariant,
     Indexer,
     IndexFacade,
     PouchdbDatastore,
@@ -24,8 +23,7 @@ import { Settings } from '../services/settings/settings';
 import { SampleDataLoader } from '../services/datastore/field/sampledata/sample-data-loader';
 import { ExpressServer } from '../services/express-server';
 import { ConfigurationIndex } from '../services/configuration/index/configuration-index';
-import { to } from 'tsfun';
-
+import { checkForDBThumbnails } from '../migration/thumbnail-copy';
 
 interface Services {
 
@@ -140,49 +138,6 @@ const loadSettings = async (settingsService: SettingsService, progress: Initiali
     await progress.setEnvironment(settings.dbs[0], Settings.getLocale());
 
     return settings;
-};
-
-
-const isThumbBroken = (data: Blob|any|undefined) => data === undefined || data.size === 0 || data.size === 2;
-
-/**
- * This is a migration function for upgrading the client version 2 to 3, which moved thumbnail data from couch/pouchdb to
- * the file system.
- */
-const checkForDBThumbnails = async (
-    project: string,
-    pouchDatastore: PouchdbDatastore,
-    imageStore: ImageStore
-): Promise<void> => {
-
-    const thumbnailCount = Object.keys(await imageStore.getFileInfos(project, [ImageVariant.THUMBNAIL])).length;
-    if (thumbnailCount !== 0) {
-        console.log('Found thumbnails in image store directory, not copying from database.');
-        return;
-    } else {
-        console.log('Copying thumbnails from database to filesystem.');
-    }
-
-    const options = {
-        include_docs: true,
-        attachments: true,
-        binary: true
-    };
-
-    const db = pouchDatastore.getDb();
-
-    const imageDocuments = (await db.allDocs(options)).rows.map(to('doc'));
-
-    for (const imageDocument of imageDocuments) {
-        if (imageDocument._attachments?.thumb && !isThumbBroken(imageDocument._attachments.thumb.data)) {
-            try {
-                const data = Buffer.from(await imageDocument._attachments.thumb.data.arrayBuffer());
-                await imageStore.store(imageDocument.resource.id, data, project, ImageVariant.THUMBNAIL);
-            } catch (err) {
-                console.error('Failed to save thumbnail for image: ' + imageDocument.resource.id, err);
-            }
-        }
-    }
 };
 
 
---
+++ desktop/src/app/migration/thumbnail-copy.ts
@@ -0,0 +1,49 @@
+import {
+    ImageStore,
+    ImageVariant,
+    PouchdbDatastore
+} from 'idai-field-core';
+
+import { to } from 'tsfun';
+
+const isThumbBroken = (data: Blob|any|undefined) => data === undefined || data.size === 0 || data.size === 2;
+
+/**
+ * This is a migration function for upgrading the client version 2 to 3, which moved thumbnail data from couch/pouchdb to
+ * the file system.
+ */
+export const checkForDBThumbnails = async (
+    project: string,
+    pouchDatastore: PouchdbDatastore,
+    imageStore: ImageStore
+): Promise<void> => {
+
+    const thumbnailCount = Object.keys(await imageStore.getFileInfos(project, [ImageVariant.THUMBNAIL])).length;
+    if (thumbnailCount !== 0) {
+        console.log('Found thumbnails in image store directory, not copying from database.');
+        return;
+    } else {
+        console.log('Copying thumbnails from database to filesystem.');
+    }
+
+    const options = {
+        include_docs: true,
+        attachments: true,
+        binary: true
+    };
+
+    const db = pouchDatastore.getDb();
+
+    const imageDocuments = (await db.allDocs(options)).rows.map(to('doc'));
+
+    for (const imageDocument of imageDocuments) {
+        if (imageDocument._attachments?.thumb && !isThumbBroken(imageDocument._attachments.thumb.data)) {
+            try {
+                const data = Buffer.from(await imageDocument._attachments.thumb.data.arrayBuffer());
+                await imageStore.store(imageDocument.resource.id, data, project, ImageVariant.THUMBNAIL);
+            } catch (err) {
+                console.error('Failed to save thumbnail for image: ' + imageDocument.resource.id, err);
+            }
+        }
+    }
+}

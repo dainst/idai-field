e99b900052d964b3ea8ea67d9ccacfb0368ca556
Thomas Kleinke <thomas.kleinke@dainst.de>
2022-02-28 12:37:15 +0100

    Clean up view modal components code

--- desktop/src/app/components/viewmodal/image/view-modal-drop-area.component.ts
+++ desktop/src/app/components/viewmodal/image/view-modal-drop-area.component.ts
@@ -1,25 +1,21 @@
-import {Component, EventEmitter, Output, ViewChild} from '@angular/core';
-import {DropAreaComponent} from '../../image/grid/drop-area.component';
-import {ImageUploader} from '../../image/upload/image-uploader';
-import {Messages} from '../../messages/messages';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { DropAreaComponent } from '../../image/grid/drop-area.component';
+import { ImageUploader } from '../../image/upload/image-uploader';
+import { Messages } from '../../messages/messages';
 
 
 @Component({
     selector: 'view-modal-drop-area',
     templateUrl: './view-modal-drop-area.html'
 })
-/**
- */
 export class ViewModalDropAreaComponent extends DropAreaComponent {
-
-    @ViewChild('popover', { static: false }) private popover: any;
 
     @Output() startEditImages = new EventEmitter<any>();
 
-    public constructor(
-        imageUploader: ImageUploader,
-        messages: Messages
-    ) {
+
+    public constructor(imageUploader: ImageUploader,
+                       messages: Messages) {
+
         super(imageUploader, messages);
     }
 }

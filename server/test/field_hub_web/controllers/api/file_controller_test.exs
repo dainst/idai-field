3ed8aa5f1397489e0fd2b785c9eb34c07a40bd3d
Simon Hohl <mail@dersmon.net>
2022-03-01 10:30:31 +0100

    Add further file controller tests

--- server/test/field_hub_web/controllers/api/file_controller_test.exs
+++ server/test/field_hub_web/controllers/api/file_controller_test.exs
@@ -9,40 +9,162 @@ defmodule FieldHubWeb.Api.FileControllerTest do
   @project "test_project"
   @user_name "test_user"
   @user_password "test_password"
+  @file_directory_root Application.get_env(:field_hub, :file_directory_root)
 
-  describe "couchdb interaction" do
+  @schema File.read!("../core/api-schemas/files-list.json")
+    |> Jason.decode!()
+    |> ExJsonSchema.Schema.resolve()
 
-    setup do
-      TestHelper.create_test_db_and_user(@project, @user_name, @user_password)
+  setup_all do
+    on_exit(fn ->
+      # Ran after all tests
+      File.rm_rf!(@file_directory_root)
+    end)
+  end
 
-      on_exit(fn ->
-        TestHelper.remove_test_db_and_user(@project, @user_name)
-      end)
-      :ok
-    end
+  setup do
+    # Ran before each test
+    TestHelper.create_test_db_and_user(@project, @user_name, @user_password)
 
-    test "GET /files/:project without valid credentials yields 401", %{conn: conn} do
+    on_exit(fn ->
+      # Ran after each test
+      TestHelper.remove_test_db_and_user(@project, @user_name)
+    end)
+    :ok
+  end
 
-      credentials = Base.encode64("non_existant_user:made_up_password")
+  test "GET /files/:project without valid credentials yields 401", %{conn: conn} do
 
-      conn =
-        conn
-        |> put_req_header("authorization", "Basic #{credentials}")
-        |> get("/files/test_project")
+    credentials = Base.encode64("non_existant_user:made_up_password")
 
-      assert conn.status == 401
-    end
+    conn =
+      conn
+      |> put_req_header("authorization", "Basic #{credentials}")
+      |> get("/files/test_project")
 
-    test "GET /files/:project with valid credentials yields 200", %{conn: conn} do
+    assert conn.status == 401
+  end
 
-      credentials = Base.encode64("#{@user_name}:#{@user_password}")
+  test "GET /files/:project with valid credentials yields 200", %{conn: conn} do
 
-      conn =
-        conn
-        |> put_req_header("authorization", "Basic #{credentials}")
-        |> get("/files/test_project")
+    credentials = Base.encode64("#{@user_name}:#{@user_password}")
 
-      assert conn.status == 200
-    end
+    conn =
+      conn
+      |> put_req_header("authorization", "Basic #{credentials}")
+      |> get("/files/test_project")
+
+    assert conn.status == 200
+  end
+
+
+  test "GET /files/:project without images yields valid json", %{conn: conn} do
+
+    credentials = Base.encode64("#{@user_name}:#{@user_password}")
+
+    conn =
+      conn
+      |> put_req_header("authorization", "Basic #{credentials}")
+      |> get("/files/test_project")
+
+    json_response =
+      conn.resp_body
+      |> Jason.decode!()
+
+    assert json_response == %{}
+    assert ExJsonSchema.Validator.valid?(@schema, json_response)
+  end
+
+  test "GET /files/:project with image yields valid json", %{conn: conn} do
+
+    credentials = Base.encode64("#{@user_name}:#{@user_password}")
+
+    content = File.read!("test/fixtures/logo.png")
+
+    FileStore.store_file(%{uuid: "1234", project: @project, type: :original_image, content: content})
+
+    conn =
+      conn
+      |> put_req_header("authorization", "Basic #{credentials}")
+      |> get("/files/test_project")
+
+    json_response =
+      conn.resp_body
+      |> Jason.decode!()
+
+    assert %{"1234" => %{"deleted" => false, "types" => ["original_image"]}} = json_response
+    assert ExJsonSchema.Validator.valid?(@schema, json_response)
+  end
+
+
+  test "GET /files/:project different file variants get added to the response", %{conn: conn} do
+
+    credentials = Base.encode64("#{@user_name}:#{@user_password}")
+
+    content = File.read!("test/fixtures/logo.png")
+
+    FileStore.store_file(%{uuid: "1234", project: @project, type: :original_image, content: content})
+
+    conn =
+      conn
+      |> put_req_header("authorization", "Basic #{credentials}")
+      |> get("/files/test_project")
+
+    json_response =
+      conn.resp_body
+      |> Jason.decode!()
+
+    assert %{"1234" => %{"deleted" => false, "types" => ["original_image"]}} = json_response
+    assert ExJsonSchema.Validator.valid?(@schema, json_response)
+
+    FileStore.store_file(%{uuid: "1234", project: @project, type: :thumbnail_image, content: content})
+
+    conn =
+      conn
+      |> recycle()
+      |> put_req_header("authorization", "Basic #{credentials}")
+      |> get("/files/test_project")
+
+    json_response =
+      conn.resp_body
+      |> Jason.decode!()
+
+    assert %{"1234" => %{"deleted" => false, "types" => ["thumbnail_image", "original_image"]}} = json_response
+    assert ExJsonSchema.Validator.valid?(@schema, json_response)
+  end
+
+  test "GET /files/:project yields files with tombstones flagged as deleted", %{conn: conn} do
+    credentials = Base.encode64("#{@user_name}:#{@user_password}")
+
+    content = File.read!("test/fixtures/logo.png")
+
+    FileStore.store_file(%{uuid: "1234", project: @project, type: :original_image, content: content})
+
+    conn =
+      conn
+      |> put_req_header("authorization", "Basic #{credentials}")
+      |> get("/files/test_project")
+
+    json_response =
+      conn.resp_body
+      |> Jason.decode!()
+
+    assert %{"1234" => %{"deleted" => false, "types" => ["original_image"]}} = json_response
+    assert ExJsonSchema.Validator.valid?(@schema, json_response)
+
+    FileStore.delete(%{uuid: "1234", project: @project})
+
+    conn =
+      conn
+      |> recycle()
+      |> put_req_header("authorization", "Basic #{credentials}")
+      |> get("/files/test_project")
+
+    json_response =
+      conn.resp_body
+      |> Jason.decode!()
+
+    assert %{"1234" => %{"deleted" => true, "types" => ["original_image"]}} = json_response
+    assert ExJsonSchema.Validator.valid?(@schema, json_response)
   end
 end

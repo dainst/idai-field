0f0c5bc46fa0084a2634bed68fd270c18411a3a5
Thomas Kleinke <thomas.kleinke@dainst.de>
2022-03-15 12:43:59 +0100

    Scroll to bottom when opening advanced settings

--- desktop/src/app/components/settings/settings.component.ts
+++ desktop/src/app/components/settings/settings.component.ts
@@ -1,14 +1,14 @@
-import {Component, OnInit} from '@angular/core';
-import {equal} from 'tsfun';
-import {M} from '../messages/m';
-import {TabManager} from '../../services/tabs/tab-manager';
-import {Messages} from '../messages/messages';
-import {reload} from '../../services/reload';
-import {Settings} from '../../services/settings/settings';
-import {SettingsProvider} from '../../services/settings/settings-provider';
-import {SettingsService} from '../../services/settings/settings-service';
-import {Menus} from '../../services/menus';
-import {MenuContext} from '../../services/menu-context';
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { equal } from 'tsfun';
+import { M } from '../messages/m';
+import { TabManager } from '../../services/tabs/tab-manager';
+import { Messages } from '../messages/messages';
+import { reload } from '../../services/reload';
+import { Settings } from '../../services/settings/settings';
+import { SettingsProvider } from '../../services/settings/settings-provider';
+import { SettingsService } from '../../services/settings/settings-service';
+import { Menus } from '../../services/menus';
+import { MenuContext } from '../../services/menu-context';
 
 const address = typeof window !== 'undefined' ? window.require('address') : require('address');
 const remote = typeof window !== 'undefined' ? window.require('@electron/remote') : undefined;
@@ -25,12 +25,16 @@ const remote = typeof window !== 'undefined' ? window.require('@electron/remote'
  * @author Sebastian Cuy
  * @author Thomas Kleinke
  */
-export class SettingsComponent implements OnInit {
+export class SettingsComponent implements OnInit, AfterViewChecked {
+
+    @ViewChild('settingsContainer') settingsContainer: ElementRef;
 
     public settings: Settings;
     public ipAddress: string = address.ip();
-    public saving = false;
-    public areAdvancedSettingCollapsed = true;
+    public saving: boolean = false;
+    public areAdvancedSettingCollapsed: boolean = true;
+    public scrollToBottom: boolean = false;
+
 
     constructor(private settingsProvider: SettingsProvider,
                 private settingsService: SettingsService,
@@ -42,6 +46,15 @@ export class SettingsComponent implements OnInit {
     ngOnInit() {
 
         this.settings = this.settingsProvider.getSettings();
+    }
+
+
+    ngAfterViewChecked() {
+
+        if (this.scrollToBottom) {
+            this.settingsContainer.nativeElement.scrollTo(0, this.settingsContainer.nativeElement.scrollHeight);
+            this.scrollToBottom = false;
+        }
     }
 
 
@@ -62,6 +75,13 @@ export class SettingsComponent implements OnInit {
     public toggleAutoUpdate() {
 
         this.settings.isAutoUpdateActive = !this.settings.isAutoUpdateActive;
+    }
+
+
+    public toggleAdvancedSettings() {
+
+        this.areAdvancedSettingCollapsed = !this.areAdvancedSettingCollapsed;
+        if (!this.areAdvancedSettingCollapsed) this.scrollToBottom = true;
     }
 
 

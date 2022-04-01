aa74167d0190e2fdcac287d5282cc3f1f542aaa7
Thomas Kleinke <thomas.kleinke@dainst.de>
2022-04-01 13:17:00 +0200

    Shorten log messages for missing image files

--- desktop/src/app/components/image/log-messages.ts
+++ desktop/src/app/components/image/log-messages.ts
@@ -1,24 +1,16 @@
-function _(id: string) {
-
-    return 'The filename of the missing image' +
-    ' is \'' + id + '\' (note: no file extension!). ' +
-    'It should be found in the project folder below the imagestore (see settings page).'
-}
-
-
 export function showMissingImageMessageOnConsole(id: string) {
 
-    console.error('Neither original nor thumbnail found for image: ' + _(id));
+    console.error('Neither original nor thumbnail found for image: ' + id);
 }
 
 
 export function showMissingThumbnailMessageOnConsole(id: string) {
 
-    console.error('No thumbnail found for image: ' + _(id));
+    console.error('No thumbnail found for image: ' + id);
 }
 
 
 export function showMissingOriginalImageMessageOnConsole(id: string) {
 
-    console.warn('No original found for image: ' + _(id));
+    console.warn('No original found for image: ' + id);
 }

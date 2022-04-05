0dcadd06d3307c966e1eea403b0c030b74f780f2
Simon Hohl <simon.hohl@dainst.org>
2022-04-05 12:41:03 +0200

    Set version to 3.0.0

--- server/mix.exs
+++ server/mix.exs
@@ -4,7 +4,7 @@ defmodule FieldHub.MixProject do
   def project do
     [
       app: :field_hub,
-      version: "0.1.0",
+      version: "3.0.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),

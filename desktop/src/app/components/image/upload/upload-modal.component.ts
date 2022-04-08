24bc6b71c323a3e588fba25a5ced4a855c913f47
Thomas Kleinke <thomas.kleinke@dainst.de>
2022-04-08 11:00:05 +0200

    Adjust imports

--- desktop/src/app/components/image/upload/upload-modal.component.ts
+++ desktop/src/app/components/image/upload/upload-modal.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
-import {UploadStatus} from './upload-status';
+import { Component } from '@angular/core';
+import { UploadStatus } from './upload-status';
+
 
 @Component({
     selector: 'upload-modal',

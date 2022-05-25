63a77bcf5b8a4b7c2163453a894b6400fac5835d
Simon Hohl <mail@dersmon.net>
2022-05-25 17:45:34 +0200

    Add option to allow incoming large file uploads
    This is disabled by default and blocks original images from being pushed by another machine to your machine.

--- desktop/src/app/services/settings/settings.ts
+++ desktop/src/app/services/settings/settings.ts
@@ -24,6 +24,7 @@ export interface Settings {
     imagestorePath: string;
     hideHiddenFieldsInConfigurationEditor?: boolean;
     highlightCustomElements?: boolean;
+    allowLargeFileUploads?: boolean;
 }
 
 

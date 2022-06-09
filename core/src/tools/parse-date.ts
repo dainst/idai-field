14a3bed899356246cc918424e17f49bff77f1e81
Thomas Kleinke <thomas.kleinke@dainst.de>
2022-06-09 11:19:28 +0200

    Support date values of format yyyy & mm.yyyy

--- core/src/tools/parse-date.ts
+++ core/src/tools/parse-date.ts
@@ -2,13 +2,32 @@ export function parseDate(dateString: string): Date|undefined {
 
     try {
         const dateComponents: string[] = dateString.split('.');
+        if (dateComponents.filter(component => !isNumber(component)).length > 0) return undefined;
 
-        return new Date(
-            parseInt(dateComponents[2]),
-            parseInt(dateComponents[1]) - 1,
-            parseInt(dateComponents[0])
-        );
+        if (dateComponents.length === 1) {
+            return new Date(
+                parseInt(dateComponents[0])
+            );
+        } else if (dateComponents.length === 2) {
+            return new Date(
+                parseInt(dateComponents[1]) - 1,
+                parseInt(dateComponents[0])
+            );
+        } else if (dateComponents.length === 3) {
+            return new Date(
+                parseInt(dateComponents[2]),
+                parseInt(dateComponents[1]) - 1,
+                parseInt(dateComponents[0])
+            );
+        }
+        
     } catch (_) {
         return undefined;
     }
+}
+
+
+function isNumber(value: string): boolean {
+
+    return value.match(/^[0-9]*$/) !== null;
 }

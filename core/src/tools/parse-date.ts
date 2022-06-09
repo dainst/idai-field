e631b553cb3530da06a06dd3efe3764d9e435330
Thomas Kleinke <thomas.kleinke@dainst.de>
2022-06-09 12:06:21 +0200

    Fix parsing dates

--- core/src/tools/parse-date.ts
+++ core/src/tools/parse-date.ts
@@ -6,12 +6,12 @@ export function parseDate(dateString: string): Date|undefined {
 
         if (dateComponents.length === 1) {
             return new Date(
-                parseInt(dateComponents[0])
+                parseInt(dateComponents[0]), 0
             );
         } else if (dateComponents.length === 2) {
             return new Date(
-                parseInt(dateComponents[1]) - 1,
-                parseInt(dateComponents[0])
+                parseInt(dateComponents[1]),
+                parseInt(dateComponents[0]) - 1
             );
         } else if (dateComponents.length === 3) {
             return new Date(

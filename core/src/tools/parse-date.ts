acd618fe06a25e3588bfa79292360cfe50c636b8
Thomas Kleinke <thomas.kleinke@dainst.de>
2022-06-09 12:25:12 +0200

    Fix beginning/end date check for month & year values

--- core/src/tools/parse-date.ts
+++ core/src/tools/parse-date.ts
@@ -1,4 +1,4 @@
-export function parseDate(dateString: string): Date|undefined {
+export function parseDate(dateString: string, latestPossibleDate: boolean = false): Date|undefined {
 
     try {
         const dateComponents: string[] = dateString.split('.');
@@ -6,13 +6,21 @@ export function parseDate(dateString: string): Date|undefined {
 
         if (dateComponents.length === 1) {
             return new Date(
-                parseInt(dateComponents[0]), 0
+                parseInt(dateComponents[0]),
+                latestPossibleDate ? 11 : 0,
+                latestPossibleDate ? 31 : 1
             );
         } else if (dateComponents.length === 2) {
-            return new Date(
-                parseInt(dateComponents[1]),
-                parseInt(dateComponents[0]) - 1
-            );
+            return latestPossibleDate
+                ? new Date(
+                    parseInt(dateComponents[1]),
+                    parseInt(dateComponents[0]),
+                    0
+                ) : new Date(
+                    parseInt(dateComponents[1]),
+                    parseInt(dateComponents[0]) - 1,
+                    1
+                );
         } else if (dateComponents.length === 3) {
             return new Date(
                 parseInt(dateComponents[2]),

58aa38760193ae5ca95f5c826bf14d989867b9d1
Thomas Kleinke <thomas.kleinke@dainst.de>
2022-06-14 12:55:09 +0200

    Improve date validation

--- core/src/tools/parse-date.ts
+++ core/src/tools/parse-date.ts
@@ -3,34 +3,62 @@ export function parseDate(dateString: string, latestPossibleDate: boolean = fals
     try {
         const dateComponents: string[] = dateString.split('.');
         if (dateComponents.filter(component => !isNumber(component)).length > 0) return undefined;
-
-        if (dateComponents.length === 1) {
-            return new Date(
-                parseInt(dateComponents[0]),
-                latestPossibleDate ? 11 : 0,
-                latestPossibleDate ? 31 : 1
-            );
-        } else if (dateComponents.length === 2) {
-            return latestPossibleDate
-                ? new Date(
-                    parseInt(dateComponents[1]),
-                    parseInt(dateComponents[0]),
-                    0
-                ) : new Date(
-                    parseInt(dateComponents[1]),
-                    parseInt(dateComponents[0]) - 1,
-                    1
-                );
-        } else if (dateComponents.length === 3) {
-            return new Date(
-                parseInt(dateComponents[2]),
-                parseInt(dateComponents[1]) - 1,
-                parseInt(dateComponents[0])
-            );
-        }
         
+        const date: Date|undefined = parse(dateComponents, latestPossibleDate);
+
+        return (date && isValid(date, dateComponents)) ? date : undefined;
     } catch (_) {
         return undefined;
+    }
+}
+
+
+function parse(dateComponents: string[], latestPossibleDate: boolean = false): Date|undefined {
+
+    if (dateComponents.length === 1) {
+        return new Date(
+            parseInt(dateComponents[0]),
+            latestPossibleDate ? 11 : 0,
+            latestPossibleDate ? 31 : 1
+        );
+    } else if (dateComponents.length === 2) {
+        return latestPossibleDate
+            ? new Date(
+                parseInt(dateComponents[1]),
+                parseInt(dateComponents[0]),
+                0
+            ) : new Date(
+                parseInt(dateComponents[1]),
+                parseInt(dateComponents[0]) - 1,
+                1
+            );
+    } else if (dateComponents.length === 3) {
+        return new Date(
+            parseInt(dateComponents[2]),
+            parseInt(dateComponents[1]) - 1,
+            parseInt(dateComponents[0])
+        );
+    }
+}
+
+
+function isValid(date: Date, dateComponents: string[]): boolean {
+
+    const normalizedDateComponents: string[] = dateComponents.map(dateComponent => {
+        if (dateComponent.startsWith('0')) {
+            return dateComponent.slice(1);
+        } else {
+            return dateComponent;
+        }
+    });
+
+    if (normalizedDateComponents.length === 2) {
+        return (date.getMonth() + 1).toString() === normalizedDateComponents[0];
+    } else if (normalizedDateComponents.length === 3) {
+        return (date.getMonth() + 1).toString() === normalizedDateComponents[1]
+            && date.getDate().toString() === normalizedDateComponents[0];
+    } else {
+        return true;
     }
 }
 
--- core/test/tools/parse-date.spec.ts
+++ core/test/tools/parse-date.spec.ts
@@ -8,15 +8,29 @@ describe('parseDate', () => {
 
     it('parse dates', () => {
 
-        expect(parseDate('04.10.2021').toLocaleDateString()).toEqual('4.10.2021');
-        expect(parseDate('10.2021').toLocaleDateString()).toEqual('1.10.2021');
-        expect(parseDate('2021').toLocaleDateString()).toEqual('1.1.2021');
+        expect(parseDate('04.10.2021')?.toLocaleDateString()).toEqual('4.10.2021');
+        expect(parseDate('4.10.2021')?.toLocaleDateString()).toEqual('4.10.2021');
+        expect(parseDate('10.09.2021')?.toLocaleDateString()).toEqual('10.9.2021');
+        expect(parseDate('10.9.2021')?.toLocaleDateString()).toEqual('10.9.2021');
+        expect(parseDate('29.02.2020')?.toLocaleDateString()).toEqual('29.2.2020');
+        expect(parseDate('10.2021')?.toLocaleDateString()).toEqual('1.10.2021');
+        expect(parseDate('09.2021')?.toLocaleDateString()).toEqual('1.9.2021');
+        expect(parseDate('9.2021')?.toLocaleDateString()).toEqual('1.9.2021');
+        expect(parseDate('2021')?.toLocaleDateString()).toEqual('1.1.2021');
     });
 
 
     it('parse dates as latest possible date', () => {
 
-        expect(parseDate('10.2021', true).toLocaleDateString()).toEqual('31.10.2021');
-        expect(parseDate('2021', true).toLocaleDateString()).toEqual('31.12.2021');
+        expect(parseDate('10.2021', true)?.toLocaleDateString()).toEqual('31.10.2021');
+        expect(parseDate('2021', true)?.toLocaleDateString()).toEqual('31.12.2021');
+    });
+
+
+    it('do not parse invalid dates', () => {
+
+        expect(parseDate('32.01.2021')).toBeUndefined();
+        expect(parseDate('29.02.2021')).toBeUndefined();
+        expect(parseDate('01.14.2021')).toBeUndefined();
     });
 });

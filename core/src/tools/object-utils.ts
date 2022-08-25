b5def9dd102a192f61f100b90ced5fbfebba077e
Thomas Kleinke <thomas.kleinke@dainst.de>
2022-08-25 11:22:08 +0200

    Remove unused function

--- core/src/tools/object-utils.ts
+++ core/src/tools/object-utils.ts
@@ -4,7 +4,5 @@
  */
 export module ObjectUtils {
 
-    export const jsonEqual = (l: any) => (r: any) => JSON.stringify(l) === JSON.stringify(r);
-
     export function jsonClone(x: any) { return JSON.parse(JSON.stringify(x)); }
 }

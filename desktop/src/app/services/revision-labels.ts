ec393a89c8a638875974dabc465d0416394e2c01
Thomas Kleinke <thomas.kleinke@dainst.de>
2022-09-02 13:26:13 +0200

    Translate revision labels

--- desktop/src/app/services/revision-labels.ts
+++ desktop/src/app/services/revision-labels.ts
@@ -1,6 +1,7 @@
 import { Document } from 'idai-field-core';
 
 const moment = typeof window !== 'undefined' ? window.require('moment') : require('moment');
+const remote = typeof window !== 'undefined' ? window.require('@electron/remote') : undefined;
 
 
 /**
@@ -18,7 +19,13 @@ export module RevisionLabels {
 
     export function getLastModifiedDateLabel(revision: Document): string {
 
-        moment.locale('de');
-        return moment(Document.getLastModified(revision).date).format('DD. MMMM YYYY HH:mm:ss [Uhr]');
+        const locale: string = remote.getGlobal('getLocale')();
+        moment.locale(locale);
+
+        const lastModifiedDate: Date = Document.getLastModified(revision).date;
+
+        return moment(lastModifiedDate).format('LL') + ' '
+            + moment(lastModifiedDate).format('LTS')
+            + (locale === 'de' ? ' Uhr' : '');
     }
 }

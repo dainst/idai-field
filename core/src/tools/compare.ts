2e27f895d50941ed7c893d551ff943fb7fb4ce81
Thomas Kleinke <thomas.kleinke@dainst.de>
2022-10-20 11:49:46 +0200

    Compare order of arrays in compare function

--- core/src/tools/compare.ts
+++ core/src/tools/compare.ts
@@ -1,19 +1,10 @@
-import { equal, same, sameset } from 'tsfun';
+import { equal, same } from 'tsfun';
 
 
 export function compare(value1: any, value2: any): boolean {
 
     if (value1 === undefined && value2 === undefined) return true;
     if ((value1 && !value2) || (!value1 && value2)) return false;
-
-    const type1: string = getType(value1);
-    const type2: string = getType(value2);
-
-    if (type1 !== type2) return false;
-
-    if (type1 === 'array' && type2 === 'array') {
-        return sameset(equal, value1, value2);
-    }
 
     return equal(value1)(value2);
 }
@@ -23,13 +14,3 @@ export const notCompareInBoth = (l: any, r: any) => (key: string) => !compare((l
 
 
 export const notBothEqual = (l: any, r: any) => (key: string) => !r[key] || !same(l[key], r[key]);
-
-
-function getType(value: any): string {
-
-    return typeof value === 'object'
-        ? value instanceof Array
-            ? 'array'
-            : 'object'
-        : 'flat';
-}

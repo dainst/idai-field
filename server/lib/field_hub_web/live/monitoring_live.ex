6452ead58e3e9d69f3dd9118eb5f0d834514ad0c
Simon Hohl <mail@dersmon.net>
2022-12-16 13:25:49 +0100

    Add issues to monitoring web view

--- server/lib/field_hub_web/live/monitoring_live.ex
+++ server/lib/field_hub_web/live/monitoring_live.ex
@@ -1,10 +1,13 @@
 defmodule FieldHubWeb.MonitoringLive do
+  alias FieldHub.Issues
   alias FieldHub.{
     CouchService,
     Statistics
   }
 
   use Phoenix.LiveView
+
+  require Logger
 
   def mount(%{"project" => project}, %{"user" => user, "password" => password}, socket) do
 
@@ -15,11 +18,13 @@ defmodule FieldHubWeb.MonitoringLive do
       }
 
    Process.send(self(), :update, [])
+   Process.send(self(), :update_issues, [])
 
     {
       :ok,
       socket
       |> assign(:stats, :loading)
+      |> assign(:issues, :loading)
       |> assign(:project, project)
       |> assign(:credentials, credentials)
     }
@@ -36,6 +41,27 @@ defmodule FieldHubWeb.MonitoringLive do
     {:noreply, assign(socket, :stats, stats)}
   end
 
+  def handle_info(:update_issues, %{assigns: %{credentials: credentials, project: project}} = socket) do
+
+    issues =
+      credentials
+      |> Issues.check_file_store(project)
+
+    timer =
+      case Enum.count(issues) * 10 do
+        count when count < 1000 ->
+          1000
+        count ->
+          count
+      end
+
+    Logger.debug("Running next issue update in #{timer} ms.")
+
+    Process.send_after(self(), :update_issues, timer)
+
+    {:noreply, assign(socket, :issues, issues)}
+  end
+
   def get_file_label(key) do
     case key do
       :original_image ->
@@ -45,4 +71,7 @@ defmodule FieldHubWeb.MonitoringLive do
     end
   end
 
+  def issue_classes(:info), do: "monitoring-issue-info"
+  def issue_classes(:warning), do: "monitoring-issue-warning"
+  def issue_classes(:error), do: "monitoring-issue-error"
 end

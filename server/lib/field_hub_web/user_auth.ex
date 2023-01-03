d5d1df4386468f195cf67316108894d6031faccd
Simon Hohl <simon.hohl@dainst.de>
2023-01-03 15:56:13 +0100

    Remove unused function
    Got copied over when exerimenting.

--- server/lib/field_hub_web/user_auth.ex
+++ server/lib/field_hub_web/user_auth.ex
@@ -146,23 +146,7 @@ defmodule FieldHubWeb.UserAuth do
   end
 
   @doc """
-  Used for routes that require the user to not be authenticated.
-  """
-  def redirect_if_user_is_authenticated(conn, _opts) do
-    if conn.assigns[:current_user] do
-      conn
-      |> redirect(to: signed_in_path(conn))
-      |> halt()
-    else
-      conn
-    end
-  end
-
-  @doc """
   Used for routes that require the user to be authenticated.
-
-  If you want to enforce the user email is confirmed before
-  they use the application at all, here would be a good place.
   """
   def require_authenticated_user(conn, _opts) do
     if conn.assigns[:current_user] do

82e301433dedaf37f97bf39f20c75606a2f7dd5d
Simon Hohl <mail@dersmon.net>
2023-01-04 13:03:01 +0100

    Refactor API status view

--- server/lib/field_hub_web/views/error_view.ex
+++ server/lib/field_hub_web/views/error_view.ex
@@ -2,10 +2,6 @@
 defmodule FieldHubWeb.ErrorView do
   use FieldHubWeb, :view
 
-  def render("400.json", %{message: msg}) do
-    msg
-  end
-
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.html" becomes
   # "Not Found".

77fc65e480b1d6432008c7dfd5f88d3af283924b
Simon Hohl <mail@dersmon.net>
2023-01-04 16:39:43 +0100

    Add test for project list on landing page

--- server/test/test_helper.exs
+++ server/test/test_helper.exs
@@ -7,6 +7,7 @@ defmodule FieldHub.TestHelper do
     CLI.create_project(project)
     CLI.create_user(user_name, user_password)
     CLI.add_user_as_project_member(user_name, project)
+    CLI.add_user_as_project_member(Application.get_env(:field_hub, :couchdb_user_name), project)
   end
 
   def remove_test_db_and_user(project, user_name) do

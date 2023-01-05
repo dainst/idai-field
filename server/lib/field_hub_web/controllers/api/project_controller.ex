b13248af9a4fc5229a37efc5f37129b7df6e4e9f
Simon Hohl <mail@dersmon.net>
2023-01-05 22:15:36 +0100

    Add :list, :show and stub for :create project api

---
+++ server/lib/field_hub_web/controllers/api/project_controller.ex
@@ -0,0 +1,67 @@
+defmodule FieldHubWeb.Api.ProjectController do
+  alias FieldHub.{
+    Statistics,
+    CouchService
+  }
+
+  alias FieldHubWeb.Api.StatusView
+
+  use FieldHubWeb, :controller
+
+  def index(%{assigns: %{current_user: user_name}} = conn, _params) do
+    render(conn, "list.json", %{projects: CouchService.get_databases_for_user(user_name)})
+  end
+
+  def show(conn, %{"project" => project_name}) do
+    render(conn, "show.json", %{project: Statistics.evaluate_project(project_name)})
+  end
+
+  def create(conn, %{"project" => _project_name}) do
+    # STUB/TODO: Refactor CLI module to more general Administration module and provide useful
+    # return values, which the CLI module currently lacks.
+    conn
+    |> put_status(:created)
+    |> put_view(StatusView)
+    |> render(%{info: "Project created."})
+
+    #   result =
+    #     read_body(conn)
+    #     |> case do
+    #       {:ok, "", _conn} ->
+    #         password = CouchService.create_password()
+
+    #         {
+    #           password,
+    #           FieldHub.CLI.create_project_with_default_user(
+    #             project_name,
+    #             password
+    #           )
+    #           |> IO.inspect()
+    #         }
+
+    #       {:ok, body, _conn} ->
+    #         body
+    #         |> Jason.decode()
+    #         |> case do
+    #           {:ok, %{password: password}} ->
+    #             {password, FieldHub.CLI.create_project_with_default_user(project_name, password)}
+
+    #           _ ->
+    #             {:invalid_payload, body}
+    #         end
+    #     end
+
+    #   case result do
+    #     {password, %{status_code: 200}} ->
+    #       conn
+    #       |> put_view(StatusView)
+    #       |> render(%{info: %{password: password}})
+
+    #     {:invalid_payload, payload} ->
+    #       conn
+    #       |> put_status(:bad_request)
+    #       |> put_view(StatusView)
+    #       |> render(%{error: "Invalid payload #{payload}"})
+    #   end
+  end
+end

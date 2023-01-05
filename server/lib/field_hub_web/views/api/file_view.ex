a025e2272185eeabd3a2427a03cbe26e45e4517b
Simon Hohl <mail@dersmon.net>
2023-01-05 21:04:00 +0100

    Remove unused function, match template explicitely
    Errors are logged using the StatusView module.

--- server/lib/field_hub_web/views/api/file_view.ex
+++ server/lib/field_hub_web/views/api/file_view.ex
@@ -1,11 +1,7 @@
 defmodule FieldHubWeb.Api.FileView do
   use FieldHubWeb, :view
 
-  def render(_, %{files: files}) do
+  def render("list.json", %{files: files}) do
     files
-  end
-
-  def render(_, %{error: reason}) do
-    %{reason: reason}
   end
 end

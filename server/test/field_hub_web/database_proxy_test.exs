1edb6890a2e6e9275fd25171f22a814312626425
Simon Hohl <simon.hohl@dainst.de>
2023-01-10 15:12:41 +0100

    Add test for `/db` route

---
+++ server/test/field_hub_web/database_proxy_test.exs
@@ -0,0 +1,22 @@
+defmodule FieldHubWeb.DatabaseProxyTest do
+  use FieldHubWeb.ConnCase
+
+  test "connection threw proxy plug can be established", %{conn: conn} do
+
+    conn =
+      conn
+      |> get("/db")
+
+    assert conn.status == 200
+
+    response =
+      conn.resp_body
+      |> Jason.decode!()
+
+    assert %{
+      "couchdb" => "Welcome",
+      "vendor" => %{"name" => "The Apache Software Foundation"},
+      "version" => "3.2.1"
+    } = response
+  end
+end

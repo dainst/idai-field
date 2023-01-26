24dead9afb660030fcd8a8689308f6ea78874e55
tkleinke <thomas.kleinke@gbv.de>
2023-01-26 15:28:20 +0100

    Skip parsing georeference if image file is not a tiff

--- desktop/src/app/components/image/georeference/geotiff-import.ts
+++ desktop/src/app/components/image/georeference/geotiff-import.ts
@@ -7,8 +7,17 @@ import { ImageGeoreference } from 'idai-field-core';
 export async function getGeoreferenceFromGeotiff(buffer: Buffer): Promise<ImageGeoreference|undefined> {
 
     const GeoTIFF = await import('geotiff');
-    const tiff = await GeoTIFF.fromBlob(new Blob([buffer]));
+
+    let tiff;
+    try {
+        tiff = await GeoTIFF.fromBlob(new Blob([buffer]));
+    } catch (err) {
+        console.warn('Failed to parse georeference. The file is probably not a tiff file.', err);
+        return undefined;
+    }
+    
     const image = await tiff.getImage();
+    
     if (image.getGeoKeys()) {
         return createGeoreference(image.getBoundingBox());
     } else {

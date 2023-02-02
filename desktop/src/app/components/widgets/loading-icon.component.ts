c026b92d161f511de526875b6bcd2d88aedd75fb
tkleinke <thomas.kleinke@gbv.de>
2023-02-02 10:57:23 +0100

    Clean up imports

--- desktop/src/app/components/widgets/loading-icon.component.ts
+++ desktop/src/app/components/widgets/loading-icon.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input} from '@angular/core';
-import {Loading} from './loading';
+import { Component, Input } from '@angular/core';
+import { Loading } from './loading';
+
 
 @Component({
     selector: 'loading-icon',

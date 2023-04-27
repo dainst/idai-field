0cc88d6acbb50563099349c6574526d716ed02e9
Dan <dan@eighttrigrams.net>
2023-04-27 17:42:17 +0100

    Fetch input fields and prepare input field based filters

--- web/ui/src/idai_field/filter/CategoryFilter.tsx
+++ web/ui/src/idai_field/filter/CategoryFilter.tsx
@@ -1,8 +1,9 @@
 import React, { CSSProperties, ReactElement, ReactNode } from 'react';
-import { Col, Dropdown, Row } from 'react-bootstrap';
+import { Col, Dropdown, DropdownButton, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { flatten } from 'tsfun';
-import { FilterBucketTreeNode, ResultFilter } from '../../api/result';
+import { Tree, Forest } from 'idai-field-core';
+import { FilterBucketTreeNode, FilterBucket, ResultFilter } from '../../api/result';
 import CategoryIcon from '../../shared/document/CategoryIcon';
 import { getLabel } from '../../shared/languages';
 import { ProjectView } from '../project/Project';
@@ -20,20 +21,82 @@ export default function CategoryFilter({ filter, searchParams = new URLSearchPar
     return <div onMouseLeave={ () => onMouseLeave && onMouseLeave([]) }>
         { filter.values.map((bucket: FilterBucketTreeNode) =>
             renderFilterValue(filter.name, bucket, searchParams, onMouseEnter, projectId, projectView)) }
+
+        { false && // TODO remove later
+            (projectId && projectView)
+            && (searchParams.getAll('resource.category.name').length === 1)
+            &&
+            <InputFieldFilters
+              projectId={ projectId }
+              projectView={ projectView }
+              searchParams={ searchParams }
+              filter={ filter } />
+        }
+    </div>;
+}
+
+function InputFieldFilters({ projectId, projectView, searchParams, filter }: { projectId: string,
+    projectView: ProjectView, searchParams: URLSearchParams, filter: ResultFilter}) {
+
+    const fieldNames = getInputFieldNames(searchParams, filter);
+    console.log('fieldNames', fieldNames);
+
+    return <div>
+        <DropdownButton id="basicbutton" title="abc">
+            {
+                fieldNames.map(fieldName => renderFieldName(fieldName))
+            }
+        </DropdownButton>
+
+        <Link to={ `/project/${projectId}/${projectView}?`
+            + buildParamsForFilterValue(searchParams, 'resource.shortDescription', 'Ein') }>xo
+        </Link>
     </div>;
 }
 
 
+const renderFieldName = (fieldName: string) => {
+    return <Dropdown.Item key={ fieldName }>
+        { fieldName }
+    </Dropdown.Item>;
+};
+
+
+const getInputFieldNames = (searchParams: URLSearchParams, filter: ResultFilter) => {
+
+    const filterBucket = findFilterBucket(searchParams.get('resource.category.name'), filter.values);
+    if (!filterBucket) return [];
+    
+    const groups = filterBucket.value['groups'];
+    if (!groups) return [];
+
+    const fields = groups.map(group => group['fields']);
+
+    return (flatten(fields) as unknown as { inputType: string, name: string }[])
+        .filter(field => field.inputType === 'input')
+        .map(field => field.name);
+};
+
+
+const findFilterBucket = (match: string, t: (FilterBucketTreeNode|FilterBucket)[]): FilterBucket|undefined => {
+
+    const result: FilterBucketTreeNode = Tree.find(t as undefined as Forest<FilterBucket>,
+        item => item.value.name === match );
+    return result ? result.item : undefined;
+};
+
+
 const renderFilterValue = (key: string, bucket: FilterBucketTreeNode, params: URLSearchParams,
         onMouseEnter?: (categories: string[]) => void, projectId?: string, projectView?: ProjectView,
-        level: number = 1): ReactNode =>
-    <React.Fragment key={ bucket.item.value.name }>
+        level: number = 1): ReactNode => {
+    return <React.Fragment key={ bucket.item.value.name }>
+
         <Dropdown.Item
                 as={ Link }
                 style={ filterValueStyle(level) }
                 onMouseOver={ () => onMouseEnter && onMouseEnter(getCategoryAndSubcategoryNames(bucket)) }
                 to={ ((projectId && projectView) ? `/project/${projectId}/${projectView}?` : '/?')
-                    + buildParamsForFilterValue(params, key, bucket.item.value.name) }>
+                    + buildParamsForFilterValue(params, key, bucket.item.value.name) + '' }>
             <Row>
                 <Col xs={ 1 }><CategoryIcon category={ bucket.item.value }
                                             size="30" /></Col>
@@ -54,6 +117,7 @@ const renderFilterValue = (key: string, bucket: FilterBucketTreeNode, params: UR
             renderFilterValue(key, b, params, onMouseEnter, projectId, projectView, level + 1))
         }
     </React.Fragment>;
+    };
 
 
 const getCategoryAndSubcategoryNames = (bucket: FilterBucketTreeNode): string[] => {

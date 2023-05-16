0e18b6cfa14e3b7c74598a57a4bd740a28e018f0
Simon Hohl <simon.hohl@dainst.de>
2023-05-16 11:12:56 +0200

    Upgrade to Elixir 1.14

--- web/api/mix.exs
+++ web/api/mix.exs
@@ -5,7 +5,7 @@ defmodule Api.MixProject do
     [
       app: :api,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()

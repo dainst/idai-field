9de266836f42d41d0c72c631d54dbec39717035a
Danilo Guzzo <guzzo.danilo91@gmail.com>
2023-06-22 15:42:02 +0200

    Long name conversion into initials+clickable icon

--- desktop/src/app/components/navbar/taskbar-editor.component.ts
+++ desktop/src/app/components/navbar/taskbar-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';;
+import { Component, OnInit } from '@angular/core';
 import { SettingsProvider } from '../../services/settings/settings-provider';
 import { AppComponent } from '../app.component';
 
@@ -9,25 +9,40 @@ import { AppComponent } from '../app.component';
 /**
  * @author Danilo Guzzo
  */
-
 export class TaskbarEditorComponent implements OnInit {
     public currentEditor: string;
+    public initials: string;
 
     constructor(
         private settingsProvider: SettingsProvider,
         private appComponent: AppComponent
     ) {
         this.currentEditor = this.settingsProvider.getSettings().username;
-        this.settingsProvider.settingsChangesNotifications().subscribe(settings => this.currentEditor = settings.username);
+        this.createInitials();
+        this.settingsProvider
+            .settingsChangesNotifications()
+            .subscribe((settings) => (this.currentEditor = settings.username));
     }
     public openModal = () => this.appComponent.promptEditorName();
 
     ngOnInit() {
-
         this.currentEditor = this.settingsProvider.getSettings().username;
     }
 
-    public getEditorName() {
+    public createInitials() {
+        let tempInitials: string = '';
+        if (this.currentEditor.length > 20) {
+            let splitted: string[] = this.currentEditor.split(' ');
+            for (let i = 0; i <= splitted.length - 1; i++) {
+                tempInitials += splitted[i].charAt(0).toUpperCase() + '.';
+            }
+            return tempInitials;
+        } else {
+            return this.currentEditor;
+        }
+    }
+
+    public getEditorName(): string {
         return this.currentEditor;
     }
 }

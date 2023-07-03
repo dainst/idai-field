051148a618f830cb4088de80877829a3501e10a0
Simon Hohl <mail@dersmon.net>
2023-07-03 14:34:14 +0200

    Initial commit for FieldPublicaiton

---
+++ publication/lib/field_publication/application.ex
@@ -0,0 +1,36 @@
+defmodule FieldPublication.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Start the Telemetry supervisor
+      FieldPublicationWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: FieldPublication.PubSub},
+      # Start Finch
+      {Finch, name: FieldPublication.Finch},
+      # Start the Endpoint (http/https)
+      FieldPublicationWeb.Endpoint
+      # Start a worker by calling: FieldPublication.Worker.start_link(arg)
+      # {FieldPublication.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: FieldPublication.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    FieldPublicationWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
---
+++ publication/lib/field_publication_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule FieldPublicationWeb.PageController do
+  use FieldPublicationWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end

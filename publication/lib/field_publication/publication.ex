940041fecf8878ecc068bffbbe22d5f416725d60
Simon Hohl <mail@dersmon.net>
2023-07-03 22:24:23 +0200

    Add publication's configuration to project db

--- publication/lib/field_publication/publication.ex
+++ publication/lib/field_publication/publication.ex
@@ -26,7 +26,7 @@ defmodule FieldPublication.Publication do
              source_password,
              publication_name
            ),
-         :ok <- create_publication_metadata(publication_name) do
+         {:ok, _} <- create_publication_metadata(project_name, publication_name) do
       CouchService.add_application_user(publication_name)
 
       %{
@@ -45,44 +45,32 @@ defmodule FieldPublication.Publication do
     end
   end
 
-  defp create_publication_metadata(publication_name) do
+  defp create_publication_metadata(project_name, publication_name) do
     url = Application.get_env(:field_publication, :couchdb_url)
 
-    translations_database = "#{publication_name}#{@translation_db_suffix}"
-    styles_database = "#{publication_name}#{@styles_db_suffix}"
+    {:ok, full_config} = create_full_configuration(url, publication_name)
 
-    with {:ok, %Finch.Response{status: val}} when val == 201 or val == 412 <-
-           CouchService.create_database(translations_database),
-         {:ok, %Finch.Response{status: val}} when val == 201 or val == 412 <-
-           CouchService.create_database(styles_database),
-         {:ok, full_config} <- create_full_configuration(url, publication_name),
-         flattened_categories <-
-           Enum.reduce(full_config, [], &flatten_full_configuration/2) do
-      flattened_categories
-      |> Enum.map(&parse_category/1)
-      |> Enum.map(fn parsed_category ->
-        # Ugly split between translations and :color
-        CouchService.store_document(
-          translations_database,
-          "category_#{parsed_category.name}",
-          Map.delete(parsed_category, :color)
-        )
+    metadata = %{
+      publication_name => %{
+        date: DateTime.to_iso8601(DateTime.now!("Etc/UTC")),
+        configuration: full_config
+      }
+    }
 
-        CouchService.store_document(
-          styles_database,
-          "category_#{parsed_category.name}",
-          %{color: Map.get(parsed_category, :color)}
-        )
-      end)
+    CouchService.retrieve_document(project_name, "publications")
+    |> case do
+      {:ok, %Finch.Response{status: 404}} ->
+        CouchService.store_document(project_name, "publications", metadata)
+      {:ok, %Finch.Response{body: body, status: 200}} ->
+        updated =
+          body
+          |> Jason.decode!()
+          |> then(fn(existing) ->
+            existing
+            |> Map.merge(metadata)
+          end)
 
-      CouchService.add_application_user(translations_database)
-      CouchService.add_application_user(styles_database)
-
-      :ok
-    else
-      error ->
-        Logger.error(error)
-        :error
+        CouchService.store_document(project_name, "publications", updated)
     end
   end
 
@@ -106,43 +94,43 @@ defmodule FieldPublication.Publication do
     end
   end
 
-  defp flatten_full_configuration(%{"item" => item, "trees" => trees}, acc) do
-    Enum.reduce(trees, acc ++ [item], &flatten_full_configuration/2)
-  end
+  # defp flatten_full_configuration(%{"item" => item, "trees" => trees}, acc) do
+  #   Enum.reduce(trees, acc ++ [item], &flatten_full_configuration/2)
+  # end
 
-  defp parse_category(%{
-         "color" => color,
-         "description" => description,
-         "label" => label,
-         "name" => name,
-         "groups" => groups
-       }) do
-    %{
-      name: name,
-      description: Enum.map(description, &parse_language_map/1),
-      color: color,
-      label: Enum.map(label, &parse_language_map/1),
-      groups: Enum.map(groups, &parse_group/1)
-    }
-  end
+  # defp parse_category(%{
+  #        "color" => color,
+  #        "description" => description,
+  #        "label" => label,
+  #        "name" => name,
+  #        "groups" => groups
+  #      }) do
+  #   %{
+  #     name: name,
+  #     description: Enum.map(description, &parse_language_map/1),
+  #     color: color,
+  #     label: Enum.map(label, &parse_language_map/1),
+  #     groups: Enum.map(groups, &parse_group/1)
+  #   }
+  # end
 
-  defp parse_group(%{"label" => label, "name" => name, "fields" => fields}) do
-    %{
-      label: Enum.map(label, &parse_language_map/1),
-      name: name,
-      fields: Enum.map(fields, &parse_field/1)
-    }
-  end
+  # defp parse_group(%{"label" => label, "name" => name, "fields" => fields}) do
+  #   %{
+  #     label: Enum.map(label, &parse_language_map/1),
+  #     name: name,
+  #     fields: Enum.map(fields, &parse_field/1)
+  #   }
+  # end
 
-  defp parse_field(%{"description" => description, "label" => label, "name" => name}) do
-    %{
-      label: Enum.map(label, &parse_language_map/1),
-      name: name,
-      description: Enum.map(description, &parse_language_map/1)
-    }
-  end
+  # defp parse_field(%{"description" => description, "label" => label, "name" => name}) do
+  #   %{
+  #     label: Enum.map(label, &parse_language_map/1),
+  #     name: name,
+  #     description: Enum.map(description, &parse_language_map/1)
+  #   }
+  # end
 
-  defp parse_language_map({key, value}) do
-    %{String.to_atom(key) => value}
-  end
+  # defp parse_language_map({key, value}) do
+  #   %{String.to_atom(key) => value}
+  # end
 end

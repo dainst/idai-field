e5018f5e544e740915542beafb8367d23d349d95
Danilo Guzzo <guzzo.danilo91@gmail.com>
2023-07-11 16:32:14 +0200

    Add QR Code generator

--- desktop/src/app/components/widgets/documentinfo/document-info.component.ts
+++ desktop/src/app/components/widgets/documentinfo/document-info.component.ts
@@ -1,6 +1,26 @@
-import { Component, Input, Output, ElementRef, ViewChild, EventEmitter, OnChanges } from '@angular/core';
-import { Document, Named, FieldDocument, Groups, ProjectConfiguration, Datastore, Hierarchy } from 'idai-field-core';
-
+import {
+    Component,
+    Input,
+    Output,
+    ElementRef,
+    ViewChild,
+    EventEmitter,
+    OnChanges,
+} from '@angular/core';
+import { NgbModalRef, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { SettingsProvider } from '../../../services/settings/settings-provider';
+import {
+    Document,
+    Named,
+    FieldDocument,
+    Groups,
+    ProjectConfiguration,
+    Datastore,
+    Hierarchy
+} from 'idai-field-core';
+import { QrCodeModalComponent } from './qrcode-modal';
+import { Menus } from '../../../services/menus';
+import { MenuContext} from '../../../services/menu-context';
 
 @Component({
     selector: 'document-info',
@@ -10,7 +30,6 @@ import { Document, Named, FieldDocument, Groups, ProjectConfiguration, Datastore
  * @author Thomas Kleinke
  */
 export class DocumentInfoComponent implements OnChanges {
-
     @ViewChild('documentInfo', { static: false }) documentInfoElement: ElementRef;
 
     @Input() document: Document;
@@ -22,21 +41,30 @@ export class DocumentInfoComponent implements OnChanges {
     @Input() transparentBackground: boolean = false;
 
     @Output() onStartEdit: EventEmitter<void> = new EventEmitter<void>();
-    @Output() onJumpToResource: EventEmitter<FieldDocument> = new EventEmitter<FieldDocument>();
+    @Output() onJumpToResource: EventEmitter<FieldDocument> =
+        new EventEmitter<FieldDocument>();
     @Output() onThumbnailClicked: EventEmitter<void> = new EventEmitter<void>();
-    @Output() onCloseButtonClicked: EventEmitter<void> = new EventEmitter<void>();
+    @Output() onCloseButtonClicked: EventEmitter<void> =
+        new EventEmitter<void>();
 
-    public openSection: string|undefined = Groups.STEM;
-    public parentDocument: FieldDocument|undefined;
+    public openSection: string | undefined = Groups.STEM;
+    public parentDocument: FieldDocument | undefined;
+    public project: string;
 
-
-    constructor(private projectConfiguration: ProjectConfiguration,
-                private datastore: Datastore) {}
-
+    constructor(
+        private projectConfiguration: ProjectConfiguration,
+        private datastore: Datastore,
+        private settingsProvider: SettingsProvider,
+        private menus: Menus,
+        private modalService: NgbModal,
+    ) {
+        this.project = this.settingsProvider.getSettings().selectedProject;
+    }
 
     public startEdit = () => this.onStartEdit.emit();
 
-    public jumpToResource = (document: FieldDocument) => this.onJumpToResource.emit(document);
+    public jumpToResource = (document: FieldDocument) =>
+        this.onJumpToResource.emit(document);
 
     public close = () => this.onCloseButtonClicked.emit();
 
@@ -44,46 +72,61 @@ export class DocumentInfoComponent implements OnChanges {
 
     public isReadonly = () => this.document.project !== undefined;
 
-
     async ngOnChanges() {
-
         this.parentDocument = this.document
             ? await this.getParentDocument()
             : undefined;
     }
 
-
     public toggleExpandAllGroups() {
-
         this.setExpandAllGroups(!this.getExpandAllGroups());
     }
 
-
     public setOpenSection(section: string) {
-
         this.openSection = section;
         this.setExpandAllGroups(false);
     }
 
-
     public isImageDocument() {
-
-        return this.projectConfiguration.getImageCategories().map(Named.toName)
+        return this.projectConfiguration
+            .getImageCategories()
+            .map(Named.toName)
             .includes(this.document.resource.category);
     }
 
-
     public isThumbnailShown(): boolean {
-
-        return this.showThumbnail && Document.hasRelations(this.document, 'isDepictedIn');
+        return (
+            this.showThumbnail &&
+            Document.hasRelations(this.document, 'isDepictedIn')
+        );
     }
 
-
-    private async getParentDocument(): Promise<FieldDocument|undefined> {
-
-        return await Hierarchy.getParentDocument(
-            id => this.datastore.get(id),
+    private async getParentDocument(): Promise<FieldDocument | undefined> {
+        return (await Hierarchy.getParentDocument(
+            (id) => this.datastore.get(id),
             this.document
-        ) as FieldDocument;
+        )) as FieldDocument;
+    }
+
+    public async openQRCodeModal() {
+
+        try {
+            this.menus.setContext(MenuContext.MODAL);
+
+            const modalRef: NgbModalRef = this.modalService.open(
+                QrCodeModalComponent, { animation: false, backdrop: 'static' }
+            );
+            
+            modalRef.componentInstance.project = this.project;
+            modalRef.componentInstance.documentId = this.document._id;
+            modalRef.componentInstance.identifier = this.document.resource.identifier;
+
+            modalRef.componentInstance.render();
+            return true;
+        } catch (_) {
+            return false;
+        } finally {
+            this.menus.setContext(MenuContext.DEFAULT);
+        }
     }
 }
---
+++ desktop/src/app/components/widgets/documentinfo/qrcode-modal.ts
@@ -0,0 +1,35 @@
+import { Component, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+var  QRCode = require('qrcode')
+
+@Component({
+    selector: 'qrcode-modal',
+    templateUrl: './qrcode-modal.html'
+})
+/**
+ * @author Danilo Guzzo
+ */
+export class QrCodeModalComponent {
+   
+    @Input()
+    public project: string;
+    @Input()
+    public documentId: string;
+    @Input()
+    public identifier: string;
+
+    constructor(
+        public activeModal: NgbActiveModal
+    ) { }
+
+    public close = () => this.activeModal.close();
+
+    public render() {
+        var container = document.getElementById('qrcode-canvas')
+        QRCode.toCanvas(
+            container, 
+            `${this.project}-${this.documentId}-${this.identifier}`, 
+            {width: 300}
+        );
+    }
+}

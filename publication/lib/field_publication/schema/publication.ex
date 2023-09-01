c5c380a52702a1104f664de220d960c3cfcf0e57
Simon Hohl <mail@dersmon.net>
2023-09-01 19:41:00 +0200

    Use draft date as primary key for publications

--- publication/lib/field_publication/schema/publication.ex
+++ publication/lib/field_publication/schema/publication.ex
@@ -6,21 +6,20 @@ defmodule FieldPublication.Schema.Publication do
   alias FieldPublication.Schema.Translation
 
   @derive Jason.Encoder
-  @primary_key {:database, :string, autogenerate: false}
+  @primary_key {:draft_date, :date, autogenerate: false}
   embedded_schema do
-    field :status, Ecto.Enum, values: [:draft, :public], default: :draft
     field :source_url, :string
     field :source_project_name, :string
-    field :draft_date, :date
     field :publication_date, :date
     field :configuration_doc, :string
+    field :database, :string
     embeds_many :comments, Translation
   end
 
   def changeset(publication, attrs \\ %{}) do
     publication
-    |> cast(attrs, [:status, :source_url, :source_project_name, :draft_date, :publication_date, :configuration_doc, :database])
+    |> cast(attrs, [:source_url, :source_project_name, :draft_date, :publication_date, :configuration_doc, :database])
     |> cast_embed(:comments)
-    |> validate_required([:status, :source_url, :source_project_name, :draft_date, :configuration_doc, :database])
+    |> validate_required([:source_url, :source_project_name, :draft_date, :configuration_doc, :database])
   end
 end

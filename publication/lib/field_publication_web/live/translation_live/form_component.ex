c54569b1f906d6cc824085051c606fb68b40241c
Simon Hohl <mail@dersmon.net>
2023-09-04 16:42:59 +0200

    Minor reformatting for translation input

--- publication/lib/field_publication_web/live/translation_live/form_component.ex
+++ publication/lib/field_publication_web/live/translation_live/form_component.ex
@@ -5,13 +5,17 @@ defmodule FieldPublicationWeb.TranslationLive.FormComponent do
   def render(assigns) do
     ~H"""
     <div>
-      <div class="flex flex-row">
-        <.inputs_for :let={translation} field={@form_field}>
-          <.input type="select" options={ISO639.iso639_1_codes()} field={translation[:language]}/>
-          <.input type="textarea" field={translation[:text]}/>
+      <.inputs_for :let={translation} field={@form_field}>
+        <div class="flex flex-row w-full">
+          <span>
+            <.input type="select" options={ISO639.iso639_1_codes()} field={translation[:language]} label="Language"/>
+          </span>
+          <span class="pl-2 w-96">
+            <.input type="textarea" field={translation[:text]} label="Comment"/>
+          </span>
           <span phx-click={@remove} phx-value-id={translation.id}>X</span>
-        </.inputs_for>
-      </div>
+        </div>
+      </.inputs_for>
       <a phx-click={@add} type="button">Add comment</a>
     </div>
     """

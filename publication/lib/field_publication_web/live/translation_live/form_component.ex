bc9861ae74622fc7f9000b22f8598fe095681b00
Simon Hohl <mail@dersmon.net>
2023-09-05 11:09:53 +0200

    Additional styling for translation form

--- publication/lib/field_publication_web/live/translation_live/form_component.ex
+++ publication/lib/field_publication_web/live/translation_live/form_component.ex
@@ -6,25 +6,29 @@ defmodule FieldPublicationWeb.TranslationLive.FormComponent do
     ~H"""
     <div>
       <.inputs_for :let={translation} field={@form_field}>
-        <div class="flex flex-row w-full">
-          <span>
+        <div class="flex flex-row w-full border-2 p-2 m-2 bg-slate-100 rounded">
+          <div>
             <.input type="select" options={ISO639.iso639_1_codes()} field={translation[:language]} label="Language"/>
-          </span>
-          <span class="pl-2 w-96">
+          </div>
+          <div class="pl-2 w-96">
             <.input type="textarea" field={translation[:text]} label="Comment"/>
-          </span>
-          <span phx-click={@remove} phx-target={
+          </div>
+          <a class="cursor-pointer" phx-click={@remove} phx-target={
             # This form can be used by both views and live components. If live components want to
             # handle the data themselves, they can pass target={@myself}. Otherwise their view will have to
             # implement `handle_info/3`.
             if assigns[:target], do: @target
-          } phx-value-id={translation.id}>X</span>
+          } phx-value-id={translation.id}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </a>
         </div>
       </.inputs_for>
-      <a phx-click={@add} phx-target={
+      <button type="button" phx-click={@add} phx-target={
         # Same as for @remove above.
         if assigns[:target], do: @target
-      } type="button">Add comment</a>
+      } type="button">Add comment</button>
     </div>
     """
   end

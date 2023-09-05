fdfe4455817f36081b01a38e763f05fed8fa4a51
Simon Hohl <mail@dersmon.net>
2023-09-05 11:11:20 +0200

    Reduce title width in data lists

--- publication/lib/field_publication_web/components/core_components.ex
+++ publication/lib/field_publication_web/components/core_components.ex
@@ -529,7 +529,7 @@ defmodule FieldPublicationWeb.CoreComponents do
     <div class="mt-14">
       <dl class="-my-4 divide-y divide-zinc-100">
         <div :for={item <- @item} class="flex gap-4 py-4 text-sm leading-6 sm:gap-8">
-          <dt class="w-1/4 flex-none text-zinc-500"><%= item.title %></dt>
+          <dt class="w-1/12 flex-none text-zinc-500"><%= item.title %></dt>
           <dd class="text-zinc-700"><%= render_slot(item) %></dd>
         </div>
       </dl>

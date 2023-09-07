09a04de46be6eb3bd7b706e47bc3e101ddb84dc4
Simon Hohl <mail@dersmon.net>
2023-09-07 11:19:17 +0200

    Adjust layout for translation form

--- publication/lib/field_publication_web/live/translation_live/form_component.ex
+++ publication/lib/field_publication_web/live/translation_live/form_component.ex
@@ -13,16 +13,18 @@ defmodule FieldPublicationWeb.TranslationLive.FormComponent do
           <div class="pl-2 w-96">
             <.input type="textarea" field={translation[:text]} label="Comment"/>
           </div>
-          <a class="cursor-pointer" phx-click={@remove} phx-target={
-            # This form can be used by both views and live components. If live components want to
-            # handle the data themselves, they can pass target={@myself}. Otherwise their view will have to
-            # implement `handle_info/3`.
-            if assigns[:target], do: @target
-          } phx-value-id={translation.id}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </a>
+          <div>
+            <a class="cursor-pointer" phx-click={@remove} phx-target={
+              # This form can be used by both views and live components. If live components want to
+              # handle the data themselves, they can pass target={@myself}. Otherwise their view will have to
+              # implement `handle_info/3`.
+              if assigns[:target], do: @target
+            } phx-value-id={translation.id}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </a>
+          </div>
         </div>
       </.inputs_for>
       <button type="button" phx-click={@add} phx-target={

01238a0f9d9a6cc90d12cf1c58937e9c94206fdd
Simon Hohl <simon.hohl@dainst.de>
2023-09-08 08:41:03 +0200

    Fix compile env
    Missed in  dcd90ab3f3cec6d6bf1358eb64f4d364cff26d0f.

--- publication/lib/field_publication/replication/couch_replication.ex
+++ publication/lib/field_publication/replication/couch_replication.ex
@@ -1,5 +1,5 @@
 defmodule FieldPublication.Replication.CouchReplication do
-  @fix_source_url Application.compile_env(:field_publication, :development_mode, false)
+  @fix_source_url Application.compile_env(:field_publication, :dev_routes)
   @poll_frequency 1000
 
   alias Phoenix.PubSub

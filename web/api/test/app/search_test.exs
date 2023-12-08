03474229a1dede2a903b48aa1694cc61fca984cc
Dan <dan@eighttrigrams.net>
2023-12-08 19:48:25 +0000

    Test adding the groups to filter when exactly one category selected

--- web/api/test/app/search_test.exs
+++ web/api/test/app/search_test.exs
@@ -16,9 +16,14 @@ defmodule Api.AppTest.SearchTest do
   @user1 {"user-1", "pass-1"}
 
   setup context do
+   filters =if context[:category_selected] do
+        ["project:a", "category:Operation"]
+      else
+        ["project:a"]
+      end
     context
       |> put_in([:method], :post)
-      |> put_in([:body], %{ filters: ["project:a"]})
+      |> put_in([:body], %{ filters: filters })
       |> AppTestHelper.perform_query
   end
 
@@ -56,5 +61,13 @@ defmodule Api.AppTest.SearchTest do
         ]
       }
     ] == context.body.filters
+  end
+
+  @tag path: @documents_path, login: @user1, category_selected: true
+  test "search with category selected (when exactly one category selected, it will add groups to the filter value for that category)", context do
+    assert ["stem", "parent", "dimension", "position"] == context.body.filters
+      |> Enum.find(&(&1.name == "resource.category.name"))
+      |> get_in([:values, Access.at(0), :item, :value, :groups])
+      |> Enum.map(&(&1.name))
   end
 end

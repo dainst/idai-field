a5a621e8452f5c6d9a412d57515a1a28d364917a
tkleinke <thomas.kleinke@gbv.de>
2024-02-07 17:44:10 +0100

    Improve performance

--- desktop/src/app/components/navbar/projects.component.ts
+++ desktop/src/app/components/navbar/projects.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChangesStream } from 'idai-field-core';
 import { MenuModalLauncher } from '../../services/menu-modal-launcher';
 import { SettingsProvider } from '../../services/settings/settings-provider';
 import { ProjectLabelProvider } from '../../services/project-label-provider';
@@ -14,23 +15,34 @@ import { ProjectLabelProvider } from '../../services/project-label-provider';
  */
 export class ProjectsComponent {
 
+    public projectLabel: string;
     public username: string;
 
 
     constructor(private settingsProvider: SettingsProvider,
                 private menuModalLauncher: MenuModalLauncher,
-                private projectLabelProvider: ProjectLabelProvider) {
+                private projectLabelProvider: ProjectLabelProvider,
+                private changesStream: ChangesStream,
+                private changeDetectorRef: ChangeDetectorRef) {
 
         this.username = this.settingsProvider.getSettings().username;
-        this.settingsProvider.settingsChangesNotifications().subscribe((settings) => {
+        this.projectLabel = this.projectLabelProvider.getProjectLabel();
+
+        this.settingsProvider.settingsChangesNotifications().subscribe(settings => {
             this.username = settings.username;
         });
+        this.changesStream.projectDocumentNotifications().subscribe(() => this.updateProjectLabel());
     }
 
-
-    public getProjectLabel = () => this.projectLabelProvider.getProjectLabel();
 
     public openModal = () => this.menuModalLauncher.editProject();
 
     public openUsernameModal = () => this.menuModalLauncher.openUpdateUsernameModal();
+
+
+    private updateProjectLabel() {
+
+        this.projectLabel = this.projectLabelProvider.getProjectLabel();
+        this.changeDetectorRef.detectChanges();
+    }
 }

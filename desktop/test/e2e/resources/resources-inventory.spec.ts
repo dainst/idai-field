d29da8ade026b455dd84adabfc59b2a934123297
tkleinke <thomas.kleinke@gbv.de>
2024-02-19 16:38:06 +0100

    Add e2e test for showing linked find for storage place

--- desktop/test/e2e/resources/resources-inventory.spec.ts
+++ desktop/test/e2e/resources/resources-inventory.spec.ts
@@ -1,7 +1,9 @@
-import { getText, navigateTo, resetApp, start, stop, waitForExist, waitForNotExist, pause } from '../app';
+import { getText, navigateTo, resetApp, start, stop, waitForExist, waitForNotExist } from '../app';
 import { ResourcesPage } from './resources.page';
 import { ResourcesGridListPage } from './resources-grid-list.page';
 import { SearchBarPage } from '../widgets/search-bar.page';
+import { DoceditPage } from '../docedit/docedit.page';
+import { DoceditRelationsPage } from '../docedit/docedit-relations.page';
 
 const { test, expect } = require('@playwright/test');
 
@@ -28,6 +30,38 @@ test.describe('resources/inventory --', () => {
     test.afterAll(async () => {
 
         await stop();
+    });
+
+
+    async function linkWithFind() {
+
+        await ResourcesGridListPage.clickEditButton();
+        await DoceditPage.clickGotoInventoryTab();
+        await DoceditRelationsPage.clickAddRelationForGroupWithIndex('isStoragePlaceOf');
+        await DoceditRelationsPage.typeInRelation('isStoragePlaceOf', 'testf1');
+        await DoceditRelationsPage.clickChooseRelationSuggestion(0);
+        return DoceditPage.clickSaveDocument();
+    }
+
+
+    test('show linked find for storage place', async () => {
+
+        await ResourcesPage.performCreateResource('SP1', 'storageplace', undefined, undefined, false, true);
+        await ResourcesGridListPage.clickGridElement('SP1');
+        await ResourcesPage.performCreateResource('SP2', 'storageplace', undefined, undefined, false, true);
+        await ResourcesGridListPage.clickGridElement('SP2');
+        await waitForNotExist(await ResourcesGridListPage.getLinkedDocumentsGrid());
+
+        await linkWithFind();
+
+        await ResourcesGridListPage.clickToggleLinkedDocumentsSectionButton();
+        await waitForExist(await ResourcesGridListPage.getGridElement('testf1'));
+
+        await ResourcesPage.clickNavigationButton('SP1');
+        await waitForExist(await ResourcesGridListPage.getGridElement('testf1'));
+
+        const text = await ResourcesGridListPage.getLinkedDocumentBadgeText('testf1');
+        expect(text).toBe('SP2');
     });
 
 

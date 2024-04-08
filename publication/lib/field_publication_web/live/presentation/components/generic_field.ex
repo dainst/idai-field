c1de354ed712dafa76be3ca202a982570c409d83
Simon Hohl <mail@dersmon.net>
2024-04-08 23:22:29 +0200

    Add support for further fields

--- publication/lib/field_publication_web/live/presentation/components/generic_field.ex
+++ publication/lib/field_publication_web/live/presentation/components/generic_field.ex
@@ -5,7 +5,7 @@ defmodule FieldPublicationWeb.Presentation.Components.GenericField do
   alias FieldPublicationWeb.Presentation.Components.I18n
 
   def render(%{type: single_value} = assigns)
-      when single_value in ["input", "text"] do
+      when single_value in ["input", "text", "simpleInput"] do
     ~H"""
     <dt class="font-bold"><I18n.text values={@labels} /></dt>
     <dd class="ml-4"><I18n.text values={@values} /></dd>
@@ -20,7 +20,7 @@ defmodule FieldPublicationWeb.Presentation.Components.GenericField do
     """
   end
 
-  def render(%{type: "checkboxes"} = assigns) do
+  def render(%{type: type} = assigns) when type in ["checkboxes", "simpleMultiInput"] do
     ~H"""
     <dt class="font-bold"><I18n.text values={@labels} /></dt>
     <%= for value <- @values do %>
@@ -37,6 +37,24 @@ defmodule FieldPublicationWeb.Presentation.Components.GenericField do
         do: Gettext.gettext(FieldPublicationWeb.Gettext, "Yes"),
         else: Gettext.gettext(FieldPublicationWeb.Gettext, "No") %>
     </dd>
+    """
+  end
+
+  def render(%{type: "literature", values: values} = assigns) when is_list(values) do
+    ~H"""
+    <dt class="font-bold"><I18n.text values={@labels} /></dt>
+    <%= for value <- @values do %>
+      <dd class="ml-4 mb-1">
+        <%= case value do %>
+          <% %{"doi" => url, "quotation" => quotation} -> %>
+            <a class="text-[#5882c2] hover:text-[#375d97]" href={url}><%= quotation %></a>
+          <% %{"zenonId" => url, "quotation" => quotation} -> %>
+            <a class="text-[#5882c2] hover:text-[#375d97]" href={url}><%= quotation %></a>
+          <% %{"quotation" => quotation} -> %>
+            <%= quotation %>
+        <% end %>
+      </dd>
+    <% end %>
     """
   end
 
--- publication/lib/field_publication_web/live/presentation/components/project_document.ex
+++ publication/lib/field_publication_web/live/presentation/components/project_document.ex
@@ -4,7 +4,7 @@ defmodule FieldPublicationWeb.Presentation.Components.ProjectDocument do
   alias FieldPublicationWeb.Presentation.Components.{
     I18n,
     Image,
-    DocumentLink
+    GenericField
   }
 
   alias FieldPublication.Publications.Data
@@ -22,12 +22,15 @@ defmodule FieldPublicationWeb.Presentation.Components.ProjectDocument do
           <.header>
             <%= gettext("project_doc_about_project") %>
           </.header>
-          <I18n.markdown values={Data.get_field_values(@doc, "description")} lang={@lang} />
-
-          <.header>
+          <div class="bg-slate-100 p-2 rounded">
+            <I18n.markdown values={Data.get_field_values(@doc, "description")} lang={@lang} />
+          </div>
+          <.header class="mt-3">
             <%= gettext("project_doc_about_publication") %>
           </.header>
-          <I18n.markdown values={@publication_comments} lang={@lang} />
+          <div class="bg-slate-100 p-2 rounded">
+            <I18n.markdown values={@publication_comments} lang={@lang} />
+          </div>
 
           <% depicted_in = Data.get_relation_by_name(@doc, "isDepictedIn") %>
           <%= if depicted_in do %>
@@ -56,28 +59,64 @@ defmodule FieldPublicationWeb.Presentation.Components.ProjectDocument do
 
         <div class="basis-1/3 m-5">
           <dl>
-            <dt class="font-semibold">
-              <I18n.text values={Data.get_field_labels(@doc, "institution")} />
-            </dt>
-            <dd class="ml-2">
-              <I18n.text values={Data.get_field_values(@doc, "institution")} />
-            </dd>
-            <dt class="font-semibold">
-              <I18n.text values={Data.get_field_labels(@doc, "projectSupervisor")} />
-            </dt>
-            <dd class="ml-2">
-              <%= Data.get_field_values(@doc, "projectSupervisor") %>
-            </dd>
-            <dt class="font-semibold">
-              <I18n.text values={Data.get_field_labels(@doc, "contactPerson")} />
-            </dt>
-            <dd class="ml-2">
-              <%= Data.get_field_values(@doc, "contactPerson") %> (<%= Data.get_field_values(
-                @doc,
-                "contactMail"
-              ) %>)
-            </dd>
+            <% institution = Data.get_field(@doc, "institution") %>
+            <%= if institution do %>
+              <GenericField.render
+                values={institution["values"]}
+                labels={institution["labels"]}
+                lang={@lang}
+                type={institution["type"]}
+              />
+            <% end %>
+
+            <% supervisor = Data.get_field(@doc, "projectSupervisor") %>
+            <%= if supervisor do %>
+              <GenericField.render
+                values={supervisor["values"]}
+                labels={supervisor["labels"]}
+                lang={@lang}
+                type={supervisor["type"]}
+              />
+            <% end %>
+
+            <% staff = Data.get_field(@doc, "staff") %>
+            <%= if staff do %>
+              <GenericField.render
+                values={staff["values"]}
+                labels={staff["labels"]}
+                lang={@lang}
+                type={staff["type"]}
+              />
+            <% end %>
+
+            <% bibliographic_references = Data.get_field(@doc, "bibliographicReferences") %>
+            <%= if bibliographic_references do %>
+              <GenericField.render
+                values={bibliographic_references["values"]}
+                labels={bibliographic_references["labels"]}
+                lang={@lang}
+                type={bibliographic_references["type"]}
+              />
+            <% end %>
           </dl>
+
+          <% contact_mail = Data.get_field_values(@doc, "contactMail") %>
+          <%= if contact_mail do %>
+            <div class="mt-2">
+              <a href={"mailto:#{contact_mail}"}>
+                <.icon name="hero-envelope" class="h-6 w-6 mr-1" /><%= gettext("contact") %>
+              </a>
+            </div>
+          <% end %>
+
+          <% url = Data.get_field_values(@doc, "projectURI") %>
+          <%= if url do %>
+            <div class="mt-2">
+              <a href={url}>
+                <.icon name="hero-link" class="h-6 w-6 mr-1" /><%= gettext("project_home_page") %>
+              </a>
+            </div>
+          <% end %>
         </div>
       </div>
     </div>

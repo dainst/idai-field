4a9643240b78e3162a4e33c29030c48dffa8b7d0
tkleinke <thomas.kleinke@gbv.de>
2024-06-26 16:31:56 +0200

    Adjust BackupLoadingComponent tests

--- desktop/test/unit/components/backup/backup-loading.component.spec.ts
+++ desktop/test/unit/components/backup/backup-loading.component.spec.ts
@@ -44,7 +44,6 @@ describe('BackupLoadingComponent', () => {
             backupProvider,
             tabManager,
             menuService,
-            undefined,
             undefined
         );
 

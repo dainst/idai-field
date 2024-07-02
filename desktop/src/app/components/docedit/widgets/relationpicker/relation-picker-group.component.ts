0d98c1ea0aa5c37d485c6924444a7d36d645dd92
tkleinke <thomas.kleinke@gbv.de>
2024-07-02 15:10:45 +0200

    Adjust imports

--- desktop/src/app/components/docedit/widgets/relationpicker/relation-picker-group.component.ts
+++ desktop/src/app/components/docedit/widgets/relationpicker/relation-picker-group.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges} from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { isEmpty } from 'tsfun';
 import { Relation, Resource } from 'idai-field-core';
 

9ac51af61ff8ff21b2bb92ee6ff9e68133ee15c6
Simon Hohl <mail@dersmon.net>
2024-07-05 11:23:22 +0200

    Show all relations in image view component
    ...also hides group heading if there are no fields within a group.

--- publication/lib/field_publication_web/live/presentation/document_components/image_document.ex
+++ publication/lib/field_publication_web/live/presentation/document_components/image_document.ex
@@ -10,7 +10,6 @@ defmodule FieldPublicationWeb.Presentation.DocumentComponents.Image do
     ClipboardCopy
   }
 
-  alias FieldPublication.Publications.Data
   import FieldPublicationWeb.CoreComponents
 
   import FieldPublicationWeb.Presentation.Components.Typography
@@ -19,39 +18,47 @@ defmodule FieldPublicationWeb.Presentation.DocumentComponents.Image do
     ~H"""
     <div>
       <.document_heading>
-        <DocumentLink.show project={@project_name} date={@publication_date} lang={@lang} doc={@doc} />
+        <DocumentLink.show project={@project_name} date={@draft_date} lang={@lang} doc={@doc} />
       </.document_heading>
 
       <div class="flex flex-row">
         <div class="basis-1/3 m-5">
-          <% depicts = Data.get_relation_by_name(@doc, "depicts") %>
-          <%= if depicts do %>
-            <I18n.text values={depicts["labels"]} />:
-            <div class="overflow-auto overscroll-contain max-h-[190px]">
-              <%= for doc <- depicts["values"] do %>
+          <%= for relations <- @doc["relations"] do %>
+            <.group_heading>
+              <I18n.text values={relations["labels"]} /> (<%= Enum.count(relations["values"]) %>)
+            </.group_heading>
+            <div class="overflow-auto overscroll-contain max-h-[200px]">
+              <%= for doc <- relations["values"] do %>
                 <DocumentLink.show
                   project={@project_name}
-                  date={@publication_date}
+                  date={@draft_date}
                   lang={@lang}
                   doc={doc}
+                  image_count={0}
                 />
               <% end %>
             </div>
           <% end %>
           <%= for group <- @doc["groups"] do %>
-            <section>
-              <.group_heading>
-                <I18n.text values={group["labels"]} />
-              </.group_heading>
+            <% fields =
+              Enum.reject(group["fields"], fn %{"key" => key} ->
+                key in ["identifier", "category", "geometry"]
+              end) %>
+            <%= unless fields == [] do %>
+              <section>
+                <.group_heading>
+                  <I18n.text values={group["labels"]} />
+                </.group_heading>
 
-              <dl>
-                <%= for field <- group["fields"] |> Enum.reject(fn(%{"key" => key}) -> key in ["identifier", "category"] end)  do %>
-                  <div>
-                    <GenericField.render field={field} lang={@lang} />
-                  </div>
-                <% end %>
-              </dl>
-            </section>
+                <dl>
+                  <%= for field <- fields do %>
+                    <div>
+                      <GenericField.render field={field} lang={@lang} />
+                    </div>
+                  <% end %>
+                </dl>
+              </section>
+            <% end %>
           <% end %>
           <hr class="mt-4" />
 
@@ -65,18 +72,12 @@ defmodule FieldPublicationWeb.Presentation.DocumentComponents.Image do
               </a>
             </li>
             <li>
-              <a
-                target="_blank"
-                href={~p"/api/raw/csv/#{@project_name}/#{@publication_date}/#{@uuid}"}
-              >
+              <a target="_blank" href={~p"/api/raw/csv/#{@project_name}/#{@draft_date}/#{@uuid}"}>
                 <.icon name="hero-table-cells-solid" /> Download CSV
               </a>
             </li>
             <li>
-              <a
-                target="_blank"
-                href={~p"/api/json/raw/#{@project_name}/#{@publication_date}/#{@uuid}"}
-              >
+              <a target="_blank" href={~p"/api/json/raw/#{@project_name}/#{@draft_date}/#{@uuid}"}>
                 <span class="text-center inline-block w-[20px]" style="block">{}</span> Download JSON
               </a>
             </li>

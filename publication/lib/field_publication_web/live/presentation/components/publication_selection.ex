16a6e57ec027b95b867b09734b793e8d657c4425
Simon Hohl <mail@dersmon.net>
2024-07-05 15:17:15 +0200

    Refactor publication selection component

--- publication/lib/field_publication_web/live/presentation/components/publication_selection.ex
+++ publication/lib/field_publication_web/live/presentation/components/publication_selection.ex
@@ -4,63 +4,55 @@ defmodule FieldPublicationWeb.Presentation.Components.PublicationSelection do
 
   def render(assigns) do
     ~H"""
-    <form id="project_options" phx-change="project_options_changed">
-      <div class="flex flex-row">
-        <div class="mt-4">
-          <.link navigate={~p"/"}>
-            <.icon class="ml-1 mb-1" name="hero-globe-europe-africa-solid" />
-          </.link>
-          /
-        </div>
+    <form
+      id="project_options"
+      class="flex flex-row gap-2 items-center"
+      phx-change="project_options_changed"
+    >
+      <.link navigate={~p"/"}>
+        <.icon name="hero-globe-europe-africa-solid" />
+      </.link>
+      <div>/</div>
+      <div><%= @current_publication.project_name %></div>
+      <div>/</div>
 
-        <div class=" ml-2 mt-4">
-          <%= @project_name %> /
+      <% draft_dates =
+        Enum.map(@publications, fn %{draft_date: draft_date} -> Date.to_iso8601(draft_date) end) %>
+      <%= if Enum.count(draft_dates) == 1 do %>
+        <div>
+          <%= List.first(draft_dates) %>
         </div>
-        <div class="ml-2">
-          <%= if Enum.count(@draft_dates) == 1 do %>
-            <div class="mt-4">
-              <%= List.first(@draft_dates) %>
-            </div>
-          <% else %>
-            <.input
-              type="select"
-              name="project_date_selection"
-              options={@draft_dates}
-              value={@selected_date}
-            />
-          <% end %>
+      <% else %>
+        <.input
+          type="select"
+          name="project_date_selection"
+          options={draft_dates}
+          value={Date.to_iso8601(@current_publication.draft_date)}
+        />
+      <% end %>
+      <div>/</div>
+      <%= if Enum.count(@current_publication.languages) == 1 do %>
+        <div><%= List.first(@current_publication.languages) %></div>
+      <% else %>
+        <.input
+          type="select"
+          name="project_language_selection"
+          options={@current_publication.languages}
+          value={@selected_lang}
+        />
+      <% end %>
+      <div>/</div>
+      <.link patch={
+        ~p"/projects/#{@current_publication.project_name}/#{@current_publication.draft_date}/#{@selected_lang}"
+      }>
+        <.icon name="hero-home-solid" />
+      </.link>
+      <%= if @identifier do %>
+        <div>/</div>
+        <div>
+          <%= @identifier %>
         </div>
-        <div class="mt-4 ml-2">/</div>
-        <div class="ml-2">
-          <%= if Enum.count(@languages) == 1 do %>
-            <div class="mt-4"><%= List.first(@languages) %></div>
-          <% else %>
-            <.input
-              type="select"
-              name="project_language_selection"
-              options={@languages}
-              value={@selected_lang}
-            />
-          <% end %>
-        </div>
-        <div class="mt-4 ml-2">
-          /
-          <.link patch={~p"/projects/#{@project_name}/#{@selected_date}/#{@selected_lang}"}>
-            <.icon class="mb-1" name="hero-home-solid" />
-          </.link>
-        </div>
-        <%= if @identifier do %>
-          <div class="mt-4 ml-2">/</div>
-          <div class="mt-4 ml-2">
-            <%= @identifier %>
-          </div>
-        <% end %>
-        <div class="ml-4 mt-4">
-          <.link patch={~p"/search?#{%{filters: [{:project_name, @project_name}]}}"}>
-            <.icon class="mb-1" name="hero-magnifying-glass" />
-          </.link>
-        </div>
-      </div>
+      <% end %>
     </form>
     """
   end

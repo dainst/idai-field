df141aed2714275d47e0893698c1830b5e91cfd5
tkleinke <thomas.kleinke@gbv.de>
2024-07-08 17:38:26 +0200

    Run FieldHub integration tests in node environment

--- desktop/test/jest.config.ts
+++ desktop/test/jest.config.ts
@@ -147,7 +147,7 @@ const config: Config = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: 'jsdom',
+  // testEnvironment: '',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},

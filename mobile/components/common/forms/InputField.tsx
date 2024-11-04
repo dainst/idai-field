0358597d2a9f540e66b16c100a1d7a791ebd3f82
hulkoba <jacoba@neighbourhood.ie>
2024-11-04 11:17:38 +0100

    lint: InputField component

--- mobile/components/common/forms/InputField.tsx
+++ mobile/components/common/forms/InputField.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, TextInput, View } from 'react-native';
+
 import { colors } from '@/utils/colors';
 import ScanBarcodeButton from '@/components/Project/ScanBarcodeButton';
 import Row from '../Row';
@@ -7,23 +8,19 @@ import { FieldBaseProps } from './common-props';
 import { FORM_FONT_SIZE } from './constants';
 import FieldLabel from './FieldLabel';
 
-
 const InputField: React.FC<FieldBaseProps> = ({ setFunction, field, currentValue }) => {
 
     const [value, setValue] = useState<string>('');
 
     useEffect(() => {
-    
         setValue(currentValue && typeof currentValue === 'string' ? currentValue : '');
     },[currentValue]);
-
 
     const changeTextHandler = (text: string) => {
         setValue(text);
         setFunction(field.name, text.trimEnd());
     };
 
-    
     return (
         <View style={ styles.container }>
             <FieldLabel field={ field } />
@@ -33,13 +30,12 @@ const InputField: React.FC<FieldBaseProps> = ({ setFunction, field, currentValue
                     value={ value }
                     onChangeText={ changeTextHandler }
                     style={ styles.textInput }
-                    autoCompleteType="off"
+                    autoComplete="off"
                     testID={ `inputField_${field.name}` } />
                 {field.name === 'identifier' && <View style={ { marginLeft: 'auto' } }>
                     <ScanBarcodeButton onQrCodeScanned={ changeTextHandler } />
                 </View>}
             </Row>
-
         </View>
     );
 };
@@ -60,7 +56,6 @@ const styles = StyleSheet.create({
         fontSize: FORM_FONT_SIZE,
         padding: 5
     },
-
 });
 
 export default InputField;

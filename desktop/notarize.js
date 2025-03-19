6527c1acc224eb11f05c5028128ebb195e66d0e5
tkleinke <thomas.kleinke@gbv.de>
2025-03-19 16:12:07 +0100

    Fix notarize.js [notarize]

--- desktop/notarize.js
+++ desktop/notarize.js
@@ -1,7 +1,7 @@
 import { notarize } from '@electron/notarize';
 
 
-export async function performNotarization(context) {
+export default async function performNotarization(context) {
 
     if (process.env.MAC_NOTARIZE !== 'true') return;
 

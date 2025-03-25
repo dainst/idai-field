8b11c15a14fa1c59e03978a8bd51f601a9e64f38
Simon Hohl <mail@dersmon.net>
2025-03-25 13:51:10 +0100

    Remove local dependency declaration

--- publication/mix.exs
+++ publication/mix.exs
@@ -55,7 +55,7 @@ defmodule FieldPublication.MixProject do
       {:iso639_elixir, "~> 0.2.1"},
       {:cachex, "~> 4.0"},
       {:vix, "~> 0.33.0"},
-      {:iiif_image_plug, path: "../../iiif_plug"}
+      {:iiif_image_plug, "~> 0.1.1"}
     ]
   end
 

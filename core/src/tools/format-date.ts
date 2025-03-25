9736c07e7fc112449acae78110347fdbb3f76e94
tkleinke <thomas.kleinke@gbv.de>
2025-03-25 15:36:19 +0100

    Use date-fns in formatDate

--- core/src/tools/format-date.ts
+++ core/src/tools/format-date.ts
@@ -1,8 +1,7 @@
+import { format } from 'date-fns';
+
+
 export function formatDate(date: Date): string {
 
-    const day: string = date.getDate().toString().padStart(2, '0');
-    const month: string = (date.getMonth() + 1).toString().padStart(2, '0');
-    const year: string = date.getFullYear().toString();
-
-    return `${day}.${month}.${year}`;
+    return format(date, 'dd.MM.yyyy');
 }

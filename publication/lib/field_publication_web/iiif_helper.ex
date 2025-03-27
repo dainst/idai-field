5d0c659b90417326fccef4e11e0f07f3110a0860
Simon Hohl <mail@dersmon.net>
2025-03-27 16:57:28 +0100

    Do not attempt to encode binary image data as JSON

--- publication/lib/field_publication_web/iiif_helper.ex
+++ publication/lib/field_publication_web/iiif_helper.ex
@@ -4,13 +4,13 @@ defmodule FieldPublicationWeb.IIIFHelper do
       URI.decode(identifier)
       |> String.split("/")
 
-    "#{FieldPublication.FileService.get_raw_image_data_path(project)}/#{uuid}"
+    "#{FieldPublication.FileService.get_raw_image_data_path(project)}/ptif/#{uuid}"
   end
 
   def handle_404(conn, plug_info) do
     response_body =
       if Plug.Conn.request_url(conn) |> String.ends_with?(".json") do
-        plug_info
+        Jason.encode!(plug_info)
       else
         File.read!(
           Application.app_dir(:field_publication, "priv/static/images/image_not_found.png")
@@ -20,7 +20,7 @@ defmodule FieldPublicationWeb.IIIFHelper do
     Plug.Conn.resp(
       conn,
       404,
-      Jason.encode!(response_body)
+      response_body
     )
   end
 end

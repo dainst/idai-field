0e98d61f63950e9563f85a4d9b8ff3bb3dac1366
tkleinke <thomas.kleinke@gbv.de>
2025-04-01 18:07:19 +0200

    Fix sorting workflow steps by date

--- desktop/src/app/components/resources/actions/edit-workflow/sort-workflow-steps.ts
+++ desktop/src/app/components/resources/actions/edit-workflow/sort-workflow-steps.ts
@@ -1,10 +1,19 @@
-import { parseDate, Document } from 'idai-field-core';
+import { parseDate, Document, DateSpecification } from 'idai-field-core';
 
 
 export function sortWorkflowSteps(workflowSteps: Array<Document>) {
 
     workflowSteps.sort((workflowStep1: Document, workflowStep2: Document) => {
-        return parseDate(workflowStep1.resource.executionDate).getTime()
-            - parseDate(workflowStep2.resource.executionDate).getTime();
+        return getDateTime(workflowStep1.resource.executionDate)
+            - getDateTime(workflowStep2.resource.executionDate);
     });
+}
+
+
+function getDateTime(date: DateSpecification): number {
+
+    if (!date) return 0;
+
+    const dateValue: string = date.endValue ?? date.value;
+    return parseDate(dateValue).getTime();
 }

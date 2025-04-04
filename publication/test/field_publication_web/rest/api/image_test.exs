f37d0a486a0030b1fd0e920303b1cbbe770c2cc7
Simon Hohl <simon.hohl@dainst.de>
2025-04-04 11:03:55 +0200

    Update test for 460c776d73ddb57a2ee3c6783d049d682c94d4b9

--- publication/test/field_publication_web/rest/api/image_test.exs
+++ publication/test/field_publication_web/rest/api/image_test.exs
@@ -52,7 +52,7 @@ defmodule FieldPublicationWeb.Rest.Api.ImageTest do
     # Try a query for image meta data.
     %{
       "@context" => "http://iiif.io/api/image/3/context.json",
-      "id" => "http://www.example.com:80" <> path,
+      "id" => "http://localhost:80" <> path,
       "protocol" => "http://iiif.io/api/image"
     } =
       get(

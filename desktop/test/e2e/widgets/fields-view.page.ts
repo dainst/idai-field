2f613e05ed68f4f5d2e8060dffbb3da2ffa5c3a8
tkleinke <thomas.kleinke@gbv.de>
2025-05-09 11:22:53 +0200

    Add typing to FieldsViewPage

--- desktop/test/e2e/widgets/fields-view.page.ts
+++ desktop/test/e2e/widgets/fields-view.page.ts
@@ -13,36 +13,37 @@ export class FieldsViewPage {
     }
 
 
-    public static clickAccordionTab(cardIndex) {
+    public static clickAccordionTab(cardIndex: number) {
 
         return click('fields-view div:nth-child(' + (cardIndex + 1) + ') .card-header');
-    };
+    }
 
 
-    public static async clickRelation(cardIndex, relationIndex) {
+    public static async clickRelation(cardIndex: number, relationIndex: number) {
 
         await waitForExist('fields-view div:nth-child(' + (cardIndex + 1) + ') .relation-value');
         const elements = await getLocator('fields-view div:nth-child(' + (cardIndex + 1) + ') .relation-value');
         return click(elements.nth(relationIndex));
-    };
+    }
 
 
-    public static getFieldValue(cardIndex, index, entryIndex?) {
+    public static getFieldValue(cardIndex: number, index: number, entryIndex?: number) {
 
         return getText('fields-view div:nth-child(' + (cardIndex + 1) + ') .card-body '
             + 'div:nth-child(' + (index + 1) + ') .field-value'
             + (entryIndex !== undefined ? (':nth-child(' + (entryIndex + 1) + ')') : ''));
-    };
+    }
 
 
-    public static getFieldName(cardIndex, index) {
+    public static getFieldName(cardIndex: number, index: number) {
 
         return getText('fields-view div:nth-child(' + (cardIndex + 1) + ') .card-body '
             + 'div:nth-child(' + (index + 1) + ') .field-label');
-    };
+    }
 
 
-    public static getCompositeSubfieldValue(cardIndex, fieldIndex, entryIndex, subfieldIndex) {
+    public static getCompositeSubfieldValue(cardIndex: number, fieldIndex: number, entryIndex: number,
+                                            subfieldIndex: number) {
 
         return getText('fields-view div:nth-child(' + (cardIndex + 1) + ') .card-body '
             + 'div:nth-child(' + (fieldIndex + 1) + ') .composite-field-entry:nth-child(' + (entryIndex + 1) + ') '
@@ -50,7 +51,8 @@ export class FieldsViewPage {
     }
 
 
-    public static getCompositeSubfieldName(cardIndex, fieldIndex, entryIndex, subfieldIndex) {
+    public static getCompositeSubfieldName(cardIndex: number, fieldIndex: number, entryIndex: number,
+                                           subfieldIndex: number) {
 
         return getText('fields-view div:nth-child(' + (cardIndex + 1) + ') .card-body '
             + 'div:nth-child(' + (fieldIndex + 1) + ') .composite-field-entry:nth-child(' + (entryIndex + 1) + ') '
@@ -58,43 +60,43 @@ export class FieldsViewPage {
     }
 
 
-    public static async getFields(cardIndex) {
+    public static async getFields(cardIndex: number) {
 
         await waitForExist('fields-view');
         return getLocator('fields-view div:nth-child(' + (cardIndex + 1) + ') .card-body > div');
-    };
+    }
 
 
     /**
      * @param cardIndex
      * @param index counting from 0 for the first field
      */
-    public static async getRelationValue(cardIndex, index) {
+    public static async getRelationValue(cardIndex: number, index: number) {
 
         const cardElement = (await getLocator('.card')).nth(cardIndex);
         const relationElement = (await cardElement.locator('.relation-value .title')).nth(index);
         return getText(relationElement);
-    };
+    }
 
 
     /**
      * @param cardIndex
      * @param index counting from 0 for the first field
      */
-    public static async getRelationName(cardIndex, index) {
+    public static async getRelationName(cardIndex: number, index: number) {
 
         const cardElement = (await getLocator('.card')).nth(cardIndex);
         const labelElement = (await cardElement.locator('.field-label')).nth(index);
         return getText(labelElement);
-    };
+    }
 
 
     /**
      * @param cardIndex
      */
-    public static async getRelations(cardIndex) {
+    public static async getRelations(cardIndex: number) {
 
         const cardElement = (await getLocator('.card')).nth(cardIndex);
         return cardElement.locator('.relation-value');
-    };
+    }
 }

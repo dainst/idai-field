62f266330689574b50ed04167cd1551cebec10f1
tkleinke <thomas.kleinke@gbv.de>
2025-08-26 17:47:22 +0200

    Refactor function exportData

--- desktop/src/app/services/express-server/endpoints/export.ts
+++ desktop/src/app/services/express-server/endpoints/export.ts
@@ -3,6 +3,15 @@ import { GeoJsonExporter } from '../../../components/export/geojson-exporter';
 import { CsvExporter } from '../../../components/export/csv/csv-exporter';
 import { ExportResult, ExportRunner } from '../../../components/export/export-runner';
 
+interface RequestParameters {
+    format: string;
+    categoryName: string;
+    context: string;
+    csvSeparator: string;
+    combineHierarchicalRelations: boolean;
+    formatted: boolean;
+} 
+
 
 /**
  * @author Thomas Kleinke
@@ -11,39 +20,15 @@ export async function exportData(request: any, response: any, projectConfigurati
                                  datastore: Datastore) {
     
     try {
-        const format: 'csv'|'geojson' = request.params.format === 'geojson' ? 'geojson' : 'csv';
-        const categoryName: string = request.query.category ?? 'Project';
-        let context: string = request.query.context; 
-        const csvSeparator: string = request.query.separator ?? ',';
-        const combineHierarchicalRelations: boolean = request.query.combineHierarchicalRelations !== 'false';
-        const formatted: boolean = request.query.formatted !== 'false';
+        const { format, categoryName, csvSeparator, combineHierarchicalRelations,
+            formatted, context } = await getRequestParameters(request, datastore);
 
         const category: CategoryForm = projectConfiguration.getCategory(categoryName);
         if (!category) throw 'Unconfigured category: ' + categoryName;
-
-        if (context && context !== 'project') {
-            const documents: Array<Document> = (await datastore.find(
-                { constraints: { 'identifier:match': context } }
-            )).documents;
-            context = documents.length === 1
-                ? documents[0].resource.id
-                : undefined;
-        }
             
         if (format === 'csv') {
-            const result: ExportResult = await ExportRunner.performExport(
-                (query: Query) => datastore.find(query),
-                (async resourceId => (await datastore.get(resourceId)).resource.identifier),
-                context,
-                category,
-                projectConfiguration
-                    .getRelationsForDomainCategory(categoryName)
-                    .map(_ => _.name),
-                CsvExporter.performExport(
-                    projectConfiguration.getProjectLanguages(),
-                    csvSeparator,
-                    combineHierarchicalRelations
-                )
+            const result: ExportResult = await performCsvExport(projectConfiguration, datastore, context, category,
+                csvSeparator, combineHierarchicalRelations
             );
             
             response.header('Content-Type', 'text/csv')
@@ -61,4 +46,56 @@ export async function exportData(request: any, response: any, projectConfigurati
         const errorMessage: string = err?.message ?? err;
         response.status(400).send({ error: errorMessage });
     }
+}
+
+
+async function getRequestParameters(request: any, datastore: Datastore): Promise<RequestParameters> {
+
+    const format: 'csv'|'geojson' = request.params.format === 'geojson' ? 'geojson' : 'csv';
+    const categoryName: string = request.query.category ?? 'Project';
+    const csvSeparator: string = request.query.separator ?? ',';
+    const combineHierarchicalRelations: boolean = request.query.combineHierarchicalRelations !== 'false';
+    const formatted: boolean = request.query.formatted !== 'false';
+    const context: string = await getContext(request, datastore);
+
+    return { format, categoryName, csvSeparator, combineHierarchicalRelations, formatted, context };
+}
+
+
+async function getContext(request: any, datastore: Datastore): Promise<string> {
+
+    let context: string = request.query.context ?? 'project';
+
+    if (context !== 'project') {
+        const documents: Array<Document> = (await datastore.find(
+            { constraints: { 'identifier:match': context } }
+        )).documents;
+
+        context = documents.length === 1
+            ? documents[0].resource.id
+            : undefined;
+    }
+
+    return context;
+}
+
+
+async function performCsvExport(projectConfiguration: ProjectConfiguration, datastore: Datastore, context: string,
+                                category: CategoryForm, csvSeparator: string,
+                                combineHierarchicalRelations: boolean): Promise<ExportResult> {
+
+    return ExportRunner.performExport(
+        (query: Query) => datastore.find(query),
+        (async resourceId => (await datastore.get(resourceId)).resource.identifier),
+        context,
+        category,
+        projectConfiguration
+            .getRelationsForDomainCategory(category.name)
+            .map(relation => relation.name),
+        CsvExporter.performExport(
+            projectConfiguration.getProjectLanguages(),
+            csvSeparator,
+            combineHierarchicalRelations
+        )
+    );
 }

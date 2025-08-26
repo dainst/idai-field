54b5c5900ac2239e49c49d7e1f9454e7a15efbd8
tkleinke <thomas.kleinke@gbv.de>
2025-08-26 18:06:35 +0200

    Refactor function importData

--- desktop/src/app/services/express-server/endpoints/import.ts
+++ desktop/src/app/services/express-server/endpoints/import.ts
@@ -1,7 +1,19 @@
 import { to } from 'tsfun';
-import { CategoryForm, Datastore, Document, IdGenerator, Named, ProjectConfiguration, RelationsManager } from 'idai-field-core';
+import { CategoryForm, Datastore, Document, IdGenerator, Named, ProjectConfiguration,
+    RelationsManager } from 'idai-field-core';
 import { Importer, ImporterFormat, ImporterOptions, ImporterReport } from '../../../components/import/importer';
 import { Settings } from '../../settings/settings';
+
+
+interface RequestParameters {
+    operationIdentifier: string;
+    categoryName: string;
+    format: string;
+    mergeMode: boolean;
+    permitDeletions: boolean;
+    ignoreUnconfiguredFields: boolean;
+    separator: string;
+}
 
 
 /**
@@ -12,58 +24,17 @@ export async function importData(request: any, response: any, projectConfigurati
                                  settings: Settings) {
 
     try {
-        const operationIdentifier: string = request.query.operation;
-        const categoryName: string = request.query.category ?? 'Project';
+        const { operationIdentifier, categoryName, mergeMode, permitDeletions, ignoreUnconfiguredFields,
+        separator, format } = getRequestParameters(request);
+
         const category: CategoryForm = projectConfiguration.getCategory(categoryName);
         if (!category) throw 'Unconfigured category: ' + categoryName;
 
-        let format: string = request.params.format;
-        if (!['geojson', 'csv', 'jsonl'].includes(format)) throw 'Unsupported format: ' + format;
-        if (format === 'jsonl') format = 'native';
-
-        let operationId: string;
-        if (operationIdentifier) {
-            const documents: Array<Document> = (await datastore.find(
-                { constraints: { 'identifier:match': operationIdentifier } }
-            )).documents;
-            operationId = documents.length === 1
-                ? documents[0].resource.id
-                : undefined;
-        }
-
-        const fileContent: string = request.body;
-
-        const options: ImporterOptions = {
-            format: format as ImporterFormat,
-            mergeMode: request.query.mergeMode === 'true',
-            permitDeletions: request.query.permitDeletions === 'true',
-            selectedOperationId: operationId,
-            ignoreUnconfiguredFields: request.query.ignoreUnconfiguredFields === 'true',
-            selectedCategory: category,
-            separator: request.query.separator ?? ',',
-            sourceType: 'file'
-        };
-        if (options.mergeMode === true) options.selectedOperationId = '';
-
-        const documents: Array<Document> = await Importer.doParse(options,fileContent);
-
-        const report: ImporterReport = await Importer.doImport(
-            {
-                datastore: datastore,
-                relationsManager: relationsManager,
-                imageRelationsManager: undefined,
-                imagestore: undefined
-            },
-            {
-                settings,
-                projectConfiguration: projectConfiguration,
-                operationCategories: projectConfiguration.getOperationCategories().map(Named.toName)
-            },
-            () => idGenerator.generateId(),
-            options,
-            documents,
-            projectConfiguration.getTypeCategories().map(to(Named.NAME)),
-            projectConfiguration.getImageCategories().map(to(Named.NAME))
+        const options: ImporterOptions = await getImporterOptions(format as ImporterFormat, mergeMode, permitDeletions,
+            operationIdentifier, ignoreUnconfiguredFields, category, separator, datastore);
+        const documents: Array<Document> = await Importer.doParse(options, request.body);
+        const report: ImporterReport = await performImport(projectConfiguration, datastore, relationsManager,
+            idGenerator, settings, options, documents
         );
 
         if (report.errors?.length) {
@@ -80,3 +51,91 @@ export async function importData(request: any, response: any, projectConfigurati
         response.status(400).send({ error: errorMessage });
     }
 }
+
+
+function getRequestParameters(request: any): RequestParameters {
+
+    const operationIdentifier: string = request.query.operation;
+    const categoryName: string = request.query.category ?? 'Project';
+    const mergeMode: boolean = request.query.mergeMode === 'true';
+    const permitDeletions: boolean = request.query.permitDeletions === 'true';
+    const ignoreUnconfiguredFields: boolean = request.query.ignoreUnconfiguredFields === 'true';
+    const separator: string = request.query.separator ?? ',';
+    const format = getFormat(request);
+
+    return { operationIdentifier, categoryName, mergeMode, permitDeletions, ignoreUnconfiguredFields,
+        separator, format };
+}
+
+
+function getFormat(request: any): string {
+
+    let format: string = request.params.format;
+    if (!['geojson', 'csv', 'jsonl'].includes(format)) throw 'Unsupported format: ' + format;
+
+    if (format === 'jsonl') format = 'native';
+
+    return format;
+}
+
+
+async function getImporterOptions(format: ImporterFormat, mergeMode: boolean, permitDeletions: boolean,
+                                  operationIdentifier: string, ignoreUnconfiguredFields: boolean,
+                                  category: CategoryForm, separator: string,
+                                  datastore: Datastore): Promise<ImporterOptions> {
+
+    const options: ImporterOptions = {
+        format,
+        mergeMode,
+        permitDeletions,
+        selectedOperationId: await getOperationId(operationIdentifier, datastore),
+        ignoreUnconfiguredFields,
+        selectedCategory: category,
+        separator,
+        sourceType: 'file'
+    };
+
+    if (options.mergeMode === true) options.selectedOperationId = '';
+
+    return options;
+}
+
+
+async function getOperationId(operationIdentifier: string, datastore: Datastore): string|undefined {
+
+    if (!operationIdentifier) return undefined;
+
+    const documents: Array<Document> = (await datastore.find(
+        { constraints: { 'identifier:match': operationIdentifier } }
+    )).documents;
+
+    return documents.length === 1
+        ? documents[0].resource.id
+        : undefined;
+}
+
+
+function performImport(projectConfiguration: ProjectConfiguration, datastore: Datastore,
+                       relationsManager: RelationsManager, idGenerator: IdGenerator, settings: Settings,
+                       options: ImporterOptions, documents: Array<Document>): Promise<ImporterReport> {
+
+    return Importer.doImport(
+        {
+            datastore: datastore,
+            relationsManager: relationsManager,
+            imageRelationsManager: undefined,
+            imagestore: undefined
+        },
+        {
+            settings,
+            projectConfiguration: projectConfiguration,
+            operationCategories: projectConfiguration.getOperationCategories().map(Named.toName)
+        },
+        () => idGenerator.generateId(),
+        options,
+        documents,
+        projectConfiguration.getTypeCategories().map(to(Named.NAME)),
+        projectConfiguration.getImageCategories().map(to(Named.NAME))
+    );
+}
+

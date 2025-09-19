175c477a48e0c476a314a61a33d7cdff1292b411
tkleinke <thomas.kleinke@gbv.de>
2025-09-19 17:27:35 +0200

    Show configuration info popover in valuelist view

--- desktop/src/app/components/configuration/widgets/valuelist-view.component.ts
+++ desktop/src/app/components/configuration/widgets/valuelist-view.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { clone, Map } from 'tsfun';
 import { Labels, Valuelist } from 'idai-field-core';
+import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
+import { AngularUtility } from '../../../angular/angular-utility';
+import { ComponentHelpers } from '../../component-helpers';
 
 
 @Component({
@@ -20,6 +23,9 @@ export class ValuelistViewComponent implements OnChanges {
 
     public valueLabels: Map<string> = {};
     public valueDescriptions: Map<string> = {};
+    public valueInfoPopover: NgbPopover;
+
+    private listener: any;
 
 
     constructor(private labels: Labels) {}
@@ -37,7 +43,24 @@ export class ValuelistViewComponent implements OnChanges {
 
     public getValuelistDescription = (valuelist: Valuelist) => this.labels.getDescription(valuelist);
 
-    public getValues = (valuelist: Valuelist) => this.labels.orderKeysByLabels(valuelist);
+    public getValueIds = (valuelist: Valuelist) => this.labels.orderKeysByLabels(valuelist);
+
+
+    public async openPopover(popover: NgbPopover) {
+
+        await AngularUtility.refresh();
+        this.valueInfoPopover = popover;
+        this.valueInfoPopover.open();
+        this.initializeListeners();
+    }
+
+
+    public closePopover() {
+
+        if (this.valueInfoPopover) this.valueInfoPopover.close();
+        this.valueInfoPopover = undefined;
+        this.removeListeners();
+    }
 
 
     private getLabelAndDescription(valueId: string): { label: string, description?: string } {
@@ -46,5 +69,35 @@ export class ValuelistViewComponent implements OnChanges {
         value.name = valueId;
 
         return this.labels.getLabelAndDescription(value);
+    }
+    
+    
+    private initializeListeners() {
+    
+        this.listener = this.onMouseEvent.bind(this);
+        window.addEventListener('click', this.listener, true);
+        window.addEventListener('scroll', this.listener, true);
+        window.addEventListener('contextmenu', this.listener, true);
+        window.addEventListener('resize', this.listener, true);
+    }
+
+
+    private removeListeners() {
+
+        if (this.listener) {
+            window.removeEventListener('click', this.listener, true);
+            window.removeEventListener('scroll', this.listener, true);
+            window.removeEventListener('contextmenu', this.listener, true);
+            window.removeEventListener('resize', this.listener, true);
+            this.listener = undefined;
+        }
+    }
+    
+    
+    private onMouseEvent(event: MouseEvent) {
+
+        if (!ComponentHelpers.isInside(event.target, target => target.localName === 'configuration-info')) {
+            this.closePopover();
+        }
     }
 }

6da5232b3346e4eba4a3ab7e5f294ac106a03bf1
tkleinke <thomas.kleinke@gbv.de>
2025-09-29 19:07:47 +0200

    Temporarily use electron nativeImage for generating thumbnails

--- desktop/src/app/services/imagestore/create-display-variant.ts
+++ desktop/src/app/services/imagestore/create-display-variant.ts
@@ -1,5 +1,4 @@
-import { ImageDocument, ImageStore, ImageVariant } from 'idai-field-core';
-import { ImageManipulation } from './image-manipulation';
+import { ImageDocument, ImageStore } from 'idai-field-core';
 
 
 /**
@@ -10,49 +9,8 @@ import { ImageManipulation } from './image-manipulation';
 export async function createDisplayVariant(document: ImageDocument, imagestore: ImageStore,
                                            originalData: Buffer): Promise<Buffer|undefined> {
     
-    console.log('Checking if creation of display variant is necessary...');
+    console.log('--- Always use original variant temporarily---');
     const imageId: string = document.resource.id;
-    const fileExtension: string = ImageDocument.getOriginalFileExtension(document);
-    const width: number = document.resource.width;
-    const height: number = document.resource.height;
-
-    const sharpImageHandle = ImageManipulation.getSharpImage(originalData);
-
-    console.log('Checking if conversion to JPG is necessary...');
-
-    const convertToJpeg: boolean = await shouldConvertToJpeg(
-        width * height, fileExtension, sharpImageHandle
-    );
-
-    const resize: boolean = width > ImageManipulation.MAX_DISPLAY_WIDTH 
-        || height > ImageManipulation.MAX_DISPLAY_HEIGHT;
-
-    if (!convertToJpeg && !resize) {
-        console.log('Use original file');
-        await imagestore.addUseOriginalMarker(imageId);
-        return undefined;
-    }
-
-    console.log('Creating display image...');
-
-    const displayVariantData: Buffer = await ImageManipulation.createDisplayImage(
-        sharpImageHandle, convertToJpeg, resize
-    );
-
-    console.log('Storing display image in imagestore...');
-
-    await imagestore.store(imageId, displayVariantData, undefined, ImageVariant.DISPLAY);
-
-    console.log('Finished creating display image');
-    return displayVariantData;
-};
-
-
-async function shouldConvertToJpeg(pixels: number, fileExtension: string, sharpLibHandle: any): Promise<boolean> {
-
-    if (fileExtension.toLowerCase().includes('tif')) return true;
-    
-    if (fileExtension.toLowerCase().includes('png') && pixels > ImageManipulation.MAX_ORIGINAL_PIXELS) {
-        return await ImageManipulation.isOpaque(sharpLibHandle);
-    }
+    await imagestore.addUseOriginalMarker(imageId);
+    return undefined;
 }
--- desktop/src/app/services/imagestore/image-manipulation.ts
+++ desktop/src/app/services/imagestore/image-manipulation.ts
@@ -1,5 +1,4 @@
-const sharp = window.require('sharp');
-sharp.cache(false);
+const nativeImage = window.require('electron').nativeImage;
 
 
 export module ImageManipulationErrors {
@@ -19,61 +18,16 @@ export module ImageManipulation {
     export const MAX_DISPLAY_HEIGHT = 10000;
 
 
-    /**
-     * Create a sharp image instance based on raw buffer data.
-     * 
-     * See also https://sharp.pixelplumbing.com.
-     * 
-     * @param buffer, the raw image data.
-     * @returns A sharp instance or Error for invalid buffer parameters (for example 
-     * if the absolute number of pixels exceeds Field Desktop's maximum)
-     */
-    export function getSharpImage(buffer: Buffer): any {
-
-        console.log('Generating sharp image...');
-        const sharpImage = sharp(buffer, { limitInputPixels: MAX_INPUT_PIXELS });
-        console.log('Finished generating sharp image');
-        return sharpImage;
-    }
-
-
     export async function createThumbnail(buffer: Buffer, targetHeight: number,
                                           targetJpegQuality: number): Promise<Buffer> {
 
         try {
-            console.log('Starting thumbnail creation...');
-            console.log('Sharp status:', sharp.counters());
-            sharp.concurrency(1);
-            console.log('Max sharp threads:', sharp.concurrency());
-            const sharpImage = getSharpImage(buffer);
-            console.log('Resizing image to target height: ' + targetHeight);
-            const resizedImage = sharpImage.resize(undefined, targetHeight);
-            console.log('Converting to JPEG...');
-            const jpegImage = resizedImage.jpeg({ quality: targetJpegQuality });
-            console.log('Converting to buffer...');
-            const result = await jpegImage.toBuffer();
-            console.log('Finished generating thumbnail');
-            return result;
+            return nativeImage.createFromBuffer(buffer)
+                .resize({ height: targetHeight })
+                .toJPEG(targetJpegQuality);
         } catch (err) {
             console.error('Failed to generate thumbnail:', err);
             return undefined;
         }
-    }
-
-
-    export async function isOpaque(image: any): Promise<boolean> {
-
-        const stats = await image.stats();
-        return stats.isOpaque;
-    }
-
-    
-    export async function createDisplayImage(image: any, convertToJpeg: boolean,
-                                             resize: boolean): Promise<Buffer> {
-
-        if (convertToJpeg) image = image.jpeg();
-        if (resize) image = image.resize(MAX_DISPLAY_WIDTH, MAX_DISPLAY_HEIGHT, { fit: 'inside' });
-        
-        return image.toBuffer();
     }
 }

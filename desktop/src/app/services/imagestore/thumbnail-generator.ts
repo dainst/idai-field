1652cc20d860da9da4b064cf8cb2e9986d5606d1
tkleinke <thomas.kleinke@gbv.de>
2025-10-08 18:10:59 +0200

    Remove outdated comment

--- desktop/src/app/services/imagestore/thumbnail-generator.ts
+++ desktop/src/app/services/imagestore/thumbnail-generator.ts
@@ -12,10 +12,6 @@ const THUMBNAIL_TARGET_JPEG_QUALITY = 60;
  * @author Daniel de Oliveira
  * @author Thomas Kleinke
  * @author Simon Hohl
- *
- * The Electron nativeImage module is used per default. If the conversion process fails (which may happen
- * e. g. for CMYK images), Jimp is used. As Jimp is slower than nativeImage in most cases, it is only
- * used as a fallback in case the nativeImage conversion doesn't work.
  */
 export class ThumbnailGenerator implements ThumbnailGeneratorInterface {
 

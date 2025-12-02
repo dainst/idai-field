9b422f216ee92a25f6a330ac1d645dbc8dee64e6
Simon Hohl <simon.hohl@dainst.de>
2025-12-02 09:34:09 +0100

    Set cache-control and content-type header for map tiles

--- publication/lib/field_publication_web/rest/api/image.ex
+++ publication/lib/field_publication_web/rest/api/image.ex
@@ -1,11 +1,14 @@
 defmodule FieldPublicationWeb.Api.Image do
-  alias FieldPublication.FileService
   use FieldPublicationWeb, :controller
+
+  import Plug.Conn
+
+  alias FieldPublication.FileService
 
   def raw(conn, %{"project_name" => name, "uuid" => uuid} = _params) do
     base_path = FileService.get_raw_image_data_path(name)
 
-    Plug.Conn.send_file(conn, 200, "#{base_path}/#{uuid}")
+    send_file(conn, 200, "#{base_path}/#{uuid}")
   end
 
   def tile(
@@ -14,6 +17,34 @@ defmodule FieldPublicationWeb.Api.Image do
       ) do
     base_path = FileService.get_map_tiles_path(name)
 
-    Plug.Conn.send_file(conn, 200, "#{base_path}/#{uuid}/#{z}/#{y}/#{x}.webp")
+    path = "#{base_path}/#{uuid}/#{z}/#{y}/#{x}.webp"
+
+    if File.exists?(path) do
+      cache_type =
+        Cachex.exists?(:published_images, {name, uuid})
+        |> case do
+          {:ok, false} ->
+            # Caching in users' browsers only.
+            "private"
+
+          _ ->
+            "public"
+        end
+
+      conn
+      |> put_resp_header("cache-control", "#{cache_type}, max-age=31536000, immutable")
+      |> put_resp_header("content-type", "image/webp")
+      |> send_file(
+        200,
+        path
+      )
+    else
+      conn
+      |> put_resp_header("content-type", "image/png")
+      |> send_file(
+        404,
+        Application.app_dir(:field_publication, "priv/static/images/image_not_found.png")
+      )
+    end
   end
 end

fe19c70e22a435002482295e514acc2ec5e61238
the-last-pastafarian <antunes.nicolas@gmail.com>
2025-12-11 10:33:14 +0100

    modification tests, add liveview

--- server/test/field_hub_web/controllers/page_controller_test.exs
+++ server/test/field_hub_web/controllers/page_controller_test.exs
@@ -1,6 +1,9 @@
 defmodule FieldHubWeb.PageControllerTest do
   use FieldHubWeb.ConnCase
 
+  import Phoenix.ConnTest
+  import Phoenix.LiveViewTest
+
   alias FieldHub.TestHelper
   alias FieldHubWeb.UserAuth
 
@@ -8,12 +11,40 @@ defmodule FieldHubWeb.PageControllerTest do
   @user_name "test_user"
   @user_password "test_password"
 
+  @admin_user Application.compile_env(:field_hub, :couchdb_admin_name)
+
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Field Hub"
   end
 
-  describe "Test user -" do
+  test "project users get redirected to their project immediately", %{conn: conn} do
+    TestHelper.create_test_db_and_user(@project, @user_name, @user_password)
+
+    on_exit(fn ->
+      TestHelper.remove_test_db_and_user(@project, @user_name)
+    end)
+
+    token = UserAuth.generate_user_session_token(@user_name)
+
+    conn =
+      conn
+      |> Map.replace!(:secret_key_base, FieldHubWeb.Endpoint.config(:secret_key_base))
+      |> init_test_session(%{})
+      |> put_session(:user_token, token)
+
+    expected_redirect = "/ui/projects/show/#{@user_name}"
+
+    assert {
+             :error,
+             {
+               :live_redirect,
+               %{to: ^expected_redirect}
+             }
+           } = live(conn, "/")
+  end
+
+  describe "admin users" do
     setup %{conn: conn} do
       # Run before each test
       TestHelper.create_test_db_and_user(@project, @user_name, @user_password)
@@ -23,23 +54,30 @@ defmodule FieldHubWeb.PageControllerTest do
         TestHelper.remove_test_db_and_user(@project, @user_name)
       end)
 
+      token = UserAuth.generate_user_session_token(@admin_user)
+
       conn =
         conn
         |> Map.replace!(:secret_key_base, FieldHubWeb.Endpoint.config(:secret_key_base))
         |> init_test_session(%{})
+        |> put_session(:user_token, token)
 
       %{conn: conn}
     end
 
-    test "GET / with valid session token displays projects", %{conn: conn} do
-      token = UserAuth.generate_user_session_token(@user_name)
+    test "see the dashboard when only one project is in the system", %{conn: conn} do
+      {
+        :ok,
+        view,
+        html
+      } = live(conn, "/")
 
-      conn =
-        conn
-        |> put_session(:user_token, token)
-        |> get("/")
+      assert html =~ "Loading projects list..."
 
-      assert html_response(conn, 200) =~ "Loading projects list..."
+      html = render_async(view)
+
+      refute html =~ "Loading projects list..."
+      assert html =~ @user_name
     end
 
     test "GET / without valid session token does not display projects", %{conn: conn} do

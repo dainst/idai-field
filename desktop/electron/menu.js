c63e37bf0407efd995ebba3afe9bafe06962bd3d
tkleinke <thomas.kleinke@gbv.de>
2026-03-09 12:33:48 +0100

    Revert "Do not focus main window when closing info modal"
    This reverts commit 892de8fc79d4d40ab38e72ea893dfe7a809f83cc.

--- desktop/electron/menu.js
+++ desktop/electron/menu.js
@@ -260,6 +260,9 @@ const getTemplate = (mainWindow, context, config) => {
                     );
                     modal.show();
                 });
+                modal.on('close', () => {
+                    mainWindow.focus();
+                });
 
                 ipcMain.once('close-info-modal', () => {
                     modal.close();

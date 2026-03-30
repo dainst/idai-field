558ec0737e5dfeb9f1137b1d6761b78b912d9878
tkleinke <thomas.kleinke@gbv.de>
2026-03-30 12:21:26 +0200

    Adjust to code style conventions

--- desktop/src/app/components/import/util.ts
+++ desktop/src/app/components/import/util.ts
@@ -2,13 +2,14 @@ import { Associative, is, isArray, isEmpty, isObject } from 'tsfun';
 
 
 /**
- * @param struct
  * @author Daniel de Oliveira
  */
 export function hasEmptyAssociatives(struct: Associative<any>): boolean {
 
     if (!isArray(struct) && !isObject(struct)) return false;
+
     if (isEmpty(struct)) return true;
+
     return Object.values(struct)
         .map(hasEmptyAssociatives)
         .some(is(true));

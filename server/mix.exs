26a583ecdff1a2286d20ccd6b1f31da676831a92
Simon Hohl <mail@dersmon.net>
2026-05-04 08:05:50 +0200

    Add `Phoenix.CodeReloader` to listeners

--- server/mix.exs
+++ server/mix.exs
@@ -8,6 +8,7 @@ defmodule FieldHub.MixProject do
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
+      listeners: [Phoenix.CodeReloader],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),

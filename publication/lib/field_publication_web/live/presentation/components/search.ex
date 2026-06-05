e39b25b84b45cb17a73c669d3af0ba7389e6222b
Simon Hohl <mail@dersmon.net>
2026-06-05 10:42:57 +0200

    Split search UI into reusable components

---
+++ publication/lib/field_publication_web/live/presentation/components/search.ex
@@ -0,0 +1,261 @@
+defmodule FieldPublicationWeb.Presentation.Components.Search do
+  use FieldPublicationWeb, :html
+
+  import FieldPublicationWeb.Components.Data.DocumentLink
+
+  alias FieldPublication.Publications.Search
+  alias FieldPublication.Publications.Search.SearchDocument
+
+  def search_input(assigns) do
+    ~H"""
+    <form
+      phx-change="search"
+      phx-submit="search"
+      class="pb-2"
+    >
+      <input
+        class="w-full border p-2"
+        type="text"
+        value={@current_query}
+        name="search_input"
+        phx-debounce="100"
+      />
+    </form>
+    <%= if @total < 10000 do %>
+      Found {@total} matches.
+    <% else %>
+      Found more than 10000 matches.
+    <% end %>
+    """
+  end
+
+  def filter_list(assigns) do
+    ~H"""
+    <%= if @active_filters != %{} do %>
+      <.group_heading>Active filters</.group_heading>
+      <%= for {field, value} <- @active_filters do %>
+        <.aggregation_deselection
+          field_name={field}
+          value={value}
+        />
+      <% end %>
+    <% end %>
+
+    <.group_heading>Available filters</.group_heading>
+    <div class="mt-2 p-1 bg-panel overflow-auto">
+      <%= for {field, buckets} <- @aggregations do %>
+        <%= if !(Enum.find(@active_filters, fn({already_filtered_field, _value}) -> already_filtered_field == field end ) ) and buckets != [] do %>
+          <.aggregation_selection
+            field_name={field}
+            buckets={buckets}
+          />
+        <% end %>
+      <% end %>
+    </div>
+    """
+  end
+
+  def result_list(assigns) do
+    ~H"""
+    <div class="ml-4 w-full">
+      <div class="flex flex-row mb-2 sticky top-4">
+        <% previous_from =
+          if @query_params.from - @limit > 0,
+            do: @query_params.from - @limit,
+            else: 0 %>
+        <.link
+          class={"p-2 #{if @query_params.from == 0, do: "bg-zinc-700 cursor-default border-(--primary-color)", else: "bg-(--primary-color) hover:bg-(--primary-color-hover)"}"}
+          patch={@update_from.(@query_params, previous_from)}
+        >
+          <span class="text-white">
+            Previous
+          </span>
+        </.link>
+        <div class="bg-white grow p-2 border-t border-b border-(--primary-color) text-center">
+          Showing {@query_params.from + 1} - {if @query_params.from + @limit > @total,
+            do: @total,
+            else: @query_params.from + @limit} of {@total} results
+        </div>
+        <% next_from =
+          if @query_params.from + @limit >= @total,
+            do: @query_params.from,
+            else: @query_params.from + @limit %>
+        <.link
+          class={"p-2 #{if next_from == @query_params.from, do: "bg-zinc-700 cursor-default", else: "bg-(--primary-color) hover:bg-(--primary-color-hover)"} text-white"}
+          patch={@update_from.(@query_params, next_from)}
+        >
+          <span class="text-white">
+            Next
+          </span>
+        </.link>
+      </div>
+      <div :for={%SearchDocument{full_doc: doc} <- @docs}>
+        <.document_link
+          doc={doc}
+          image_count={10}
+          image_height={128}
+        />
+      </div>
+    </div>
+    """
+  end
+
+  def aggregation_selection(assigns) do
+    ~H"""
+    <strong>
+      {render_label(get_filter_label(@field_name))}
+    </strong>
+    <%= for %{count: count, key: key} <- @buckets do %>
+      <div
+        class="pl-2 mt-1 cursor-pointer border border-gray-100 hover:border-primary rounded "
+        phx-click="toggle_filter"
+        phx-value-key={@field_name}
+        phx-value-value={key}
+      >
+        <div class="flex pl-2 pr-2 font-thin rounded">
+          <span class="grow mr-2">{render_label(get_filter_value_label(@field_name, key))}</span>
+          ({count})
+        </div>
+      </div>
+    <% end %>
+    """
+  end
+
+  def aggregation_deselection(assigns) do
+    ~H"""
+    <div
+      class="pl-2 mt-1 text-primary-inverse hover:text-primary-hover-inverse cursor-pointer bg-primary hover:bg-primary-hover hover:line-through rounded"
+      phx-click="toggle_filter"
+      phx-value-key={@field_name}
+      phx-value-value={@value}
+    >
+      <div class="h-full pl-2 pr-2 font-thin rounded">
+        <strong>{render_label(get_filter_label(@field_name))}:</strong> {render_label(
+          get_filter_value_label(
+            @field_name,
+            @value
+          )
+        )}
+      </div>
+    </div>
+    """
+  end
+
+  def render_label(%{secondary: _secondary} = assigns) do
+    ~H"""
+    <div class="relative group">
+      <span class="thin hero-information-circle mb-1"></span>
+      {@primary}
+      <div class="p-2 mt-1 w-full z-10 bg-yellow-100 hidden group-hover:block absolute">
+        <span class="font-semibold">Varying usage:</span>
+        <%= for {text, _count, projects} <- @secondary do %>
+          <div>
+            {text} <span class="italic">({Enum.join(projects, ", ")})</span>
+          </div>
+        <% end %>
+      </div>
+    </div>
+    """
+  end
+
+  def render_label(assigns) do
+    ~H"""
+    {@primary}
+    """
+  end
+
+  defp get_filter_label("project_key") do
+    %{primary: Gettext.gettext(FieldPublicationWeb.Translate, "Project")}
+  end
+
+  defp get_filter_label(opensearch_field_name) do
+    data_field_name = String.replace_suffix(opensearch_field_name, "_keyword", "")
+
+    label_info = Search.get_system_wide_label_usage()
+
+    case label_info[:field_labels][data_field_name]["labels"] do
+      nil ->
+        opensearch_field_name
+
+      labels ->
+        Map.get(
+          labels,
+          Gettext.get_locale(FieldPublicationWeb.Translate),
+          data_field_name
+        )
+    end
+    |> case do
+      [{text, _count, _projects}] ->
+        %{primary: text}
+
+      [{text, _count, _projects} = _primary | rest] ->
+        %{primary: text, secondary: rest}
+
+      some_string when is_binary(some_string) ->
+        %{primary: some_string}
+    end
+  end
+
+  defp get_filter_value_label("category", opensearch_field_value) do
+    label_info = Search.get_system_wide_label_usage()
+
+    case label_info[:category_labels][opensearch_field_value] do
+      nil ->
+        opensearch_field_value
+
+      values ->
+        Map.get(
+          values,
+          Gettext.get_locale(FieldPublicationWeb.Translate),
+          opensearch_field_value
+        )
+    end
+    |> case do
+      [{text, _count, _projects}] ->
+        %{primary: text}
+
+      [{text, _count, _projects} = _primary | rest] ->
+        %{primary: text, secondary: rest}
+
+      text when is_binary(text) ->
+        %{primary: text}
+    end
+  end
+
+  defp get_filter_value_label(opensearch_field_name, opensearch_field_value) do
+    label_info = Search.get_system_wide_label_usage()
+    data_field_name = String.replace_suffix(opensearch_field_name, "_keyword", "")
+
+    case label_info[:field_labels][data_field_name]["value_labels"] do
+      nil ->
+        opensearch_field_value
+
+      empty_map when empty_map == %{} ->
+        opensearch_field_value
+
+      value_translations_mapping ->
+        Map.get(value_translations_mapping, opensearch_field_value, opensearch_field_value)
+    end
+    |> case do
+      some_string when is_binary(some_string) ->
+        some_string
+
+      value_labels ->
+        Map.get(
+          value_labels,
+          Gettext.get_locale(FieldPublicationWeb.Translate),
+          opensearch_field_value
+        )
+    end
+    |> case do
+      [{text, _count, _projects}] ->
+        %{primary: text}
+
+      [{text, _count, _projects} = _primary | rest] ->
+        %{primary: text, secondary: rest}
+
+      text when is_binary(text) ->
+        %{primary: text}
+    end
+  end
+end

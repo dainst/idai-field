1cf9efecfef16c0041ee4b090730cceea3e0b4d0
Simon Hohl <simon.hohl@dainst.de>
2026-07-01 11:51:11 +0200

    For map image tiling, switch to webp
    This applies to the intermediate helper image as well as the final map
    tiles.

--- web/api/lib/worker/images/image_magick_image_converter.ex
+++ web/api/lib/worker/images/image_magick_image_converter.ex
@@ -11,20 +11,22 @@ defmodule Api.Worker.Images.ImageMagickImageConverter do
   @required_imagemagick_version [6, 9]
   @required_delegates ["jp2", "png", "jpeg"]
 
-  @intermediate_format_suffix "jpg"
+  @intermediate_format_suffix "webp"
 
   # as we want to provide it with cantaloupe
   @display_format_suffix "jp2"
 
   def environment_ready() do
     {version, delegates, status} = get_im_version_and_delegates()
-    status == 0
-    and required_version_matches(version, @required_imagemagick_version)
-    and required_delegates_present(delegates)
+
+    status == 0 and
+      required_version_matches(version, @required_imagemagick_version) and
+      required_delegates_present(delegates)
   end
 
   defp img_path(project, image_id) do
-    "#{@imageroot}/#{project}/#{image_id}" # todo use Path.
+    # todo use Path.
+    "#{@imageroot}/#{project}/#{image_id}"
   end
 
   @doc """
@@ -37,13 +39,17 @@ defmodule Api.Worker.Images.ImageMagickImageConverter do
     project_dir = Path.join([@imageroot, project])
     sources_dir = Path.join(project_dir, "sources")
 
-    if (not sources_exist?(project)), do: raise "Sources do not exist for '#{project}'"
+    if not sources_exist?(project), do: raise("Sources do not exist for '#{project}'")
 
-    {:ok, files_and_folders} = File.ls sources_dir                             # files (and folders) in sources dir
+    # files (and folders) in sources dir
+    {:ok, files_and_folders} = File.ls(sources_dir)
+
     files_and_folders
-    |> Enum.filter(&(not String.contains?(&1, ".")))                           # without file extension
-    |> Enum.filter(fn file -> not File.dir?(Path.join(sources_dir, file)) end) # only files
-    |> Enum.map(&(convert_file(sources_dir, project_dir, &1, @display_format_suffix)))
+    # without file extension
+    |> Enum.filter(&(not String.contains?(&1, ".")))
+    # only files
+    |> Enum.filter(fn file -> not File.dir?(Path.join(sources_dir, file)) end)
+    |> Enum.map(&convert_file(sources_dir, project_dir, &1, @display_format_suffix))
   end
 
   # Returns true if everything went fine
@@ -75,23 +81,28 @@ defmodule Api.Worker.Images.ImageMagickImageConverter do
   def rescale(project, image_id, rescale) do
     source_img_path = Path.absname(Path.join([@imageroot, project, "sources", image_id])) <> "[0]"
     target_dir = Path.join([@imageroot, project, image_id])
-    File.mkdir_p target_dir # todo do in caller
+    # todo do in caller
+    File.mkdir_p(target_dir)
 
-    target_img_path = "#{img_path(project, image_id)}/#{image_id}.#{rescale}.#{@intermediate_format_suffix}"
+    target_img_path =
+      "#{img_path(project, image_id)}/#{image_id}.#{rescale}.#{@intermediate_format_suffix}"
 
-    if File.exists? target_img_path do
-      true # don't overwrite existing files
+    if File.exists?(target_img_path) do
+      # don't overwrite existing files
+      true
     else
       {cmd, args} = {
         @im_cmd,
         [
           source_img_path,
-          "-scale", # instead -resize, to prevent cache-resources-exhausted; cranking up resource limits did not work
+          # instead -resize, to prevent cache-resources-exhausted; cranking up resource limits did not work
+          "-scale",
           "#{rescale}x#{rescale}",
           target_img_path
         ]
       }
-      {_, status} = System.cmd cmd, args
+
+      {_, status} = System.cmd(cmd, args)
       status == 0
     end
   end
@@ -100,50 +111,58 @@ defmodule Api.Worker.Images.ImageMagickImageConverter do
   Returns true is everything went fine
   """
   def crop(
-         project,
-         tile_size,
-         image_id,
-         rescale,
-         z_index,
-         %{x_index: x_index, y_index: y_index, x_pos: x_pos, y_pos: y_pos}) do
-
-    x_folder = Path.join(
-      [
+        project,
+        tile_size,
+        image_id,
+        rescale,
+        z_index,
+        %{x_index: x_index, y_index: y_index, x_pos: x_pos, y_pos: y_pos}
+      ) do
+    x_folder =
+      Path.join([
         @imageroot,
         project,
         image_id,
         Integer.to_string(z_index),
         Integer.to_string(x_index)
-      ]
-    )
-    File.mkdir_p x_folder
+      ])
 
-    source_img_path = "#{img_path(project, image_id)}/#{image_id}.#{rescale}.#{@intermediate_format_suffix}"
-    target_img_path = "#{img_path(project, image_id)}/#{z_index}/#{x_index}/#{y_index}.png"
+    File.mkdir_p(x_folder)
 
-    if File.exists? target_img_path do
-      true # don't overwrite existing files
+    source_img_path =
+      "#{img_path(project, image_id)}/#{image_id}.#{rescale}.#{@intermediate_format_suffix}"
+
+    target_img_path = "#{img_path(project, image_id)}/#{z_index}/#{x_index}/#{y_index}.webp"
+
+    if File.exists?(target_img_path) do
+      # don't overwrite existing files
+      true
     else
       {cmd, args} = {
         @im_cmd,
         [
           source_img_path,
-          "-quiet", # suppress 'convert: geometry does not contain image', some tiles simply will have no content with tiled rectangular images
-          "-crop", "#{tile_size}x#{tile_size}+#{x_pos}+#{y_pos}",
-          "-background", "transparent",
-          "-extent", "#{tile_size}x#{tile_size}",
+          # suppress 'convert: geometry does not contain image', some tiles simply will have no content with tiled rectangular images
+          "-quiet",
+          "-crop",
+          "#{tile_size}x#{tile_size}+#{x_pos}+#{y_pos}",
+          "-background",
+          "transparent",
+          "-extent",
+          "#{tile_size}x#{tile_size}",
           target_img_path
         ]
       }
-      {_, status} = System.cmd cmd, args
+
+      {_, status} = System.cmd(cmd, args)
       status == 0
     end
   end
 
   defp required_version_matches(version, [required_major, required_minor]) do
     [major, minor, _] = String.split(version, ".")
-    {major, _} = Integer.parse major
-    {minor, _} = Integer.parse minor
+    {major, _} = Integer.parse(major)
+    {minor, _} = Integer.parse(minor)
     major > required_major or (major == required_major and minor >= required_minor)
   end
 
@@ -158,7 +177,10 @@ defmodule Api.Worker.Images.ImageMagickImageConverter do
     [_, delegates] = String.split(delegates, ":")
     delegates = String.split(delegates)
     [version] = Enum.filter(lines, fn line -> String.starts_with?(line, "Version") end)
-    [version] = Enum.filter(String.split(version), fn part -> String.match?(part, ~r/\d*\.\d\.\d/) end)
+
+    [version] =
+      Enum.filter(String.split(version), fn part -> String.match?(part, ~r/\d*\.\d\.\d/) end)
+
     {version, delegates, result_status}
   end
 end

db1c3c5748b3d2056d8d7672bf9b7c6395fb81c9
tkleinke <thomas.kleinke@gbv.de>
2026-09-07 12:40:25 +0200

    Fix provider for comparator in AppModule

--- desktop/src/app/components/app.module.ts
+++ desktop/src/app/components/app.module.ts
@@ -218,7 +218,7 @@ registerLocaleData(localeUk, 'uk');
         },
         {
             provide: Comparator,
-            useFactory: (serviceLocator: AppInitializerServiceLocator) => serviceLocator.projectConfiguration,
+            useFactory: (serviceLocator: AppInitializerServiceLocator) => serviceLocator.comparator,
             deps: [AppInitializerServiceLocator]
         },
         {
